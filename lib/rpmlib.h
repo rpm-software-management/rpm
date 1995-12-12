d1f254f84bc8b6a4d6b73bf7b16137e1a4dc7a0a
root <devnull@localhost>
1995-12-12 17:49:04 +0000

    added some tags
    CVS patchset: 7
    CVS date: 1995/12/12 17:49:04

--- lib/rpmlib.h
+++ lib/rpmlib.h
@@ -1,9 +1,10 @@
 #ifndef H_RPMLIB
 #define H_RPMLIB
 
-/* This is the *only* modules users of rpmlib should need to include */
+/* This is the *only* module users of rpmlib should need to include */
 
 /* these tags are for both the database and packages */
+/* none of these can be 0 !!                         */
 
 #define RPMTAG_NAME  			1000
 #define RPMTAG_VERSION			1001
@@ -19,5 +20,14 @@
 #define RPMTAG_VENDOR			1011
 #define RPMTAG_GIF			1012
 #define RPMTAG_XPM			1013
+#define RPMTAG_COPYRIGHT                1014
+#define RPMTAG_PACKAGER                 1015
+#define RPMTAG_GROUP                    1016
+#define RPMTAG_CHANGELOG                1017
+#define RPMTAG_SOURCE                   1018
+#define RPMTAG_PATCH                    1019
+#define RPMTAG_URL                      1020
+#define RPMTAG_OS                       1021
+#define RPMTAG_ARCH                     1022
 
 #endif
