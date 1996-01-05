ce54f1b8883baadb2589744010dd195d3fcdde6e
marc <devnull@localhost>
1996-01-05 02:10:08 +0000

    added RPMLEAD_SIGNONE RPMLEAD_SIZE added signature_type to the lead structure pad lead structure to 96 bytes, 8 byte aligned    should be enough for just about forever.
    CVS patchset: 124
    CVS date: 1996/01/05 02:10:08

--- lib/rpmlead.h
+++ lib/rpmlead.h
@@ -4,10 +4,15 @@
 #define RPMLEAD_BINARY 0
 #define RPMLEAD_SOURCE 1
 
+/* Signature types */
+#define RPMLEAD_SIGNONE 0
+
 #define RPMLEAD_MAGIC0 0xed
 #define RPMLEAD_MAGIC1 0xab
 #define RPMLEAD_MAGIC2 0xee
 #define RPMLEAD_MAGIC3 0xdb
+
+#define RPMLEAD_SIZE 96
 
 struct rpmlead {
     char magic[4];
@@ -16,6 +21,8 @@ struct rpmlead {
     short archnum;
     char name[66];
     short osnum;
+    short signature_type;
+    char reserved[16];      /* pads to 96 bytes -- 8 byte aligned! */
 } ;
 
 #endif
