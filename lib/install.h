c16c83dffc37ba40dccf446417276f78a772af18
ewt <devnull@localhost>
1996-01-08 19:20:17 +0000

    Initial revision
    CVS patchset: 172
    CVS date: 1996/01/08 19:20:17

---
+++ lib/install.h
@@ -0,0 +1,17 @@
+#ifndef H_INSTALL
+#define H_INSTALL
+
+#include "header.h"
+#include "rpmlib.h"
+
+struct sharedFile {
+    int mainFileNumber;
+    int secRecOffset;
+    int secFileNumber;
+} ;
+
+int findSharedFiles(rpmdb db, int offset, char ** fileList, int fileCount,
+		    struct sharedFile ** listPtr, int * listCountPtr);
+int runScript(char * prefix, Header h, int tag);
+
+#endif
