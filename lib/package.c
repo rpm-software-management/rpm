a82bae4fa4144756cb04125144b09a34ac9d6541
root <devnull@localhost>
1996-04-05 17:36:11 +0000

    packages with bad headers shouldn't cause an error, just a differnet return code
    CVS patchset: 529
    CVS date: 1996/04/05 17:36:11

--- lib/package.c
+++ lib/package.c
@@ -31,7 +31,6 @@ int pkgReadHeader(int fd, Header * hdr, int * isSource) {
    
     if (lead.magic[0] != RPMLEAD_MAGIC0 || lead.magic[1] != RPMLEAD_MAGIC1 ||
 	lead.magic[2] != RPMLEAD_MAGIC2 || lead.magic[3] != RPMLEAD_MAGIC3) {
-	error(RPMERR_BADMAGIC, "file is not an RPM");
 	return 1;
     }
 
