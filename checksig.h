a5886c0e5c875d268f1c0d9c1ee0a3f1f320b24e
root <devnull@localhost>
1996-07-08 03:25:36 +0000

    change soReSign() to handle addition of signatures
    CVS patchset: 740
    CVS date: 1996/07/08 03:25:36

--- checksig.h
+++ checksig.h
@@ -3,6 +3,9 @@
 
 int doCheckSig(int pgp, char **argv);
 
-int doReSign(char *passPhrase, char **argv);
+int doReSign(int add, char *passPhrase, char **argv);
+
+#define ADD_SIGNATURE 1
+#define NEW_SIGNATURE 0
 
 #endif
