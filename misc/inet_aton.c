5c38e242fc27dde2b500afa13ca76b9228e6ff03
ewt <devnull@localhost>
1996-09-29 02:30:43 +0000

    Initial revision
    CVS patchset: 1078
    CVS date: 1996/09/29 02:30:43

---
+++ misc/inet_aton.c
@@ -0,0 +1,21 @@
+#ifdef HAVE_NETINET_IN_SYSTM_H
+# include <sys/types.h>
+# include <netinet/in_systm.h>
+#endif
+
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#include "inet_aton.h"
+
+int inet_aton(const char *cp, struct in_addr *inp) {
+    long addr;
+
+    addr = inet_addr(cp);
+    if (addr == ((long) -1)) return 0;
+
+    memcpy(inp, &addr, sizeof(addr));
+    return 1;
+}
---
+++ misc/inet_aton.h
@@ -0,0 +1,5 @@
+#ifndef HAVE_INET_ATON
+
+int inet_aton(const char *cp, struct in_addr *inp);
+
+#endif
