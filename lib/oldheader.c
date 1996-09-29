ca102e04a7adddda814484fcc6bd7f6e41ab1263
ewt <devnull@localhost>
1996-09-29 02:28:08 +0000

    removed use of various endian.h files, as they weren't needed anyway
    CVS patchset: 1069
    CVS date: 1996/09/29 02:28:08

--- lib/oldheader.c
+++ lib/oldheader.c
@@ -1,9 +1,3 @@
-#if defined HAVE_ENDIAN_H
-# include <endian.h>
-#else defined HAVE_MACHINE_ENDIAN_H
-# include <machine/endian.h>
-#endif
-
 #include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
