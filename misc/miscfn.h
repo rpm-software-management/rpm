fa6881186405884687724ab893323c50f0557310
ewt <devnull@localhost>
1997-01-17 15:48:09 +0000

    use config.h now
    CVS patchset: 1308
    CVS date: 1997/01/17 15:48:09

--- misc/miscfn.h
+++ misc/miscfn.h
@@ -1,18 +1,18 @@
 #ifndef H_MISCFN
 #define H_MISCFN
 
-#include "misc-config.h"
+#include "config.h"
 
 #if HAVE_FNMATCH_H
 #include <fnmatch.h>
 #else
-#include <misc-fnmatch.h>
+#include "misc-fnmatch.h"
 #endif
 
 #if HAVE_GLOB_H
 #include <glob.h>
 #else
-#include <misc-glob.h>
+#include "misc-glob.h"
 #endif
 
 #endif
