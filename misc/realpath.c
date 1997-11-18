d134038e40d3d41f7465248ae94cd68b14399b4b
ewt <devnull@localhost>
1997-11-18 03:07:10 +0000

    Force string.h to be present
    CVS patchset: 1902
    CVS date: 1997/11/18 03:07:10

--- misc/realpath.c
+++ misc/realpath.c
@@ -18,11 +18,7 @@
 #include <unistd.h>
 #endif
 #include <stdio.h>
-#ifdef HAVE_STRING_H
 #include <string.h>
-#else
-#include <strings.h>
-#endif
 #ifdef _POSIX_VERSION
 #include <limits.h>			/* for PATH_MAX */
 #else
