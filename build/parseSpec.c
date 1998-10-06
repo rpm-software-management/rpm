081efe9cfae95e1a231e57da3183fceac1149f44
jbj <devnull@localhost>
1998-10-06 23:11:54 +0000

    Initialize fileStack.
    CVS patchset: 2411
    CVS date: 1998/10/06 23:11:54

--- build/parseSpec.c
+++ build/parseSpec.c
@@ -286,6 +286,9 @@ int parseSpec(Spec *specp, char *specFile, char *buildRoot,
     /* Set up a new Spec structure with no packages. */
     spec = newSpec();
 
+    spec->fileStack = newOpenFileInfo();
+    spec->fileStack->fileName = strdup(specFile);
+
     spec->specFile = strdup(specFile);
     if (buildRoot) {
 	spec->gotBuildRoot = 1;
