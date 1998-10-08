7e8efa2f984512efdd92f76c1e13be97c36943a2
jbj <devnull@localhost>
1998-10-08 11:19:57 +0000

    automake linux fiddles.
    CVS patchset: 2421
    CVS date: 1998/10/08 11:19:57

--- acconfig.h
+++ acconfig.h
@@ -14,6 +14,15 @@
    Leave the following blank line there!!  Autoheader needs it.  */
 ^L
 
+/* Define to the name of the distribution.  */
+#undef PACKAGE
+
+/* Define to the version of the distribution.  */
+#undef VERSION
+
+/* Define to 1 if ANSI function prototypes are usable.  */
+#undef PROTOTYPES
+
 /* Define to 1 if NLS is requested.  */
 #undef ENABLE_NLS
 
