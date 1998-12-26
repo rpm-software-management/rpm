55d6927c9d4d63bd8864e86d376312340b9db667
ewt <devnull@localhost>
1998-12-26 18:31:45 +0000

    1) allow generic key types 2) possibly copy the key around 3) optionally return the key stored in the table
    CVS patchset: 2616
    CVS date: 1998/12/26 18:31:45

--- lib/hash.c
+++ lib/hash.c
@@ -5,7 +5,7 @@
 #include "hash.h"
 
 struct hashBucket {
-    const char * key;
+    void * key;
     void ** data;
     int dataCount;
     struct hashBucket * next;
@@ -13,13 +13,19 @@ struct hashBucket {
 
 struct hashTable_s {
     int numBuckets;
+    int keySize;
     struct hashBucket ** buckets;
+    hashFunctionType fn;
+    hashEqualityType eq;
 };
 
-static unsigned int hashFunction(const char * string, int numBuckets);
-static struct hashBucket * findEntry(hashTable ht, const char * key);
+static struct hashBucket * findEntry(hashTable ht, const void * key);
 
-static unsigned int hashFunction(const char * string, int numBuckets) {
+int hashEqualityString(const void * key1, const void * key2) {
+    return strcmp(key1, key2);
+}
+
+unsigned int hashFunctionString(const void * string) {
     char xorValue = 0;
     char sum = 0;
     short len;
@@ -32,32 +38,41 @@ static unsigned int hashFunction(const char * string, int numBuckets) {
 	sum += *chp;
     }
 
-    return ((len << 16) + (sum << 8) + xorValue) % numBuckets;
+    return ((len << 16) + (sum << 8) + xorValue);
 };
 
-hashTable htCreate(int numBuckets) {
+hashTable htCreate(int numBuckets, int keySize, hashFunctionType fn,
+		   hashEqualityType eq) {
     hashTable ht;
 
     ht = malloc(sizeof(*ht));
     ht->numBuckets = numBuckets;
     ht->buckets = calloc(sizeof(*ht->buckets), numBuckets);
+    ht->keySize = keySize;
+    ht->fn = fn;
+    ht->eq = eq;
 
     return ht;
 }
 
-void htAddEntry(hashTable ht, const char * key, void * data) {
+void htAddEntry(hashTable ht, const void * key, void * data) {
     unsigned int hash;
     struct hashBucket * b, * ob;
 
-    hash = hashFunction(key, ht->numBuckets);
+    hash = ht->fn(key) % ht->numBuckets;
     b = ob = ht->buckets[hash]; 
 
-    while (b && b->key && strcmp(b->key, key)) 
+    while (b && b->key && !ht->eq(b->key, key)) 
 	b = b->next;
    
     if (!b) {
 	b = malloc(sizeof(*b));
-	b->key = key;
+	if (ht->keySize) {
+	    b->key = malloc(ht->keySize);
+	    memcpy(b->key, key, ht->keySize);
+	} else {
+	    b->key = (void *) key;
+	}
 	b->dataCount = 0;
 	b->next = ht->buckets[hash];
 	b->data = NULL;
@@ -74,6 +89,7 @@ void htFree(hashTable ht) {
 
     for (i = 0; i < ht->numBuckets; i++) {
 	b = ht->buckets[i];
+	if (ht->keySize && b) free(b->key);
 	while (b) {
 	    n = b->next;
 	    free(b);
@@ -85,33 +101,34 @@ void htFree(hashTable ht) {
     free(ht);
 }
 
-static struct hashBucket * findEntry(hashTable ht, const char * key) {
+static struct hashBucket * findEntry(hashTable ht, const void * key) {
     unsigned int hash;
     struct hashBucket * b;
 
-    hash = hashFunction(key, ht->numBuckets);
+    hash = ht->fn(key) % ht->numBuckets;
     b = ht->buckets[hash]; 
 
-    while (b && b->key && strcmp(b->key, key)) 
+    while (b && b->key && !ht->eq(b->key, key)) 
 	b = b->next;
 
     return b;
 }
 
-int htHasEntry(hashTable ht, const char * key) {
+int htHasEntry(hashTable ht, const void * key) {
     struct hashBucket * b;
 
     if (!(b = findEntry(ht, key))) return 0; else return 1;
 }
 
-int htGetEntry(hashTable ht, const char * key, void *** data, 
-	       int * dataCount) {
+int htGetEntry(hashTable ht, const void * key, void *** data, 
+	       int * dataCount, void ** tableKey) {
     struct hashBucket * b;
 
     if (!(b = findEntry(ht, key))) return 1;
 
     *data = b->data;
     *dataCount = b->dataCount;
+    if (tableKey) *tableKey = b->key;
 
     return 0;
 }
--- lib/hash.h
+++ lib/hash.h
@@ -1,16 +1,24 @@
 #ifndef H_HASH
 #define H_HASH
 
-/* These hash tables are *very* skeptical about duplicating data. Be warned. */
-
 typedef struct hashTable_s * hashTable;
 
-hashTable htCreate(int numBuckets);
-void htAddEntry(hashTable ht, const char * key, void * data);
+typedef unsigned int (*hashFunctionType)(const void * string);
+typedef int (*hashEqualityType)(const void * key1, const void * key2);
+
+unsigned int hashFunctionString(const void * string);
+int hashEqualityString(const void * key1, const void * key2);
+
+/* if keySize > 0, the key is duplicated within the table (which costs
+   memory, but may be usefull anyway */
+hashTable htCreate(int numBuckets, int keySize, hashFunctionType fn,
+		   hashEqualityType eq); 
+void htAddEntry(hashTable ht, const void * key, void * data);
 void htFree(hashTable ht);
-/* returns 0 on success, 1 if the item is not found */
-int htGetEntry(hashTable ht, const char * key, void *** data, int * dataCount);
+/* returns 0 on success, 1 if the item is not found. tableKey may be NULL */
+int htGetEntry(hashTable ht, const void * key, void *** data, int * dataCount,
+	       void ** tableKey);
 /* returns 1 if the item is present, 0 otherwise */
-int htHasEntry(hashTable ht, const char * key);
+int htHasEntry(hashTable ht, const void * key);
 
 #endif
