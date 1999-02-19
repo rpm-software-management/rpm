bbd07e2c481ac60a82ccddd3ec7bad1308703589
jbj <devnull@localhost>
1999-02-19 15:23:34 +0000

    Typo.
    CVS patchset: 2812
    CVS date: 1999/02/19 15:23:34

--- lib/cpio.c
+++ lib/cpio.c
@@ -357,7 +357,7 @@ static int expandRegular(CFD_t * cfd, struct cpioHeader * hdr,
     /* Rename the old file before attempting unlink to avoid EBUSY errors */
     if (!lstat(hdr->path, &sb)) {
 	strcpy(buf, hdr->path);
-	strcpy(buf, "-RPMDELETE");
+	strcat(buf, "-RPMDELETE");
 	if (rename(hdr->path, buf)) {
 	    fprintf(stderr, _("can't rename %s to %x: %s\n"),
 		hdr->path, buf, strerror(errno));
