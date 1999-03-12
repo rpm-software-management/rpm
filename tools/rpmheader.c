572c470125abc3b068216c9bee539eba5f0f9fd3
jbj <devnull@localhost>
1999-03-12 05:55:51 +0000

    Don't segfault if filename does not exist.
    CVS patchset: 2880
    CVS date: 1999/03/12 05:55:51

--- tools/rpmheader.c
+++ tools/rpmheader.c
@@ -16,6 +16,10 @@ int main(int argc, char **argv)
 	fdi = fdDup(STDIN_FILENO);
     } else {
 	fdi = fdOpen(argv[1], O_RDONLY, 0644);
+	if (fdFileno(fdi) < 0) {
+	    perror(argv[1]);
+	    exit(1);
+	}
     }
 
     readLead(fdi, &lead);
