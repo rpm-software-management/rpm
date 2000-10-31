6a0534e5d909191f33b835b15f981bfc282a29db
jbj <devnull@localhost>
2000-10-31 18:24:05 +0000

    Return unsatisfied immediately if "rpmlib(" dependency fails to match.
    CVS patchset: 4241
    CVS date: 2000/10/31 18:24:05

--- lib/depends.c
+++ lib/depends.c
@@ -1084,6 +1084,7 @@ static int unsatisfiedDepend(rpmTransactionSet rpmdep,
 			keyType, keyDepend+2);
 	    goto exit;
 	}
+	goto unsatisfied;
     }
 
     if (alSatisfiesDepend(&rpmdep->addedPackages, keyType, keyDepend, keyName, keyEVR, keyFlags)) {
@@ -1127,6 +1128,7 @@ static int unsatisfiedDepend(rpmTransactionSet rpmdep,
 	*suggestion = alSatisfiesDepend(&rpmdep->availablePackages, NULL, NULL,
 				keyName, keyEVR, keyFlags);
 
+unsatisfied:
     rpmMessage(RPMMESS_DEBUG, _("%s: %-45s NO\n"), keyType, keyDepend+2);
     rc = 1;	/* dependency is unsatisfied */
 
