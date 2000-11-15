fa7f37728fa08ff9ce066ed2928b646b0b8dcf1f
jbj <devnull@localhost>
2000-11-15 20:44:06 +0000

    Avoid gcc enum warnings, remove ugly braces.
    CVS patchset: 4278
    CVS date: 2000/11/15 20:44:06

--- build/parseBuildInstallClean.c
+++ build/parseBuildInstallClean.c
@@ -10,21 +10,17 @@ int parseBuildInstallClean(Spec spec, rpmParseState parsePart)
 {
     int nextPart, rc;
     StringBuf *sbp = NULL;
-    char *name = NULL;
+    const char *name = NULL;
 
-    switch (parsePart) {
-      case PART_BUILD:
+    if (parsePart == PART_BUILD) {
 	sbp = &(spec->build);
 	name = "%build";
-	break;
-      case PART_INSTALL:
+    } else if (parsePart == PART_INSTALL) {
 	sbp = &(spec->install);
 	name = "%install";
-	break;
-      case PART_CLEAN:
+    } else if (parsePart == PART_CLEAN) {
 	sbp = &(spec->clean);
 	name = "%clean";
-	break;
     }
     
     if (*sbp != NULL) {
@@ -35,21 +31,17 @@ int parseBuildInstallClean(Spec spec, rpmParseState parsePart)
     *sbp = newStringBuf();
 
     /* There are no options to %build, %install, or %clean */
-    if ((rc = readLine(spec, STRIP_NOTHING)) > 0) {
+    if ((rc = readLine(spec, STRIP_NOTHING)) > 0)
 	return PART_NONE;
-    }
-    if (rc) {
+    if (rc)
 	return rc;
-    }
     
     while (! (nextPart = isPart(spec->line))) {
 	appendStringBuf(*sbp, spec->line);
-	if ((rc = readLine(spec, STRIP_NOTHING)) > 0) {
+	if ((rc = readLine(spec, STRIP_NOTHING)) > 0)
 	    return PART_NONE;
-	}
-	if (rc) {
+	if (rc)
 	    return rc;
-	}
     }
 
     return nextPart;
