f5d0541223f386cc3d97b6323ce83d996fa46fe4
jbj <devnull@localhost>
2001-05-11 04:06:12 +0000

    - rpm database has rpm.rpm g+w permissions to share db3 mutexes. - expose more db3 macro configuration tokens. - move fprint.[ch] and hash.[ch] to rpmdb directory. - detect and fiddle incompatible mixtures of db3 env/open flags. - add DBI_WRITECURSOR to map to db3 flags with CDB database model. - add rpmdbSetIteratorRewrite to warn of pending lazy (re-)writes.
    CVS patchset: 4766
    CVS date: 2001/05/11 04:06:12

---
+++ rpmdb/fprint.c
@@ -0,0 +1,251 @@
+/**
+ * \file rpmdb/fprint.c
+ */
+
+#include "system.h"
+#include <rpmlib.h>
+#include <rpmmacro.h>	/* XXX for rpmCleanPath */
+
+#include "fprint.h"
+#include "debug.h"
+
+fingerPrintCache fpCacheCreate(int sizeHint)
+{
+    fingerPrintCache fpc;
+
+    fpc = xmalloc(sizeof(*fpc));
+    fpc->ht = htCreate(sizeHint * 2, 0, 1, hashFunctionString,
+		       hashEqualityString);
+    return fpc;
+}
+
+void fpCacheFree(fingerPrintCache cache)
+{
+    htFree(cache->ht);
+    free(cache);
+}
+
+/**
+ * Find directory name entry in cache.
+ * @param cache		pointer to fingerprint cache
+ * @param dirName	string to locate in cache
+ * @return pointer to directory name entry (or NULL if not found).
+ */
+static /*@null@*/ const struct fprintCacheEntry_s * cacheContainsDirectory(
+			    fingerPrintCache cache,
+			    const char * dirName)
+{
+    const void ** data;
+
+    if (htGetEntry(cache->ht, dirName, &data, NULL, NULL))
+	return NULL;
+    return data[0];
+}
+
+/**
+ * Return finger print of a file path.
+ * @param cache		pointer to fingerprint cache
+ * @param dirName	leading directory name of path
+ * @param baseName	file name of path
+ * @param scareMemory
+ * @return pointer to the finger print associated with a file path.
+ */
+static fingerPrint doLookup(fingerPrintCache cache,
+	const char * dirName, const char * baseName, int scareMemory)
+{
+    char dir[PATH_MAX];
+    const char * cleanDirName;
+    size_t cdnl;
+    char * end;		    /* points to the '\0' at the end of "buf" */
+    fingerPrint fp;
+    struct stat sb;
+    char * buf;
+    const struct fprintCacheEntry_s * cacheHit;
+
+    /* assert(*dirName == '/' || !scareMemory); */
+
+    /* XXX WATCHOUT: fp.subDir is set below from relocated dirName arg */
+    cleanDirName = dirName;
+    cdnl = strlen(cleanDirName);
+
+    if (*cleanDirName == '/') {
+	if (!scareMemory)
+	    cleanDirName =
+		rpmCleanPath(strcpy(alloca(cdnl+1), dirName));
+    } else {
+	scareMemory = 0;	/* XXX causes memory leak */
+
+	/* Using realpath on the arg isn't correct if the arg is a symlink,
+	 * especially if the symlink is a dangling link.  What we 
+	 * do instead is use realpath() on `.' and then append arg to
+	 * the result.
+	 */
+
+	/* if the current directory doesn't exist, we might fail. 
+	   oh well. likewise if it's too long.  */
+	dir[0] = '\0';
+	if ( /*@-unrecog@*/ realpath(".", dir) /*@=unrecog@*/ != NULL) {
+	    end = dir + strlen(dir);
+	    if (end[-1] != '/')	*end++ = '/';
+	    end = stpncpy(end, cleanDirName, sizeof(dir) - (end - dir));
+	    *end = '\0';
+	    (void)rpmCleanPath(dir); /* XXX possible /../ from concatenation */
+	    end = dir + strlen(dir);
+	    if (end[-1] != '/')	*end++ = '/';
+	    *end = '\0';
+	    cleanDirName = dir;
+	    cdnl = end - dir;
+	}
+    }
+    fp.entry = NULL;
+    fp.subDir = NULL;
+    fp.baseName = NULL;
+    /*@-nullret@*/
+    if (cleanDirName == NULL) return fp;	/* XXX can't happen */
+    /*@=nullret@*/
+
+    buf = strcpy(alloca(cdnl + 1), cleanDirName);
+    end = buf + cdnl;
+
+    /* no need to pay attention to that extra little / at the end of dirName */
+    if (buf[1] && end[-1] == '/') {
+	end--;
+	*end = '\0';
+    }
+
+    while (1) {
+
+	/* as we're stating paths here, we want to follow symlinks */
+
+	cacheHit = cacheContainsDirectory(cache, (*buf != '\0' ? buf : "/"));
+	if (cacheHit != NULL) {
+	    fp.entry = cacheHit;
+	} else if (!stat((*buf != '\0' ? buf : "/"), &sb)) {
+	    size_t nb = sizeof(*fp.entry) + (*buf != '\0' ? (end-buf) : 1) + 1;
+	    char * dn = xmalloc(nb);
+	    struct fprintCacheEntry_s * newEntry = (void *)dn;
+
+	    dn += sizeof(*newEntry);
+	    strcpy(dn, (*buf != '\0' ? buf : "/"));
+	    newEntry->ino = sb.st_ino;
+	    newEntry->dev = sb.st_dev;
+	    newEntry->isFake = 0;
+	    newEntry->dirName = dn;
+	    fp.entry = newEntry;
+
+	    /*@-kepttrans@*/
+	    htAddEntry(cache->ht, dn, fp.entry);
+	    /*@@kepttrans@*/
+	}
+
+        if (fp.entry) {
+	    fp.subDir = cleanDirName + (end - buf);
+	    if (fp.subDir[0] == '/' && fp.subDir[1] != '\0')
+		fp.subDir++;
+	    if (fp.subDir[0] == '\0' ||
+	    /* XXX don't bother saving '/' as subdir */
+	       (fp.subDir[0] == '/' && fp.subDir[1] == '\0'))
+		fp.subDir = NULL;
+	    fp.baseName = baseName;
+	    if (!scareMemory && fp.subDir != NULL)
+		fp.subDir = xstrdup(fp.subDir);
+	    return fp;
+	}
+
+        /* stat of '/' just failed! */
+	if (end == buf + 1)
+	    abort();
+
+	end--;
+	while ((end > buf) && *end != '/') end--;
+	if (end == buf)	    /* back to stat'ing just '/' */
+	    end++;
+
+	*end = '\0';
+    }
+
+    /*@notreached@*/
+
+    /*@-nullret@*/ return fp; /*@=nullret@*/	/* LCL: can't happen. */
+}
+
+fingerPrint fpLookup(fingerPrintCache cache, const char * dirName, 
+			const char * baseName, int scareMemory)
+{
+    return doLookup(cache, dirName, baseName, scareMemory);
+}
+
+unsigned int fpHashFunction(const void * key)
+{
+    const fingerPrint * fp = key;
+    unsigned int hash = 0;
+    char ch;
+    const char * chptr;
+
+    ch = 0;
+    chptr = fp->baseName;
+    while (*chptr != '\0') ch ^= *chptr++;
+
+    hash |= ((unsigned)ch) << 24;
+    hash |= (((((unsigned)fp->entry->dev) >> 8) ^ fp->entry->dev) & 0xFF) << 16;
+    hash |= fp->entry->ino & 0xFFFF;
+    
+    return hash;
+}
+
+int fpEqual(const void * key1, const void * key2)
+{
+    const fingerPrint *k1 = key1;
+    const fingerPrint *k2 = key2;
+
+    /* If the addresses are the same, so are the values. */
+    if (k1 == k2)
+	return 0;
+
+    /* Otherwise, compare fingerprints by value. */
+    /*@-nullpass@*/	/* LCL: whines about (*k2).subdir */
+    if (FP_EQUAL(*k1, *k2))
+	return 0;
+    /*@=nullpass@*/
+    return 1;
+
+}
+
+void fpLookupList(fingerPrintCache cache, const char ** dirNames, 
+		  const char ** baseNames, const int * dirIndexes, 
+		  int fileCount, fingerPrint * fpList)
+{
+    int i;
+
+    for (i = 0; i < fileCount; i++) {
+	/* If this is in the same directory as the last file, don't bother
+	   redoing all of this work */
+	if (i > 0 && dirIndexes[i - 1] == dirIndexes[i]) {
+	    fpList[i].entry = fpList[i - 1].entry;
+	    fpList[i].subDir = fpList[i - 1].subDir;
+	    fpList[i].baseName = baseNames[i];
+	} else {
+	    fpList[i] = doLookup(cache, dirNames[dirIndexes[i]], baseNames[i],
+				 1);
+	}
+    }
+}
+
+void fpLookupHeader(fingerPrintCache cache, Header h, fingerPrint * fpList)
+{
+    HGE_t hge = (HGE_t)headerGetEntryMinMemory;
+    HFD_t hfd = headerFreeData;
+    const char ** baseNames, ** dirNames;
+    int bnt, dnt;
+    int_32 * dirIndexes;
+    int fileCount;
+
+    if (!hge(h, RPMTAG_BASENAMES, &bnt, (void **) &baseNames, &fileCount))
+	return;
+
+    (void) hge(h, RPMTAG_DIRNAMES, &dnt, (void **) &dirNames, NULL);
+    (void) hge(h, RPMTAG_DIRINDEXES, NULL, (void **) &dirIndexes, NULL);
+    fpLookupList(cache, dirNames, baseNames, dirIndexes, fileCount, fpList);
+    dirNames = hfd(dirNames, dnt);
+    baseNames = hfd(baseNames, bnt);
+}
---
+++ rpmdb/fprint.h
@@ -0,0 +1,136 @@
+#ifndef H_FINGERPRINT
+#define H_FINGERPRINT
+
+/** \ingroup rpmtrans
+ * \file rpmdb/fprint.h
+ * Identify a file name path by a unique "finger print".
+ */
+
+#include "rpmhash.h"
+#include "header.h"
+
+/**
+ * Finger print cache entry.
+ * This is really a directory and symlink cache. We don't differentiate between
+ * the two. We can prepopulate it, which allows us to easily conduct "fake"
+ * installs of a system w/o actually mounting filesystems.
+ */
+struct fprintCacheEntry_s {
+    const char * dirName;		/*!< path to existing directory */
+    dev_t dev;				/*!< stat(2) device number */
+    ino_t ino;				/*!< stat(2) inode number */
+    int isFake;				/*!< (currently unused) */
+};
+
+/**
+ * Finger print cache.
+ */
+typedef /*@abstract@*/ struct fprintCache_s {
+    hashTable ht;			/*!< hashed by dirName */
+} * fingerPrintCache;
+
+/**
+ * Associates a trailing sub-directory and final base name with an existing
+ * directory finger print.
+ */
+typedef struct fingerPrint_s {
+/*! directory finger print entry (the directory path is stat(2)-able */
+    const struct fprintCacheEntry_s * entry;
+/*! trailing sub-directory path (directories that are not stat(2)-able */
+/*@owned@*/ /*@null@*/ const char * subDir;
+/*@dependent@*/ const char * baseName;	/*!< file base name */
+} fingerPrint;
+
+/* only if !scarceMemory */
+/** */
+#define fpFree(a) free((void *)(a).baseName)
+
+/** */
+#define	FP_ENTRY_EQUAL(a, b) (((a)->dev == (b)->dev) && ((a)->ino == (b)->ino))
+
+/** */
+#define FP_EQUAL(a, b) ( \
+	FP_ENTRY_EQUAL((a).entry, (b).entry) && \
+	!strcmp((a).baseName, (b).baseName) && ( \
+	    ((a).subDir == (b).subDir) || \
+	    ((a).subDir && (b).subDir && !strcmp((a).subDir, (b).subDir)) \
+	) \
+    )
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Be carefull with the memory... assert(*fullName == '/' || !scareMemory) */
+
+/**
+ * Create finger print cache.
+ * @param sizeHint	number of elements expected
+ * @return pointer to initialized fingerprint cache
+ */
+/*@only@*/ fingerPrintCache fpCacheCreate(int sizeHint)	/*@*/;
+
+/**
+ * Destroy finger print cache.
+ * @param cache		pointer to fingerprint cache
+ */
+void		fpCacheFree(/*@only@*/ fingerPrintCache cache);
+
+/**
+ * Return finger print of a file path.
+ * @param cache		pointer to fingerprint cache
+ * @param dirName	leading directory name of file path
+ * @param baseName	base name of file path
+ * @param scareMemory
+ * @return pointer to the finger print associated with a file path.
+ */
+fingerPrint	fpLookup(fingerPrintCache cache, const char * dirName, 
+			const char * baseName, int scareMemory)	/*@*/;
+
+/**
+ * Return hash value for a finger print.
+ * Hash based on dev and inode only!
+ * @param key		pointer to finger print entry
+ * @return hash value
+ */
+unsigned int fpHashFunction(const void * key)	/*@*/;
+
+/**
+ * Compare two finger print entries.
+ * exactly equivalent to FP_EQUAL macro.
+ * @param key1		finger print 1
+ * @param key2		finger print 2
+ * @return result of comparing key1 and key2
+ */
+int fpEqual(const void * key1, const void * key2)	/*@*/;
+
+/**
+ * Return finger prints of an array of file paths.
+ * @warning: scareMemory is assumed!
+ * @param cache		pointer to fingerprint cache
+ * @param dirNames	directory names
+ * @param baseNames	file base names
+ * @param dirIndexes	index into dirNames for each baseNames
+ * @param fileCount	number of file entries
+ * @retval fpList	pointer to array of finger prints
+ */
+void fpLookupList(fingerPrintCache cache, const char ** dirNames, 
+		  const char ** baseNames, const int * dirIndexes, 
+		  int fileCount, fingerPrint * fpList)
+			/*@modifies cache, *fpList @*/;
+
+/**
+ * Return finger prints of all file names in header.
+ * @warning: scareMemory is assumed!
+ * @param cache		pointer to fingerprint cache
+ * @param h		package header
+ * @retval fpList	pointer to array of finger prints
+ */
+void fpLookupHeader(fingerPrintCache cache, Header h, fingerPrint * fpList)
+	/*@modifies h, cache, *fpList @*/;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
