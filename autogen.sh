30b650580233b357cc7e68e95accc9db4186e5b5
jbj <devnull@localhost>
2002-10-23 00:09:06 +0000

    Upgrade to autoconf-2.54 and elfutils-0.51.
    CVS patchset: 5802
    CVS date: 2002/10/23 00:09:06

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@ export CFLAGS
 export LDFLAGS
 
 LTV="libtoolize (GNU libtool) 1.4.2"
-ACV="autoconf (GNU Autoconf) 2.53"
+ACV="autoconf (GNU Autoconf) 2.54"
 AMV="automake (GNU automake) 1.6.3"
 USAGE="
 This script documents the versions of the tools I'm using to build rpm:
 	libtool-1.4.2
-	autoconf-2.53
-	automake-1.6.2
+	autoconf-2.54
+	automake-1.6.3
 Simply edit this script to change the libtool/autoconf/automake versions
 checked if you need to, as rpm should build (and has built) with all
 recent versions of libtool/autoconf/automake.
