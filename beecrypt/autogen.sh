f98e6cf1a20a9d22942b1bbe7a5b411bc1b8754d
jbj <devnull@localhost>
2004-04-07 17:30:07 +0000

    Make peace with libtool-1.5.3.
    CVS patchset: 7217
    CVS date: 2004/04/07 17:30:07

--- beecrypt/autogen.sh
+++ beecrypt/autogen.sh
@@ -3,12 +3,12 @@
 export CFLAGS
 export LDFLAGS
 
-LTV="libtoolize (GNU libtool) 1.5.2"
+LTV="libtoolize (GNU libtool) 1.5.3"
 ACV="autoconf (GNU Autoconf) 2.59"
 AMV="automake (GNU automake) 1.8.3"
 USAGE="
 This script documents the versions of the tools I'm using to build rpm:
-	libtool-1.5.2
+	libtool-1.5.3
 	autoconf-2.59
 	automake-1.8.3
 Simply edit this script to change the libtool/autoconf/automake versions
