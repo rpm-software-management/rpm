351af63e8c87fc52f0e6eaf013f4df8dd372bf66
jbj <devnull@localhost>
2004-11-06 21:02:15 +0000

    Orphan.
    CVS patchset: 7551
    CVS date: 2004/11/06 21:02:15

---
+++ rpmio/tget.c
@@ -0,0 +1,85 @@
+#include "system.h"
+
+#include <rpmio_internal.h>
+#include <rpmmacro.h>
+#include <rpmmessages.h>
+#include <popt.h>
+
+#include "debug.h"
+
+static int _debug = 0;
+
+#define	HTTPSPATH	"https://wellfleet.jbj.org/test.txt"
+#define	HTTPPATH	"http://wellfleet.jbj.org/test.txt"
+#define	FTPPATH		"ftp://wellfleet.jbj.org/test.txt"
+#define	DIRPATH		"/var/ftp/test.txt"
+static char * httpspath = HTTPSPATH;
+static char * httppath = HTTPPATH;
+static char * ftppath = FTPPATH;
+static char * dirpath = DIRPATH;
+
+static void printFile(const char * path)
+{
+    FD_t fd;
+
+fprintf(stderr, "===== %s\n", path);
+    fd = Fopen(path, "r.ufdio");
+    if (fd != NULL) {
+	char buf[BUFSIZ];
+	size_t len = Fread(buf, 1, sizeof(buf), fd);
+	int xx;
+
+	if (len > 0)
+	    fwrite(buf, 1, len, stderr);
+	xx = Fclose(fd);
+    }
+}
+
+static struct poptOption optionsTable[] = {
+ { "debug", 'd', POPT_ARG_VAL,	&_debug, -1,		NULL, NULL },
+ { "ftpdebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_ftp_debug, -1,
+	N_("debug protocol data stream"), NULL},
+ { "rpmiodebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_rpmio_debug, -1,
+	N_("debug rpmio I/O"), NULL},
+ { "urldebug", '\0', POPT_ARG_VAL|POPT_ARGFLAG_DOC_HIDDEN, &_url_debug, -1,
+	N_("debug URL cache handling"), NULL},
+ { "verbose", 'v', 0, 0, 'v',				NULL, NULL },
+  POPT_AUTOHELP
+  POPT_TABLEEND
+};
+
+int
+main(int argc, const char *argv[])
+{
+    poptContext optCon = poptGetContext(argv[0], argc, argv, optionsTable, 0);
+    int rc;
+
+    while ((rc = poptGetNextOpt(optCon)) > 0) {
+	switch (rc) {
+	case 'v':
+	    rpmIncreaseVerbosity();
+	    /*@switchbreak@*/ break;
+	default:
+            /*@switchbreak@*/ break;
+	}
+    }
+
+    if (_debug) {
+	rpmIncreaseVerbosity();
+	rpmIncreaseVerbosity();
+    }
+
+_av_debug = -1;
+_ftp_debug = -1;
+_dav_debug = -1;
+#if 0
+    printFile(dirpath);
+    printFile(ftppath);
+#endif
+    printFile(httppath);
+    printFile(httpspath);
+
+/*@i@*/ urlFreeCache();
+
+    return 0;
+}
