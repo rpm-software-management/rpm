d7a0e859ba4842548bfe344f6408af80c10b1c50
pauln <devnull@localhost>
2004-11-15 14:12:19 +0000

    Re-export hidden VSF symbols 139221
    CVS patchset: 7581
    CVS date: 2004/11/15 14:12:19

--- python/rpm/__init__.py
+++ python/rpm/__init__.py
@@ -6,3 +6,9 @@ This module enables you to manipulate rpms and the rpm database.
 
 from _rpm import *
 
+import _rpm
+_RPMVSF_NODIGESTS = _rpm._RPMVSF_NODIGESTS
+_RPMVSF_NOHEADER = _rpm._RPMVSF_NOHEADER
+_RPMVSF_NOPAYLOAD = _rpm._RPMVSF_NOPAYLOAD
+_RPMVSF_NOSIGNATURES = _rpm._RPMVSF_NOSIGNATURES
+
