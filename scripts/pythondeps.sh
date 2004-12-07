fd0b82c71af015861a246541d7f8ed694dbad701
jbj <devnull@localhost>
2004-12-07 00:58:50 +0000

    Redirect grep to /dev/null.
    CVS patchset: 7637
    CVS date: 2004/12/07 00:58:50

--- scripts/pythondeps.sh
+++ scripts/pythondeps.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 [ $# -ge 1 ] || {
     cat > /dev/null
@@ -9,12 +9,12 @@ PYVER=`python -c "import sys; v=sys.version_info[:2]; print '%d.%d'%v"`
 case $1 in
 -P|--provides)
     shift
-    grep "/usr/bin/python\*\$" 2> /dev/null && echo "python(abi) = ${PYVER}"
+    grep "/usr/bin/python\*\$" >& /dev/null && echo "python(abi) = ${PYVER}"
     exit 0
     ;;
 -R|--requires)
     shift
-    grep "/usr/lib[^/]*/python${PYVER}/" 2> /dev/null && echo "python(abi) = ${PYVER}"
+    grep "/usr/lib[^/]*/python${PYVER}/" >& /dev/null && echo "python(abi) = ${PYVER}"
     exit 0
     ;;
 esac
