6abb0b372c2cf8a08b1e0b7f97aa600c352da701
jbj <devnull@localhost>
2005-03-18 16:02:13 +0000

    Sanity.
    CVS patchset: 7822
    CVS date: 2005/03/18 16:02:13

--- rpmdb/tsql.c
+++ rpmdb/tsql.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include <sqlite3.h>
 
-static const char *dbname = "P";
+#include "sqlite.c"
 
-static const char * stmts = "\
-create table tbl1(one varchar(10), two smallint);\n\
-insert into tbl1 values('hello!',10);\n\
-insert into tbl1 values('goodbye', 20);\n\
-select * from tbl1;\n\
-";
-
-static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+static int sql_query(sqlite3 *db, const char *cmd)
 {
-  int i;
-  for(i = 0; i<argc; i++)
-    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-  printf("\n");
-  return 0;
-}
-
-static int sql_exec(sqlite3 *db, const char *stmt)
-{
-    char *zErrMsg = 0;
+    SCP_t scp = scpNew();
+    const char * zTail;
     int rc;
+    int i;
 
-fprintf(stderr, "*** %s:\n%s", __FUNCTION__, stmts);
-    rc = sqlite3_exec(db, stmts, callback, 0, &zErrMsg);
-    if (rc != SQLITE_OK)
-	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+fprintf(stderr, "*** %s:\n%s\n", __FUNCTION__, cmd);
+
+#if 0
+    rc = sqlite3_prepare(db, cmd, strlen(cmd), &scp->pStmt, &zTail);
+    if (rc) fprintf(stderr, "prepare: %d %s\n", rc, sqlite3_errmsg(db));
+
+    rc = sql_step(db, scp);
+#else
+    rc = sqlite3_get_table(db, cmd,
+        &scp->av, &scp->nr, &scp->nc, &scp->pzErrmsg);
+    if (rc) fprintf(stderr, "get_table: %d %s\n", rc, sqlite3_errmsg(db));
+
+fprintf(stderr, "%s: DONE scp %p [%d:%d] av %p avlen %p nr %d nc %d\n", __FUNCTION__, scp, scp->ac, scp->nalloc, scp->av, scp->avlen, scp->nr, scp->nc);
+
+    fprintf(stderr, "\tav[0] %p %s\n", scp->av[0], scp->av[0]);
+    for (i = 1; i < scp->nr; i++) {
+	fprintf(stderr, "\tav[%d] %p %d\n", i, scp->av[i], *(int *)scp->av[i]);
+    }
+#endif
+
+    scp = scpReset(scp);
+
     return rc;
 }
+
+static const char *dbname = "/var/lib/rpm/Packages";
+static const char *qcmd = "SELECT key from 'Packages';";
 
 int main(int argc, char **argv)
 {
   sqlite3 *db;
-  char *zErrMsg = 0;
   int rc;
 
-  (void) unlink(dbname);
   rc = sqlite3_open(dbname, &db);
   if (rc) {
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -45,7 +54,7 @@ int main(int argc, char **argv)
     exit(1);
   }
 
-  rc = sql_exec(db, stmts);
+  rc = sql_query(db, qcmd);
 
   sqlite3_close(db);
 
