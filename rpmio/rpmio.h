0b42a940c169bdc449067b04162e77537031da9d
jbj <devnull@localhost>
2005-07-02 18:54:08 +0000

    GCC fidgets, add parentheses around timedRead vector.
    CVS patchset: 7864
    CVS date: 2005/07/02 18:54:08

--- rpmio/rpmio.h
+++ rpmio/rpmio.h
@@ -637,7 +637,7 @@ int ufdGetFile( /*@killref@*/ FD_t sfd, FD_t tfd)
 /*@unused@*/ int timedRead(FD_t fd, /*@out@*/ void * bufptr, int length)
 	/*@globals fileSystem @*/
 	/*@modifies fd, *bufptr, fileSystem @*/;
-#define	timedRead	ufdio->read
+#define	timedRead	(ufdio->read)
 
 /*@-exportlocal@*/
 /**
