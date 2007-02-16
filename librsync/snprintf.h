d12a7441fc14b9b1802ca7b4e288a49f4484351d
Paul Nasrat <pnasrat@redhat.com>
2007-02-16 16:23:54 +0000

    Remove internal librsync

--- librsync/snprintf.h
+++
@@ -1,8 +0,0 @@
-/* Id: snprintf.h,v 1.2 2000/08/08 09:20:34 mbp Exp  */
-
-#ifndef HAVE_SNPRINTF
-int snprintf (char *str, size_t count, const char *fmt, ...);
-#endif
-#ifndef HAVE_VSPRINTF
-int vsnprintf (char *str, size_t count, const char *fmt, va_list arg);
-#endif
