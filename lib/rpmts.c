82885665ce59795aceb439452cfd3bddbae04afe
Panu Matilainen <pmatilai@redhat.com>
2007-06-21 15:13:04 +0300

    ARM arch update.
    Adds more ARM sub-archs, and adds some bits to deal properly with the new
    ARM ABI (EABI). Patch from  Lennert Buytenhek.

--- lib/rpmts.c
+++ lib/rpmts.c
@@ -236,7 +236,7 @@ static int isArch(const char * arch)
 	"m68k",
 	"rs6000",
 	"ia64",
-	"armv3l", "armv4b", "armv4l",
+	"armv3l", "armv4b", "armv4l", "armv4tl", "armv5tel", "armv5tejl", "armv6l",
 	"s390", "i370", "s390x",
 	"sh", "xtensa",
 	"noarch",
