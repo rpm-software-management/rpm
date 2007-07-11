fa262a27649a5908b3641e479bb4061b2b773d19
Panu Matilainen <pmatilai@redhat.com>
2007-07-11 14:45:34 +0300

    Add missing include for mergesort

--- rpmdb/merge.c
+++ rpmdb/merge.c
@@ -55,6 +55,7 @@ static char sccsid[] = "@(#)merge.c	8.2 (Berkeley) 2/14/94";
  */
 
 #include "system.h"
+#include "rpmdb.h" /* XXX for mergesort */
 
 #define ISIZE sizeof(int)
 #define PSIZE sizeof(unsigned char *)
