cd4fb0f9f4c583d60f4a86e62f0c8cc268af408d
Panu Matilainen <pmatilai@redhat.com>
2007-07-12 10:00:43 +0300

    Silence yet another char ptr signedness warning

--- misc/fnmatch.c
+++ misc/fnmatch.c
@@ -35,7 +35,7 @@ __strchrnul (const char *s, int c)
 
   /* Handle the first few characters by reading one character at a time.
      Do this until CHAR_PTR is aligned on a longword boundary.  */
-  for (char_ptr = s; ((unsigned long int) char_ptr
+  for (char_ptr = (const unsigned char *)s; ((unsigned long int) char_ptr
 		      & (sizeof (longword) - 1)) != 0;
        ++char_ptr)
     if (*char_ptr == c || *char_ptr == '\0')
