466df80338fdae61367369b45bea2b0d508261d3
Panu Matilainen <pmatilai@redhat.com>
2007-09-05 13:43:57 +0300

    Handle mire matching against binary blobs (rhbz#277161) - convert blob to hex string, miregexec() against that - use tagType() to determine if header data needs freeing

--- rpmdb/rpmdb.c
+++ rpmdb/rpmdb.c
@@ -2080,8 +2080,16 @@ static int mireSkip (const rpmdbMatchIterator mi)
 		    }
 		}
 		/*@switchbreak@*/ break;
-	    case RPM_NULL_TYPE:
 	    case RPM_BIN_TYPE:
+		{
+		const char * str = bin2hex((const char*) u.ptr, c);
+		rc = miregexec(mire, str);
+		if ((!rc && !mire->notmatch) || (rc && mire->notmatch))
+		    anymatch++;
+		_free(str);
+		}
+		/*@switchbreak@*/ break;
+	    case RPM_NULL_TYPE:
 	    default:
 		/*@switchbreak@*/ break;
 	    }
@@ -2094,7 +2102,10 @@ static int mireSkip (const rpmdbMatchIterator mi)
 	}
 /*@=boundsread@*/
 
-	u.ptr = hfd(u.ptr, t);
+	if ((tagType(mire->tag) & RPM_MASK_RETURN_TYPE) == 
+	    RPM_ARRAY_RETURN_TYPE) {
+	    u.ptr = hfd(u.ptr, t);
+	}
 
 	ntags++;
 	if (anymatch)
