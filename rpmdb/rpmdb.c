58481133903041e14755434ad60260ed69bd0ad5
Panu Matilainen <pmatilai@redhat.com>
2007-09-05 13:41:13 +0300

    Fix false mire matches on nonexistent headers.

--- rpmdb/rpmdb.c
+++ rpmdb/rpmdb.c
@@ -2032,8 +2032,10 @@ static int mireSkip (const rpmdbMatchIterator mi)
 	int anymatch;
 
 	if (!hge(mi->mi_h, mire->tag, &t, (void **)&u, &c)) {
-	    if (mire->tag != RPMTAG_EPOCH)
+	    if (mire->tag != RPMTAG_EPOCH) {
+		ntags++;
 		continue;
+	    }
 	    t = RPM_INT32_TYPE;
 /*@-immediatetrans@*/
 	    u.i32p = &zero;
