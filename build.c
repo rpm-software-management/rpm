47122e9634fb09ff19efe319f6a3fbf64c8be630
Panu Matilainen <pmatilai@redhat.com>
2007-09-07 10:01:55 +0300

    Handle no spec in tarball cleanly (rhbz#281391)

--- build.c
+++ build.c
@@ -149,7 +149,7 @@ static int buildForTarget(rpmts ts, const char * arg, BTA_t ba)
 	(void) isCompressed(arg, &res);
 
 	cmd = alloca(strlen(arg) + 50 + strlen(tmpSpecFile));
-	sprintf(cmd, "%s < '%s' | tar xOvf - Specfile 2>&1 > '%s'",
+	sprintf(cmd, "%s < '%s' | tar xOvf - Specfile 2> /dev/null > '%s'",
 			zcmds[res & 0x3], arg, tmpSpecFile);
 	if (!(fp = popen(cmd, "r"))) {
 	    rpmError(RPMERR_POPEN, _("Failed to open tar pipe: %m\n"));
@@ -161,7 +161,7 @@ static int buildForTarget(rpmts ts, const char * arg, BTA_t ba)
 	    /* Try again */
 	    (void) pclose(fp);
 
-	    sprintf(cmd, "%s < '%s' | tar xOvf - --wildcards \\*.spec 2>&1 > '%s'",
+	    sprintf(cmd, "%s < '%s' | tar xOvf - --wildcards \\*.spec 2> /dev/null > '%s'",
 		    zcmds[res & 0x3], arg, tmpSpecFile);
 	    if (!(fp = popen(cmd, "r"))) {
 		rpmError(RPMERR_POPEN, _("Failed to open tar pipe: %m\n"));
