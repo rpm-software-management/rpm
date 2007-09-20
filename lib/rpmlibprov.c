4df06c5df7dadab2baa85d80522044da9dbe5733
Panu Matilainen <pmatilai@redhat.com>
2007-09-20 12:19:34 +0300

    Conditionalize rpmlib(PayloadIsBzip2) provide on HAVE_BZLIB_H

--- lib/rpmlibprov.c
+++ lib/rpmlibprov.c
@@ -26,9 +26,11 @@ static struct rpmlibProvides_s rpmlibProvides[] = {
     { "rpmlib(CompressedFileNames)",	"3.0.4-1",
 	(RPMSENSE_RPMLIB|RPMSENSE_EQUAL),
     N_("file name(s) stored as (dirName,baseName,dirIndex) tuple, not as path.")},
+#if HAVE_BZLIB_H
     { "rpmlib(PayloadIsBzip2)",		"3.0.5-1",
 	(RPMSENSE_RPMLIB|RPMSENSE_EQUAL),
     N_("package payload can be compressed using bzip2.") },
+#endif
     { "rpmlib(PayloadFilesHavePrefix)",	"4.0-1",
 	(RPMSENSE_RPMLIB|RPMSENSE_EQUAL),
     N_("package payload file(s) have \"./\" prefix.") },
