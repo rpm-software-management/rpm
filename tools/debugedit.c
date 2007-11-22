3553b86aff3e77ac57a3103d9e84f21ac43f5c14
Ralf Corsépius <corsepiu@fedoraproject.org>
2007-11-22 13:41:06 +0100

    Include system.h

--- tools/debugedit.c
+++ tools/debugedit.c
@@ -16,6 +16,8 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
+#include "system.h"
+
 /* Needed for libelf */
 #define _FILE_OFFSET_BITS 64
 
