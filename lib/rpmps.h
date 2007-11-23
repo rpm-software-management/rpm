2d396ce001ff48c1f644bcd8e2bb6d5a3cc4d297
Panu Matilainen <pmatilai@redhat.com>
2007-11-23 14:32:45 +0200

    Add doxygen grouping to rpmps methods

--- lib/rpmps.h
+++ lib/rpmps.h
@@ -19,14 +19,14 @@ extern int _rpmps_debug;
  */
 typedef struct rpmProblem_s * rpmProblem;
 
-/**
+/** \ingroup rpmps
  * Transaction problems found while processing a transaction set/
  */
 typedef struct rpmps_s * rpmps;
 
 typedef struct rpmpsi_s * rpmpsi;
 
-/**
+/** \ingroup rpmps
  * Enumerate transaction set problem types.
  */
 typedef enum rpmProblemType_e {
@@ -43,7 +43,7 @@ typedef enum rpmProblemType_e {
     RPMPROB_DISKNODES,	/*!< installing package ... needs ... on the ... filesystem */
  } rpmProblemType;
 
-/**
+/** \ingroup rpmps
  * Create a problem item.
  * @param type		type of problem
  * @param pkgNEVR	package name
@@ -61,27 +61,27 @@ rpmProblem rpmProblemCreate(rpmProblemType type,
                             const char * altNEVR,
                             unsigned long ulong1);
 
-/**
+/** \ingroup rpmps
  * Destroy a problem item.
  * @param prob		rpm problem
  * @return		rpm problem (NULL)
  */
 rpmProblem rpmProblemFree(rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Return package NEVR
  * @param prob		rpm problem
  * @return		package NEVR
  */
 const char * rpmProblemGetPkgNEVR(const rpmProblem prob);
-/**
+/** \ingroup rpmps
  * Return related (e.g. through a dependency) package NEVR
  * @param prob		rpm problem
  * @return		related (e.g. through a dependency) package NEVR
  */
 const char * rpmProblemGetAltNEVR(const rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Return type of problem (dependency, diskpace etc)
  * @param prob		rpm problem
  * @return		type of problem
@@ -89,21 +89,21 @@ const char * rpmProblemGetAltNEVR(const rpmProblem prob);
 
 rpmProblemType rpmProblemGetType(const rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Return filename or python object address of a problem
  * @param prob		rpm problem
  * @return		filename or python object address
  */
 fnpyKey rpmProblemGetKey(const rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Return a generic data string from a problem
  * @param prob		rpm problem
  * @return		a generic data string
  * @todo		needs a better name
  */
 const char * rpmProblemGetStr(const rpmProblem prob);
-/**
+/** \ingroup rpmps
  * Return generic pointer/long attribute from a problem
  * @param prob		rpm problem
  * @return		a generic pointer/long attribute
@@ -111,14 +111,14 @@ const char * rpmProblemGetStr(const rpmProblem prob);
  */
 unsigned long rpmProblemGetLong(const rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Return formatted string representation of a problem.
  * @param prob		rpm problem
  * @return		formatted string (malloc'd)
  */
 extern const char * rpmProblemString(const rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Unreference a problem set instance.
  * @param ps		problem set
  * @param msg
@@ -132,7 +132,7 @@ rpmps XrpmpsUnlink (rpmps ps,
 		const char * msg, const char * fn, unsigned ln);
 #define	rpmpsUnlink(_ps, _msg)	XrpmpsUnlink(_ps, _msg, __FILE__, __LINE__)
 
-/**
+/** \ingroup rpmps
  * Reference a problem set instance.
  * @param ps		transaction set
  * @param msg
@@ -145,69 +145,69 @@ rpmps XrpmpsLink (rpmps ps,
 		const char * msg, const char * fn, unsigned ln);
 #define	rpmpsLink(_ps, _msg)	XrpmpsLink(_ps, _msg, __FILE__, __LINE__)
 
-/**
+/** \ingroup rpmps
  * Return number of problems in set.
  * @param ps		problem set
  * @return		number of problems
  */
 int rpmpsNumProblems(rpmps ps);
 
-/**
+/** \ingroup rpmps
  * Initialize problem set iterator.
  * @param ps		problem set
  * @return		problem set iterator
  */
 rpmpsi rpmpsInitIterator(rpmps ps);
 
-/**
+/** \ingroup rpmps
  * Destroy problem set iterator.
  * @param psi		problem set iterator
  * @return		problem set iterator (NULL)
  */
 rpmpsi rpmpsFreeIterator(rpmpsi psi);
 
-/**
+/** \ingroup rpmps
  * Return next problem set iterator index
  * @param psi		problem set iterator
  * @return		iterator index, -1 on termination
  */
 int rpmpsNextIterator(rpmpsi psi);
 
-/**
+/** \ingroup rpmps
  * Return current problem from problem set
  * @param psi		problem set iterator
  * @return		current rpmProblem 
  */
 rpmProblem rpmpsGetProblem(rpmpsi psi);
 
-/**
+/** \ingroup rpmps
  * Create a problem set.
  * @return		new problem set
  */
 rpmps rpmpsCreate(void);
 
-/**
+/** \ingroup rpmps
  * Destroy a problem set.
  * @param ps		problem set
  * @return		NULL always
  */
 rpmps rpmpsFree(rpmps ps);
 
-/**
+/** \ingroup rpmps
  * Print problems to file handle.
  * @param fp		file handle (NULL uses stderr)
  * @param ps		problem set
  */
 void rpmpsPrint(FILE *fp, rpmps ps);
 
-/**
+/** \ingroup rpmps
  * Append a problem to current set of problems.
  * @param ps		problem set
  * @param prob		rpmProblem 
  */
 void rpmpsAppendProblem(rpmps ps, rpmProblem prob);
 
-/**
+/** \ingroup rpmps
  * Append a problem to current set of problems.
  * @param ps		problem set
  * @param type		type of problem
@@ -225,7 +225,7 @@ void rpmpsAppend(rpmps ps, rpmProblemType type,
 		const char * altNEVR,
 		unsigned long ulong1);
 
-/**
+/** \ingroup rpmps
  * Filter a problem set.
  *
  * As the problem sets are generated in an order solely dependent
