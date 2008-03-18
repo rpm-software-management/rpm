9df54a8622b6640e54288835e62c6c0d0b23c748
Panu Matilainen <pmatilai@redhat.com>
2008-03-18 08:55:05 +0200

    More xstr[n]casecmp to rpmstring along the other string-stuff...

--- rpmio/rpmstring.c
+++ rpmio/rpmstring.c
@@ -135,3 +135,44 @@ void appendStringBufAux(StringBuf sb, const char *s, int nl)
 	sb->free--;
     }
 }
+
+int xstrcasecmp(const char * s1, const char * s2)
+{
+  const char * p1 = s1;
+  const char * p2 = s2;
+  char c1, c2;
+
+  if (p1 == p2)
+    return 0;
+
+  do
+    {
+      c1 = xtolower (*p1++);
+      c2 = xtolower (*p2++);
+      if (c1 == '\0')
+        break;
+    }
+  while (c1 == c2);
+
+  return (int)(c1 - c2);
+}
+
+int xstrncasecmp(const char *s1, const char *s2, size_t n)
+{
+  const char * p1 = s1;
+  const char * p2 = s2;
+  char c1, c2;
+
+  if (p1 == p2 || n == 0)
+    return 0;
+
+  do
+    {
+      c1 = xtolower (*p1++);
+      c2 = xtolower (*p2++);
+      if (c1 == '\0' || c1 != c2)
+	break;
+    } while (--n > 0);
+
+  return (int)(c1 - c2);
+}
