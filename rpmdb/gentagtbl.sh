a413ad7de22601e59e7a89f19e4986eaf70c8fbb
Panu Matilainen <pmatilai@redhat.com>
2008-04-06 19:18:44 +0300

    Move the tagtbl.c generator monster awk out of Makefile.am - stick it into a separate script, reformat so it's possible to actually   read it...

---
+++ rpmdb/gentagtbl.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+cat << EOF
+#include "system.h"
+#include <rpm/header.h>
+#include <rpm/rpmtag.h>
+#include "debug.h"
+
+static const struct headerTagTableEntry_s rpmTagTbl[] = {
+EOF
+
+${AWK} '/[\t ](RPMTAG_[A-Z0-9]*)[ \t]+([0-9]*)/ && !/internal/ {
+	tt = "NULL"
+	ta = "ANY"
+	if ($5 == "c") {
+		tt = "CHAR"
+		ta = "SCALAR"
+	}
+	if ($5 == "c[]") {
+		tt = "CHAR"
+		ta = "ARRAY"
+	} 
+	if ($5 == "h") {
+		tt = "INT16"
+		ta = "SCALAR"
+	}
+	if ($5 == "h[]") {
+		tt = "INT16"
+		ta = "ARRAY"
+	}
+	if ($5 == "i") {
+		tt = "INT32"
+		ta = "SCALAR"
+	}
+	if ($5 == "i[]") {
+		tt = "INT32"
+		ta = "ARRAY"
+	}
+	if ($5 == "l") {
+		tt = "INT64"
+		ta = "SCALAR"
+	}
+	if ($5 == "l[]") {
+		tt = "INT64"
+		ta = "ARRAY"
+	}
+	if ($5 == "s") {
+		tt = "STRING"
+		ta = "SCALAR"
+	}
+	if ($5 == "s[]") {
+		tt = "STRING_ARRAY"
+		ta = "ARRAY"
+	}
+	if ($5 == "s{}") {
+		tt = "I18NSTRING"
+		ta = "SCALAR"
+	} 
+	if ($5 == "x") {
+		tt = "BIN"
+		ta = "SCALAR"
+	}
+	if ($2 == "=") {
+		printf("    { \"%s\", %s RPM_%s_TYPE + RPM_%s_RETURN_TYPE },\n", $1, $3, tt, ta)
+	} else {
+		printf("    { \"%s\", %s, RPM_%s_TYPE + RPM_%s_RETURN_TYPE  },\n", $2, $3, tt, ta)
+	}
+}' < $1 | sort
+
+cat << EOF
+    { NULL, 0, 0 }
+};
+
+const struct headerTagTableEntry_s * const rpmTagTable = rpmTagTbl;
+const int rpmTagTableSize = sizeof(rpmTagTbl) / sizeof(rpmTagTbl[0]) - 1;
+EOF
+
