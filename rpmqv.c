3e6009a7440ea5fc3edc21bfe299288018e4e3ea
Panu Matilainen <pmatilai@redhat.com>
2008-05-02 11:40:29 +0300

    Ensure default SIGPIPE handler for --pipe (rhbz#444389)

--- rpmqv.c
+++ rpmqv.c
@@ -583,6 +583,7 @@ int main(int argc, char *argv[])
 	(void) pipe(p);
 
 	if (!(pipeChild = fork())) {
+	    (void) signal(SIGPIPE, SIG_DFL);
 	    (void) close(p[1]);
 	    (void) dup2(p[0], STDIN_FILENO);
 	    (void) close(p[0]);
