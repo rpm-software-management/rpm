fb992745595893789a02a465833299493313c0a7
Panu Matilainen <pmatilai@redhat.com>
2008-10-09 15:32:04 +0300

    Mark uidToUname() and friends as internal

--- lib/misc.h
+++ lib/misc.h
@@ -20,19 +20,26 @@ extern "C" {
  * @param dname		directory use string
  * @return		rpmRC return code
  */
+/* XXX This should be RPM_GNUC_INTERNAL too but toplevel build.c uses ATM */
 rpmRC rpmMkdirPath (const char * dpath, const char * dname);
 
 /*
  * These may be called w/ a NULL argument to flush the cache -- they return
  * -1 if the user can't be found.
  */
+RPM_GNUC_INTERNAL
 int     unameToUid(const char * thisUname, uid_t * uid);
+
+RPM_GNUC_INTERNAL
 int     gnameToGid(const char * thisGname, gid_t * gid);
 
 /*
  * Call w/ -1 to flush the cache, returns NULL if the user can't be found.
  */
+RPM_GNUC_INTERNAL
 const char * uidToUname(uid_t uid);
+
+RPM_GNUC_INTERNAL
 const char * gidToGname(gid_t gid);
 
 #ifdef __cplusplus
