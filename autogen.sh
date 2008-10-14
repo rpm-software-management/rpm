9fe51e6e2844a5b1d7d4d4116f15449d0f327b35
Panu Matilainen <pmatilai@redhat.com>
2008-10-14 14:24:30 +0300

    Rip silly "usage" message from autogen

--- autogen.sh
+++ autogen.sh
@@ -3,19 +3,6 @@
 export CFLAGS
 export LDFLAGS
 
-LTV="libtoolize (GNU libtool) 1.5.22"
-ACV="autoconf (GNU Autoconf) 2.61"
-AMV="automake (GNU automake) 1.10"
-USAGE="
-This script documents the versions of the tools I'm using to build rpm:
-	libtool-1.5.22
-	autoconf-2.61
-	automake-1.10
-Simply edit this script to change the libtool/autoconf/automake versions
-checked if you need to, as rpm should build (and has built) with all
-recent versions of libtool/autoconf/automake.
-"
-
 libtoolize=`which glibtoolize 2>/dev/null`
 case $libtoolize in
 /*) ;;
@@ -26,17 +13,13 @@ case $libtoolize in
     esac
 esac
 
-[ "`$libtoolize --version | head -1`" != "$LTV" ] && echo "$USAGE" # && exit 1
-[ "`autoconf --version | head -1`" != "$ACV" ] && echo "$USAGE" # && exit 1
-[ "`automake --version | head -1 | sed -e 's/1\.4[a-z]/1.4/'`" != "$AMV" ] && echo "$USAGE" # && exit 1
-
 myopts=
 if [ X"$@" = X  -a "X`uname -s`" = "XDarwin" -a -d /opt/local ]; then
     export myopts="--prefix=/usr --disable-nls"
     export CPPFLAGS="-I${myprefix}/include"
 fi
 
-libtoolize --copy --force
+$libtoolize --copy --force
 autopoint --force
 aclocal
 autoheader
