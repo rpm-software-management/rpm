6be065aa1a400588821dbe3c280f14995959e394
Panu Matilainen <pmatilai@redhat.com>
2008-10-31 10:17:07 +0200

    Adjust for file output change (rhbz#468129) - file util used reported file details along with mime type, newer ones don't (cherry picked from commit 81660b26bc8e2c03f55cf928955f7ed84e00785a)

--- scripts/find-debuginfo.sh
+++ scripts/find-debuginfo.sh
@@ -90,7 +90,7 @@ strip_to_debug()
 {
   local g=
   $strip_g && case "$(file -bi "$2")" in
-  application/x-sharedlib,*) g=-g ;;
+  application/x-sharedlib*) g=-g ;;
   esac
   eu-strip --remove-comment $g -f "$1" "$2" || exit
 }
