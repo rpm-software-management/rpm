f9ed52c0c7a29aa64bc44f94f7b102775ce4be44
Panu Matilainen <pmatilai@redhat.com>
2008-11-20 11:47:52 +0200

    fpCacheCreate() already doubles the size-hint, dont do it twice

--- lib/transaction.c
+++ lib/transaction.c
@@ -1012,7 +1012,7 @@ int rpmtsRun(rpmts ts, rpmps okProbs, rpmprobFilterFlags ignoreSet)
 
     ts->ht = rpmFpHashCreate(totalFileCount * 2 + 1, fpHashFunction, fpEqual,
 			     NULL, NULL);
-    fpc = fpCacheCreate(totalFileCount * 2 + 10001);
+    fpc = fpCacheCreate(totalFileCount + 10001);
 
     /* ===============================================
      * Add fingerprint for each file not skipped.
