a2999caaf4fb6a1d121b71d009d15a7a651d2bf0
Panu Matilainen <pmatilai@redhat.com>
2009-10-19 11:06:07 +0300

    PyVarObject_HEAD_INIT() compatibility macro for Python < 2.6

--- python/rpmsystem-py.h
+++ python/rpmsystem-py.h
@@ -15,8 +15,15 @@ typedef ssize_t Py_ssize_t;
 typedef Py_ssize_t (*lenfunc)(PyObject *);
 #endif  
 
+/* Compatibility macros for Python < 2.6 */
+#ifndef PyVarObject_HEAD_INIT
+#define PyVarObject_HEAD_INIT(type, size) \
+	PyObject_HEAD_INIT(type) size,
+#endif 
+
 #ifndef Py_TYPE
 #define Py_TYPE(o) ((o)->ob_type)
 #endif
+
 
 #endif	/* H_SYSTEM_PYTHON */
