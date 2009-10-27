714ebe92a1052e67967221e73ac8b788140e684c
Panu Matilainen <pmatilai@redhat.com>
2009-10-27 15:15:20 +0200

    Argh, typo in previous commit

--- python/rpmds-py.c
+++ python/rpmds-py.c
@@ -472,7 +472,7 @@ PyTypeObject rpmds_Type = {
 	PyObject_GenericGetAttr,	/* tp_getattro */
 	PyObject_GenericSetAttr,	/* tp_setattro */
 	0,				/* tp_as_buffer */
-	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE /* tp_flags */
+	Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
 	rpmds_doc,			/* tp_doc */
 	0,				/* tp_traverse */
 	0,				/* tp_clear */
