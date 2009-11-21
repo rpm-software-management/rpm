6711bcc6b948f8f60373af42e232e160c8cb89ed
Panu Matilainen <pmatilai@redhat.com>
2009-11-21 11:07:29 +0200

    Remove unnecessary hdr.instance() method - this is available through tag extension now, no point having a   separate method for it

--- python/header-py.c
+++ python/header-py.c
@@ -239,11 +239,6 @@ static PyObject * hdrFormat(hdrObject * s, PyObject * args, PyObject * kwds)
     return result;
 }
 
-static PyObject *hdrInstance(hdrObject *s)
-{
-    return Py_BuildValue("i", headerGetInstance(s->h));
-}
-
 static PyObject *hdrIsSource(hdrObject *s)
 {
     return PyBool_FromLong(headerIsSource(s->h));
@@ -334,8 +329,6 @@ static struct PyMethodDef hdr_methods[] = {
     {"has_key",		(PyCFunction) hdrHasKey,	METH_O,
 	NULL },
     {"sprintf",		(PyCFunction) hdrFormat,	METH_VARARGS|METH_KEYWORDS,
-	NULL },
-    {"instance",	(PyCFunction)hdrInstance,	METH_NOARGS,
 	NULL },
     {"isSource",	(PyCFunction)hdrIsSource,	METH_NOARGS, 
 	NULL },
