rpm-software-management/rpm

commit af8b41c64af39ce07d85fcd92fa78d566747d815

Use rpmds methods to check for duplicates in addReqProv() - simplifies things quite a bit...

build/reqprov.c
@@ -13,15 +13,10 @@ int addReqProv(rpmSpec spec, Header h, rpmTag tagN,
 		const char * N, const char * EVR, rpmsenseFlags Flags,
 		int index)
 {
-    HGE_t hge = (HGE_t)headerGetEntryMinMemory;
-    HFD_t hfd = headerFreeData;
-    const char ** names;
-    rpmTagType dnt;
     rpmTag nametag = 0;
     rpmTag versiontag = 0;
     rpmTag flagtag = 0;
     rpmTag indextag = 0;
-    rpm_count_t len;
     rpmsenseFlags extra = RPMSENSE_ANY;
     int xx;
     
@@ -62,39 +57,11 @@ int addReqProv(rpmSpec spec, Header h, rpmTag tagN,
 	EVR = "";
     
     /* Check for duplicate dependencies. */
-    if (hge(h, nametag, &dnt, (rpm_data_t *) &names, &len)) {
-	const char ** versions = NULL;
-	rpmTagType dvt = RPM_STRING_ARRAY_TYPE;
-	int *flags = NULL;
-	int *indexes = NULL;
-	int duplicate = 0;
-
-	if (flagtag) {
-	    xx = hge(h, versiontag, &dvt, (rpm_data_t *) &versions, NULL);
-	    xx = hge(h, flagtag, NULL, (rpm_data_t *) &flags, NULL);
-	}
-	if (indextag)
-	    xx = hge(h, indextag, NULL, (rpm_data_t *) &indexes, NULL);
-
-	while (len > 0) {
-	    len--;
-	    if (strcmp(names[len], N))
-		continue;
-	    if (flagtag && versions != NULL &&
-		(strcmp(versions[len], EVR) || flags[len] != Flags))
-		continue;
-	    if (indextag && indexes != NULL && indexes[len] != index)
-		continue;
-
-	    /* This is a duplicate dependency. */
-	    duplicate = 1;
-
-	    break;
-	}
-	names = hfd(names, dnt);
-	versions = hfd(versions, dvt);
-	if (duplicate)
-	    return 0;
+    rpmds hds = rpmdsNew(h, nametag, 0);
+    rpmds newds = rpmdsSingle(nametag, N, EVR, Flags);
+    /* already got it, don't bother */
+    if (rpmdsFind(hds, newds) >= 0) {
+	goto exit;
     }
 
     /* Add this dependency. */
@@ -108,6 +75,10 @@ int addReqProv(rpmSpec spec, Header h, rpmTag tagN,
     if (indextag)
 	xx = headerAddOrAppendEntry(h, indextag, RPM_INT32_TYPE, &index, 1);
 
+exit:
+    rpmdsFree(hds);
+    rpmdsFree(newds);
+	
     return 0;
 }
 

commit 6e90bd42a577d425add4638afc9e49d05fc4e8b2

Fix duplicate dependency checking on build - Broken by commit af8b41c64af39ce07d85fcd92fa78d566747d815 which   simplified too much. - There's no guarantee that rpmdsNew() returns a sorted dependency set   so rpmdsFind() doesn't work correctly here. Walk the ds manually instead. - With multiple triggers of same type, identical trigger conditions on   different trigger script were seen as duplicates (RhBug:490378) - Split the duplicate checking to separate helper function for clarity (cherry picked from commit 10772ac7dfad60cb5a20681d22cf851468d0a8f9)

build/reqprov.c
@@ -9,6 +9,38 @@
 #include <rpm/rpmbuild.h>
 #include "debug.h"
 
+static int isNewDep(Header h, rpmTag nametag,
+		  const char *N, const char *EVR, rpmsenseFlags Flags,
+		  rpmTag indextag, uint32_t index)
+{
+    int new = 1;
+    struct rpmtd_s idx;
+    rpmds ads = rpmdsNew(h, nametag, 0);
+    rpmds bds = rpmdsSingle(nametag, N, EVR, Flags);
+
+    if (indextag) {
+	headerGet(h, indextag, &idx, HEADERGET_MINMEM);
+    }
+
+    /* XXX there's no guarantee the ds is sorted here so rpmdsFind() wont do */
+    rpmdsInit(ads);
+    while (new && rpmdsNext(ads) >= 0) {
+	if (strcmp(rpmdsN(ads), rpmdsN(bds))) continue;
+	if (strcmp(rpmdsEVR(ads), rpmdsEVR(bds))) continue;
+	if (rpmdsFlags(ads) != rpmdsFlags(bds)) continue;
+	if (indextag && rpmtdSetIndex(&idx, rpmdsIx(ads)) >= 0 &&
+			rpmtdGetNumber(&idx) != index) continue;
+	new = 0;
+    }
+    
+    if (indextag) {
+	rpmtdFreeData(&idx);
+    }
+    rpmdsFree(ads);
+    rpmdsFree(bds);
+    return new;
+}
+
 int addReqProv(rpmSpec spec, Header h, rpmTag tagN,
 		const char * N, const char * EVR, rpmsenseFlags Flags,
 		uint32_t index)
@@ -55,28 +87,18 @@ int addReqProv(rpmSpec spec, Header h, rpmTag tagN,
     if (EVR == NULL)
 	EVR = "";
     
-    /* Check for duplicate dependencies. */
-    rpmds hds = rpmdsNew(h, nametag, 0);
-    rpmds newds = rpmdsSingle(nametag, N, EVR, Flags);
-    /* already got it, don't bother */
-    if (rpmdsFind(hds, newds) >= 0) {
-	goto exit;
+    /* Avoid adding duplicate dependencies. */
+    if (isNewDep(h, nametag, N, EVR, Flags, indextag, index)) {
+	headerPutString(h, nametag, N);
+	if (flagtag) {
+	    headerPutString(h, versiontag, EVR);
+	    headerPutUint32(h, flagtag, &Flags, 1);
+	}
+	if (indextag) {
+	    headerPutUint32(h, indextag, &index, 1);
+	}
     }
 
-    /* Add this dependency. */
-    headerPutString(h, nametag, N);
-    if (flagtag) {
-	headerPutString(h, versiontag, EVR);
-	headerPutUint32(h, flagtag, &Flags, 1);
-    }
-    if (indextag) {
-	headerPutUint32(h, indextag, &index, 1);
-    }
-
-exit:
-    rpmdsFree(hds);
-    rpmdsFree(newds);
-	
     return 0;
 }
 