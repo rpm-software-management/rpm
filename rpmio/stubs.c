8209448f2457c880c6fb3a5e97c8080ce41c4323
Panu Matilainen <pmatilai@redhat.com>
2009-12-23 15:02:00 +0200

    Oops, update stubs.c too for the removed replacements

--- rpmio/stubs.c
+++ rpmio/stubs.c
@@ -42,32 +42,8 @@
 #include "misc/stpncpy.c"
 #endif
 
-#if !defined(HAVE_STRCSPN)
-#include "misc/strcspn.c"
-#endif
-
 #if !defined(HAVE_STRSPN)
 #include "misc/strdup.c"
-#endif
-
-#if !defined(HAVE_STRERROR)
-#include "misc/error.c"
-#endif
-
-#if !defined(HAVE_STRTOL)
-#include "misc/strtol.c"
-#endif
-
-#if !defined(HAVE_STRTOUL)
-#include "misc/strtoul.c"
-#endif
-
-#if !defined(HAVE_STRSPN)
-#include "misc/strspn.c"
-#endif
-
-#if !defined(HAVE_STRSTR)
-#include "misc/strstr.c"
 #endif
 
 #if defined(USE_GNU_GLOB)
