3125bac65f75e0e67258df6c5fc55d0752f245af
Panu Matilainen <pmatilai@redhat.com>
2010-01-04 14:44:26 +0200

    Lose the hysterical fakefork hack - we're not particularly interested in AmigaOS quirks from 1996

--- misc/fakefork.c
+++
@@ -1,8 +0,0 @@
-/* This is really, really dumb. But AmigaOS gives us vfork(), but not
-   fork() and this should make things work despite their brokenness */
-
-#include "system.h"
-
-int fork() {
-    return vfork();
-}
