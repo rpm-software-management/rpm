cd174374ffd0ee1cf5bea77905db4dbbc962aeda
Panu Matilainen <pmatilai@redhat.com>
2010-01-05 11:33:51 +0200

    Remove unnecessary and broken time.h related configure + system.h checks - the AC_HEADER_TIME check is unnecessary for any remotely recent systems,   and the HAVE_SYS_TIME_H conditional in system.h is just broken as we   dont even check for <sys/time.h> header in configure - dont include from system.h, the time.h and sys/time.h get already   included through our public headers where necessary

--- system.h
+++ system.h
@@ -32,17 +32,6 @@ extern char ** environ;
 #endif
 #endif
 
-#if TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
-#endif
-
 #ifdef HAVE_STRING_H
 # if !STDC_HEADERS && HAVE_MEMORY_H
 #  include <memory.h>
