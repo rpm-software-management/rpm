ae66316c2cf9a49a804369fc1629eda426fa0ecd
Panu Matilainen <pmatilai@redhat.com>
2010-05-19 15:32:54 +0300

    Add public pgpValString() function + enum for the various types - The equivalent used to be possible in 4.4.x era as the value tables   were all exported, but this way we need to export far less   and the implementation details stay internal. (cherry picked from commit 2be413810cd78b0a503699ceca3f8e1505f1b425)

--- rpmio/rpmpgp.c
+++ rpmio/rpmpgp.c
@@ -202,20 +202,38 @@ static void pgpPrtStr(const char *pre, const char *s)
     fprintf(stderr, " %s", s);
 }
 
-/** \ingroup rpmpgp
- * Return string representation of am OpenPGP value.
- * @param vs		table of (string,value) pairs
- * @param val		byte value to lookup
- * @return		string value of byte
- */
-static inline
-const char * pgpValStr(pgpValTbl vs, uint8_t val)
+static const char * pgpValStr(pgpValTbl vs, uint8_t val)
 {
     do {
 	if (vs->val == val)
 	    break;
     } while ((++vs)->val != -1);
     return vs->str;
+}
+
+static pgpValTbl pgpValTable(pgpValType type)
+{
+    switch (type) {
+    case PGPVAL_TAG:		return pgpTagTbl;
+    case PGPVAL_ARMORBLOCK:	return pgpArmorTbl;
+    case PGPVAL_ARMORKEY:	return pgpArmorKeyTbl;
+    case PGPVAL_SIGTYPE:	return pgpSigTypeTbl;
+    case PGPVAL_SUBTYPE:	return pgpSubTypeTbl;
+    case PGPVAL_PUBKEYALGO:	return pgpPubkeyTbl;
+    case PGPVAL_SYMKEYALGO:	return pgpSymkeyTbl;
+    case PGPVAL_COMPRESSALGO:	return pgpCompressionTbl;
+    case PGPVAL_HASHALGO: 	return pgpHashTbl;
+    case PGPVAL_SERVERPREFS:	return pgpKeyServerPrefsTbl;
+    default:
+	break;
+    }
+    return NULL;
+}
+
+const char * pgpValString(pgpValType type, uint8_t val)
+{
+    pgpValTbl tbl = pgpValTable(type);
+    return (tbl != NULL) ? pgpValStr(tbl, val) : NULL;
 }
 
 static void pgpPrtHex(const char *pre, const uint8_t *p, size_t plen)
