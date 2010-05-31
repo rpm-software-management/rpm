90a37c2f5c848eaf37c9afeac1e85dfe11e1301a
Panu Matilainen <pmatilai@redhat.com>
2010-05-31 13:11:47 +0300

    Add a simple API for dealing with chroot state changes - Chroot is a process global state so it needs to be tracked globally.   A process can (in theory) have several transaction sets, each with   different roots (although only one can be active at any time), so   associating the chroot state with transaction set (as currently done)   is not right. - "Reference count" chroot entering and exiting so callers dont need   to track the state changes individually when they need to go   in and out of chroot if not already done. - This should probably go to librpmio eventually but as there are no   needs outside librpm currently, keeping this internal so we're free   to fiddle with the api if necessary

---
+++ lib/rpmchroot.c
@@ -0,0 +1,104 @@
+#include "system.h"
+#include <stdlib.h>
+#include <rpm/rpmstring.h>
+#include <rpm/rpmlog.h>
+#include "lib/rpmchroot.h"
+#include "debug.h"
+
+struct rootState_s {
+    char *rootDir;
+    int chrootDone;
+    int cwd;
+};
+
+/* Process global chroot state */
+static struct rootState_s rootState = {
+   .rootDir = NULL,
+   .chrootDone = 0,
+   .cwd = -1,
+}; 
+
+int rpmChrootSet(const char *rootDir)
+{
+    int rc = 0;
+
+    /* Setting same rootDir again is a no-op and not an error */
+    if (rootDir && rootState.rootDir && rstreq(rootDir, rootState.rootDir))
+	return 0;
+
+    /* Resetting only permitted in neutral state */
+    if (rootState.chrootDone != 0)
+	return -1;
+
+    rootState.rootDir = rfree(rootState.rootDir);
+    if (rootState.cwd >= 0) {
+	close(rootState.cwd);
+	rootState.cwd = -1;
+    }
+
+    if (rootDir != NULL) {
+	rootState.rootDir = rstrdup(rootDir);
+	rootState.cwd = open(".", O_RDONLY);
+	if (rootState.cwd < 0) {
+	    rc = -1;
+	}
+    }
+
+    return rc;
+}
+
+int rpmChrootIn(void)
+{
+    int rc = 0;
+
+    if (rootState.rootDir == NULL || rstreq(rootState.rootDir, "/"))
+	return 0;
+
+    if (rootState.cwd < 0) {
+	rpmlog(RPMLOG_ERR, _("%s: chroot directory not set\n"), __func__);
+	return -1;
+    }
+
+    /* "refcounted" entry to chroot */
+    if (rootState.chrootDone > 0) {
+	rootState.chrootDone++;
+    } else if (rootState.chrootDone == 0) {
+	if (chdir("/") == 0 && chroot(rootState.rootDir) == 0) {
+	    rootState.chrootDone = 1;
+	} else {
+	    rpmlog(RPMLOG_ERR, _("Unable to change root directory: %m\n"));
+	    rc = -1;
+	}
+    }
+    return rc;
+}
+
+int rpmChrootOut(void)
+{
+    int rc = 0;
+    if (rootState.rootDir == NULL || rstreq(rootState.rootDir, "/"))
+	return 0;
+
+    if (rootState.cwd < 0) {
+	rpmlog(RPMLOG_ERR, _("%s: chroot directory not set\n"), __func__);
+	return -1;
+    }
+
+    /* "refcounted" return from chroot */
+    if (rootState.chrootDone > 1) {
+	rootState.chrootDone--;
+    } else if (rootState.chrootDone == 1) {
+	if (chroot(".") == 0 && fchdir(rootState.cwd) == 0) {
+	    rootState.chrootDone = 0;
+	} else {
+	    rpmlog(RPMLOG_ERR, _("Unable to restore root directory: %m\n"));
+	    rc = -1;
+	}
+    }
+    return rc;
+}
+
+int rpmChrootDone(void)
+{
+    return (rootState.chrootDone > 0);
+}
---
+++ lib/rpmchroot.h
@@ -0,0 +1,36 @@
+#ifndef _RPMCHROOT_H
+#define _RPMCHROOT_H
+
+#include <rpm/rpmutil.h>
+
+/** \ingroup rpmchroot
+ * Set or clear process-wide chroot directory.
+ * Calling this while chrooted is an error.
+ * param rootDir	new chroot directory (or NULL to reset)
+ * return		-1 on error, 0 on success
+ */
+RPM_GNUC_INTERNAL
+int rpmChrootSet(const char *rootDir);
+
+/** \ingroup rpmchroot
+ * Enter chroot if necessary.
+ * return		-1 on error, 0 on success.
+ */
+RPM_GNUC_INTERNAL
+int rpmChrootIn(void);
+
+/** \ingroup rpmchroot
+ * Return from chroot if necessary.
+ * return		-1 on error, 0 succes.
+ */
+RPM_GNUC_INTERNAL
+int rpmChrootOut(void);
+
+/** \ingroup rpmchroot
+ * Return chrooted status.
+ * return		1 if chrooted, 0 otherwise
+ */
+RPM_GNUC_INTERNAL
+int rpmChrootDone(void);
+
+#endif /* _RPMCHROOT_H */
