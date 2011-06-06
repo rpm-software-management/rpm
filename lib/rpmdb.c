1bdfeb3fb5c4ecba448e238fce85c56a3d5f127a
Panu Matilainen <pmatilai@redhat.com>
2011-06-06 13:09:15 +0300

    Cosmetics: function blocks start on a new line

--- lib/rpmdb.c
+++ lib/rpmdb.c
@@ -417,22 +417,26 @@ static int dbiPruneSet(dbiIndexSet set, void * recs, int nrecs,
 }
 
 /* Count items in index database set. */
-static unsigned int dbiIndexSetCount(dbiIndexSet set) {
+static unsigned int dbiIndexSetCount(dbiIndexSet set)
+{
     return set->count;
 }
 
 /* Return record offset of header from element in index database set. */
-static unsigned int dbiIndexRecordOffset(dbiIndexSet set, int recno) {
+static unsigned int dbiIndexRecordOffset(dbiIndexSet set, int recno)
+{
     return set->recs[recno].hdrNum;
 }
 
 /* Return file index from element in index database set. */
-static unsigned int dbiIndexRecordFileNumber(dbiIndexSet set, int recno) {
+static unsigned int dbiIndexRecordFileNumber(dbiIndexSet set, int recno)
+{
     return set->recs[recno].tagNum;
 }
 
 /* Destroy set of index database items */
-static dbiIndexSet dbiFreeIndexSet(dbiIndexSet set) {
+static dbiIndexSet dbiFreeIndexSet(dbiIndexSet set)
+{
     if (set) {
 	set->recs = _free(set->recs);
 	set = _free(set);
