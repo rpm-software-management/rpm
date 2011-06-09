rpm-software-management/rpm

commit 7e4415fcc5e11cfd4cd9d0dfe19568be73f15d74

Error/notfound case is handled the same for both these cases

lib/rpmdb.c
@@ -1973,10 +1973,6 @@ rpmdbMatchIterator rpmdbInitIterator(rpmdb db, rpmDbiTagVal rpmtag,
             } else {
 		rc = dbiGetToSet(dbi, keyp, keylen, &set);
 	    }
-            if (rc)	{	/* error/not found */
-                set = dbiIndexSetFree(set);
-                goto exit;
-            }
 	} else {
             /* get all entries from index */
 	    dbiCursor dbc = dbiCursorInit(dbi, 0);
@@ -2000,12 +1996,12 @@ rpmdbMatchIterator rpmdbInitIterator(rpmdb db, rpmDbiTagVal rpmtag,
             }
 
 	    dbiCursorFree(dbc);
-
-            if (rc != DB_NOTFOUND)	{	/* error */
-                set = dbiIndexSetFree(set);
-                goto exit;
-            }
         }
+
+	if (rc)	{	/* error/not found */
+	    set = dbiIndexSetFree(set);
+	    goto exit;
+	}
     }
 
     /* Copy the retrieval key, byte swapping header instance if necessary. */

commit 211bc3d356ccbbc2b1026015d32e21986f6a95b0

Fix the non-keyed match iteration case wrt DB_NOTFOUND - This broke in commit 7e4415fcc5e11cfd4cd9d0dfe19568be73f15d74,   the cases are not identical afterall: With non-keyed cursor   retrievals DB_NOTFOUND is returned when there's nothing more to   get, this is not an error situation.

lib/rpmdb.c
@@ -1989,6 +1989,10 @@ rpmdbMatchIterator rpmdbInitIterator(rpmdb db, rpmDbiTagVal rpmtag,
 		rc = dbiCursorGetToSet(dbc, NULL, 0, &set);
 	    } while (rc == 0);
 
+	    /* If we got some results, not found is not an error */
+	    if (rc == DB_NOTFOUND && set != NULL)
+		rc = 0;
+
 	    dbiCursorFree(dbc);
         }
 