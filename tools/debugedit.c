7c463c32fc5e1893369e6d883c157d58d16872f7
Panu Matilainen <pmatilai@redhat.com>
2011-12-02 12:11:05 +0200

    Teach debugedit about .debug_macro dwarf section (RhBug:759272)

--- tools/debugedit.c
+++ tools/debugedit.c
@@ -225,6 +225,7 @@ static struct
 #define DEBUG_FRAME	9
 #define DEBUG_RANGES	10
 #define DEBUG_TYPES	11
+#define DEBUG_MACRO	12
     { ".debug_info", NULL, NULL, 0, 0, 0 },
     { ".debug_abbrev", NULL, NULL, 0, 0, 0 },
     { ".debug_line", NULL, NULL, 0, 0, 0 },
@@ -237,6 +238,7 @@ static struct
     { ".debug_frame", NULL, NULL, 0, 0, 0 },
     { ".debug_ranges", NULL, NULL, 0, 0, 0 },
     { ".debug_types", NULL, NULL, 0, 0, 0 },
+    { ".debug_macro", NULL, NULL, 0, 0, 0 },
     { NULL, NULL, NULL, 0, 0, 0 }
   };
 
