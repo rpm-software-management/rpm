rpm-software-management/rpm

commit 06a2f1269b035a3a76464149834f2a5a8c4e89f2

Don't assume rpmfiNew() always succeeds
- Add NULL checks and add/adjust comments where appropriate.
- The remaining callers should handle NULL fi gracefully if not
  entirely correctly: rpmfiFC() returns 0 on NULL fi, so these
  callers just see the erronous file info set as "no files" case.
  Something to fine-tune later...

lib/rpmte.c
@@ -676,6 +676,7 @@ static Header rpmteFDHeader(rpmte te)
 
 static int rpmteOpen(rpmte te, int reload_fi)
 {
+    int rc = 0; /* assume failure */
     Header h = NULL;
     if (te == NULL || te->ts == NULL || rpmteFailed(te))
 	goto exit;
@@ -694,13 +695,16 @@ static int rpmteOpen(rpmte te, int reload_fi)
 	if (reload_fi) {
 	    te->fi = getFI(te, h);
 	}
+
+	/* This can fail if we get a different, bad header from callback */
+	rc = (te->fi != NULL);
 	
 	rpmteSetHeader(te, h);
 	headerFree(h);
     }
 
 exit:
-    return (h != NULL);
+    return rc;
 }
 
 static int rpmteClose(rpmte te, int reset_fi)

build/files.c
@@ -1239,6 +1239,9 @@ static void genCpioListAndHeader(FileList fl,
     headerConvert(h, HEADERCONV_COMPRESSFILELIST);
     fi = rpmfiNew(NULL, h, RPMTAG_BASENAMES, flags);
 
+    if (fi == NULL)
+	return;
+
     /* 
      * Grab the real filenames from ORIGFILENAMES and put into OLDFILENAMES,
      * remove temporary cruft and side-effects from filelist compression 
@@ -1896,6 +1899,8 @@ static rpmRC processPackageFiles(rpmSpec spec, rpmBuildPkgFlags pkgFlags,
 			"PartialHardlinkSets", "4.0.4-1");
 
     genCpioListAndHeader(&fl, &pkg->cpioList, pkg->header, 0);
+    if (pkg->cpioList == NULL)
+	fl.processingFailed = 1;
 
 exit:
     fl.buildRoot = _free(fl.buildRoot);
@@ -2031,9 +2036,13 @@ rpmRC processSourceFiles(rpmSpec spec, rpmBuildPkgFlags pkgFlags)
     argvFree(files);
 
     if (! fl.processingFailed) {
-	if (spec->sourceHeader != NULL)
+	if (spec->sourceHeader != NULL) {
 	    genCpioListAndHeader(&fl, &spec->sourceCpioList,
 			spec->sourceHeader, 1);
+	    if (spec->sourceCpioList == NULL) {
+		fl.processingFailed = 1;
+	    }
+	}
     }
 
     fl.fileList = freeFileList(fl.fileList, fl.fileListRecsUsed);

lib/psm.c
@@ -322,7 +322,7 @@ rpmRC rpmInstallSourcePackage(rpmts ts, FD_t fd,
     fi = rpmfiNew(ts, h, RPMTAG_BASENAMES, RPMFI_KEEPHEADER);
     h = headerFree(h);
 
-    if (fi == NULL) {	/* XXX can't happen */
+    if (fi == NULL) {
 	goto exit;
     }
     fi->apath = filenames.data; /* Ick */

lib/transaction.c
@@ -990,6 +990,7 @@ void checkInstalledFiles(rpmts ts, uint64_t fileCount, rpmFpHash ht, fingerPrint
 		switch (rpmteType(p)) {
 		case TR_ADDED:
 		    if (!otherFi) {
+			/* XXX What to do if this fails? */
 		        otherFi = rpmfiNew(ts, h, RPMTAG_BASENAMES, RPMFI_KEEPHEADER);
 		    }
 		    rpmfiSetFX(fi, recs[j].fileno);

lib/verify.c
@@ -339,6 +339,9 @@ static int verifyHeader(rpmts ts, Header h, rpmVerifyAttrs omitMask, int ghosts)
     int ec = 0;		/* assume no problems */
     rpmfi fi = rpmfiNew(ts, h, RPMTAG_BASENAMES, RPMFI_FLAGS_VERIFY);
 
+    if (fi == NULL)
+	return 1;
+
     rpmfiInit(fi, 0);
     while (rpmfiNext(fi) >= 0) {
 	rpmfileAttrs fileAttrs = rpmfiFFlags(fi);

python/rpmfi-py.c
@@ -307,6 +307,11 @@ static PyObject * rpmfi_new(PyTypeObject * subtype, PyObject *args, PyObject *kw
 
     fi = rpmfiNew(NULL, h, tagN, flags);
 
+    if (fi == NULL) {
+	PyErr_SetString(PyExc_ValueError, "invalid file data in header");
+	return NULL;
+    }
+
     return rpmfi_Wrap(subtype, fi);
 }
 

commit 274dbf557d1cac90f7c278f9b6d6af05997d92df

Don't free up file info sets on transaction test-runs, take II
- Despite commit cef18c94807af0935b7796c462aab8ed39f0f376, we'd still
  end up freeing the file info sets via rpmteClose() while going
  through the test-transaction packages. This together with commit
  06a2f1269b035a3a76464149834f2a5a8c4e89f2 caused install failures
  on packages which have %pretrans scriptlets, if a test-transaction
  was first performed on the same transaction set that gets used
  for the "real" transaction as well. How wonderfully obscure...

lib/rpmte.c
@@ -917,7 +917,8 @@ int rpmteProcess(rpmte te, pkgGoal goal)
 {
     /* Only install/erase resets pkg file info */
     int scriptstage = (goal != PKG_INSTALL && goal != PKG_ERASE);
-    int reset_fi = (scriptstage == 0);
+    int test = (rpmtsFlags(te->ts) & RPMTRANS_FLAG_TEST);
+    int reset_fi = (scriptstage == 0 && test == 0);
     int failed = 1;
 
     /* Dont bother opening for elements without pre/posttrans scripts */