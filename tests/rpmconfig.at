rpm-software-management/rpm

commit 1845c1d711b88723e596c88e8a7730a360029089

Prepare for killing the fsm: Move code for erasing files from FSM_COMMIT to FSM_ERASE

lib/fsm.c
@@ -1779,10 +1779,50 @@ static int fsmStage(FSM_t fsm, fileStage stage)
 		break;
 	    }
 
-            if (!fsm->postpone) {
-                /* Rename/erase item. */
-		rc = fsmNext(fsm, FSM_COMMIT);
-            }
+            /* Remove erased files. */
+	    if (!fsm->postpone && fsm->action == FA_ERASE) {
+		rpmte te = fsmGetTe(fsm);
+		if (S_ISDIR(st->st_mode)) {
+		    rc = fsmRmdir(fsm->path);
+		    if (!rc) break;
+		    switch (rc) {
+		    case CPIOERR_ENOENT: /* XXX rmdir("/") linux 2.2.x kernel hack */
+		    case CPIOERR_ENOTEMPTY:
+	/* XXX make sure that build side permits %missingok on directories. */
+			if (fsm->fflags & RPMFILE_MISSINGOK)
+			    break;
+
+			/* XXX common error message. */
+			rpmlog(
+			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
+			    _("%s rmdir of %s failed: Directory not empty\n"),
+				rpmteTypeString(te), fsm->path);
+			break;
+		    default:
+			rpmlog(
+			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
+				_("%s rmdir of %s failed: %s\n"),
+				rpmteTypeString(te), fsm->path, strerror(errno));
+			break;
+		    }
+		} else {
+		    rc = fsmUnlink(fsm->path, fsm->mapFlags);
+		    if (!rc) break;
+		    switch (rc) {
+		    case CPIOERR_ENOENT:
+			if (fsm->fflags & RPMFILE_MISSINGOK)
+			    break;
+		    default:
+			rpmlog(
+			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
+				_("%s unlink of %s failed: %s\n"),
+				rpmteTypeString(te), fsm->path, strerror(errno));
+			break;
+		    }
+		}
+	    }
+	    /* XXX Failure to remove is not (yet) cause for failure. */
+	    if (!strict_erasures) rc = 0;
 
             if (rc) break;
 
@@ -1943,54 +1983,6 @@ static int fsmStage(FSM_t fsm, fileStage stage)
 	    }
 	    free(path);
 	    free(opath);
-	}
-
-	/* Remove erased files. */
-	if (fsm->goal == FSM_PKGERASE) {
-	    if (fsm->action == FA_ERASE) {
-		rpmte te = fsmGetTe(fsm);
-		if (S_ISDIR(st->st_mode)) {
-		    rc = fsmRmdir(fsm->path);
-		    if (!rc) break;
-		    switch (rc) {
-		    case CPIOERR_ENOENT: /* XXX rmdir("/") linux 2.2.x kernel hack */
-		    case CPIOERR_ENOTEMPTY:
-	/* XXX make sure that build side permits %missingok on directories. */
-			if (fsm->fflags & RPMFILE_MISSINGOK)
-			    break;
-
-			/* XXX common error message. */
-			rpmlog(
-			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
-			    _("%s rmdir of %s failed: Directory not empty\n"), 
-				rpmteTypeString(te), fsm->path);
-			break;
-		    default:
-			rpmlog(
-			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
-				_("%s rmdir of %s failed: %s\n"),
-				rpmteTypeString(te), fsm->path, strerror(errno));
-			break;
-		    }
-		} else {
-		    rc = fsmUnlink(fsm->path, fsm->mapFlags);
-		    if (!rc) break;
-		    switch (rc) {
-		    case CPIOERR_ENOENT:
-			if (fsm->fflags & RPMFILE_MISSINGOK)
-			    break;
-		    default:
-			rpmlog(
-			    (strict_erasures ? RPMLOG_ERR : RPMLOG_DEBUG),
-				_("%s unlink of %s failed: %s\n"),
-				rpmteTypeString(te), fsm->path, strerror(errno));
-			break;
-		    }
-		}
-	    }
-	    /* XXX Failure to remove is not (yet) cause for failure. */
-	    if (!strict_erasures) rc = 0;
-	    break;
 	}
 
 	/* XXX Special case /dev/log, which shouldn't be packaged anyways */

commit 3f67160a2edb7050835eefa861c5ef85d2d00db0

And yet more %config test-cases
- Test behavior when installing over a pre-existing file
- Test behavior on erasure
- Two of the erasure-tests are currently failing due to regression
  introduced in commit 1845c1d711b88723e596c88e8a7730a360029089

tests/rpmconfig.at
@@ -2,6 +2,178 @@
 
 AT_BANNER([RPM config file behavior])
 
+# Install over existing config file
+AT_SETUP([install config on existiting file])
+AT_KEYWORDS([install])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+cf="${RPMTEST}"/etc/my.conf
+rm -f "${cf}" "${cf}".rpm*
+rm -rf "${TOPDIR}"
+
+runroot rpmbuild --quiet -bb \
+        --define "ver 1.0" \
+	--define "filedata foo" \
+          /data/SPECS/configtest.spec
+
+echo "otherstuff" > "${cf}"
+runroot rpm -U "${TOPDIR}"/RPMS/noarch/configtest-1.0-1.noarch.rpm
+cat "${cf}"
+runroot rpm -e configtest
+cat "${cf}.rpmorig"
+],
+[0],
+[foo
+otherstuff
+],
+[warning: /etc/my.conf saved as /etc/my.conf.rpmorig]
+)
+AT_CLEANUP
+
+# Install over existing config file
+AT_SETUP([install config(noreplace) on existiting file])
+AT_KEYWORDS([install])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+cf="${RPMTEST}"/etc/my.conf
+rm -f "${cf}" "${cf}".rpm*
+rm -rf "${TOPDIR}"
+
+runroot rpmbuild --quiet -bb \
+        --define "ver 1.0" \
+	--define "filedata foo" \
+          /data/SPECS/configtest.spec
+
+echo "otherstuff" > "${cf}"
+runroot rpm -U "${TOPDIR}"/RPMS/noarch/configtest-1.0-1.noarch.rpm
+cat "${cf}"
+runroot rpm -e configtest
+cat "${cf}.rpmorig"
+],
+[0],
+[foo
+otherstuff
+],
+[warning: /etc/my.conf saved as /etc/my.conf.rpmorig]
+)
+AT_CLEANUP
+
+# Install over existing identical config file, no backup needed
+AT_SETUP([install config on existiting identical file])
+AT_KEYWORDS([install])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+cf="${RPMTEST}"/etc/my.conf
+rm -f "${cf}" "${cf}".rpm*
+rm -rf "${TOPDIR}"
+
+runroot rpmbuild --quiet -bb \
+        --define "ver 1.0" \
+	--define "filedata foo" \
+          /data/SPECS/configtest.spec
+
+echo "foo" > "${cf}"
+runroot rpm -U "${TOPDIR}"/RPMS/noarch/configtest-1.0-1.noarch.rpm
+cat "${cf}"
+runroot rpm -e configtest
+test ! -f "${cf}"
+],
+[0],
+[foo
+],
+[])
+AT_CLEANUP
+
+# Erase unmodified config file, no backup here
+AT_SETUP([erase unchanged config])
+AT_KEYWORDS([install])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+cf="${RPMTEST}"/etc/my.conf
+rm -f "${cf}" "${cf}".rpm*
+rm -rf "${TOPDIR}"
+
+runroot rpmbuild --quiet -bb \
+        --define "ver 1.0" \
+	--define "filedata foo" \
+          /data/SPECS/configtest.spec
+
+runroot rpm -U "${TOPDIR}"/RPMS/noarch/configtest-1.0-1.noarch.rpm
+cat "${cf}"
+runroot rpm -e configtest
+test ! -f "${cf}"
+],
+[0],
+[foo
+],
+[])
+AT_CLEANUP
+
+# Erase modified config file
+AT_SETUP([erase changed config])
+AT_KEYWORDS([install])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+cf="${RPMTEST}"/etc/my.conf
+rm -f "${cf}" "${cf}".rpm*
+rm -rf "${TOPDIR}"
+
+runroot rpmbuild --quiet -bb \
+        --define "ver 1.0" \
+	--define "filedata foo" \
+          /data/SPECS/configtest.spec
+
+runroot rpm -U "${TOPDIR}"/RPMS/noarch/configtest-1.0-1.noarch.rpm
+cat "${cf}"
+echo "otherstuff" > "${cf}"
+runroot rpm -e configtest
+cat "${cf}.rpmsave"
+test ! -f "${cf}"
+],
+[0],
+[foo
+otherstuff
+],
+[warning: /etc/my.conf saved as /etc/my.conf.rpmsave]
+)
+AT_CLEANUP
+
+# Erase modified config(noreplace) file
+AT_SETUP([erase changed config(noreplace)])
+AT_KEYWORDS([install])
+AT_CHECK([
+RPMDB_CLEAR
+RPMDB_INIT
+cf="${RPMTEST}"/etc/my.conf
+rm -f "${cf}" "${cf}".rpm*
+rm -rf "${TOPDIR}"
+
+runroot rpmbuild --quiet -bb \
+        --define "ver 1.0" \
+	--define "filedata foo" \
+	--define "noreplace 1" \
+          /data/SPECS/configtest.spec
+
+runroot rpm -U "${TOPDIR}"/RPMS/noarch/configtest-1.0-1.noarch.rpm
+cat "${cf}"
+echo "otherstuff" > "${cf}"
+runroot rpm -e configtest
+cat "${cf}.rpmsave"
+test ! -f "${cf}"
+],
+[0],
+[foo
+otherstuff
+],
+[warning: /etc/my.conf saved as /etc/my.conf.rpmsave]
+)
+AT_CLEANUP
+
 # ------------------------------
 # (Build and) upgrade package with config file, no backup here
 AT_SETUP([upgrade unchanged config])