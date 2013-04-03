40335819cfb2ef9d4380f05c93a12281e26d0b15
Panu Matilainen <pmatilai@redhat.com>
2013-04-03 12:25:37 +0300

    Change syslog plugin to use plugin private data instead of globals
    - Some previously required things probably no longer make no sense
      now, and OTOH this isn't all that important for the logging
      plugin but just to test the stuff with something...

--- plugins/syslog.c
+++ plugins/syslog.c
@@ -3,43 +3,51 @@
 #include <rpm/rpmts.h>
 #include "lib/rpmplugin.h"
 
-static int logging = 0;
-static unsigned int scriptfail = 0;
-static unsigned int pkgfail = 0;
+struct logstat {
+    int logging;
+    unsigned int scriptfail;
+    unsigned int pkgfail;
+};
 
 static rpmRC PLUGINHOOK_INIT_FUNC(rpmPlugin plugin,
 				rpmts ts, const char * name, const char * opts)
 {
     /* XXX make this configurable? */
     const char * log_ident = "[RPM]";
+    struct logstat * state = rcalloc(1, sizeof(*state));
 
+    rpmPluginSetData(plugin, state);
     openlog(log_ident, (LOG_PID), LOG_USER);
     return RPMRC_OK;
 }
 
 static void PLUGINHOOK_CLEANUP_FUNC(rpmPlugin plugin)
 {
+    struct logstat * state = rpmPluginGetData(plugin);
+    free(state);
     closelog();
 }
 
 static rpmRC PLUGINHOOK_TSM_PRE_FUNC(rpmPlugin plugin, rpmts ts)
 {
+    struct logstat * state = rpmPluginGetData(plugin);
+    
     /* Reset counters */
-    scriptfail = 0;
-    pkgfail = 0;
+    state->scriptfail = 0;
+    state->pkgfail = 0;
 
     /* Assume we are logging but... */
-    logging = 1;
+    state->logging = 1;
 
     /* ...don't log test transactions */
     if (rpmtsFlags(ts) & (RPMTRANS_FLAG_TEST|RPMTRANS_FLAG_BUILD_PROBS))
-	logging = 0;
+	state->logging = 0;
 
     /* ...don't log chroot transactions */
     if (!rstreq(rpmtsRootDir(ts), "/"))
-	logging = 0;
+	state->logging = 0;
 
-    if (logging) {
+    if (state->logging) {
 	syslog(LOG_NOTICE, "Transaction ID %x started", rpmtsGetTid(ts));
     }
 
@@ -48,22 +56,26 @@ static rpmRC PLUGINHOOK_TSM_PRE_FUNC(rpmPlugin plugin, rpmts ts)
 
 static rpmRC PLUGINHOOK_TSM_POST_FUNC(rpmPlugin plugin, rpmts ts, int res)
 {
-    if (logging) {
-	if (pkgfail || scriptfail) {
+    struct logstat * state = rpmPluginGetData(plugin);
+
+    if (state->logging) {
+	if (state->pkgfail || state->scriptfail) {
 	    syslog(LOG_WARNING, "%u elements failed, %u scripts failed",
-		   pkgfail, scriptfail);
+		   state->pkgfail, state->scriptfail);
 	}
 	syslog(LOG_NOTICE, "Transaction ID %x finished: %d",
 		rpmtsGetTid(ts), res);
     }
 
-    logging = 0;
+    state->logging = 0;
     return RPMRC_OK;
 }
 
 static rpmRC PLUGINHOOK_PSM_POST_FUNC(rpmPlugin plugin, rpmte te, int res)
 {
-    if (logging) {
+    struct logstat * state = rpmPluginGetData(plugin);
+
+    if (state->logging) {
 	int lvl = LOG_NOTICE;
 	const char *op = (rpmteType(te) == TR_ADDED) ? "install" : "erase";
 	const char *outcome = "success";
@@ -73,7 +85,7 @@ static rpmRC PLUGINHOOK_PSM_POST_FUNC(rpmPlugin plugin, rpmte te, int res)
 	if (res != RPMRC_OK) {
 	    lvl = LOG_WARNING;
 	    outcome = "failure";
-	    pkgfail++;
+	    state->pkgfail++;
 	}
 
 	syslog(lvl, "%s %s: %s", op, pkg, outcome);
@@ -84,9 +96,11 @@ static rpmRC PLUGINHOOK_PSM_POST_FUNC(rpmPlugin plugin, rpmte te, int res)
 static rpmRC PLUGINHOOK_SCRIPTLET_POST_FUNC(rpmPlugin plugin,
 					const char *s_name, int type, int res)
 {
-    if (logging && res) {
+    struct logstat * state = rpmPluginGetData(plugin);
+
+    if (state->logging && res) {
 	syslog(LOG_WARNING, "scriptlet %s failure: %d\n", s_name, res);
-	scriptfail++;
+	state->scriptfail++;
     }
     return RPMRC_OK;
 }
