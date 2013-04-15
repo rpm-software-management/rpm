c6101677a1d6bc6eae84f4729aae5af1fabe6a8b
Panu Matilainen <pmatilai@redhat.com>
2013-04-15 17:42:49 +0300

    Sanitize duplicate dependency checking at build
    - Creating a new rpmds from header each time isNewDep() gets hideously
      expensive when the number of manual dependencies is high.
      Add prco etc rpmds structs to each package and use those to determine
      whether a dependency has already been seen: this way the ds'es remain
      sorted so rpmdsMerge() will do the right thing for regular
      dependencies. Triggers are somewhat different beasts with their
      index tags so at least for now just do a linear search like we
      always did for correctness.
    - Makes worlds of difference for complex specs... the time to parse
      texlive.spec from Fedora goes down from ~27s to ~10s on my laptop
      with this change.
    (cherry picked from commit d642df2a5c5913010dae5b5b6a043359325cf055)

--- build/reqprov.c
+++ build/reqprov.c
@@ -7,37 +7,39 @@
 
 #include <rpm/header.h>
 #include <rpm/rpmstring.h>
+#include <rpm/rpmlog.h>
 #include "build/rpmbuild_internal.h"
 #include "debug.h"
 
-static int isNewDep(Header h, rpmTagVal nametag,
+static int isNewDep(rpmds *dsp, rpmTagVal nametag,
 		  const char *N, const char *EVR, rpmsenseFlags Flags,
-		  rpmTagVal indextag, uint32_t index)
+		  Header h, rpmTagVal indextag, uint32_t index)
 {
     int isnew = 1;
-    struct rpmtd_s idx;
-    rpmds ads = rpmdsNew(h, nametag, 0);
     rpmds bds = rpmdsSingle(nametag, N, EVR, Flags);
 
-    if (indextag) {
+    if (!indextag) {
+	/* With normal deps, we can just merge and see if anything got added */
+	isnew = (rpmdsMerge(dsp, bds) > 0);
+    } else {
+	struct rpmtd_s idx;
+	rpmds ads = *dsp;
 	headerGet(h, indextag, &idx, HEADERGET_MINMEM);
+
+	/* rpmdsFind/Merge() probably isn't realiable with triggers... */
+	rpmdsInit(ads);
+	while (isnew && rpmdsNext(ads) >= 0) {
+	    if (!rstreq(rpmdsN(ads), rpmdsN(bds))) continue;
+	    if (!rstreq(rpmdsEVR(ads), rpmdsEVR(bds))) continue;
+	    if (rpmdsFlags(ads) != rpmdsFlags(bds)) continue;
+	    if (indextag && rpmtdSetIndex(&idx, rpmdsIx(ads)) >= 0 &&
+			    rpmtdGetNumber(&idx) != index) continue;
+	    isnew = 0;
+	}
+	rpmtdFreeData(&idx);
+	rpmdsMerge(dsp, bds);
     }
 
-    /* XXX there's no guarantee the ds is sorted here so rpmdsFind() wont do */
-    rpmdsInit(ads);
-    while (isnew && rpmdsNext(ads) >= 0) {
-	if (!rstreq(rpmdsN(ads), rpmdsN(bds))) continue;
-	if (!rstreq(rpmdsEVR(ads), rpmdsEVR(bds))) continue;
-	if (rpmdsFlags(ads) != rpmdsFlags(bds)) continue;
-	if (indextag && rpmtdSetIndex(&idx, rpmdsIx(ads)) >= 0 &&
-			rpmtdGetNumber(&idx) != index) continue;
-	isnew = 0;
-    }
-    
-    if (indextag) {
-	rpmtdFreeData(&idx);
-    }
-    rpmdsFree(ads);
     rpmdsFree(bds);
     return isnew;
 }
@@ -51,30 +53,36 @@ int addReqProv(Package pkg, rpmTagVal tagN,
     rpmTagVal indextag = 0;
     rpmsenseFlags extra = RPMSENSE_ANY;
     Header h = pkg->header; /* just a shortcut */
+    rpmds *dsp = NULL;
 
     switch (tagN) {
     case RPMTAG_PROVIDENAME:
 	versiontag = RPMTAG_PROVIDEVERSION;
 	flagtag = RPMTAG_PROVIDEFLAGS;
 	extra = Flags & RPMSENSE_FIND_PROVIDES;
+	dsp = &pkg->provides;
 	break;
     case RPMTAG_OBSOLETENAME:
 	versiontag = RPMTAG_OBSOLETEVERSION;
 	flagtag = RPMTAG_OBSOLETEFLAGS;
+	dsp = &pkg->obsoletes;
 	break;
     case RPMTAG_CONFLICTNAME:
 	versiontag = RPMTAG_CONFLICTVERSION;
 	flagtag = RPMTAG_CONFLICTFLAGS;
+	dsp = &pkg->conflicts;
 	break;
     case RPMTAG_ORDERNAME:
 	versiontag = RPMTAG_ORDERVERSION;
 	flagtag = RPMTAG_ORDERFLAGS;
+	dsp = &pkg->order;
 	break;
     case RPMTAG_TRIGGERNAME:
 	versiontag = RPMTAG_TRIGGERVERSION;
 	flagtag = RPMTAG_TRIGGERFLAGS;
 	indextag = RPMTAG_TRIGGERINDEX;
 	extra = Flags & RPMSENSE_TRIGGER;
+	dsp = &pkg->triggers;
 	break;
     case RPMTAG_REQUIRENAME:
     default:
@@ -82,6 +90,7 @@ int addReqProv(Package pkg, rpmTagVal tagN,
 	versiontag = RPMTAG_REQUIREVERSION;
 	flagtag = RPMTAG_REQUIREFLAGS;
 	extra = Flags & _ALL_REQUIRES_MASK;
+	dsp = &pkg->requires;
     }
 
     /* rpmlib() dependency sanity: only requires permitted, ensure sense bit */
@@ -96,7 +105,7 @@ int addReqProv(Package pkg, rpmTagVal tagN,
 	EVR = "";
     
     /* Avoid adding duplicate dependencies. */
-    if (isNewDep(h, tagN, N, EVR, Flags, indextag, index)) {
+    if (isNewDep(dsp, tagN, N, EVR, Flags, h, indextag, index)) {
 	headerPutString(h, tagN, N);
 	headerPutString(h, versiontag, EVR);
 	headerPutUint32(h, flagtag, &Flags, 1);
