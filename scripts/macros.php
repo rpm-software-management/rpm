e9c5f52bdd88ac6b18e8a410e66b6f3df2b27437
Panu Matilainen <pmatilai@redhat.com>
2013-12-16 10:46:09 +0200

    Remove bogus __find_requires|provides macro definitions (RhBug:1043149)
    - The paths defined in macros.perl have never even existed in rpm,
      would've always been suspect and nowadays mostly just plain useless
      as they'd only be used by the deprecated external dependency
      generator.
    
    (cherry picked from commit 8a461bae4fb580a413ccab9a50761e419337969f)

--- scripts/macros.php
+++ scripts/macros.php
@@ -2,8 +2,5 @@
 # To make use of these macros insert the following line into your spec file:
 # %include %{_rpmconfigdir}/macros.php
 
-%define		__find_requires	%{_rpmconfigdir}/find-php-requires
-%define		__find_provides	%{_rpmconfigdir}/find-php-provides
-
 %define		php_pear_dir	%{_datadir}/pear
 
