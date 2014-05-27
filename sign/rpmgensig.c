rpm-software-management/rpm

commit 90833a57c523c1a5a074cae81c0c84fb3726fb6d

Reserve space for gpg signature during building of package.
- During building of a package a dummy tag is added to the signature
header. This tag reserves some space for gpg signatures. So during
signing of the package the gpg signatures can be put in this reserved
space and it is not necessary to rewrite the whole package to make some
space for gpg signatures.

Signed-off-by: Panu Matilainen <pmatilai@redhat.com>

sign/rpmgensig.c
@@ -477,12 +477,15 @@ static int rpmSign(const char *rpm, int deleting, const char *passPhrase)
     rpmRC rc;
     struct rpmtd_s utd;
     off_t headerStart;
+    off_t sigStart;
     struct sigTarget_s sigt1;
     struct sigTarget_s sigt2;
+    unsigned int origSigSize;
+    int insSig = 0;
 
     fprintf(stdout, "%s:\n", rpm);
 
-    if (manageFile(&fd, rpm, O_RDONLY))
+    if (manageFile(&fd, rpm, O_RDWR))
 	goto exit;
 
     if ((rc = rpmLeadRead(fd, &lead, NULL, &msg)) != RPMRC_OK) {
@@ -491,6 +494,7 @@ static int rpmSign(const char *rpm, int deleting, const char *passPhrase)
 	goto exit;
     }
 
+    sigStart = Ftell(fd);
     rc = rpmReadSignature(fd, &sigh, RPMSIGTYPE_HEADERSIG, &msg);
     if (rc != RPMRC_OK) {
 	rpmlog(RPMLOG_ERR, _("%s: rpmReadSignature failed: %s"), rpm,
@@ -530,6 +534,7 @@ static int rpmSign(const char *rpm, int deleting, const char *passPhrase)
 	sigh = headerLink(nh);
 	headerFree(nh);
     }
+    origSigSize = headerSizeof(sigh, HEADER_MAGIC_YES);
 
     if (deleting) {	/* Nuke all the signature tags. */
 	deleteSigs(sigh);
@@ -557,49 +562,88 @@ static int rpmSign(const char *rpm, int deleting, const char *passPhrase)
 	}
     }
 
+    /* Try to make new signature smaller to have size of original signature */
+    rpmtdReset(&utd);
+    if (headerGet(sigh, RPMSIGTAG_RESERVEDSPACE, &utd, HEADERGET_MINMEM)) {
+	int diff;
+	int count;
+	char *reservedSpace = NULL;
+
+	count = utd.count;
+	diff = headerSizeof(sigh, HEADER_MAGIC_YES) - origSigSize;
+
+	if (diff < count) {
+	    reservedSpace = xcalloc(count - diff, sizeof(char));
+	    headerDel(sigh, RPMSIGTAG_RESERVEDSPACE);
+	    rpmtdReset(&utd);
+	    utd.tag = RPMSIGTAG_RESERVEDSPACE;
+	    utd.count = count - diff;
+	    utd.type = RPM_BIN_TYPE;
+	    utd.data = reservedSpace;
+	    headerPut(sigh, &utd, HEADERPUT_DEFAULT);
+	    free(reservedSpace);
+	    insSig = 1;
+	}
+    }
+
     /* Reallocate the signature into one contiguous region. */
     sigh = headerReload(sigh, RPMTAG_HEADERSIGNATURES);
     if (sigh == NULL)	/* XXX can't happen */
 	goto exit;
 
-    rasprintf(&trpm, "%s.XXXXXX", rpm);
-    ofd = rpmMkTemp(trpm);
-    if (ofd == NULL || Ferror(ofd)) {
-	rpmlog(RPMLOG_ERR, _("rpmMkTemp failed\n"));
-	goto exit;
-    }
+    if (insSig) {
+	/* Insert new signature into original rpm */
+	if (Fseek(fd, sigStart, SEEK_SET) < 0) {
+	    rpmlog(RPMLOG_ERR, _("Could not seek in file %s: %s\n"),
+		    rpm, Fstrerror(fd));
+	    goto exit;
+	}
 
-    /* Write the lead/signature of the output rpm */
-    rc = rpmLeadWrite(ofd, lead);
-    if (rc != RPMRC_OK) {
-	rpmlog(RPMLOG_ERR, _("%s: writeLead failed: %s\n"), trpm,
-	    Fstrerror(ofd));
-	goto exit;
-    }
+	if (rpmWriteSignature(fd, sigh)) {
+	    rpmlog(RPMLOG_ERR, _("%s: rpmWriteSignature failed: %s\n"), trpm,
+		Fstrerror(ofd));
+	    goto exit;
+	}
+    } else {
+	/* Replace orignal rpm with new rpm containing new signature */
+	rasprintf(&trpm, "%s.XXXXXX", rpm);
+	ofd = rpmMkTemp(trpm);
+	if (ofd == NULL || Ferror(ofd)) {
+	    rpmlog(RPMLOG_ERR, _("rpmMkTemp failed\n"));
+	    goto exit;
+	}
 
-    if (rpmWriteSignature(ofd, sigh)) {
-	rpmlog(RPMLOG_ERR, _("%s: rpmWriteSignature failed: %s\n"), trpm,
-	    Fstrerror(ofd));
-	goto exit;
-    }
+	/* Write the lead/signature of the output rpm */
+	rc = rpmLeadWrite(ofd, lead);
+	if (rc != RPMRC_OK) {
+	    rpmlog(RPMLOG_ERR, _("%s: writeLead failed: %s\n"), trpm,
+		Fstrerror(ofd));
+	    goto exit;
+	}
 
-    if (Fseek(fd, headerStart, SEEK_SET) < 0) {
-	rpmlog(RPMLOG_ERR, _("Could not seek in file %s: %s\n"),
-		rpm, Fstrerror(fd));
-	goto exit;
-    }
+	if (rpmWriteSignature(ofd, sigh)) {
+	    rpmlog(RPMLOG_ERR, _("%s: rpmWriteSignature failed: %s\n"), trpm,
+		Fstrerror(ofd));
+	    goto exit;
+	}
 
-    /* Append the header and archive from the temp file */
-    if (copyFile(&fd, rpm, &ofd, trpm) == 0) {
-	struct stat st;
+	if (Fseek(fd, headerStart, SEEK_SET) < 0) {
+	    rpmlog(RPMLOG_ERR, _("Could not seek in file %s: %s\n"),
+		    rpm, Fstrerror(fd));
+	    goto exit;
+	}
+	/* Append the header and archive from the temp file */
+	if (copyFile(&fd, rpm, &ofd, trpm) == 0) {
+	    struct stat st;
 
-	/* Move final target into place, restore file permissions. */
-	if (stat(rpm, &st) == 0 && unlink(rpm) == 0 &&
-		    rename(trpm, rpm) == 0 && chmod(rpm, st.st_mode) == 0) {
-	    res = 0;
-	} else {
-	    rpmlog(RPMLOG_ERR, _("replacing %s failed: %s\n"),
-		   rpm, strerror(errno));
+	    /* Move final target into place, restore file permissions. */
+	    if (stat(rpm, &st) == 0 && unlink(rpm) == 0 &&
+			rename(trpm, rpm) == 0 && chmod(rpm, st.st_mode) == 0) {
+		res = 0;
+	    } else {
+		rpmlog(RPMLOG_ERR, _("replacing %s failed: %s\n"),
+		       rpm, strerror(errno));
+	    }
 	}
     }
 

build/pack.c
@@ -288,6 +288,8 @@ static rpmRC generateSignature(char *SHA1, uint8_t *MD5, rpm_loff_t size,
     rpmTagVal payloadtag;
     rpm_tagtype_t typetag;
     rpmRC rc = RPMRC_OK;
+    char *reservedSpace;
+    int spaceSize = 0;
 
     /* Prepare signature */
     sig = rpmNewSignature();
@@ -329,6 +331,18 @@ static rpmRC generateSignature(char *SHA1, uint8_t *MD5, rpm_loff_t size,
     td.type = typetag;
     td.data = &size;
     headerPut(sig, &td, HEADERPUT_DEFAULT);
+
+    spaceSize = rpmExpandNumeric("%{__gpg_reserved_space}");
+    if(spaceSize > 0) {
+	reservedSpace = xcalloc(spaceSize, sizeof(char));
+	rpmtdReset(&td);
+	td.tag = RPMSIGTAG_RESERVEDSPACE;
+	td.count = spaceSize;
+	td.type = RPM_BIN_TYPE;
+	td.data = reservedSpace;
+	headerPut(sig, &td, HEADERPUT_DEFAULT);
+	free(reservedSpace);
+    }
 
     /* Reallocate the signature into one contiguous region. */
     sig = headerReload(sig, RPMTAG_HEADERSIGNATURES);

lib/rpmtag.h
@@ -367,6 +367,7 @@ typedef enum rpmSigTag_e {
     RPMSIGTAG_GPG	= 1005, /*!< internal GnuPG signature. */
     RPMSIGTAG_PGP5	= 1006,	/*!< internal PGP5 signature @deprecated legacy. */
     RPMSIGTAG_PAYLOADSIZE = 1007,/*!< internal uncompressed payload size (32bit) in bytes. */
+    RPMSIGTAG_RESERVEDSPACE = 1008,/*!< internal space reserved for signatures */
     RPMSIGTAG_BADSHA1_1	= RPMTAG_BADSHA1_1,	/*!< internal Broken SHA1, take 1. */
     RPMSIGTAG_BADSHA1_2	= RPMTAG_BADSHA1_2,	/*!< internal Broken SHA1, take 2. */
     RPMSIGTAG_SHA1	= RPMTAG_SHA1HEADER,	/*!< internal sha1 header digest. */

macros.in
@@ -472,6 +472,13 @@ package or when debugging this package.\
 #
 %_fileattrsdir		%{_rpmconfigdir}/fileattrs
 
+# This macro defines how much space (in bytes) in package should be
+# reserved for gpg signatures during building of a package. If this space is
+# big enough for gpg signatures to fit into it then signing of the packages is
+# very quick because it is not necessary to rewrite the whole package to make
+# some space for gpg signatures.
+%__gpg_reserved_space 4096
+
 #==============================================================================
 # ---- Database configuration macros.
 #	Macros used to configure Berkley db parameters.

commit 1a8a5bade3e957349c62ea4e1c0cea92a128f304

rpmSign() always returned -1 if signatures was inserted into original rpm
- This was caused by commit 90833a57c523c1a5a074cae81c0c84fb3726fb6d but
  because of another problem fixed in previous commit it didn't exhibit.

Signed-off-by: Panu Matilainen <pmatilai@redhat.com>

sign/rpmgensig.c
@@ -605,6 +605,7 @@ static int rpmSign(const char *rpm, int deleting, const char *passPhrase)
 		Fstrerror(ofd));
 	    goto exit;
 	}
+	res = 0;
     } else {
 	/* Replace orignal rpm with new rpm containing new signature */
 	rasprintf(&trpm, "%s.XXXXXX", rpm);