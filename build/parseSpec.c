rpm-software-management/rpm

commit a357c99c58a5e1367160dfa692f26d14bd3a3df1

Do not write the dependencies to the header instantly.
Add the dependencies generated from the files to the package and write them to the header at the end.

build/pack.c
@@ -617,6 +617,17 @@ rpmRC packageBinaries(rpmSpec spec, const char *cookie, int cheating)
 	headerPutString(pkg->header, RPMTAG_BUILDHOST, buildHost());
 	headerPutUint32(pkg->header, RPMTAG_BUILDTIME, getBuildTime(), 1);
 
+	rpmdsPutToHeader(pkg->requires, pkg->header);
+	rpmdsPutToHeader(pkg->provides, pkg->header);
+	rpmdsPutToHeader(pkg->recommends, pkg->header);
+	rpmdsPutToHeader(pkg->suggests, pkg->header);
+	rpmdsPutToHeader(pkg->supplements, pkg->header);
+	rpmdsPutToHeader(pkg->enhances, pkg->header);
+	rpmdsPutToHeader(pkg->conflicts, pkg->header);
+	rpmdsPutToHeader(pkg->obsoletes, pkg->header);
+	rpmdsPutToHeader(pkg->triggers, pkg->header);
+	rpmdsPutToHeader(pkg->order, pkg->header);
+
 	if (spec->sourcePkgId != NULL) {
 	    headerPutBin(pkg->header, RPMTAG_SOURCEPKGID, spec->sourcePkgId,16);
 	}
@@ -697,6 +708,17 @@ rpmRC packageSources(rpmSpec spec, char **cookie)
     headerPutString(sourcePkg->header, RPMTAG_RPMVERSION, VERSION);
     headerPutString(sourcePkg->header, RPMTAG_BUILDHOST, buildHost());
     headerPutUint32(sourcePkg->header, RPMTAG_BUILDTIME, getBuildTime(), 1);
+
+    rpmdsPutToHeader(sourcePkg->requires, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->provides, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->recommends, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->suggests, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->supplements, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->enhances, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->conflicts, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->obsoletes, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->triggers, sourcePkg->header);
+    rpmdsPutToHeader(sourcePkg->order, sourcePkg->header);
 
     /* XXX this should be %_srpmdir */
     {	char *fn = rpmGetPath("%{_srcrpmdir}/", spec->sourceRpmName,NULL);

build/reqprov.c
@@ -28,10 +28,7 @@ int addReqProv(Package pkg, rpmTagVal tagN,
     newds = rpmdsSinglePoolTix(pkg->pool, tagN, N, EVR,
 			       rpmSanitizeDSFlags(tagN, Flags), index);
 
-    /* Avoid adding duplicate dependencies. */
-    if (rpmdsMerge(dsp, newds) > 0) {
-	rpmdsPutToHeader(newds, pkg->header);
-    }
+    rpmdsMerge(dsp, newds);
     rpmdsFree(newds);
 
     return 0;

build/rpmfc.c
@@ -36,6 +36,7 @@ typedef struct rpmfcAttr_s {
 /**
  */
 struct rpmfc_s {
+    Package pkg;
     int nfiles;		/*!< no. of files */
     int fknown;		/*!< no. of classified files */
     int fwhite;		/*!< no. of "white" files */
@@ -58,8 +59,6 @@ struct rpmfc_s {
     rpmstrPool ddict;	/*!< file depends dictionary */
 
     rpmstrPool pool;	/*!< general purpose string storage */
-    rpmds provides;	/*!< (no. provides) package provides */
-    rpmds requires;	/*!< (no. requires) package requires */
 };
 
 struct rpmfcTokens_s {
@@ -489,7 +488,7 @@ static const char *parseDep(char **depav, int depac,
  * @return		0 on success
  */
 static int rpmfcHelper(rpmfc fc, const char *nsdep, const char *depname,
-		       rpmds *depsp, rpmsenseFlags dsContext, rpmTagVal tagN)
+		       rpmsenseFlags dsContext, rpmTagVal tagN)
 {
     ARGV_t pav = NULL;
     const char * fn = fc->fn[fc->ix];
@@ -531,7 +530,7 @@ static int rpmfcHelper(rpmfc fc, const char *nsdep, const char *depname,
 
 	    /* Add to package and file dependencies unless filtered */
 	    if (regMatch(exclude, rpmdsDNEVR(ds)+2) == 0) {
-		(void) rpmdsMerge(depsp, ds);
+		(void) rpmdsMerge(packageDependencies(fc->pkg, tagN), ds);
 		rpmfcAddFileDep(fc->ddict, fc->ix, ds,
 				tagN == RPMTAG_PROVIDENAME ? 'P' : 'R');
 	    }
@@ -565,7 +564,7 @@ static int rpmfcHelperProvides(rpmfc fc, const char * nsdep)
     if (fc->skipProv)
 	return 0;
 
-    return rpmfcHelper(fc, nsdep, "provides", &fc->provides,
+    return rpmfcHelper(fc, nsdep, "provides",
 			RPMSENSE_FIND_PROVIDES, RPMTAG_PROVIDENAME);
 }
 
@@ -580,7 +579,7 @@ static int rpmfcHelperRequires(rpmfc fc, const char * nsdep)
     if (fc->skipReq)
 	return 0;
 
-    return rpmfcHelper(fc, nsdep, "requires", &fc->requires,
+    return rpmfcHelper(fc, nsdep, "requires",
 			RPMSENSE_FIND_REQUIRES, RPMTAG_REQUIRENAME);
 }
 
@@ -702,16 +701,10 @@ void rpmfcPrint(const char * msg, rpmfc fc, FILE * fp)
     int dx;
     int fx;
 
-int nprovides;
-int nrequires;
-
     if (fp == NULL) fp = stderr;
 
     if (msg)
 	fprintf(fp, "===================================== %s\n", msg);
-
-nprovides = rpmdsCount(fc->provides);
-nrequires = rpmdsCount(fc->requires);
 
     if (fc)
     for (fx = 0; fx < fc->nfiles; fx++) {
@@ -745,32 +738,16 @@ assert(fx < fc->fddictn->nvals);
 	    const char * depval;
 	    unsigned char deptype;
 	    unsigned ix;
+	    rpmds ds;
 
 	    ix = fc->ddictx->vals[dx++];
 	    deptype = ((ix >> 24) & 0xff);
 	    ix &= 0x00ffffff;
 	    depval = NULL;
-	    switch (deptype) {
-	    default:
-assert(depval != NULL);
-		break;
-	    case 'P':
-		if (nprovides > 0) {
-assert(ix < nprovides);
-		    (void) rpmdsSetIx(fc->provides, ix-1);
-		    if (rpmdsNext(fc->provides) >= 0)
-			depval = rpmdsDNEVR(fc->provides);
-		}
-		break;
-	    case 'R':
-		if (nrequires > 0) {
-assert(ix < nrequires);
-		    (void) rpmdsSetIx(fc->requires, ix-1);
-		    if (rpmdsNext(fc->requires) >= 0)
-			depval = rpmdsDNEVR(fc->requires);
-		}
-		break;
-	    }
+	    ds = rpmfcDependencies(fc, rpmdsDToTagN(deptype));
+	    (void) rpmdsSetIx(ds, ix-1);
+	    if (rpmdsNext(ds) >= 0)
+		depval = rpmdsDNEVR(ds);
 	    if (depval)
 		fprintf(fp, "\t%s\n", depval);
 	}
@@ -799,8 +776,6 @@ rpmfc rpmfcFree(rpmfc fc)
 	rpmstrPoolFree(fc->ddict);
 	rpmstrPoolFree(fc->cdict);
 
-	rpmdsFree(fc->provides);
-	rpmdsFree(fc->requires);
 	rpmstrPoolFree(fc->pool);
 	memset(fc, 0, sizeof(*fc)); /* trash and burn */
 	free(fc);
@@ -824,14 +799,22 @@ rpmfc rpmfcNew(void)
     return rpmfcCreate(NULL, 0);
 }
 
+rpmds rpmfcDependencies(rpmfc fc, rpmTagVal tag)
+{
+    if (fc) {
+	return *packageDependencies(fc->pkg, tag);
+    }
+    return NULL;
+}
+
 rpmds rpmfcProvides(rpmfc fc)
 {
-    return (fc != NULL ? fc->provides : NULL);
+    return rpmfcDependencies(fc, RPMTAG_PROVIDENAME);
 }
 
 rpmds rpmfcRequires(rpmfc fc)
 {
-    return (fc != NULL ? fc->requires : NULL);
+    return rpmfcDependencies(fc, RPMTAG_REQUIRENAME);
 }
 
 rpmRC rpmfcApply(rpmfc fc)
@@ -890,12 +873,12 @@ rpmRC rpmfcApply(rpmfc fc)
 	    break;
 	case 'P':
 	    ds = rpmdsSinglePool(fc->pool, RPMTAG_PROVIDENAME, N, EVR, Flags);
-	    dix = rpmdsFind(fc->provides, ds);
+	    dix = rpmdsFind(rpmfcDependencies(fc, RPMTAG_PROVIDENAME), ds);
 	    rpmdsFree(ds);
 	    break;
 	case 'R':
 	    ds = rpmdsSinglePool(fc->pool, RPMTAG_REQUIRENAME, N, EVR, Flags);
-	    dix = rpmdsFind(fc->requires, ds);
+	    dix = rpmdsFind(rpmfcDependencies(fc, RPMTAG_REQUIRENAME), ds);
 	    rpmdsFree(ds);
 	    break;
 	}
@@ -1281,42 +1264,25 @@ rpmRC rpmfcGenerateDepends(const rpmSpec spec, Package pkg)
     av[ac] = NULL;
 
     fc = rpmfcCreate(spec->buildRoot, 0);
+    fc->pkg = pkg;
     fc->skipProv = !pkg->autoProv;
     fc->skipReq = !pkg->autoReq;
 
-    /* Copy (and delete) manually generated dependencies to dictionary. */
-    if (!fc->skipProv) {
-	rpmdsMerge(&fc->provides, pkg->provides);
-
-	headerDel(pkg->header, RPMTAG_PROVIDENAME);
-	headerDel(pkg->header, RPMTAG_PROVIDEVERSION);
-	headerDel(pkg->header, RPMTAG_PROVIDEFLAGS);
-
+    if (!fc->skipProv && genConfigDeps) {
 	/* Add config dependency, Provides: config(N) = EVR */
-	if (genConfigDeps) {
-	    rpmds ds = rpmdsSingleNS(fc->pool, RPMTAG_PROVIDENAME, "config",
-			       rpmdsN(pkg->ds), rpmdsEVR(pkg->ds),
-			       (RPMSENSE_EQUAL|RPMSENSE_CONFIG));
-	    rpmdsMerge(&fc->provides, ds);
-	    rpmdsFree(ds);
-	}
+	rpmds ds = rpmdsSingleNS(fc->pool, RPMTAG_PROVIDENAME, "config",
+				 rpmdsN(pkg->ds), rpmdsEVR(pkg->ds),
+				 (RPMSENSE_EQUAL|RPMSENSE_CONFIG));
+	//printf("%s\n", rpmdsDNEVR(ds));
+	rpmdsMerge(packageDependencies(pkg, RPMTAG_PROVIDENAME), ds);
+	rpmdsFree(ds);
     }
-
-    if (!fc->skipReq) {
-	rpmdsMerge(&fc->requires, pkg->requires);
-
-	headerDel(pkg->header, RPMTAG_REQUIRENAME);
-	headerDel(pkg->header, RPMTAG_REQUIREVERSION);
-	headerDel(pkg->header, RPMTAG_REQUIREFLAGS);
-
-	/* Add config dependency,  Requires: config(N) = EVR */
-	if (genConfigDeps) {
-	    rpmds ds = rpmdsSingleNS(fc->pool, RPMTAG_REQUIRENAME, "config",
-			       rpmdsN(pkg->ds), rpmdsEVR(pkg->ds),
-			       (RPMSENSE_EQUAL|RPMSENSE_CONFIG));
-	    rpmdsMerge(&fc->requires, ds);
-	    rpmdsFree(ds);
-	}
+    if (!fc->skipReq && genConfigDeps) {
+	rpmds ds = rpmdsSingleNS(fc->pool, RPMTAG_REQUIRENAME, "config",
+				 rpmdsN(pkg->ds), rpmdsEVR(pkg->ds),
+				 (RPMSENSE_EQUAL|RPMSENSE_CONFIG));
+	rpmdsMerge(packageDependencies(pkg, RPMTAG_REQUIRENAME), ds);
+	rpmdsFree(ds);
     }
 
     /* Build file class dictionary. */
@@ -1343,30 +1309,6 @@ rpmRC rpmfcGenerateDepends(const rpmSpec spec, Package pkg)
 
     /* Add per-file classes(#files) */
     headerPutUint32(pkg->header, RPMTAG_FILECLASS, fc->fcdictx, fc->nfiles);
-
-    /* Add Provides: */
-    if (!fc->skipProv) {
-	rpmds pi = rpmdsInit(fc->provides);
-	while (rpmdsNext(pi) >= 0) {
-	    rpmsenseFlags flags = rpmdsFlags(pi);
-	
-	    headerPutString(pkg->header, RPMTAG_PROVIDENAME, rpmdsN(pi));
-	    headerPutString(pkg->header, RPMTAG_PROVIDEVERSION, rpmdsEVR(pi));
-	    headerPutUint32(pkg->header, RPMTAG_PROVIDEFLAGS, &flags, 1);
-	}
-    }
-
-    /* Add Requires: */
-    if (!fc->skipReq) {
-	rpmds pi = rpmdsInit(fc->requires);
-	while (rpmdsNext(pi) >= 0) {
-	    rpmsenseFlags flags = rpmdsFlags(pi);
-	
-	    headerPutString(pkg->header, RPMTAG_REQUIRENAME, rpmdsN(pi));
-	    headerPutString(pkg->header, RPMTAG_REQUIREVERSION, rpmdsEVR(pi));
-	    headerPutUint32(pkg->header, RPMTAG_REQUIREFLAGS, &flags, 1);
-	}
-    }
 
     /* Add dependency dictionary(#dependencies) */
     if (rpmtdFromArgi(&td, RPMTAG_DEPENDSDICT, fc->ddictx)) {

build/rpmfc.h
@@ -106,6 +106,14 @@ rpmds rpmfcProvides(rpmfc fc);
  */
 rpmds rpmfcRequires(rpmfc fc);
 
+/** \ingroup rpmfc
+ * Retrieve file classification dependencies
+ * @param fc		file classifier
+ * @param tagN		name tag of the wanted dependency
+ * @return		rpmds dependency set of fc requires
+ */
+rpmds rpmfcDependencies(rpmfc fc, rpmTagVal tagN);
+
 #ifdef __cplusplus
 }
 #endif

commit 0bda2faa4de368a87f85084856a5fed701774acb

Populate manual dependencies into spec pkg headers early too
- Similar to commit 1b41c91431d37295701281ff208f99a51f660c89, rpmspec
  and other tools expect to find manually specified dependencies
  from the headers of a freshly parsed spec. This means we need to
  add this cruft two times: once for the manual dependencies and then
  scratch all that and redo from start after automatic dependencies
  have been discovered at the end of package build.
- Fixes another regression (rpmspec dependency queries went dead)
  introduced in commit a357c99c58a5e1367160dfa692f26d14bd3a3df1

build/pack.c
@@ -618,6 +618,11 @@ rpmRC packageBinaries(rpmSpec spec, const char *cookie, int cheating)
 	headerPutUint32(pkg->header, RPMTAG_BUILDTIME, getBuildTime(), 1);
 
 	for (int i=0; i<PACKAGE_NUM_DEPS; i++) {
+	    /* Nuke any previously added dependencies from the header */
+	    headerDel(pkg->header, rpmdsTagN(pkg->dependencies[i]));
+	    headerDel(pkg->header, rpmdsTagEVR(pkg->dependencies[i]));
+	    headerDel(pkg->header, rpmdsTagF(pkg->dependencies[i]));
+	    /* ...and add again, now with automatic dependencies included */
 	    rpmdsPutToHeader(pkg->dependencies[i], pkg->header);
 	}
 

build/parseSpec.c
@@ -548,6 +548,11 @@ static void addTargets(Package Pkgs)
 	headerPutString(pkg->header, RPMTAG_PLATFORM, platform);
 	headerPutString(pkg->header, RPMTAG_OPTFLAGS, optflags);
 
+	/* Add manual dependencies early for rpmspec etc to look at */
+	for (int i=0; i<PACKAGE_NUM_DEPS; i++) {
+	    rpmdsPutToHeader(pkg->dependencies[i], pkg->header);
+	}
+
 	pkg->ds = rpmdsThis(pkg->header, RPMTAG_REQUIRENAME, RPMSENSE_EQUAL);
 	addPackageProvides(pkg);
     }