rpm-software-management/rpm

commit eb7b41a68faf0ffea61ea688e3da17ddb8b56599

Properly import all public key certificates into rpmdb.
When somebody tried to import public key certificates from one file
that contained several certificates then only one package gpg-pubkey-*
was created in rpmdb. And it contained all certificates but it behaves
as if it had contained just one certificate. The fix properly splits
certificates into several gpg-pubkey-* packages (rhbz:#1238717).

lib/rpmchecksig.c
@@ -35,14 +35,32 @@ static int doImport(rpmts ts, const char *fn, char *buf, ssize_t blen)
 
     do {
 	uint8_t *pkt = NULL;
+	uint8_t *pkti = NULL;
 	size_t pktlen = 0;
+	size_t certlen;
 	
 	/* Read pgp packet. */
 	if (pgpParsePkts(start, &pkt, &pktlen) == PGPARMOR_PUBKEY) {
-	    /* Import pubkey packet(s). */
-	    if (rpmtsImportPubkey(ts, pkt, pktlen) != RPMRC_OK) {
-		rpmlog(RPMLOG_ERR, _("%s: key %d import failed.\n"), fn, keyno);
-		res++;
+	    pkti = pkt;
+
+	    /* Iterate over certificates in pkt */
+	    while (pktlen > 0) {
+		if(pgpPubKeyCertLen(pkti, pktlen, &certlen)) {
+		    rpmlog(RPMLOG_ERR, _("%s: key %d import failed.\n"), fn,
+			    keyno);
+		    res++;
+		    continue;
+		}
+
+		/* Import pubkey certificate. */
+		if (rpmtsImportPubkey(ts, pkti, certlen) != RPMRC_OK) {
+		    rpmlog(RPMLOG_ERR, _("%s: key %d import failed.\n"), fn,
+			    keyno);
+		    res++;
+		    continue;
+		}
+		pkti += certlen;
+		pktlen -= certlen;
 	    }
 	} else {
 	    rpmlog(RPMLOG_ERR, _("%s: key %d not an armored public key.\n"),

commit 33a8b61a01007c634aadec766e1aebe91aa82298

Fix infinite loop in case of importing invalid certificate.
Caused by commit eb7b41a68faf0ffea61ea688e3da17ddb8b56599

lib/rpmchecksig.c
@@ -49,7 +49,7 @@ static int doImport(rpmts ts, const char *fn, char *buf, ssize_t blen)
 		    rpmlog(RPMLOG_ERR, _("%s: key %d import failed.\n"), fn,
 			    keyno);
 		    res++;
-		    continue;
+		    break;
 		}
 
 		/* Import pubkey certificate. */
@@ -57,7 +57,6 @@ static int doImport(rpmts ts, const char *fn, char *buf, ssize_t blen)
 		    rpmlog(RPMLOG_ERR, _("%s: key %d import failed.\n"), fn,
 			    keyno);
 		    res++;
-		    continue;
 		}
 		pkti += certlen;
 		pktlen -= certlen;