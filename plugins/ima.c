8c70222a1fde1268f4c88fc1a54fad51b4658f16
fin@linux.vnet.ibm.com <fin@linux.vnet.ibm.com>
2015-07-21 12:00:44 -0500

    IMA plugin labels ima xattr with file signatures
    This plugin extracts file signatures from rpmfiles and writes them to
    security.ima xattr. Only non-config file signatures are installed.
    
    Changelog:
    - use rpmfi instead of rpmfiles
    - use rpmfiFN instead of fsmFsPath
    
    Signed-off-by: Lubos Kardos <lkardos@redhat.com>

---
+++ plugins/ima.c
@@ -0,0 +1,51 @@
+#include <sys/xattr.h>
+
+#include <rpm/rpmfi.h>
+#include <rpm/rpmte.h>
+#include <rpm/rpmfiles.h>
+#include <rpm/rpmtypes.h>
+#include <rpmio/rpmstring.h>
+
+#include "lib/rpmfs.h"
+#include "lib/rpmplugin.h"
+#include "lib/rpmte_internal.h"
+
+#define XATTR_NAME_IMA "security.ima"
+
+static rpmRC ima_psm_post(rpmPlugin plugin, rpmte te, int res)
+{
+	rpmfi fi = rpmteFI(te);
+	const char *fpath;
+	const unsigned char * fsig = NULL;
+	size_t len;
+	int rc = 0;
+
+	if (fi == NULL) {
+	    rc = RPMERR_BAD_MAGIC;
+	    goto exit;
+	}
+
+	while (!rc) {
+	    rc = rpmfiNext(fi);
+	    if (rc < 0) {
+		if (rc == RPMERR_ITER_END)
+		    rc = 0;
+		break;
+	    }
+
+	    /* Don't install signatures for (mutable) config files */
+	    if (!(rpmfiFFlags(fi) & RPMFILE_CONFIG)) {
+		fpath = rpmfiFN(fi);
+		fsig = rpmfiFSignature(fi, &len);
+		if (fsig) {
+		    lsetxattr(fpath, XATTR_NAME_IMA, fsig, len, 0);
+		}
+	    }
+	}
+exit:
+	return rc;
+}
+
+struct rpmPluginHooks_s ima_hooks = {
+	.psm_post = ima_psm_post,
+};
