rpm-software-management/rpm

commit d9f673bf2841bb8c854e6676871078ab563a51c9

Diagnose corrupt/empty compressed patches
Currently the failure status from the decompression processes
is ignored, resulting in silent failures to apply corrupt
compressed patches, which is easily missed.

Adjust so that patch(1) (and thus the full command line) will fail.

build/parsePrep.c
@@ -105,7 +105,8 @@ static char *doPatch(rpmSpec spec, uint32_t c, int strip, const char *db,
 
     /* Avoid the extra cost of fork and pipe for uncompressed patches */
     if (compressed != COMPRESSED_NOT) {
-	patchcmd = rpmExpand("%{uncompress: ", fn, "} | %{__patch} ", args, NULL);
+	patchcmd = rpmExpand("%{uncompress: ", fn, "} || echo patch_fail | "
+                             "%{__patch} ", args, NULL);
     } else {
 	patchcmd = rpmExpand("%{__patch} ", args, " < ", fn, NULL);
     }

commit 46ba709a3b2b7651ccbb9425e73771e1f5be5db8

Fix compressed patches
Regression caused by d9f673bf2841bb8c854e6676871078ab563a51c9 caused uncompressed patch content to be printed out to std instead of being piped into patch command.

build/parsePrep.c
@@ -101,7 +101,7 @@ static char *doPatch(rpmSpec spec, uint32_t c, int strip, const char *db,
 
     /* Avoid the extra cost of fork and pipe for uncompressed patches */
     if (compressed != COMPRESSED_NOT) {
-	patchcmd = rpmExpand("%{uncompress: ", fn, "} || echo patch_fail | "
+	patchcmd = rpmExpand("{ %{uncompress: ", fn, "} || echo patch_fail ; } | "
                              "%{__patch} ", args, NULL);
     } else {
 	patchcmd = rpmExpand("%{__patch} ", args, " < ", fn, NULL);