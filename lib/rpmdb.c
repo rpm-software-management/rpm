e297b51750598d926d053c1d1645aab78a54c46a
Lubos Kardos <lkardos@redhat.com>
2016-04-14 16:23:35 +0200

    Add enahance dependency forgotten in the recent commit [1]
    [1] 9c1e995043a999dcac05a74aa8bcf934122d40ba

--- lib/rpmdb.c
+++ lib/rpmdb.c
@@ -2349,6 +2349,7 @@ static rpmRC tag2index(dbiIndex dbi, rpmTagVal rpmtag,
 	    case RPMTAG_SUGGESTNAME:
 	    case RPMTAG_SUPPLEMENTNAME:
 	    case RPMTAG_RECOMMENDNAME:
+	    case RPMTAG_ENHANCENAME:
 		if (rpmtdType(&tagdata) == RPM_STRING_ARRAY_TYPE) {
 		    rc += updateRichDep(dbi, dbc, rpmtdGetString(&tagdata),
 			&rec, idxupdate);
