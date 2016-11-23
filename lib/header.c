5a50a67e2e2cc6eba94bbf6df7034b02cb9efc4e
Panu Matilainen <pmatilai@redhat.com>
2016-11-23 10:47:36 +0200

    Clean up headerWrite() a bit
    A switch-case for a boolean flag whether to do something or not is
    rather hysterical. A good old "if" is shorter and actually much
    easier to read. When writing the magic, we know we're writing exactly
    one of them, shuffle the arguments to better match Fwrite() interface.
    Initialize uh on declaration line to save an extra line. No actual
    functional changes.

--- lib/header.c
+++ lib/header.c
@@ -1042,19 +1042,15 @@ int headerWrite(FD_t fd, Header h, int magicp)
 {
     ssize_t nb;
     unsigned int length;
-    void * uh;
+    void * uh = headerExport(h, &length);
 
-    uh = headerExport(h, &length);
     if (uh == NULL)
 	return 1;
-    switch (magicp) {
-    case HEADER_MAGIC_YES:
-	nb = Fwrite(rpm_header_magic, sizeof(uint8_t), sizeof(rpm_header_magic), fd);
+
+    if (magicp == HEADER_MAGIC_YES) {
+	nb = Fwrite(rpm_header_magic, sizeof(rpm_header_magic), 1, fd);
 	if (nb != sizeof(rpm_header_magic))
 	    goto exit;
-	break;
-    case HEADER_MAGIC_NO:
-	break;
     }
 
     nb = Fwrite(uh, sizeof(char), length, fd);
