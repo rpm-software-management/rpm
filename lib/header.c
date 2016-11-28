rpm-software-management/rpm

commit 1c25d27895338913d84827d0d670b00887dd1d56

Enable data overlap check on 1st tag of v3 packages
The region tag data (aka region trailer) violates the regular ordering
of tag data and can't be checked with headerVerifyInfo() in one go,
but for v3 packages without the region tag start again from tag 0
to get data overlap check on the whole thing, for both headers.
As a side bonus makes the signature code again that little bit
closer to the other header...

lib/package.c
@@ -370,7 +370,8 @@ static rpmRC headerVerify(rpmKeyring keyring, rpmVSFlags vsflags,
 
     /* Sanity check the rest of the header structure. */
     if (rc != RPMRC_FAIL) {
-	if (headerVerifyInfo(il-1, dl, pe+1, dataStart, NULL, &buf))
+	int region = (rc == RPMRC_OK) ? 1 : 0;
+	if (headerVerifyInfo(il-region, dl, pe+region, dataStart, NULL, &buf))
 	    rc = RPMRC_FAIL;
     }
 

lib/signature.c
@@ -189,12 +189,15 @@ rpmRC rpmReadSignature(FD_t fd, Header * sighp, char ** msg)
     /* Verify header immutable region if there is one */
     xx = headerVerifyRegion(RPMTAG_HEADERSIGNATURES, il, dl, pe, dataStart,
 			    1, NULL, NULL, &buf);
+    /* Sanity check signature tags */
+    if (xx != RPMRC_FAIL) {
+	int region = (xx == RPMRC_OK) ? 1 : 0;
+	if (headerVerifyInfo(il-region, dl, pe+region, dataStart, NULL, &buf))
+	    xx = RPMRC_FAIL;
+    }
+
     /* Not found means a legacy V3 package with no immutable region */
     if (xx != RPMRC_OK && xx != RPMRC_NOTFOUND)
-	goto exit;
-
-    /* Sanity check signature tags */
-    if (headerVerifyInfo(il-1, dl, pe+1, dataStart, NULL, &buf))
 	goto exit;
 
     /* OK, blob looks sane, load the header. */

commit 9d8e06a74154b2283c5374fad25ae41aa7879cb0

Permit headers with legacy region in headerCheck() and friends
V3 packages don't have any regions straight off the wraps, but when
exporting those headers (such as into rpmdb on installation), we create
a "legacy region" which looks a whole lot like an immutable region
of V4 packages. This causes some twists into things that we haven't
been taking into account in recent refactoring...

headerCheck() is supposed to permit all types of headers, don't
require a specific region tag. In addition, permit legacy image
region in hdrblobVerifyRegion().

Fixes a regression from commit 1c25d27895338913d84827d0d670b00887dd1d56
(and since then transmogrified through several phases) causing v3
packages to fail verification coming off rpmdb.

lib/header.c
@@ -1772,7 +1772,8 @@ static rpmRC hdrblobVerifyRegion(rpmTagVal regionTag, int exact_size,
     ei2h(blob->pe, &einfo);
 
     if (!regionTag && (einfo.tag == RPMTAG_HEADERSIGNATURES ||
-		       einfo.tag ==RPMTAG_HEADERIMMUTABLE)) {
+		       einfo.tag == RPMTAG_HEADERIMMUTABLE ||
+		       einfo.tag == RPMTAG_HEADERIMAGE)) {
 	regionTag = einfo.tag;
     }
 

lib/package.c
@@ -233,7 +233,7 @@ rpmRC headerCheck(rpmts ts, const void * uh, size_t uc, char ** msg)
     rpmKeyring keyring = rpmtsGetKeyring(ts, 1);
     struct hdrblob_s blob;
 
-    if (hdrblobInit(uh, uc, RPMTAG_HEADERIMMUTABLE, 0, &blob, msg) == RPMRC_OK) {
+    if (hdrblobInit(uh, uc, 0, 0, &blob, msg) == RPMRC_OK) {
 	rpmswEnter(rpmtsOp(ts, RPMTS_OP_DIGEST), 0);
 	rc = headerSigVerify(keyring, vsflags, &blob, msg);
 	rpmswExit(rpmtsOp(ts, RPMTS_OP_DIGEST), uc);