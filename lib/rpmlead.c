rpm-software-management/rpm

commit 83a8640b47177d665b2aef9afc7ed8bde36f5103

...and finally bury the rpmlead datatype entirely inside rpmlead.c
Since rpmLeadWrite() takes a header there's no more need for
rpmLeadFromHeader() to be "public" and since nothing returns
leads there's no need to free them either, make static + adjust
to eliminate the need for ever allocating a lead so the free func
can go too. Whee. the remaining rpmlead.h is something we could
actually consider making public.

lib/rpmlead.c
@@ -20,8 +20,6 @@
 /* A long time ago in a galaxy far far away, signatures were not in a header */
 #define RPMSIGTYPE_HEADERSIG 5
 
-typedef struct rpmlead_s * rpmlead;
-
 static unsigned char const lead_magic[] = {
     RPMLEAD_MAGIC0, RPMLEAD_MAGIC1, RPMLEAD_MAGIC2, RPMLEAD_MAGIC3
 };
@@ -44,10 +42,8 @@ struct rpmlead_s {
     char reserved[16];      /*!< Pad to 96 bytes -- 8 byte aligned! */
 };
 
-rpmlead rpmLeadFromHeader(Header h)
+static int rpmLeadFromHeader(Header h, struct rpmlead_s *l)
 {
-    rpmlead l = NULL;
-
     if (h != NULL) {
 	int archnum, osnum;
 	char * nevr = headerGetAsString(h, RPMTAG_NEVR);
@@ -56,7 +52,6 @@ rpmlead rpmLeadFromHeader(Header h)
 	rpmGetArchInfo(NULL, &archnum);
 	rpmGetOsInfo(NULL, &osnum);
 
-	l = xcalloc(1, sizeof(*l));
 	l->major = 3;
 	l->minor = 0;
 	l->archnum = archnum;
@@ -70,41 +65,30 @@ rpmlead rpmLeadFromHeader(Header h)
 	free(nevr);
     }
 
-    return l;
-}
-
-rpmlead rpmLeadFree(rpmlead lead)
-{
-    free(lead);
-    return NULL;
+    return (h != NULL);
 }
 
 /* The lead needs to be 8 byte aligned */
 rpmRC rpmLeadWrite(FD_t fd, Header h)
 {
     rpmRC rc = RPMRC_FAIL;
+    struct rpmlead_s l;
 
-    rpmlead lead = rpmLeadFromHeader(h);
-
-    if (lead != NULL) {
-	struct rpmlead_s l;
-	memcpy(&l, lead, sizeof(l));
+    if (rpmLeadFromHeader(h, &l)) {
 	
-	l.type = htons(lead->type);
-	l.archnum = htons(lead->archnum);
-	l.osnum = htons(lead->osnum);
-	l.signature_type = htons(lead->signature_type);
+	l.type = htons(l.type);
+	l.archnum = htons(l.archnum);
+	l.osnum = htons(l.osnum);
+	l.signature_type = htons(l.signature_type);
 	    
 	if (Fwrite(&l, 1, sizeof(l), fd) == sizeof(l))
 	    rc = RPMRC_OK;
-
-	rpmLeadFree(lead);
     }
 
     return rc;
 }
 
-static rpmRC rpmLeadCheck(rpmlead lead, char **msg)
+static rpmRC rpmLeadCheck(struct rpmlead_s *lead, char **msg)
 {
     if (memcmp(lead->magic, lead_magic, sizeof(lead_magic))) {
 	*msg = xstrdup(_("not an rpm package"));

lib/rpmlead.h
@@ -19,22 +19,6 @@ extern "C" {
 
 #define RPMLEAD_SIZE 96         /*!< Don't rely on sizeof(struct) */
 
-typedef struct rpmlead_s * rpmlead;
-
-/** \ingroup lead
- * Initialize a lead structure from header
- * param h		Header
- * @return		Pointer to populated lead structure (malloced)
- */
-rpmlead rpmLeadFromHeader(Header h);
-
-/** \ingroup lead
- * Free a lead structure
- * @param lead		Pointer to lead structure
- * @return		NULL always
- */
-rpmlead rpmLeadFree(rpmlead lead);
-
 /** \ingroup lead
  * Write lead to file handle.
  * @param fd		file handle

commit 4c53e5755e3501df44029390f0b62d2a5a6a6cf9

Ensure rpmlead is fully initialized
Fix sneaky regression from commit 83a8640b47177d665b2aef9afc7ed8bde36f5103,
the lead used to be calloc()'ed so it was guaranteed all zeros, now its not.
Bulldoze to zero with memset() instead.

lib/rpmlead.c
@@ -52,6 +52,7 @@ static int rpmLeadFromHeader(Header h, struct rpmlead_s *l)
 	rpmGetArchInfo(NULL, &archnum);
 	rpmGetOsInfo(NULL, &osnum);
 
+	memset(l, 0, sizeof(*l));
 	l->major = 3;
 	l->minor = 0;
 	l->archnum = archnum;