rpm-software-management/rpm

commit 6d2124ae90187878f4f8eb081db22c8c1b34d817

Handle HEADERIMPORT_COPY in headerImport()
The whole flag is a bit unfortunate and annoying in various ways
but perhaps it's slightly less annoying here. There's also a subtle
behavior change here: we now only honor HEADERIMPORT_COPY if bsize
is also specified. This is sort of required for the next steps.

lib/header.c
@@ -212,11 +212,11 @@ Header headerFree(Header h)
     return NULL;
 }
 
-static Header headerCreate(void *blob, unsigned int pvlen, int32_t indexLen)
+static Header headerCreate(void *blob, int32_t indexLen)
 {
     Header h = xcalloc(1, sizeof(*h));
     if (blob) {
-	h->blob = (pvlen > 0) ? memcpy(xmalloc(pvlen), blob, pvlen) : blob;
+	h->blob = blob;
 	h->indexAlloced = indexLen + 1;
 	h->indexUsed = indexLen;
     } else {
@@ -236,7 +236,7 @@ static Header headerCreate(void *blob, unsigned int pvlen, int32_t indexLen)
 
 Header headerNew(void)
 {
-    return headerCreate(NULL, 0, 0);
+    return headerCreate(NULL, 0);
 }
 
 static rpmRC hdrblobVerifyInfo(hdrblob blob, char **emsg)
@@ -869,9 +869,10 @@ Header headerImport(void * blob, unsigned int bsize, headerImportFlags flags)
     if (hdrchkTags(il) || hdrchkData(dl) || pvlen >= headerMaxbytes)
 	goto errxit;
 
-    h = headerCreate(blob, (flags & HEADERIMPORT_COPY) ? pvlen : 0, il);
+    if (flags & HEADERIMPORT_COPY && bsize)
+	ei = blob = memcpy(xmalloc(bsize), ei, bsize);
+    h = headerCreate(blob, il);
 
-    ei = h->blob; /* In case we had to copy */
     pe = (entryInfo) &ei[2];
     dataStart = (unsigned char *) (pe + il);
     dataEnd = dataStart + dl;

commit d1c988b2a85e9d5f71e58ffabc45a4090abd666b

Unbreak headerCopyLoad() (manifesting as segfault on signing)
Commit 6d2124ae90187878f4f8eb081db22c8c1b34d817 broke headerCopyLoad()
which doesn't supply the size ... because it doesn't know it. Handle
zero bsize in headerImport() by parsing the supplied blob early and then
do it again on the copied data. This also depends on commit
7c6b9d057c291d051deff7ddc4865ca37cdc6d09.

lib/header.c
@@ -1949,13 +1949,19 @@ Header headerImport(void * blob, unsigned int bsize, headerImportFlags flags)
     char *buf = NULL;
     void * b = blob;
 
-    if (flags & HEADERIMPORT_COPY && bsize)
+    if (flags & HEADERIMPORT_COPY) {
+	if (bsize == 0 && hdrblobInit(b, 0, 0, 0, &hblob, &buf) == RPMRC_OK)
+	    bsize = hblob.pvlen;
+	if (bsize == 0)
+	    goto exit;
 	b = memcpy(xmalloc(bsize), b, bsize);
+    }
 
     /* Sanity checks on header intro. */
     if (hdrblobInit(b, bsize, 0, 0, &hblob, &buf) == RPMRC_OK)
 	hdrblobImport(&hblob, (flags & HEADERIMPORT_FAST), &h, &buf);
 
+exit:
     if (h == NULL && b != blob)
 	free(b);
     free(buf);
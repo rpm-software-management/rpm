d1c988b2a85e9d5f71e58ffabc45a4090abd666b
Panu Matilainen <pmatilai@redhat.com>
2017-01-11 11:34:13 +0200

    Unbreak headerCopyLoad() (manifesting as segfault on signing)
    Commit 6d2124ae90187878f4f8eb081db22c8c1b34d817 broke headerCopyLoad()
    which doesn't supply the size ... because it doesn't know it. Handle
    zero bsize in headerImport() by parsing the supplied blob early and then
    do it again on the copied data. This also depends on commit
    7c6b9d057c291d051deff7ddc4865ca37cdc6d09.

--- lib/header.c
+++ lib/header.c
@@ -1949,13 +1949,19 @@ Header headerImport(void * blob, unsigned int bsize, headerImportFlags flags)
     char *buf = NULL;
     void * b = blob;
 
-    if (flags & HEADERIMPORT_COPY && bsize)
+    if (flags & HEADERIMPORT_COPY) {
+	if (bsize == 0 && hdrblobInit(b, 0, 0, 0, &hblob, &buf) == RPMRC_OK)
+	    bsize = hblob.pvlen;
+	if (bsize == 0)
+	    goto exit;
 	b = memcpy(xmalloc(bsize), b, bsize);
+    }
 
     /* Sanity checks on header intro. */
     if (hdrblobInit(b, bsize, 0, 0, &hblob, &buf) == RPMRC_OK)
 	hdrblobImport(&hblob, (flags & HEADERIMPORT_FAST), &h, &buf);
 
+exit:
     if (h == NULL && b != blob)
 	free(b);
     free(buf);
