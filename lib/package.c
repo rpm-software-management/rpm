46ed65aca66a04332b45f1c87dfc896bacaab108
Panu Matilainen <pmatilai@redhat.com>
2017-02-21 09:22:27 +0200

    Signature check before header import on package reads
    Until now we have been reading and importing the headers, and then
    pull out the original immutable image from the header in order to
    do signature checking on unknown packages. Those header manipulations
    are relatively complex and thus vulnerable operatations that are,
    to add to the insult, totally unnecessary as the header on disk is
    just the way we want it for signature checking as it is.
    
    This changes the package reading process rather fundamentally: we first
    read (and sanity-check) the header blobs, then do signature checking on
    the still un-imported blob, and only then try to import the headers
    (which could still fail, at least in theory). This reduces the amount of
    code exposed to unverified headers considerably: the deep header internals
    are avoided entirely, and there's one less signature checking code-path
    to worry about as it's now unified with the one used by headerCheck().
    
    There's a subtle change in the "preferred" signature type: rpmpkgRead()
    used to prefer DSA over RSA, headerSigVerify() prefers RSA. However
    that's rather academic as both signature types never co-exist in
    the same package.
    
    For normal use, all this shouldn't change anything at all.

--- lib/package.c
+++ lib/package.c
@@ -320,102 +320,63 @@ static rpmRC rpmpkgRead(rpmKeyring keyring, rpmVSFlags vsflags,
 			FD_t fd,
 			Header * hdrp, unsigned int *keyidp, char **msg)
 {
-    pgpDigParams sig = NULL;
     Header sigh = NULL;
-    rpmTagVal sigtag;
-    struct rpmtd_s sigtd;
-    struct sigtInfo_s sinfo;
     Header h = NULL;
-    rpmRC xx = 0;
-    rpmRC rc = RPMRC_FAIL;	/* assume failure */
+    struct hdrblob_s blob, sigblob;
+    rpmRC xx, rc = RPMRC_FAIL;	/* assume failure */
     int leadtype = -1;
-    headerGetFlags hgeflags = HEADERGET_DEFAULT;
 
     if (hdrp) *hdrp = NULL;
 
-    rpmtdReset(&sigtd);
+    memset(&blob, 0, sizeof(blob));
+    memset(&sigblob, 0, sizeof(sigblob));
 
     if ((xx = rpmLeadRead(fd, &leadtype, msg)) != RPMRC_OK) {
 	/* Avoid message spew on manifests */
 	if (xx == RPMRC_NOTFOUND) {
 	    *msg = _free(*msg);
 	}
-	goto exit;
-    }
-
-    /* Read the signature header. */
-    if (rpmReadSignature(fd, &sigh, msg) != RPMRC_OK)
-	goto exit;
-
-#define	_chk(_mask, _tag) \
-	(sigtag == 0 && !(vsflags & (_mask)) && headerIsEntry(sigh, (_tag)))
-
-    /*
-     * Figger the most effective means of verification available, prefer
-     * signatures over digests. Legacy header+payload entries are not used.
-     * DSA will be preferred over RSA if both exist because tested first.
-     */
-    sigtag = 0;
-    if (_chk(RPMVSF_NODSAHEADER, RPMSIGTAG_DSA)) {
-	sigtag = RPMSIGTAG_DSA;
-    } else if (_chk(RPMVSF_NORSAHEADER, RPMSIGTAG_RSA)) {
-	sigtag = RPMSIGTAG_RSA;
-    } else if (_chk(RPMVSF_NOSHA1HEADER, RPMSIGTAG_SHA1)) {
-	sigtag = RPMSIGTAG_SHA1;
-    }
-
-    /* Read the metadata, computing digest(s) on the fly. */
-    if (rpmpkgReadHeader(fd, &h, msg) != RPMRC_OK)
-	goto exit;
-
-    /* Any digests or signatures to check? */
-    if (sigtag == 0) {
-	rc = RPMRC_OK;
-	goto exit;
-    }
-
-    /* Free up any previous "ok" message before signature/digest check */
-    *msg = _free(*msg);
-
-    /* Retrieve the tag parameters from the signature header. */
-    if (!headerGet(sigh, sigtag, &sigtd, hgeflags))
-	goto exit;
-
-    if (rpmSigInfoParse(&sigtd, "package", &sinfo, &sig, msg) == RPMRC_OK) {
-	struct rpmtd_s utd;
-	DIGEST_CTX ctx = rpmDigestInit(sinfo.hashalgo, RPMDIGEST_NONE);
-
-
-	if (headerGet(h, RPMTAG_HEADERIMMUTABLE, &utd, hgeflags)) {
-	    rpmDigestUpdate(ctx, rpm_header_magic, sizeof(rpm_header_magic));
-	    rpmDigestUpdate(ctx, utd.data, utd.count);
-	    rpmtdFreeData(&utd);
-	}
-	/** @todo Implement disable/enable/warn/error/anal policy. */
-	rc = rpmVerifySignature(keyring, &sigtd, sig, ctx, msg);
-
-	rpmDigestFinal(ctx, NULL, NULL, 0);
-    }
-
-exit:
-    if (xx)
 	rc = xx;
+	goto exit;
+    }
 
-    if (rc != RPMRC_FAIL && h != NULL && hdrp != NULL) {
-	applyRetrofits(h, leadtype);
+    /* Read the signature and main headers */
+    if (hdrblobRead(fd, 1, 1, RPMTAG_HEADERSIGNATURES, &sigblob, msg))
+	goto exit;
+    if (hdrblobRead(fd, 1, 1, RPMTAG_HEADERIMMUTABLE, &blob, msg))
+	goto exit;
 
-	/* Append (and remap) signature tags to the metadata. */
-	headerMergeLegacySigs(h, sigh);
+    /* Check header-only signature/digest */
+    xx = headerSigVerify(keyring, vsflags, &sigblob, &blob, keyidp, msg);
+    if (xx == RPMRC_FAIL)
+	goto exit;
 
-	/* Bump reference count for return. */
+    /* This sums up everything that's wrong with rpm signature handling... */
+    /* TODO: Implement disable/enable/warn/error/anal policy. */
+    if (xx == RPMRC_NOTFOUND)
+	xx = RPMRC_OK;
+
+    /* Finally import the headers and do whatever required retrofits etc */
+    if (hdrblobImport(&sigblob, 0, &sigh, msg))
+	goto exit;
+    if (hdrblobImport(&blob, 0, &h, msg))
+	goto exit;
+
+    /* Append (and remap) signature tags to the metadata. */
+    headerMergeLegacySigs(h, sigh);
+    applyRetrofits(h, leadtype);
+
+    /* Bump reference count for return. */
+    if (hdrp != NULL)
 	*hdrp = headerLink(h);
 
-	if (keyidp)
-	    *keyidp = getKeyid(sig);
-    }
-    rpmtdFreeData(&sigtd);
+    /* If we got this far, return the signature check result */
+    rc = xx;
+
+exit:
+    free(sigblob.ei);
+    free(blob.ei);
     h = headerFree(h);
-    pgpDigParamsFree(sig);
     sigh = headerFree(sigh);
     return rc;
 }
