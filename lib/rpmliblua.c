f13de84bab9cd5b1be30c44002285743c8c3f7a9
daurnimator <quae@daurnimator.com>
2017-02-24 17:42:21 +1100

    Don't use deprecated luaL_pushmodule() function

--- lib/rpmliblua.c
+++ lib/rpmliblua.c
@@ -28,21 +28,19 @@ static const luaL_Reg luarpmlib_f[] = {
     {NULL, NULL}
 };
 
-#ifndef lua_pushglobaltable
-#define lua_pushglobaltable(L) lua_pushvalue(L, LUA_GLOBALSINDEX)
-#endif
-
 void rpmLuaInit(void)
 {
     rpmlua lua = rpmluaGetGlobalState();
-    lua_pushglobaltable(lua->L);
 #if (LUA_VERSION_NUM < 502) || defined(LUA_COMPAT_MODULE)
-    luaL_register(lua->L, "rpm", luarpmlib_f);
+    lua_getfield(lua->L, LUA_REGISTRYINDEX, "_LOADED");
+    lua_getfield(lua->L, -1, "rpm");
+    luaL_register(lua->L, 0, luarpmlib_f);
 #else
-    luaL_pushmodule(lua->L, "rpm", 1);
-    lua_insert(lua->L, -1);
+    lua_getfield(lua->L, LUA_REGISTRYINDEX, LUA_LOADED_TABLE);
+    lua_getfield(lua->L, -1, "rpm");
     luaL_setfuncs(lua->L, luarpmlib_f, 0);
 #endif
+    lua_pop(lua->L, 2);
     return;
 }
 
