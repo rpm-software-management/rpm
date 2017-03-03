rpm-software-management/rpm

commit 6046386afd446084ed10eb57ca390920ef232efa

Use arbitrary ids instead of algorithm number to identify digests in bundles
Change the way individual digests within bundles are stored and accessed:
Previously the digest algo number was used directly as the index into
the bundle. Which works, but limits you to a single digest per algo,
and the array is likely to be rather sparse. To address both issues, use
an arbitrary integer ID to address the digests and always add new
digests in the first available slot. And yes the implementation is
a bit simplistic and taking some shortcuts, but so was the previous one.

Abstracting out the actual algorithm allows using eg rpm tag values
as the identifier, and also makes it possible to do multiple digests
of the same type on overlapping ranges. This is entirely backwards
compatible however if you simply use algorithm number also as the id,
so callers *need* to be updated.

rpmio/digest.c
@@ -10,11 +10,25 @@
 
 #define DIGESTS_MAX 12
 struct rpmDigestBundle_s {
-    int index_min;			/*!< Smallest index of active digest */
     int index_max;			/*!< Largest index of active digest */
     off_t nbytes;			/*!< Length of total input data */
-    DIGEST_CTX digests[DIGESTS_MAX];	/*!< Digest contexts indexed by algo */
+    DIGEST_CTX digests[DIGESTS_MAX];	/*!< Digest contexts identified by id */
+    int ids[DIGESTS_MAX];		/*!< Digest ID (arbitrary non-zero) */
 };
+
+static int findID(rpmDigestBundle bundle, int id)
+{
+    int ix = -1;
+    if (bundle) {
+	for (int i = 0; i < DIGESTS_MAX; i++) {
+	    if (bundle->ids[i] == id) {
+		ix = i;
+		break;
+	    }
+	}
+    }
+    return ix;
+}
 
 rpmDigestBundle rpmDigestBundleNew(void)
 {
@@ -25,7 +39,7 @@ rpmDigestBundle rpmDigestBundleNew(void)
 rpmDigestBundle rpmDigestBundleFree(rpmDigestBundle bundle)
 {
     if (bundle) {
-	for (int i = bundle->index_min; i <= bundle->index_max ; i++) {
+	for (int i = 0; i <= bundle->index_max ; i++) {
 	    if (bundle->digests[i] == NULL)
 		continue;
 	    rpmDigestFinal(bundle->digests[i], NULL, NULL, 0);
@@ -40,56 +54,55 @@ rpmDigestBundle rpmDigestBundleFree(rpmDigestBundle bundle)
 int rpmDigestBundleAdd(rpmDigestBundle bundle, int algo,
 			rpmDigestFlags flags)
 {
+    return rpmDigestBundleAddID(bundle, algo, algo, flags);
+}
+
+int rpmDigestBundleAddID(rpmDigestBundle bundle, int algo, int id,
+			rpmDigestFlags flags)
+{
     DIGEST_CTX ctx = NULL;
-    if (bundle && algo > 0 && algo < DIGESTS_MAX) {
-	if (bundle->digests[algo] == NULL) {
-	    ctx = rpmDigestInit(algo, flags);
-	    if (ctx) {
-		bundle->digests[algo] = ctx;
-		if (algo < bundle->index_min) {
-		    bundle->index_min = algo;
-		}
-		if (algo > bundle->index_max) {
-		    bundle->index_max = algo;
-		}
-	    }
+    if (id > 0) {
+	int ix = findID(bundle, 0); /* Find first free slot */
+	if (ix >= 0) {
+	    bundle->digests[ix] = rpmDigestInit(algo, flags);
+	    bundle->ids[ix]= id;
+	    if (ix > bundle->index_max)
+		bundle->index_max = ix;
 	}
     }
     return (ctx != NULL);
 }
-
 int rpmDigestBundleUpdate(rpmDigestBundle bundle, const void *data, size_t len)
 {
     int rc = 0;
     if (bundle && data && len > 0) {
-	for (int i = bundle->index_min; i <= bundle->index_max; i++) {
-	    DIGEST_CTX ctx = bundle->digests[i];
-	    if (ctx == NULL)
-		continue;
-	    rc += rpmDigestUpdate(ctx, data, len);
+	for (int i = 0; i <= bundle->index_max; i++) {
+	    if (bundle->ids[i] > 0)
+		rc += rpmDigestUpdate(bundle->digests[i], data, len);
 	}
 	bundle->nbytes += len;
     }
     return rc;
 }
 
-int rpmDigestBundleFinal(rpmDigestBundle bundle, 
-		int algo, void ** datap, size_t * lenp, int asAscii)
+int rpmDigestBundleFinal(rpmDigestBundle bundle, int id,
+			 void ** datap, size_t * lenp, int asAscii)
 {
     int rc = 0;
-    if (bundle && algo >= bundle->index_min && algo <= bundle->index_max) {
-	rc = rpmDigestFinal(bundle->digests[algo], datap, lenp, asAscii);
-	bundle->digests[algo] = NULL;
+    int ix = findID(bundle, id);
+
+    if (ix >= 0) {
+	rc = rpmDigestFinal(bundle->digests[ix], datap, lenp, asAscii);
+	bundle->digests[ix] = NULL;
+	bundle->ids[ix] = 0;
     }
     return rc;
 }
 
-DIGEST_CTX rpmDigestBundleDupCtx(rpmDigestBundle bundle, int algo)
+DIGEST_CTX rpmDigestBundleDupCtx(rpmDigestBundle bundle, int id)
 {
-    DIGEST_CTX dup = NULL;
-    if (bundle && algo >= bundle->index_min && algo <= bundle->index_max) {
-	dup = rpmDigestDup(bundle->digests[algo]);
-    }
+    int ix = findID(bundle, id);
+    DIGEST_CTX dup = (ix >= 0) ? rpmDigestDup(bundle->digests[ix]) : NULL;
     return dup;
 }
 

rpmio/rpmpgp.h
@@ -1213,7 +1213,8 @@ rpmDigestBundle rpmDigestBundleNew(void);
 rpmDigestBundle rpmDigestBundleFree(rpmDigestBundle bundle);
 
 /** \ingroup rpmpgp
- * Add a new type of digest to a bundle.
+ * Add a new type of digest to a bundle. Same as calling
+ * rpmDigestBundleAddID() with algo == id value.
  * @param bundle	digest bundle
  * @param algo		type of digest
  * @param flags		bit(s) to control digest operation
@@ -1221,6 +1222,17 @@ rpmDigestBundle rpmDigestBundleFree(rpmDigestBundle bundle);
  */
 int rpmDigestBundleAdd(rpmDigestBundle bundle, int algo,
 			rpmDigestFlags flags);
+
+/** \ingroup rpmpgp
+ * Add a new type of digest to a bundle.
+ * @param bundle	digest bundle
+ * @param algo		type of digest
+ * @param id		id of digest (arbitrary, must be > 0)
+ * @param flags		bit(s) to control digest operation
+ * @return		0 on success
+ */
+int rpmDigestBundleAddID(rpmDigestBundle bundle, int algo, int id,
+			 rpmDigestFlags flags);
 
 /** \ingroup rpmpgp
  * Update contexts within bundle with next plain text buffer.
@@ -1235,22 +1247,22 @@ int rpmDigestBundleUpdate(rpmDigestBundle bundle, const void *data, size_t len);
  * Return digest from a bundle and destroy context, see rpmDigestFinal().
  *
  * @param bundle	digest bundle
- * @param algo		type of digest to return
+ * @param id		id of digest to return
  * @retval datap	address of returned digest
  * @retval lenp		address of digest length
  * @param asAscii	return digest as ascii string?
  * @return		0 on success
  */
-int rpmDigestBundleFinal(rpmDigestBundle bundle,
-	 int algo, void ** datap, size_t * lenp, int asAscii);
+int rpmDigestBundleFinal(rpmDigestBundle bundle, int id,
+			 void ** datap, size_t * lenp, int asAscii);
 
 /** \ingroup rpmpgp
  * Duplicate a digest context from a bundle.
  * @param bundle	digest bundle
- * @param algo		type of digest to dup
+ * @param id		id of digest to dup
  * @return		duplicated digest context
  */
-DIGEST_CTX rpmDigestBundleDupCtx(rpmDigestBundle bundle, int algo);
+DIGEST_CTX rpmDigestBundleDupCtx(rpmDigestBundle bundle, int id);
 
 #ifdef __cplusplus
 }

commit ab943e93f7ed94c2ecd0cd53d7e0480ec3834828

Duplicate IDs in digest bundles are not permitted
Regresssion introduced in commit 6046386afd446084ed10eb57ca390920ef232efa,
inserting a duplicate id must not succeed.

rpmio/digest.c
@@ -61,7 +61,7 @@ int rpmDigestBundleAddID(rpmDigestBundle bundle, int algo, int id,
 			rpmDigestFlags flags)
 {
     DIGEST_CTX ctx = NULL;
-    if (id > 0) {
+    if (id > 0 && findID(bundle, id) < 0) {
 	int ix = findID(bundle, 0); /* Find first free slot */
 	if (ix >= 0) {
 	    bundle->digests[ix] = rpmDigestInit(algo, flags);