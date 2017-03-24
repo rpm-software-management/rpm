2a7e395f011291c638b64c3b89f0361d25ff393f
Panu Matilainen <pmatilai@redhat.com>
2017-03-24 13:33:02 +0200

    Determine setprogname() availability by actually testing for it
    This way we dont need to include separate tests for the entire
    BSD'ish family tree and who knows, might even cover some other
    cases too.

--- system.h
+++ system.h
@@ -98,15 +98,7 @@ extern int fdatasync(int fildes);
 
    setprogname(*pn) must be the first call in main() in order to set the name
    as soon as possible. */
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__APPLE__)
-/* `man setprogname' v. May 21, 2011 (NetBSD 6.1) says:
-   Portable programs that wish to use getprogname() should call setprogname()
-   in main(). In some systems (like NetBSD) it can be set only once and it is
-   done before an execution of main() -- therefore calling it again has no
-   effect.
-
-   Getprogname and setprogname function calls appeared in NetBSD 1.6
-   (released in 2002). So nothing to (re)implement for this platform. */
+#if defined(HAVE_SETPROGNAME)
 # include <stdlib.h> /* Make sure this header is included */
 # define xsetprogname(pn) setprogname(pn)
 # define xgetprogname(pn) getprogname(pn)
