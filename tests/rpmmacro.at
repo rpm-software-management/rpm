rpm-software-management/rpm

commit 5adc56897b9da5dac49701f704ef54390db57c59

Expand parametric macro arguments before processing (#127, RhBug:1397209)
This too is quite a fundamental change for macros: up to now, arguments
to parametric macros have not been expanded. It doesn't sound so bad
until you consider something like the case in RhBug:1397209:

        %global rev 133
        ...
        %setup %{?rev:-c}
        %autosetup %{?rev:-c}

One would expect %setup and %autosetup behave the same when you replace
one with the other, but no.  %setup gets "called" with -c, %autosetup
does not. Instead %autosetup receives a completely useless, literal
"%{?rev:-c}" as its argument.  That's just brain-meltingly non-sensical.

This certainly has the potential to break advanced macro constructs,
but OTOH what breaks might well be only written that way in order to
work around the former behavior.

There are some funny subtleties involved as the argument expansion
must occur in the callers scope, ie before we create any of the
automatic macros. For example, Fedora's font packages break if only
this or the macro scope visibility enforcement is applied but start
working again once both are present.

rpmio/macro.c
@@ -675,9 +675,10 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
     argvAdd(&argv, me->name);
     if (lastc) {
 	ARGV_t av = NULL;
-	char *s = xcalloc((lastc-se)+1, sizeof(*s));
-	memcpy(s, se, (lastc-se));
+	char *s = NULL;
 
+	/* Expand possible macros in arguments */
+	expandThis(mb, se, lastc-se, &s);
 	argvSplit(&av, s, " \t");
 	argvAppend(&argv, av);
 

commit 767d61ca3dba9745d392fa28bbe09a209bd49522

Preserve %%-escapes when expanding parametric macro arguments (#217)
There needs to be a way to pass escaped macros names (and other strings
containging %%) as parametric macro arguments. Add an internal mode
to preserve %% when expanding the arguments. Fixes a regression introduced
in commit 5adc56897b9da5dac49701f704ef54390db57c59.

Thanks to Michael Schroeder for pointing this out!

rpmio/macro.c
@@ -105,6 +105,7 @@ typedef struct MacroBuf_s {
     int error;			/*!< Errors encountered during expansion? */
     int macro_trace;		/*!< Pre-print macro to expand? */
     int expand_trace;		/*!< Post-print macro expansion? */
+    int escape;			/*!< Preserve '%%' during expansion? */
     int flags;			/*!< Flags to control behavior */
     rpmMacroContext mc;
 } * MacroBuf;
@@ -675,6 +676,7 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
      */
     argvAdd(&argv, me->name);
     if (lastc) {
+	int oescape = mb->escape;
 	ARGV_t av = NULL;
 	char *args = xmalloc(lastc-se + 1);
 	memcpy(args, se, lastc-se);
@@ -682,12 +684,14 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
 	argvSplit(&av, args, " \t");
 
 	/* Expand possible macros in arguments */
+	mb->escape = 1;
 	for (ARGV_const_t arg = av; arg && *arg; arg++) {
 	    char *s = NULL;
 	    expandThis(mb, *arg, 0, &s);
 	    argvAdd(&argv, s);
 	    free(s);
 	}
+	mb->escape = oescape;
 	argvFree(av);
 	free(args);
 
@@ -1009,6 +1013,8 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
 		if (*s != '%')
 		    break;
 		s++;	/* skip first % in %% */
+		if (mb->escape)
+		    mbAppend(mb, c);
 	    }
 	default:
 	    mbAppend(mb, c);

tests/rpmmacro.at
@@ -104,6 +104,21 @@ runroot rpm \
 ])
 AT_CLEANUP
 
+AT_SETUP([parametrized macro 4])
+AT_KEYWORDS([macros])
+AT_CHECK([
+runroot rpm \
+    --define '%bar yyy' \
+    --define '%foo() %1' \
+    --eval '%foo %bar' \
+    --eval '%foo %%bar'
+],
+[0],
+[yyy
+%bar
+])
+AT_CLEANUP
+
 AT_SETUP([uncompress macro])
 AT_KEYWORDS([macros])
 AT_CHECK([