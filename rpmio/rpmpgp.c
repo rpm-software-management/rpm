f4adb7ec52d757a396c2e2d88a1d48f224abca46
Panu Matilainen <pmatilai@redhat.com>
2017-05-26 11:35:51 +0300

    Compare tag and userid too in pgpDigParamsCmp()
    Without the tag comparison, we could mistake a pubkey for a signature, doh.
    The userid is relevant too, in that (freely quoting rfc-4880) a single
    key might be used for different purposes and the userid allows the keyholder
    to express which of their roles is making th signature.

--- rpmio/rpmpgp.c
+++ rpmio/rpmpgp.c
@@ -961,6 +961,8 @@ int pgpDigParamsCmp(pgpDigParams p1, pgpDigParams p2)
     int rc = 1; /* assume different, eg if either is NULL */
     if (p1 && p2) {
 	/* XXX Should we compare something else too? */
+	if (p1->tag != p2->tag)
+	    goto exit;
 	if (p1->hash_algo != p2->hash_algo)
 	    goto exit;
 	if (p1->pubkey_algo != p2->pubkey_algo)
@@ -970,6 +972,8 @@ int pgpDigParamsCmp(pgpDigParams p1, pgpDigParams p2)
 	if (p1->sigtype != p2->sigtype)
 	    goto exit;
 	if (memcmp(p1->signid, p2->signid, sizeof(p1->signid)) != 0)
+	    goto exit;
+	if (p1->userid && p2->userid && strcmp(p1->userid, p2->userid) != 0)
 	    goto exit;
 
 	/* Parameters match ... at least for our purposes */
