41bb29442b3bb97ee0f5ae3e9a197e8591105f18
Panu Matilainen <pmatilai@redhat.com>
2017-06-08 17:23:15 +0300

    rpmSignFiles() is an internal-only API

--- sign/rpmsignfiles.h
+++ sign/rpmsignfiles.h
@@ -1,6 +1,9 @@
 #ifndef H_RPMSIGNFILES
 #define H_RPMSIGNFILES
 
+#include <rpm/rpmtypes.h>
+#include <rpm/rpmutil.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,6 +15,7 @@ extern "C" {
  * @param keypass	signing key password
  * @return		RPMRC_OK on success
  */
+RPM_GNUC_INTERNAL
 rpmRC rpmSignFiles(Header h, const char *key, char *keypass);
 
 #ifdef _cplusplus
