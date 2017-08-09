rpm-software-management/rpm

commit 237f35f16f978bb20e4cafd78abfab1f2e5db480

Change macro scoping to be global / local to parametric macros
This changes the macro scoping rules quite fundamentally: macro
definitions are local to the parametric macro they were defined in,
and everything else is global. Among other things, this makes this
common spec idiom (RhBug:552944, RhBug:551971 etc) behave
deterministically because "foo" is placed into global scope:

        %{?!foo: %define foo bar}

In theory it's certainly possible that this breaks somebodys carefully
crafted advanced macros but it seems quite unlikely, considering how
broken the alleged block-scoping has been. OTOH for macros defined
within parametric macros, nothing actually changes as that scoping has
always been enforced by rpm. The non-global define tracking is also
no longer needed for emitting warnings, because the case where it
was needed simply no longer exists.

Note that macro recursion depth is a different thing and still needs
to be tracked separately.

rpmio/macro.c
@@ -57,8 +57,8 @@ struct rpmMacroEntry_s {
 struct rpmMacroContext_s {
     rpmMacroEntry *tab;  /*!< Macro entry table (array of pointers). */
     int n;      /*!< No. of macros. */
-    unsigned int defcnt; /*!< Non-global define tracking */
     int depth;		 /*!< Depth tracking when recursing from Lua  */
+    int level;		 /*!< Scope level tracking when recursing from Lua  */
     pthread_mutex_t lock;
     pthread_mutexattr_t lockattr;
 };
@@ -101,6 +101,7 @@ typedef struct MacroBuf_s {
     size_t tpos;		/*!< Current position in expansion buffer */
     size_t nb;			/*!< No. bytes remaining in expansion buffer. */
     int depth;			/*!< Current expansion depth. */
+    int level;			/*!< Current scoping level */
     int error;			/*!< Errors encountered during expansion? */
     int macro_trace;		/*!< Pre-print macro to expand? */
     int expand_trace;		/*!< Post-print macro expansion? */
@@ -624,7 +625,7 @@ freeArgs(MacroBuf mb, int delete)
     for (int i = 0; i < mc->n; i++) {
 	rpmMacroEntry me = mc->tab[i];
 	assert(me);
-	if (me->level < mb->depth)
+	if (me->level < mb->level)
 	    continue;
 	/* Warn on defined but unused non-automatic, scoped macros */
 	if (!(me->flags & (ME_AUTO|ME_USED))) {
@@ -634,6 +635,7 @@ freeArgs(MacroBuf mb, int delete)
 	    /* Only whine once */
 	    me->flags |= ME_USED;
 	}
+
 	if (!delete)
 	    continue;
 
@@ -642,6 +644,7 @@ freeArgs(MacroBuf mb, int delete)
 	    i--;
 	popMacro(mc, me->name);
     }
+    mb->level--;
 }
 
 /**
@@ -685,8 +688,11 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
 	       lastc : lastc + 1;
     }
 
+    /* Bump call depth on entry before first macro define */
+    mb->level++;
+
     /* Setup macro name as %0 */
-    pushMacro(mb->mc, "0", NULL, me->name, mb->depth, ME_AUTO);
+    pushMacro(mb->mc, "0", NULL, me->name, mb->level, ME_AUTO);
 
     /*
      * The macro %* analoguous to the shell's $* means "Pass all non-macro
@@ -697,7 +703,7 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
      * This is the (potential) justification for %{**} ...
     */
     args = argvJoin(argv + 1, " ");
-    pushMacro(mb->mc, "**", NULL, args, mb->depth, ME_AUTO);
+    pushMacro(mb->mc, "**", NULL, args, mb->level, ME_AUTO);
     free(args);
 
     /*
@@ -730,13 +736,13 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
 	} else {
 	    rasprintf(&body, "-%c", c);
 	}
-	pushMacro(mb->mc, name, NULL, body, mb->depth, ME_AUTO);
+	pushMacro(mb->mc, name, NULL, body, mb->level, ME_AUTO);
 	free(name);
 	free(body);
 
 	if (optarg) {
 	    rasprintf(&name, "-%c*", c);
-	    pushMacro(mb->mc, name, NULL, optarg, mb->depth, ME_AUTO);
+	    pushMacro(mb->mc, name, NULL, optarg, mb->level, ME_AUTO);
 	    free(name);
 	}
     }
@@ -744,7 +750,7 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
     /* Add argument count (remaining non-option items) as macro. */
     {	char *ac = NULL;
     	rasprintf(&ac, "%d", (argc - optind));
-    	pushMacro(mb->mc, "#", NULL, ac, mb->depth, ME_AUTO);
+	pushMacro(mb->mc, "#", NULL, ac, mb->level, ME_AUTO);
 	free(ac);
     }
 
@@ -753,14 +759,14 @@ grabArgs(MacroBuf mb, const rpmMacroEntry me, const char * se,
 	for (c = optind; c < argc; c++) {
 	    char *name = NULL;
 	    rasprintf(&name, "%d", (c - optind + 1));
-	    pushMacro(mb->mc, name, NULL, argv[c], mb->depth, ME_AUTO);
+	    pushMacro(mb->mc, name, NULL, argv[c], mb->level, ME_AUTO);
 	    free(name);
 	}
     }
 
     /* Add concatenated unexpanded arguments as yet another macro. */
     args = argvJoin(argv + optind, " ");
-    pushMacro(mb->mc, "*", NULL, args ? args : "", mb->depth, ME_AUTO);
+    pushMacro(mb->mc, "*", NULL, args ? args : "", mb->level, ME_AUTO);
     free(args);
 
 exit:
@@ -796,15 +802,18 @@ static void doLua(MacroBuf mb, const char * f, size_t fn, const char * g, size_t
     char *printbuf;
     rpmMacroContext mc = mb->mc;
     int odepth = mc->depth;
+    int olevel = mc->level;
 
     if (g != NULL && gn > 0)
 	memcpy(scriptbuf, g, gn);
     scriptbuf[gn] = '\0';
     rpmluaPushPrintBuffer(lua);
     mc->depth = mb->depth;
+    mc->level = mb->level;
     if (rpmluaRunScript(lua, scriptbuf, NULL) == -1)
 	mb->error = 1;
     mc->depth = odepth;
+    mc->level = olevel;
     printbuf = rpmluaPopPrintBuffer(lua);
     if (printbuf) {
 	mbAppendStr(mb, printbuf);
@@ -941,7 +950,6 @@ doFoo(MacroBuf mb, int negate, const char * f, size_t fn,
 static int
 expandMacro(MacroBuf mb, const char *src, size_t slen)
 {
-    rpmMacroContext mc = mb->mc;
     rpmMacroEntry *mep;
     rpmMacroEntry me;
     const char *s = src, *se;
@@ -953,7 +961,6 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
     const char * lastc;
     int chkexist;
     char *source = NULL;
-    unsigned int prevcnt = mc->defcnt;
 
     /*
      * Always make a (terminated) copy of the source string.
@@ -1132,7 +1139,7 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
 	    continue;
 	}
 	if (STREQ("define", f, fn)) {
-	    s = doDefine(mb, se, slen - (se - s), mb->depth, 0);
+	    s = doDefine(mb, se, slen - (se - s), mb->level, 0);
 	    continue;
 	}
 	if (STREQ("undefine", f, fn)) {
@@ -1270,9 +1277,6 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
     }
 
     mb->buf[mb->tpos] = '\0';
-    /* Warn on unused non-global macros */
-    if (prevcnt != mc->defcnt)
-	freeArgs(mb, 0);
     mb->depth--;
     if (mb->error != 0 || mb->expand_trace)
 	printExpansion(mb, mb->buf+tpos, mb->buf+mb->tpos);
@@ -1291,6 +1295,7 @@ static int doExpandMacros(rpmMacroContext mc, const char *src, char **target)
 
     mb->buf = NULL;
     mb->depth = mc->depth;
+    mb->level = mc->level;
     mb->macro_trace = print_macro_trace;
     mb->expand_trace = print_expand_trace;
     mb->mc = mc;
@@ -1370,10 +1375,6 @@ static void pushMacro(rpmMacroContext mc,
     /* push over previous definition */
     me->prev = *mep;
     *mep = me;
-
-    /* Track for non-global definitions */
-    if (level > 0)
-	mc->defcnt++;
 }
 
 static void popMacro(rpmMacroContext mc, const char * n)

commit c4d41c30cecc910ada85d2fe4cbeab19673065d7

Shift macro scoping level values as they are defined
Some of scoping level values are defined in rpmmacros.h:
 #define RMIL_DEFAULT    -15
 #define RMIL_MACROFILES -13
...
 #define RMIL_GLOBAL     0

Before this patch RPM set these values decreased of 1 (for all macros).
Because of the (level - 1) in
    pushMacro(dst, me->name, me->opts, me->body, (level - 1), me->flags);
in function copyMacros scoping level for some CMDLINE macros was decreased of 1.
Because of the (level - 1) in
    pushMacro(mb->mc, n, o, b, (level - 1), ME_NONE);
in function doDefine scoping level values of all other macro types and
some CMDLINE macros was decreased of 1.
The patch shifts scoping level values back to the numbers defined
in rpmmacros.h.

This was spotted when investigating a regression introduced in
commit 237f35f16f978bb20e4cafd78abfab1f2e5db480 where locally scoped
scoped macros did not get deleted on scope closing.
This also fixes the regression in question.

The first test in this patch tests automatic deleting of %defined macros.
The second test checks whether the behavior changed by
237f35f16f978bb20e4cafd78abfab1f2e5db480 is still working correctly.

Thanks to Panu Matilainen for investigation and proposal of the main part
of the patch!

rpmio/macro.c
@@ -566,7 +566,7 @@ doDefine(MacroBuf mb, const char * se, size_t slen, int level, int expandbody)
 	b = ebody;
     }
 
-    pushMacro(mb->mc, n, o, b, (level - 1), ME_NONE);
+    pushMacro(mb->mc, n, o, b, level, ME_NONE);
     rc = 0;
 
 exit:
@@ -1546,7 +1546,7 @@ static void copyMacros(rpmMacroContext src, rpmMacroContext dst, int level)
     for (int i = 0; i < src->n; i++) {
 	rpmMacroEntry me = src->tab[i];
 	assert(me);
-	pushMacro(dst, me->name, me->opts, me->body, (level - 1), me->flags);
+	pushMacro(dst, me->name, me->opts, me->body, level, me->flags);
     }
 }
 

tests/data/SPECS/macro-scope.spec
@@ -1,0 +1,80 @@
+Name:             macro-scope
+Version:          1.0
+Release:          1
+License:          Testing
+Summary:          Testing macro undefine/global behavior
+
+%description
+%{summary}
+
+
+%prep
+# define + undefine in a lower scoping level
+%define sc12() {%{?test1: %define a01 "1"}}
+%define sc11() {%{?test1: %sc12}\
+		%{?test1: %undefine a01}}
+%sc11
+
+# global in a lower scoping level
+%define sc22() {%{?test1: %global a02 "1"}}
+%define sc21() {%{?test1: %sc22}\
+		%{?test1: %undefine a02}}
+%sc21
+
+# define + undefine in a higher scoping level 1
+%define sc32() {%{?test2: %undefine a03}}
+%define sc31() {%{?test2: %define a03 "1"} %sc32}
+%sc31
+
+# global in a higher scoping level
+%define sc42() {%{?test1: %undefine a04}}
+%define sc41() {%{?test1: %global a04 "1"} %sc42}
+%sc41
+
+# define + undefine in higher scoping level 2
+%define sc52() {%{?test1: %define a05 "1"}\
+		%{?test1: %define a05 "2"}\
+		%{?test1: %undefine a05}}
+%define sc51() {%{?test1: %define a05 "3"}\
+		%{?test1: %sc52}\
+		%{?test1: %undefine a05}}
+%sc51
+
+# define + undefine in the same scoping level
+%define a06 "1"
+%undefine a06
+
+# define + undfine in higer svoping level 3
+%define sc74() {%{?test1: %define a07 "1"}}
+%define sc73() {%{?test1: %sc74}}
+%define sc72() {%{?test1: %sc73}\
+		%{?test1: %undefine a07}}
+%define sc71() {%{?test1: %sc72}}
+%sc71
+
+# define + undefine in disjoint levels 1
+%define sc81() {%{?test1: %define a08 "1"}}
+%sc81
+%define sc82() {%{?test1: %undefine a08}}
+%sc82
+
+# define + undefine in disjoint levels 2
+%define sc92() {%{?test1: %define a09 "1"}}
+%define sc91() {%{?test1: %sc92}}
+%sc91
+%define sc94() {%{?test1: %undefine a09}}
+%define sc93() {%{?test1: %sc94}}
+%sc93
+
+# define + undefine in disjoint levels 3
+%define scA2() {%{?test1: %define a0A "1"}}
+%define scA1() {%{?test1: %scA2}}
+%define scA4() {%{?test1: %undefine a0A}}
+%define scA3() {%{?test1: %scA4}}
+%define scA0() {%{?test1: %scA1} %{?test1: %scA3}}
+%scA0
+
+# define + undefine in the scoping level -1
+%define scB1() {%{?test1: %undefine a0B}}
+%define a0B "1"
+%scB1

tests/rpmmacro.at
@@ -330,3 +330,42 @@ runroot rpm --define "%foo() 1:%1 2:%2 3:%3 4:%4 5:%5" --eval "%foo xx'yy'\"zz\"
 1:xx 2:yy 3:zz 4: 5:bb
 ])
 AT_CLEANUP
+
+AT_SETUP([%undefine from different scopes])
+AT_KEYWORDS([macros %undefine])
+AT_CHECK([
+runroot rpmbuild --quiet -bb \
+	    --define '%test1 ok'\
+	    /data/SPECS/macro-scope.spec
+],
+[0],
+[],
+[warning: Macro %a01 defined but not used within scope
+warning: Macro %a05 defined but not used within scope
+warning: Macro %a07 defined but not used within scope
+warning: Macro %a08 defined but not used within scope
+warning: Macro %a09 defined but not used within scope
+warning: Macro %a0A defined but not used within scope]
+)
+AT_CLEANUP
+
+
+
+
+AT_SETUP([%define in conditional macro])
+AT_KEYWORDS([macros])
+AT_CHECK([
+runroot rpm \
+    --define 'dofoo() true'\
+    --eval '.%{!?foo: %define foo bar}.'\
+    --eval '%foo' \
+    --eval '%dofoo'\
+    --eval '%foo'
+],
+[0],
+[. .
+bar
+true
+bar
+])
+AT_CLEANUP