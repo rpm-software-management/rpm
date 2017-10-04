73c76f59997c2c4e42c6f511cade680a2a258f79
Panu Matilainen <pmatilai@redhat.com>
2017-10-04 10:15:28 +0300

    Always initialize hdrp retval of rpmReadPackageFile()
    Since commit 3ccd9ccf50c8d643f9dcd956155720018429beed, hdrp has only
    been set on success (at least half-intentionally), but turns out a
    various third party software depends on the former behavior. And on
    closer inspection, it's actually a documented feature of the API.

--- lib/package.c
+++ lib/package.c
@@ -298,6 +298,10 @@ rpmRC rpmReadPackageFile(rpmts ts, FD_t fd, const char * fn, Header * hdrp)
 	.fn = fn ? fn : Fdescr(fd),
     };
 
+    /* XXX: lots of 3rd party software relies on the behavior */
+    if (hdrp)
+	*hdrp = NULL;
+
     rpmRC rc = rpmpkgRead(keyring, vsflags, fd, handlePkgVS, &pkgdata, hdrp);
 
     rpmKeyringFree(keyring);
