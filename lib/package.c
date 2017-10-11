rpm-software-management/rpm

commit 3ccd9ccf50c8d643f9dcd956155720018429beed

Use the signature checking code to read all package files
Both read package files and verify signatures, it only makes sense
to use the same code for both.

This does have significant consequences of course: we are now validating
ALL non-disabled header-only signatures and digests when reading
packages with rpmReadPackageFile() instead of just one that we figured
might be the best, if available. On one hand, it's a bit wasteful
since a good signature could be considered to make digests moot,
but then once we have real signature policies we might need to process
more than one signature to come to a conclusion anyway. For now,
just let it waste a few more cycles verifying redundancies.

lib/package.c
@@ -301,106 +301,51 @@ void applyRetrofits(Header h, int leadtype)
 	headerConvert(h, HEADERCONV_COMPRESSFILELIST);
 }
 
-static rpmRC rpmpkgRead(rpmKeyring keyring, rpmVSFlags vsflags, 
-			FD_t fd,
-			Header * hdrp, unsigned int *keyidp, char **msg)
+struct pkgdata_s {
+    const char *fn;
+};
+
+static rpmRC handlePkgVS(struct rpmsinfo_s *sinfo, rpmRC rc, const char *msg, void *cbdata)
 {
-    Header sigh = NULL;
-    Header h = NULL;
-    struct hdrblob_s blob, sigblob;
-    rpmRC xx, rc = RPMRC_FAIL;	/* assume failure */
-    int leadtype = -1;
-
-    if (hdrp) *hdrp = NULL;
-
-    memset(&blob, 0, sizeof(blob));
-    memset(&sigblob, 0, sizeof(sigblob));
-
-    if ((xx = rpmLeadRead(fd, &leadtype, msg)) != RPMRC_OK) {
-	/* Avoid message spew on manifests */
-	if (xx == RPMRC_NOTFOUND) {
-	    *msg = _free(*msg);
-	}
-	rc = xx;
-	goto exit;
+    struct pkgdata_s *pkgdata = cbdata;
+    switch (rc) {
+    case RPMRC_OK:		/* Signature is OK. */
+	rpmlog(RPMLOG_DEBUG, "%s: %s\n", pkgdata->fn, msg);
+	break;
+    case RPMRC_NOTTRUSTED:	/* Signature is OK, but key is not trusted. */
+    case RPMRC_NOKEY:		/* Public key is unavailable. */
+	/* XXX Print NOKEY/NOTTRUSTED warning only once. */
+    {	int lvl = (stashKeyid(sinfo->keyid) ? RPMLOG_DEBUG : RPMLOG_WARNING);
+	rpmlog(lvl, "%s: %s\n", pkgdata->fn, msg);
+    }	break;
+    case RPMRC_NOTFOUND:	/* Signature/digest not present. */
+	if (msg)
+	    rpmlog(RPMLOG_WARNING, "%s: %s\n", pkgdata->fn, msg);
+	break;
+    default:
+    case RPMRC_FAIL:		/* Signature does not verify. */
+	rpmlog(RPMLOG_ERR, "%s: %s\n", pkgdata->fn, msg);
+	break;
     }
 
-    /* Read the signature and main headers */
-    if (hdrblobRead(fd, 1, 1, RPMTAG_HEADERSIGNATURES, &sigblob, msg))
-	goto exit;
-    if (hdrblobRead(fd, 1, 1, RPMTAG_HEADERIMMUTABLE, &blob, msg))
-	goto exit;
+    /* Preserve traditional behavior for now: only failure prevents install  */
+    if (rc != RPMRC_FAIL)
+	rc = RPMRC_OK;
 
-    /* Check header-only signature/digest */
-    xx = headerSigVerify(keyring, vsflags, &sigblob, &blob, keyidp, msg);
-    if (xx == RPMRC_FAIL)
-	goto exit;
-
-    /* This sums up everything that's wrong with rpm signature handling... */
-    /* TODO: Implement disable/enable/warn/error/anal policy. */
-    if (xx == RPMRC_NOTFOUND)
-	xx = RPMRC_OK;
-
-    /* Finally import the headers and do whatever required retrofits etc */
-    if (hdrblobImport(&sigblob, 0, &sigh, msg))
-	goto exit;
-    if (hdrblobImport(&blob, 0, &h, msg))
-	goto exit;
-
-    /* Append (and remap) signature tags to the metadata. */
-    headerMergeLegacySigs(h, sigh);
-    applyRetrofits(h, leadtype);
-
-    /* Bump reference count for return. */
-    if (hdrp != NULL)
-	*hdrp = headerLink(h);
-
-    /* If we got this far, return the signature check result */
-    rc = xx;
-
-exit:
-    free(sigblob.ei);
-    free(blob.ei);
-    h = headerFree(h);
-    sigh = headerFree(sigh);
     return rc;
 }
 
 rpmRC rpmReadPackageFile(rpmts ts, FD_t fd, const char * fn, Header * hdrp)
 {
-    rpmRC rc;
-    rpmVSFlags vsflags = rpmtsVSFlags(ts);
+    rpmVSFlags vsflags = rpmtsVSFlags(ts) | RPMVSF_NEEDPAYLOAD;
     rpmKeyring keyring = rpmtsGetKeyring(ts, 1);
-    unsigned int keyid = 0;
-    char *msg = NULL;
+    struct pkgdata_s pkgdata = {
+	.fn = fn ? fn : Fdescr(fd),
+    };
 
-    if (fn == NULL)
-	fn = Fdescr(fd);
+    rpmRC rc = rpmpkgVerifySignatures(keyring, vsflags, fd, handlePkgVS, &pkgdata, hdrp);
 
-    rc = rpmpkgRead(keyring, vsflags, fd, hdrp, &keyid, &msg);
-
-    switch (rc) {
-    case RPMRC_OK:		/* Signature is OK. */
-	rpmlog(RPMLOG_DEBUG, "%s: %s\n", fn, msg);
-	break;
-    case RPMRC_NOTTRUSTED:	/* Signature is OK, but key is not trusted. */
-    case RPMRC_NOKEY:		/* Public key is unavailable. */
-	/* XXX Print NOKEY/NOTTRUSTED warning only once. */
-    {	int lvl = (stashKeyid(keyid) ? RPMLOG_DEBUG : RPMLOG_WARNING);
-	rpmlog(lvl, "%s: %s\n", fn, msg);
-    }	break;
-    case RPMRC_NOTFOUND:	/* Signature is unknown type or manifest. */
-	/* msg == NULL is probably a manifest */
-	if (msg)
-	    rpmlog(RPMLOG_WARNING, "%s: %s\n", fn, msg);
-	break;
-    default:
-    case RPMRC_FAIL:		/* Signature does not verify. */
-	rpmlog(RPMLOG_ERR, "%s: %s\n", fn, msg);
-	break;
-    }
     rpmKeyringFree(keyring);
-    free(msg);
 
     return rc;
 }

tests/rpmi.at
@@ -162,6 +162,8 @@ runroot rpm -U --ignorearch --ignoreos --nodeps \
 [1],
 [],
 [error: /tmp/hello-2.0-1.x86_64-signed.rpm: Header V4 RSA/SHA256 Signature, key ID 1964c5fc: BAD
+error: /tmp/hello-2.0-1.x86_64-signed.rpm: Header SHA1 digest: BAD Expected(5cd9874c510b67b44483f9e382a1649ef7743bac) != (4261b2c1eb861a4152c2239bce20bfbcaa8971ba)
+error: /tmp/hello-2.0-1.x86_64-signed.rpm: Header SHA256 digest: BAD Expected(ef920781af3bf072ae9888eec3de1c589143101dff9cc0b561468d395fb766d9) != (29fdfe92782fb0470a9a164a6c94af87d3b138c63b39d4c30e0223ca1202ba82)
 error: /tmp/hello-2.0-1.x86_64-signed.rpm cannot be installed
 ])
 AT_CLEANUP

commit e6c88eaeab3661f5fb180b970cafba233bcd999a

Fix "substatus" (eg NOKEY) regression on package read (#330)
Commit 3ccd9ccf50c8d643f9dcd956155720018429beed introduced a regression
in the cases where package is successfully read (ie a header is returned)
but "there is a but", such as a missing pubkey. We need to return the
substatus instead of just "yes yes" in that case.

Keep track of the substatus in through the callback and return it
in case everything else was okay to restore former behavior.

lib/package.c
@@ -266,6 +266,7 @@ void applyRetrofits(Header h, int leadtype)
 
 struct pkgdata_s {
     const char *fn;
+    rpmRC rc;
 };
 
 static rpmRC handlePkgVS(struct rpmsinfo_s *sinfo, rpmRC rc, const char *msg, void *cbdata)
@@ -293,7 +294,11 @@ static rpmRC handlePkgVS(struct rpmsinfo_s *sinfo, rpmRC rc, const char *msg, vo
 
     rpmlog(lvl, "%s: %s\n", pkgdata->fn, vsmsg);
 
-    /* Preserve traditional behavior for now: only failure prevents install  */
+    /* Remember actual return code, but don't override a previous failure */
+    if (rc && pkgdata->rc != RPMRC_FAIL)
+	pkgdata->rc = rc;
+
+    /* Preserve traditional behavior for now: only failure prevents read */
     if (rc != RPMRC_FAIL)
 	rc = RPMRC_OK;
 
@@ -307,6 +312,7 @@ rpmRC rpmReadPackageFile(rpmts ts, FD_t fd, const char * fn, Header * hdrp)
     rpmKeyring keyring = rpmtsGetKeyring(ts, 1);
     struct pkgdata_s pkgdata = {
 	.fn = fn ? fn : Fdescr(fd),
+	.rc = RPMRC_OK,
     };
 
     /* XXX: lots of 3rd party software relies on the behavior */
@@ -314,6 +320,10 @@ rpmRC rpmReadPackageFile(rpmts ts, FD_t fd, const char * fn, Header * hdrp)
 	*hdrp = NULL;
 
     rpmRC rc = rpmpkgRead(keyring, vsflags, fd, handlePkgVS, &pkgdata, hdrp);
+
+    /* If there was a "substatus" (NOKEY in practise), return that instead */
+    if (rc == RPMRC_OK && pkgdata.rc)
+	rc = pkgdata.rc;
 
     rpmKeyringFree(keyring);
 