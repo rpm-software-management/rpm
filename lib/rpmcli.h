3fe0beeb073a8427c4fd124c4e944b8fc27c37c1
Panu Matilainen <pmatilai@redhat.com>
2017-11-13 16:12:20 +0200

    Move cli verify flags to rpmcli.h where they belong, lose rpmvf.h
    rpmVerifyFlags are only relevant for the cli-oriented API's,
    it makes no sense to have a separate header file just for these.
    Back then the idea was to create additional APIs around verify
    which would've kinda warranted a header of its own but that never
    happened (for our purposes circa 10 years is close enough to forever)

--- lib/rpmcli.h
+++ lib/rpmcli.h
@@ -15,7 +15,6 @@
 #include <rpm/rpmcallback.h>
 #include <rpm/rpmts.h>
 #include <rpm/rpmfi.h>
-#include <rpm/rpmvf.h>
 #include <rpm/argv.h>
 
 #ifdef __cplusplus
@@ -258,6 +257,29 @@ int showVerifyPackage(QVA_t qva, rpmts ts, Header h);
  * @return		0 on success, 1 on failure
  */
 int rpmVerifySignatures(QVA_t qva, rpmts ts, FD_t fd, const char * fn);
+
+/** \ingroup rpmvf
+ * Bit(s) to control rpmcliVerify() operation
+ */
+enum rpmVerifyFlags_e {
+    VERIFY_DEFAULT	= 0,		/*!< */
+	/* bits 1-14 unused */
+    VERIFY_CONTEXTS	= (1 << 15),	/*!< verify: from --nocontexts */
+    VERIFY_FILES	= (1 << 16),	/*!< verify: from --nofiles */
+    VERIFY_DEPS		= (1 << 17),	/*!< verify: from --nodeps */
+    VERIFY_SCRIPT	= (1 << 18),	/*!< verify: from --noscripts */
+    VERIFY_DIGEST	= (1 << 19),	/*!< verify: from --nodigest */
+    VERIFY_SIGNATURE	= (1 << 20),	/*!< verify: from --nosignature */
+    VERIFY_PATCHES	= (1 << 21),	/*!< verify: from --nopatches */
+    VERIFY_HDRCHK	= (1 << 22),	/*!< verify: from --nohdrchk */
+	/* bits 28-31 used in rpmVerifyAttrs */
+};
+
+typedef rpmFlags rpmVerifyFlags;
+
+#define	VERIFY_ALL	\
+  ( VERIFY_FILES | VERIFY_DEPS | VERIFY_SCRIPT | VERIFY_DIGEST |\
+    VERIFY_SIGNATURE | VERIFY_HDRCHK )
 
 /** \ingroup rpmcli
  * Verify package install.
