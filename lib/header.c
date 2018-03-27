db88f4806167caf69eda67f4f675551764304d78
Panu Matilainen <pmatilai@redhat.com>
2018-03-27 11:51:06 +0300

    Further relax type checking among different string tag types (#414)
    In some cases i18n strings are just strings in packages (at least
    summary in rhel-6 created src.rpms), no reason to fail because of that,
    just assume all string tag types are compatible. i18n-strings are not
    truly equal to the other types but this is close enuf for the purpose.

--- lib/header.c
+++ lib/header.c
@@ -171,9 +171,9 @@ static int hdrchkTagType(rpm_tag_t tag, rpm_tagtype_t type)
     if (t == RPM_NULL_TYPE)
 	return 0;
 
-    /* Some string tags harmlessly disagree on array vs non-array */
-    if ((t == RPM_STRING_ARRAY_TYPE && type == RPM_STRING_TYPE) ||
-	    (type == RPM_STRING_ARRAY_TYPE && t == RPM_STRING_TYPE))
+    /* Some string tags harmlessly disagree on the exact type */
+    if (rpmTagGetClass(tag) == RPM_STRING_CLASS &&
+	    (rpmTagTypeGetClass(type) == RPM_STRING_CLASS))
 	return 0;
 
     /* Known tag with mismatching type, bad bad bad. */
