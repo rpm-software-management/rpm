463bbd5f6687771618f580204101d372d3f92de5
Panu Matilainen <pmatilai@redhat.com>
2018-04-19 12:23:40 +0300

    Make retrofit etc helpers static again now that we can

--- lib/package.c
+++ lib/package.c
@@ -25,6 +25,7 @@
  * @param h		header (dest)
  * @param sigh		signature header (src)
  */
+static
 void headerMergeLegacySigs(Header h, Header sigh)
 {
     HeaderIterator hi;
@@ -230,6 +231,7 @@ exit:
     return rc;
 }
 
+static
 void applyRetrofits(Header h)
 {
     /*
