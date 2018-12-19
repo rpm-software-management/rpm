rpm-software-management/rpm

commit f13de84bab9cd5b1be30c44002285743c8c3f7a9

Don't use deprecated luaL_pushmodule() function

lib/rpmliblua.c
@@ -28,21 +28,19 @@ static const luaL_Reg luarpmlib_f[] = {
     {NULL, NULL}
 };
 
-#ifndef lua_pushglobaltable
-#define lua_pushglobaltable(L) lua_pushvalue(L, LUA_GLOBALSINDEX)
-#endif
-
 void rpmLuaInit(void)
 {
     rpmlua lua = rpmluaGetGlobalState();
-    lua_pushglobaltable(lua->L);
 #if (LUA_VERSION_NUM < 502) || defined(LUA_COMPAT_MODULE)
-    luaL_register(lua->L, "rpm", luarpmlib_f);
+    lua_getfield(lua->L, LUA_REGISTRYINDEX, "_LOADED");
+    lua_getfield(lua->L, -1, "rpm");
+    luaL_register(lua->L, 0, luarpmlib_f);
 #else
-    luaL_pushmodule(lua->L, "rpm", 1);
-    lua_insert(lua->L, -1);
+    lua_getfield(lua->L, LUA_REGISTRYINDEX, LUA_LOADED_TABLE);
+    lua_getfield(lua->L, -1, "rpm");
     luaL_setfuncs(lua->L, luarpmlib_f, 0);
 #endif
+    lua_pop(lua->L, 2);
     return;
 }
 

commit bcb4781fd8cf91bd558ef22c8c0dfd0915727036

Fix rpmlib lua extension build with Lua < 5.3.4 (#608)
It seems that "_LOADED" was replaced by macro LUA_LOADED_TABLE in lua 5.3.4.
Re-establish string "_LOADED" for pre-5.3.4 Lua.

Fixes regression with Lua < 5.3.4 introduced by commit f13de84.

lib/rpmliblua.c
@@ -3,6 +3,12 @@
 #ifdef WITH_LUA
 #include <lua.h>
 #include <lauxlib.h>
+
+#ifndef LUA_LOADED_TABLE
+/* feature introduced in Lua 5.3.4 */
+#define LUA_LOADED_TABLE "_LOADED"
+#endif
+
 #include <rpm/rpmlib.h>
 
 #define _RPMLUA_INTERNAL