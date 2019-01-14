rpm-software-management/rpm

commit bf2bc18ebb325f081ade65adc2fbb6858f0b8396

Always fail build on unknown dependency qualifiers - previously unknown qualifiers would go unnoticed if they happened   be last (including being the only qualifier), eg Requires(pre,junk)   or Requires(junk) would be cheerfully accepted but Requires(junk,pre) not.

build/parsePreamble.c
@@ -138,6 +138,7 @@ static int parseBits(const char * s, const tokenBits tokbits,
     const char * se;
     rpmsenseFlags bits = RPMSENSE_ANY;
     int c = 0;
+    int rc = RPMRC_OK;
 
     if (s) {
 	while (*s != '\0') {
@@ -151,8 +152,10 @@ static int parseBits(const char * s, const tokenBits tokbits,
 		    strlen(tb->name) == (se-s) && rstreqn(tb->name, s, (se-s)))
 		    break;
 	    }
-	    if (tb->name == NULL)
+	    if (tb->name == NULL) {
+		rc = RPMRC_FAIL;
 		break;
+	    }
 	    bits |= tb->bits;
 	    while ((c = *se) && risspace(c)) se++;
 	    if (c != ',')
@@ -160,8 +163,8 @@ static int parseBits(const char * s, const tokenBits tokbits,
 	    s = ++se;
 	}
     }
-    if (c == 0 && bp) *bp = bits;
-    return (c ? RPMRC_FAIL : RPMRC_OK);
+    *bp |= bits;
+    return rc;
 }
 
 /**

commit 59aa514d038990c250dca108d00c85f2bc19546f

parseBits: disallow syntax errors and unknown qualifiers
The previous attempt to fail build in case of syntax errors
and unknown dependency qualifiers made in commit
rpm-4.8.0-beta1-385-gbf2bc18ebb325f081ade65adc2fbb6858f0b8396
missed the following classes of erroneous dependencies:

Requires(,) -- erroneously treated as Requires(),
Requires(;) -- erroneously treated as Requires(),
Requires(,pre) -- erroneously treated as Requires(),
Requires(;pre) -- erroneously treated as Requires(),
Requires(pre,) -- erroneously treated as Requires(pre),
Requires(pre,,postun) -- erroneously treated as Requires(pre),
Requires(pre,,junk) -- erroneously treated as Requires(pre),
Requires(pre;postun) -- erroneously treated as Requires(pre),
Requires(pre;junk) -- erroneously treated as Requires(pre).

Found by code inspection.

Fixes: bf2bc18ebb32 ("Always fail build on unknown dependency qualifiers")

build/parsePreamble.c
@@ -355,19 +355,22 @@ static struct tokenBits_s const installScriptBits[] = {
 static int parseBits(const char * s, const tokenBits tokbits,
 		rpmsenseFlags * bp)
 {
-    tokenBits tb;
+    tokenBits tb = NULL;
     const char * se;
     rpmsenseFlags bits = RPMSENSE_ANY;
     int c = 0;
     int rc = RPMRC_OK;
 
     if (s) {
-	while (*s != '\0') {
+	for (;;) {
 	    while ((c = *s) && risspace(c)) s++;
 	    se = s;
 	    while ((c = *se) && risalpha(c)) se++;
-	    if (s == se)
+	    if (s == se) {
+		if (c || tb)
+		    rc = RPMRC_FAIL;
 		break;
+	    }
 	    for (tb = tokbits; tb->name; tb++) {
 		if (tb->name != NULL &&
 		    strlen(tb->name) == (se-s) && rstreqn(tb->name, s, (se-s)))
@@ -379,8 +382,11 @@ static int parseBits(const char * s, const tokenBits tokbits,
 	    }
 	    bits |= tb->bits;
 	    while ((c = *se) && risspace(c)) se++;
-	    if (c != ',')
+	    if (c != ',') {
+		if (c)
+		    rc = RPMRC_FAIL;
 		break;
+	    }
 	    s = ++se;
 	}
     }

tests/data/SPECS/test-parsebits.spec
@@ -9,13 +9,7 @@ BuildArch: noarch
 %{?extra:%{extra}}
 Requires: good.0.1
 Requires(): good.0.2 = v0
-Requires( ): good.0.3
-Requires(	 ): good.0.4 = v0
 Requires(interp): good.1.1 > v1
-Requires(interp ): good.1.2
-Requires(interp	 ): good.1.3 > v1
-Requires( interp): good.1.4
-Requires(	 interp): good.1.5 > v1
 Requires(preun): good.2.1 >= v2
 Requires(pre): good.3.1 < v3
 Requires(postun): good.4.1 <= v4
@@ -25,10 +19,6 @@ Requires(verify): good.7.1 > v7
 Requires(pretrans): good.8.1 >= v8
 Requires(posttrans): good.9.1 < v9
 Requires(pre,postun): good.34.1 <= v34
-Requires(pre ,postun): good.34.2
-Requires(pre	 ,postun): good.34.3 <= v34
-Requires(pre, postun): good.34.4
-Requires(pre,	 postun): good.34.5 <= v34
 Requires(interp,preun,pre,postun,post,rpmlib,verify,pretrans,posttrans): good.19.1
 
 %description

tests/rpmspec.at
@@ -19,21 +19,11 @@ runroot rpmspec --query \
 [0],
 [good.0.1 0
 good.0.2 8
-good.0.3 0
-good.0.4 8
 good.1.1 104
-good.1.2 100
-good.1.3 104
-good.1.4 100
-good.1.5 104
 good.19.1 1003fa0
 good.2.1 80c
 good.3.1 202
 good.34.1 120a
-good.34.2 1200
-good.34.3 120a
-good.34.4 1200
-good.34.5 120a
 good.4.1 100a
 good.5.1 400
 good.6.1 1000008
@@ -42,6 +32,36 @@ good.8.1 8c
 good.9.1 22
 ],
 [])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(,)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(,): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(,): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(;)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(;): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(;): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
 AT_CLEANUP
 
 AT_SETUP([rpmspec --query Requires(junk)])
@@ -85,6 +105,126 @@ runroot rpmspec --query \
 [1],
 [],
 [error: line 9: Bad Requirename: qualifiers: Requires(junk,pre): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(,pre)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(,pre): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(,pre): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(pre,)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(pre,): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(pre,): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(pre,,postun)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(pre,,postun): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(pre,,postun): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(pre,,junk)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(pre,,junk): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(pre,,junk): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(;pre)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(;pre): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(;pre): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(pre;)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(pre;): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(pre;): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(pre;postun)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(pre;postun): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(pre;postun): bad
+error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
+])
+AT_CLEANUP
+
+AT_SETUP([rpmspec --query Requires(pre;junk)])
+AT_KEYWORDS([rpmspec query])
+AT_CHECK([[
+runroot rpmspec --query \
+  --define 'extra Requires(pre;junk): bad' \
+  --qf '[%{REQUIRENAME} %{REQUIREFLAGS:hex} %{REQUIREVERSION}\n]' \
+  /data/SPECS/test-parsebits.spec
+]],
+[1],
+[],
+[error: line 9: Bad Requirename: qualifiers: Requires(pre;junk): bad
 error: query of specfile /data/SPECS/test-parsebits.spec failed, can't parse
 ])
 AT_CLEANUP