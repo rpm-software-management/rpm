429132cf0597c80077bce10108d2e407640d6317
Panu Matilainen <pmatilai@redhat.com>
2019-05-20 12:57:34 +0300

    Fix regression causing patch/source tags leaking to other macros
    Commit 6dafb24684baa71fe3b89be6404e6ab0362cf316 introduced a regression
    where patch/source (and likely buildroot and docdir too) values
    end up leaking to macros of other tags, due to missing reinitialization
    to NULL in the code.
    
    This only shows up in specs ordered in a specific way which is why
    our test-suite failed to trip it, but it caused eg Fedora attr.spec and
    kernel.spec to fail parse. Hijack the otherwise unused foo.spec to
    create a test-case for this.

--- build/parsePreamble.c
+++ build/parsePreamble.c
@@ -1109,8 +1109,7 @@ static int findPreambleTag(rpmSpec spec,rpmTagVal * tag,
     }
 
     *tag = p->tag;
-    if (p->ismacro && macro)
-	*macro = p->token;
+    *macro = p->ismacro ? p->token : NULL;
     return 0;
 }
 
