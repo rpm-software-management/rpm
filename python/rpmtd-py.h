dbb12c6231707830d8fbdd1fd58249f3eb784f8b
Panu Matilainen <pmatilai@redhat.com>
2019-05-22 14:19:15 +0300

    Drop unused rpmtd python object remnants
    Should've been in commit 71ff0b7e7357061ef993655438260c2ca336edb9
    but better late than never.

--- python/rpmtd-py.h
+++ python/rpmtd-py.h
@@ -1,12 +1,6 @@
 #ifndef H_RPMTD_PY
 #define H_RPMTD_PY
 
-typedef struct rpmtdObject_s rpmtdObject;
-
-extern PyTypeObject rpmtd_Type;
-
-#define rpmtdObject_Check(v)	((v)->ob_type == &rpmtd_Type)
-
 PyObject * rpmtd_ItemAsPyobj(rpmtd td, rpmTagClass tclass);
 PyObject * rpmtd_AsPyobj(rpmtd td);
 
