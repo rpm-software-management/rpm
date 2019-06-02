3acb4734bd9c5b12ebaddc86bf7987735a7c69de
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2019-06-02 20:46:01 +0200

    doScript: Restore good old message when scripts fail
    Following snippet from buildlog is not helpful to user:
    
    […]
    Executing(%prep): /bin/sh -e /var/tmp/rpm-tmp.FqlMc8
    […]
    + exit 1
    error: Exec of /var/tmp/rpm-tmp.FqlMc8 failed (%prep): No such file or directory
    
    Previous rpmbuild versions returned:
    
    […]
    Executing(%prep): /bin/sh -e /var/tmp/rpm-tmp.epLFi2
    […]
    + exit 1
    error: Bad exit status from /var/tmp/rpm-tmp.epLFi2 (%prep)
    
    So let's restore good old version of a message.
    
    References: https://github.com/rpm-software-management/rpm/issues/728
    Fixes: 18e8f4e9b2dd170d090843adf5b5084658d68cf7
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

--- build/build.c
+++ build/build.c
@@ -157,8 +157,8 @@ rpmRC doScript(rpmSpec spec, rpmBuildFlags what, const char *name,
     rpmlog(RPMLOG_NOTICE, _("Executing(%s): %s\n"), name, buildCmd);
     if (rpmfcExec((ARGV_const_t)argv, NULL, sb_stdoutp, 1,
 		  spec->buildSubdir, cmdOut)) {
-	rpmlog(RPMLOG_ERR, _("Exec of %s failed (%s): %s\n"),
-		scriptName, name, strerror(errno));
+	rpmlog(RPMLOG_ERR, _("Bad exit status from %s (%s)\n"),
+		scriptName, name);
 	goto exit;
     }
     rc = RPMRC_OK;
