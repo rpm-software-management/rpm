rpm-software-management/rpm

commit 76c429c3178c965c6517629957a633768132904c

Handle %setup archive unpack exit code centrally and consistently
Previously the exit code handling was missing from uncompressed tar-case.

build/parsePrep.c
@@ -207,11 +207,7 @@ static char *doUntar(rpmSpec spec, uint32_t c, int quietly)
 	}
 	zipper = rpmGetPath(t, NULL);
 	if (needtar) {
-	    rasprintf(&buf, "%s '%s' | %s %s - \n"
-		"STATUS=$?\n"
-		"if [ $STATUS -ne 0 ]; then\n"
-		"  exit $STATUS\n"
-		"fi", zipper, fn, tar, taropts);
+	    rasprintf(&buf, "%s '%s' | %s %s -", zipper, fn, tar, taropts);
 	} else if (needgemspec) {
 	    char *gem = rpmGetPath("%{__gem}", NULL);
 	    char *gemspec = NULL;
@@ -220,20 +216,13 @@ static char *doUntar(rpmSpec spec, uint32_t c, int quietly)
 	    rstrlcpy(gemnameversion, sp->source, strlen(sp->source) - 3);
 	    gemspec = rpmGetPath("%{_builddir}/", gemnameversion, ".gemspec", NULL);
 
-	    rasprintf(&buf, "%s '%s' && %s spec '%s' --ruby > '%s'\n"
-		"STATUS=$?\n"
-		"if [ $STATUS -ne 0 ]; then\n"
-		"  exit $STATUS\n"
-		"fi", zipper, fn, gem, fn, gemspec);
+	    rasprintf(&buf, "%s '%s' && %s spec '%s' --ruby > '%s'",
+			zipper, fn, gem, fn, gemspec);
 
 	    free(gemspec);
 	    free(gem);
 	} else {
-	    rasprintf(&buf, "%s '%s'\n"
-		"STATUS=$?\n"
-		"if [ $STATUS -ne 0 ]; then\n"
-		"  exit $STATUS\n"
-		"fi", zipper, fn);
+	    rasprintf(&buf, "%s '%s'", zipper, fn);
 	}
 	free(zipper);
     } else {
@@ -243,7 +232,11 @@ static char *doUntar(rpmSpec spec, uint32_t c, int quietly)
 exit:
     free(fn);
     free(tar);
-    return buf;
+    return rstrcat(&buf,
+		"\nSTATUS=$?\n"
+		"if [ $STATUS -ne 0 ]; then\n"
+		"  exit $STATUS\n"
+		"fi");
 }
 
 /**

commit d81acda47181c31ff53f0cdfc6be79284484f456

Stop immediately if there is a problem with Source
First issue is that doUntar() when encounters an issue related to the
spec file, returns non-NULL which indicates that no error has happened.
Second is that even if it would return that, parsePrep() is simply
ignores return code.

Also be more consistent with "goto exit;".

Fixes: 76c429c3178c965c6517629957a633768132904c
Fixes: c7a8c1e079d2a166b9848ae6afa1b0cfab1b250a
Fixes: https://github.com/rpm-software-management/rpm/issues/728
Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

build/parsePrep.c
@@ -212,11 +212,11 @@ static char *doUntar(rpmSpec spec, uint32_t c, int quietly)
 
 exit:
     free(tar);
-    return rstrcat(&buf,
-		"\nSTATUS=$?\n"
-		"if [ $STATUS -ne 0 ]; then\n"
-		"  exit $STATUS\n"
-		"fi");
+    return buf ? rstrcat(&buf,
+		    "\nSTATUS=$?\n"
+		    "if [ $STATUS -ne 0 ]; then\n"
+		    "  exit $STATUS\n"
+		    "fi") : NULL;
 }
 
 /**
@@ -493,7 +493,7 @@ int parsePrep(rpmSpec spec)
 
     if (spec->prep != NULL) {
 	rpmlog(RPMLOG_ERR, _("line %d: second %%prep\n"), spec->lineNum);
-	return PART_ERROR;
+	goto exit;
     }
 
     spec->prep = newStringBuf();
@@ -512,6 +512,7 @@ int parsePrep(rpmSpec spec)
 	    appendStringBuf(spec->prep, *lines);
 	}
 	if (rc != RPMRC_OK && !(spec->flags & RPMSPEC_FORCE)) {
+	    res = PART_ERROR;
 	    goto exit;
 	}
     }

tests/rpmbuild.at
@@ -1503,3 +1503,20 @@ rpmlib(RichDependencies) <= 4.12.0-1
 ],
 [ignore])
 AT_CLEANUP
+
+# ------------------------------
+# Check that rpmbuild aborts with missing Source
+AT_SETUP([rpmbuild -ba missing source])
+AT_KEYWORDS([build])
+AT_CHECK_UNQUOTED([
+rm -rf ${TOPDIR}
+AS_MKDIR_P(${TOPDIR}/SOURCES)
+
+run rpmbuild \
+  -bb "${abs_srcdir}"/data/SPECS/hello.spec
+],
+[1],
+[],
+[error: Bad source: ${TOPDIR}/SOURCES/hello-1.0.tar.gz: No such file or directory
+])
+AT_CLEANUP