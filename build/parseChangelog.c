rpm-software-management/rpm

commit bb6786788cb1b1d7f2d583c9a5b7767cbda1154e

Port parseChangelog() to use parseLines(), no functional changes

build/parseChangelog.c
@@ -300,7 +300,7 @@ exit:
 
 int parseChangelog(rpmSpec spec)
 {
-    int nextPart, rc, res = PART_ERROR;
+    int res = PART_ERROR;
     ARGV_t sb = NULL;
 
     if (headerIsEntry(spec->packages->header, RPMTAG_CHANGELOGTIME)) {
@@ -308,28 +308,12 @@ int parseChangelog(rpmSpec spec)
 	goto exit;
     }
     
-    /* There are no options to %changelog */
-    if ((rc = readLine(spec, STRIP_COMMENTS)) > 0) {
-	res = PART_NONE;
+    if ((res = parseLines(spec, STRIP_COMMENTS, &sb, NULL)) == PART_ERROR)
 	goto exit;
-    } else if (rc < 0) {
-	goto exit;
-    }
-    
-    while (! (nextPart = isPart(spec->line))) {
-	argvAdd(&sb, spec->line);
-	if ((rc = readLine(spec, STRIP_COMMENTS)) > 0) {
-	    nextPart = PART_NONE;
-	    break;
-	} else if (rc < 0) {
-	    goto exit;
-	}
-    }
 
     if (sb && addChangelog(spec->packages->header, sb)) {
 	goto exit;
     }
-    res = nextPart;
 
 exit:
     argvFree(sb);

commit 456e25d51a349a4d993b123b16c7f566d8c0bd6c

Return non-zero exit status if changelog order fails
Fixes a regression introduced in commit bb67867.

build/parseChangelog.c
@@ -314,6 +314,7 @@ int parseChangelog(rpmSpec spec)
 	goto exit;
 
     if (sb && addChangelog(spec->packages->header, sb)) {
+	res = PART_ERROR;
 	goto exit;
     }
 