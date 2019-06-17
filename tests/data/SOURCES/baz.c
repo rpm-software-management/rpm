7a864bffc5c6873b1c08a00ab12df1aa202ba63d
Mark Wielaard <mark@klomp.org>
2019-06-17 11:23:23 +0200

    Add some debugedit tests.
    debugedit didn't have any direct tests, it was only tested indirectly
    through some other debuginfo testcases.  So add some testcases that
    just test debugedit functionality directly.
    
    The tests create different kinds of ELF files (object files, partially
    linked files and executables) and run debugedit path replacements on
    them, checking that the resulting .debug_str, .debug_info and .debug_line
    sections look sane.
    
    Testcases verified against various different gcc and binutils versions.
    gcc 4.8 and gcc 6 generate slightly different .debuginfo
    
    Also add debugedit --version. Which makes it easy to see we are
    testing the correct version/install of debugedit in rpmtests.log.
    
    Adjust autotest paths to include rpmlibexecdir, where debuginfo
    is installed.  Note that rpmlibexecdir != libexecdir (see rpm.am).

---
+++ tests/data/SOURCES/baz.c
@@ -0,0 +1,10 @@
+#include "foobar.h"
+
+int
+main ()
+{
+  int res;
+  res = foo ();
+  res = bar (res);
+  return res + number - NUMBER;
+}
---
+++ tests/data/SOURCES/foobar.h
@@ -0,0 +1,6 @@
+#define NUMBER 42
+
+extern int number;
+
+extern int foo (void);
+extern int bar (int);
