rpm-software-management/rpm

commit 1d055ae1df90fe54cf5c056de085cd987f0f6bbf

Print an error for expressions with missing operands
Expressions like '5 +' did not print an error message before.

rpmio/expression.c
@@ -406,6 +406,11 @@ static Value doPrimary(ParseState state)
 
     v = valueMakeInteger(! v->data.i);
     break;
+
+  case TOK_EOF:
+    exprErr(&state, _("unexpected end of expression"), NULL);
+    goto err;
+
   default:
     goto err;
     break;

commit 0b3573604563b2ef2c2cd089b4ec760449575ff4

Fix type error introduced in commit 1d055ae1df90fe54cf5c056de085cd987f0f6bbf
"state" is a pointer in this function, we don't want a pointer to it.
Fixes garbage getting printed in the error message. Add a testcase too.

rpmio/expression.c
@@ -408,7 +408,7 @@ static Value doPrimary(ParseState state)
     break;
 
   case TOK_EOF:
-    exprErr(&state, _("unexpected end of expression"), NULL);
+    exprErr(state, _("unexpected end of expression"), NULL);
     goto err;
 
   default:

tests/rpmmacro.at
@@ -273,6 +273,7 @@ AT_CHECK([
 runroot rpm --eval '%{expr:a*1}'
 runroot rpm --eval '%{expr:(5+1)*4)}'
 runroot rpm --eval '%{expr:a=!b}'
+runroot rpm --eval '%{expr:4+}'
 ],
 [1],
 [],
@@ -281,6 +282,7 @@ error: syntax error in expression: (5+1)*4)
 error:                                    ^
 error: syntax error while parsing ==: a=!b
 error:                                  ^
+error: unexpected end of expression: 4+
 ])
 AT_CLEANUP
 