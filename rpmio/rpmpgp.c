998b516b8bcca5e38cdba3e93045bf7c9e2293ad
Panu Matilainen <pmatilai@redhat.com>
2019-10-31 11:56:36 +0200

    Replace use of obsolete ctime() with strftime()
    POSIX.1-2008 marked ctime() as obsolete and recommends strftime() instead.
    We get two flies on one stroke as ctime() is also classified "dangerous"
    by LGTM due to not being thread-safe.
    
    strftime(..., "%c"...) isn't exactly the same as ctime() but is consistent
    with what we use elsewhere and sufficient for debug purposes anyway.

--- rpmio/rpmpgp.c
+++ rpmio/rpmpgp.c
@@ -254,8 +254,11 @@ static void pgpPrtTime(const char * pre, const uint8_t *p, size_t plen)
     if (pre && *pre)
 	fprintf(stderr, "%s", pre);
     if (plen == 4) {
+	char buf[1024];
 	time_t t = pgpGrab(p, plen);
-	fprintf(stderr, " %-24.24s(0x%08x)", ctime(&t), (unsigned)t);
+	struct tm _tm, *tms = localtime_r(&t, &_tm);
+	if (strftime(buf, sizeof(buf), "%c", tms) > 0)
+	    fprintf(stderr, " %-24.24s(0x%08x)", buf, (unsigned)t);
     } else {
 	pgpPrtHex("", p+1, plen-1);
     }
