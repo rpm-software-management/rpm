rpm-software-management/rpm

commit 708e61307bc3fd027b016fdf5a1d1a5274c1843c

Suppress inhibition lock warning message when DBus service is not available
The message may just confuse users if DBus is not running as default,
e.g. single-user mode. We suppress it when DBus is not available,
which is done by checking two cases:
socket does not exist (DBUS_ERROR_FILE_NOT_FOUND), or
unable to connect to server (DBUS_ERROR_NO_SERVER).
Note that this is an approximate but not an exact way to detect
whether DBus service should be running in the environment or not.

plugins/systemd_inhibit.c
@@ -52,7 +52,9 @@ static int inhibit(void)
 	dbus_message_unref(reply);
     }
     
-    if (dbus_error_is_set(&err)) {
+    if (dbus_error_is_set(&err)
+	&& !dbus_error_has_name(&err, DBUS_ERROR_NO_SERVER)
+	&& !dbus_error_has_name(&err, DBUS_ERROR_FILE_NOT_FOUND)) {
 	rpmlog(RPMLOG_WARNING,
 	       "Unable to get systemd shutdown inhibition lock: %s\n",
 		err.message);

commit 87cf27ff6e1e5e716fdffa1fbd5ccccbcf358e38

Fix a minor memory leak on suppressed inhibition lock warning message
Commit 708e61307bc3fd027b016fdf5a1d1a5274c1843c introduced a memory leak
on the error object: if the message is suppressed then the error object
is never freed. Test for the suppression conditions separately to fix.

plugins/systemd_inhibit.c
@@ -52,12 +52,14 @@ static int inhibit(void)
 	dbus_message_unref(reply);
     }
     
-    if (dbus_error_is_set(&err)
-	&& !dbus_error_has_name(&err, DBUS_ERROR_NO_SERVER)
-	&& !dbus_error_has_name(&err, DBUS_ERROR_FILE_NOT_FOUND)) {
-	rpmlog(RPMLOG_WARNING,
+    if (dbus_error_is_set(&err)) {
+	if (!dbus_error_has_name(&err, DBUS_ERROR_NO_SERVER) &&
+	    !dbus_error_has_name(&err, DBUS_ERROR_FILE_NOT_FOUND))
+	{
+	    rpmlog(RPMLOG_WARNING,
 	       "Unable to get systemd shutdown inhibition lock: %s\n",
 		err.message);
+	}
 	dbus_error_free(&err);
     }
 