rpm-software-management/rpm

commit 27ea3f8624560bd158fc7bc801639310a0ffab10

Don't reload to legacy image on headerCopy()
AFAICT the only place that even remotely cares about the reload
is rpmdb rebuild, for others this just messes up things. Do the
reload manually in rpmdb and otherwise just leave it alone. This
allows headerCopy() to actually be used for, you know, copying
headers. Generally, that is.

lib/rpmdb.c
@@ -2555,8 +2555,8 @@ int rpmdbRebuild(const char * prefix, rpmts ts,
 
 	    /* Deleted entries are eliminated in legacy headers by copy. */
 	    if (headerIsEntry(h, RPMTAG_HEADERIMAGE)) {
-		Header nh = headerCopy(h);
-		rc = rpmdbAdd(newdb, nh);
+		Header nh = headerReload(headerCopy(h), RPMTAG_HEADERIMAGE);
+		rc = rpmdbAdd(newdb, h);
 		headerFree(nh);
 	    } else {
 		rc = rpmdbAdd(newdb, h);

lib/headerutil.c
@@ -31,7 +31,7 @@ Header headerCopy(Header h)
     }
     headerFreeIterator(hi);
 
-    return headerReload(nh, RPMTAG_HEADERIMAGE);
+    return nh;
 }
 
 void headerCopyTags(Header headerFrom, Header headerTo, 

commit 7c1c805d3098fdba092a88d1fbc63cbac312cfa8

Fix regression on v3 package handling on database rebuild
Introduced in commit 27ea3f8624560bd158fc7bc801639310a0ffab10, the
wrong header is being added in case of v3 packages.

Fixes: #1017

lib/rpmdb.c
@@ -2557,7 +2557,7 @@ int rpmdbRebuild(const char * prefix, rpmts ts,
 	    /* Deleted entries are eliminated in legacy headers by copy. */
 	    if (headerIsEntry(h, RPMTAG_HEADERIMAGE)) {
 		Header nh = headerReload(headerCopy(h), RPMTAG_HEADERIMAGE);
-		rc = rpmdbAdd(newdb, h);
+		rc = rpmdbAdd(newdb, nh);
 		headerFree(nh);
 	    } else {
 		rc = rpmdbAdd(newdb, h);