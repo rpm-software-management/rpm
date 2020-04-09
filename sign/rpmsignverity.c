b35dda1f19adf9e5db458a600d0688bd59a8a91c
Jes Sorensen <jsorensen@fb.com>
2020-04-09 12:58:17 -0400

    rpmsignverity: Add verity signature headers to the package
    This adds the array of verity signatures, and a signature length
    header. We use 4K block for the Merkle tree, and rely on the kernel
    doing the right thing.
    
    Signed-off-by: Jes Sorensen <jsorensen@fb.com>

--- sign/rpmsignverity.c
+++ sign/rpmsignverity.c
@@ -33,23 +33,66 @@ static int rpmVerityRead(void *opaque, void *buf, size_t size)
 	return retval;
 }
 
+static char *rpmVeritySignFile(rpmfi fi, size_t *sig_size, char *key,
+			       char *keypass, char *cert)
+{
+    struct libfsverity_merkle_tree_params params;
+    struct libfsverity_signature_params sig_params;
+    struct libfsverity_digest *digest = NULL;
+    rpm_loff_t file_size;
+    char *digest_hex, *sig_hex = NULL;
+    uint8_t *sig;
+    int status;
+
+    file_size = rpmfiFSize(fi);
+
+    memset(&params, 0, sizeof(struct libfsverity_merkle_tree_params));
+    params.version = 1;
+    params.hash_algorithm = FS_VERITY_HASH_ALG_SHA256;
+    params.block_size = RPM_FSVERITY_BLKSZ;
+    params.salt_size = 0 /* salt_size */;
+    params.salt = NULL /* salt */;
+    params.file_size = file_size;
+    status = libfsverity_compute_digest(fi, rpmVerityRead, &params, &digest);
+    if (status) {
+	rpmlog(RPMLOG_DEBUG, _("failed to compute digest\n"));
+	goto out;
+    }
+
+    digest_hex = pgpHexStr(digest->digest, digest->digest_size);
+    rpmlog(RPMLOG_DEBUG, _("digest(%i): %s\n"),
+	   digest->digest_size, digest_hex);
+    free(digest_hex);
+
+    memset(&sig_params, 0, sizeof(struct libfsverity_signature_params));
+    sig_params.keyfile = key;
+    sig_params.certfile = cert;
+    if (libfsverity_sign_digest(digest, &sig_params, &sig, sig_size)) {
+	rpmlog(RPMLOG_DEBUG, _("failed to sign digest\n"));
+	goto out;
+    }
+
+    sig_hex = pgpHexStr(sig, *sig_size + 1);
+ out:
+    free(digest);
+    free(sig);
+    return sig_hex;
+}
+
 rpmRC rpmSignVerity(FD_t fd, Header sigh, Header h, char *key,
 		    char *keypass, char *cert)
 {
-    int rc, status;
+    int rc;
     FD_t gzdi;
     rpmfiles files = NULL;
     rpmfi fi = NULL;
     rpmts ts = rpmtsCreate();
-    struct libfsverity_digest *digest = NULL;
-    struct libfsverity_merkle_tree_params params;
-    struct libfsverity_signature_params sig_params;
+    struct rpmtd_s td;
     rpm_loff_t file_size;
     off_t offset = Ftell(fd);
     const char *compr;
     char *rpmio_flags = NULL;
-    char *digest_hex;
-    uint8_t *sig;
+    char *sig_hex;
     size_t sig_size;
 
     Fseek(fd, 0, SEEK_SET);
@@ -75,43 +118,40 @@ rpmRC rpmSignVerity(FD_t fd, Header sigh, Header h, char *key,
     fi = rpmfiNewArchiveReader(gzdi, files,
 			       RPMFI_ITER_READ_ARCHIVE_OMIT_HARDLINKS);
 
-    while (rpmfiNext(fi) >= 0) {
-	if (!S_ISREG(rpmfiFMode(fi)))
-	    continue;
-	file_size = rpmfiFSize(fi);
-	rpmlog(RPMLOG_DEBUG, _("file: %s, (size %li)\n"),
-	       rpmfiFN(fi), file_size);
+    /*
+     * Should this be sigh from the cloned fd or the sigh we received?
+     */
+    headerDel(sigh, RPMSIGTAG_VERITYSIGNATURES);
 
-	memset(&params, 0, sizeof(struct libfsverity_merkle_tree_params));
-	params.version = 1;
-	params.hash_algorithm = FS_VERITY_HASH_ALG_SHA256;
-	params.block_size = sysconf(_SC_PAGESIZE);
-	params.salt_size = 0 /* salt_size */;
-	params.salt = NULL /* salt */;
-	params.file_size = file_size;
-	status = libfsverity_compute_digest(fi, rpmVerityRead,
-					    &params, &digest);
-	if (!status) {
-	    digest_hex = pgpHexStr(digest->digest, digest->digest_size);
-	    rpmlog(RPMLOG_DEBUG, _("digest(%i): %s\n"),
-		   digest->digest_size, digest_hex);
-	    free(digest_hex);
-	}
-	memset(&sig_params, 0, sizeof(struct libfsverity_signature_params));
-	sig_params.keyfile = key;
-	sig_params.certfile = cert;
-	if (libfsverity_sign_digest(digest, &sig_params, &sig, &sig_size)) {
-	    rpmlog(RPMLOG_DEBUG, _("failed to sign digest\n"));
+    rpmtdReset(&td);
+    td.tag = RPMSIGTAG_VERITYSIGNATURES;
+    td.type = RPM_STRING_ARRAY_TYPE;
+    td.count = 1;
+
+    while (rpmfiNext(fi) >= 0) {
+	file_size = rpmfiFSize(fi);
+
+	rpmlog(RPMLOG_DEBUG, _("file: %s, (size %li, link %s, idx %i)\n"),
+	       rpmfiFN(fi), file_size, rpmfiFLink(fi), rpmfiFX(fi));
+
+	sig_hex = rpmVeritySignFile(fi, &sig_size, key, keypass, cert);
+	td.data = &sig_hex;
+	rpmlog(RPMLOG_DEBUG, _("digest signed, len: %li\n"), sig_size);
+#if 0
+	rpmlog(RPMLOG_DEBUG, _("digest: %s\n"), (char *)sig_hex);
+#endif
+	if (!headerPut(sigh, &td, HEADERPUT_APPEND)) {
+	    rpmlog(RPMLOG_ERR, _("headerPutString failed\n"));
 	    rc = RPMRC_FAIL;
 	    goto out;
 	}
-	rpmlog(RPMLOG_DEBUG, _("digest signing success\n"));
-
-	free(digest);
-	free(sig);
+	free(sig_hex);
     }
 
-out:
+    rpmlog(RPMLOG_DEBUG, _("sigh size: %i\n"), headerSizeof(sigh, 0));
+
+    rc = RPMRC_OK;
+ out:
     Fseek(fd, offset, SEEK_SET);
 
     rpmfilesFree(files);
