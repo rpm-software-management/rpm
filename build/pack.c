rpm-software-management/rpm

commit 78f61f273fb2e84156d8bbe6d4e1a9e0828600cb

Refactor package set checking out of packageBinaries()
Now that we can, split the "lets run something on generated packages"
check out of packageBinaries(), it doesn't belong there at all. No
functional changes other fix attempt to check non-built packages
which return with no filename but RPMRC_OK from packageBinary().

build/pack.c
@@ -622,6 +622,27 @@ static rpmRC checkPackages(char *pkgcheck)
     return RPMRC_OK;
 }
 
+static rpmRC checkPackageSet(Package pkgs)
+{
+    char *pkglist = NULL;
+    char *pkgcheck_set = NULL;
+
+    for (Package pkg = pkgs; pkg != NULL; pkg = pkg->next) {
+	if (pkg->filename)
+	    rstrscat(&pkglist, pkg->filename, " ", NULL);
+    }
+
+    pkgcheck_set = rpmExpand("%{?_build_pkgcheck_set} ", pkglist,  NULL);
+
+    /* run only if _build_pkgcheck_set is defined */
+    if (pkgcheck_set[0] != ' ')
+	checkPackages(pkgcheck_set);
+
+    free(pkgcheck_set);
+    free(pkglist);
+    return RPMRC_OK;
+}
+
 /* watchout, argument is modified */
 static rpmRC ensureDir(char *binRpm)
 {
@@ -715,33 +736,18 @@ static rpmRC packageBinary(rpmSpec spec, Package pkg, const char *cookie, int ch
 
 rpmRC packageBinaries(rpmSpec spec, const char *cookie, int cheating)
 {
-    rpmRC rc;
+    rpmRC rc = RPMRC_OK;
     Package pkg;
-    char *pkglist = NULL;
 
     for (pkg = spec->packages; pkg != NULL; pkg = pkg->next) {
 	rc = packageBinary(spec, pkg, cookie, cheating, &pkg->filename);
-	if (rc == RPMRC_OK) {
-	    rstrcat(&pkglist, pkg->filename);
-	    rstrcat(&pkglist, " ");
-	}
-	if (rc != RPMRC_OK) {
-	    pkglist = _free(pkglist);
-	    return rc;
-	}
     }
 
     /* Now check the package set if enabled */
-    if (pkglist != NULL) {
-	char *pkgcheck_set = rpmExpand("%{?_build_pkgcheck_set} ", pkglist,  NULL);
-	if (pkgcheck_set[0] != ' ') {	/* run only if _build_pkgcheck_set is defined */
-	    checkPackages(pkgcheck_set);
-	}
-	free(pkgcheck_set);
-	pkglist = _free(pkglist);
-    }
-    
-    return RPMRC_OK;
+    if (rc == RPMRC_OK)
+	checkPackageSet(spec->packages);
+
+    return rc;
 }
 
 rpmRC packageSources(rpmSpec spec, char **cookie)

commit f44fb3c4291030262ce7c0fe25be6e3893413ece

Fail the build if %_build_pkgcheck_set failed
RPM build did not fail if rpmlint (%_build_pkgcheck_set) failed when checking binary RPMs
(it did fail correctly when rpmlint failed when checking SRPMs)

If %_nonzero_exit_pkgcheck_terminate_build is true, then the build must fail, otherwise it must not.
This regressed and the build never failed.

Probably fixes regression introduced by 78f61f273 ("Refactor package set checking out of packageBinaries()")

build/pack.c
@@ -618,6 +618,7 @@ static rpmRC checkPackageSet(Package pkgs)
 {
     char *pkglist = NULL;
     char *pkgcheck_set = NULL;
+    rpmRC rc = RPMRC_OK;
 
     for (Package pkg = pkgs; pkg != NULL; pkg = pkg->next) {
 	if (pkg->filename)
@@ -628,11 +629,11 @@ static rpmRC checkPackageSet(Package pkgs)
 
     /* run only if _build_pkgcheck_set is defined */
     if (pkgcheck_set[0] != ' ')
-	checkPackages(pkgcheck_set);
+	rc = checkPackages(pkgcheck_set);
 
     free(pkgcheck_set);
     free(pkglist);
-    return RPMRC_OK;
+    return rc;
 }
 
 /* watchout, argument is modified */
@@ -782,7 +783,7 @@ rpmRC packageBinaries(rpmSpec spec, const char *cookie, int cheating)
 
     /* Now check the package set if enabled */
     if (rc == RPMRC_OK)
-	checkPackageSet(spec->packages);
+	rc = checkPackageSet(spec->packages);
 
     free(tasks);
 