ce79e166fef8aee66b2dd0dab14d0cf7b7bf2aab
Panu Matilainen <pmatilai@redhat.com>
2020-10-09 14:02:20 +0300

    Wrap getopt() usage into internal helper
    Avoids code duplication (glibc quirks and whatnot) between macro and lua
    option parsing, isolate the global opt* variable accesses to one spot.
    Makes it easier to ensure identical behavior between the users if/when
    something changes.
    
    No behavior changes intended.

--- rpmio/rpmmacro_internal.h
+++ rpmio/rpmmacro_internal.h
@@ -19,6 +19,11 @@ extern "C" {
 RPM_GNUC_INTERNAL
 const char *findMacroEnd(const char *str);
 
+typedef int (*rgetoptcb)(int c, const char *oarg, int oint, void *data);
+
+RPM_GNUC_INTERNAL
+int rgetopt(int argc, char * const argv[], const char *opts,
+		rgetoptcb callback, void *data);
 
 #ifdef __cplusplus
 }
