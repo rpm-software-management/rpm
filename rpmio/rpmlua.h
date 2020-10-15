383885026204ffffca97db577e8235b3d95e041f
Panu Matilainen <pmatilai@redhat.com>
2020-10-15 17:10:26 +0300

    Lose the internal Lua variable API
    This whole lot has been barely used at all since its addition in 2004,
    is totally undocumented and instead of being helpful, it only ends up
    being more code using some strange add-on API. It also quite effectively
    prevents anybody from doing anything fancy with Lua because you'd have to
    wrap it up somewhere in this intermediate API.
    
    People are better off using native Lua API for data manipulation, byebye.

--- rpmio/rpmlua.h
+++ rpmio/rpmlua.h
@@ -3,15 +3,7 @@
 
 #include <rpm/argv.h>
 
-typedef enum rpmluavType_e {
-    RPMLUAV_NIL		= 0,
-    RPMLUAV_STRING	= 1,
-    RPMLUAV_NUMBER	= 2
-} rpmluavType;
-
-
 typedef struct rpmlua_s * rpmlua;
-typedef struct rpmluav_s * rpmluav;
 
 #ifdef __cplusplus
 extern "C" {
@@ -38,29 +30,6 @@ char *rpmluaPopPrintBuffer(rpmlua lua);
 void rpmluaPushPrintBuffer(rpmlua lua);
 
 void rpmluaSetNextFileFunc(char *(*func)(void *), void *funcParam);
-
-void rpmluaGetVar(rpmlua lua, rpmluav var);
-void rpmluaSetVar(rpmlua lua, rpmluav var);
-void rpmluaDelVar(rpmlua lua, const char *key, ...);
-int rpmluaVarExists(rpmlua lua, const char *key, ...);
-void rpmluaPushTable(rpmlua lua, const char *key, ...);
-void rpmluaPop(rpmlua lua);
-
-rpmluav rpmluavNew(void);
-rpmluav rpmluavFree(rpmluav var);
-void rpmluavSetListMode(rpmluav var, int flag);
-void rpmluavSetKey(rpmluav var, rpmluavType type, const void *value);
-void rpmluavSetValue(rpmluav var, rpmluavType type, const void *value);
-void rpmluavGetKey(rpmluav var, rpmluavType *type, void **value);
-void rpmluavGetValue(rpmluav var, rpmluavType *type, void **value);
-
-/* Optional helpers for numbers. */
-void rpmluavSetKeyNum(rpmluav var, double value);
-void rpmluavSetValueNum(rpmluav var, double value);
-double rpmluavGetKeyNum(rpmluav var);
-double rpmluavGetValueNum(rpmluav var);
-int rpmluavKeyIsNum(rpmluav var);
-int rpmluavValueIsNum(rpmluav var);
 
 #ifdef __cplusplus
 }
