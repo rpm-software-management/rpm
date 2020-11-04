ea6431c521e893e6350b17a924bff6ab4b95390d
Panu Matilainen <pmatilai@redhat.com>
2020-11-04 11:24:56 +0200

    Make builtin macros honor generic macro conditionals

--- rpmio/macro.c
+++ rpmio/macro.c
@@ -1508,25 +1508,6 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
 	    }
 	}
 
-	/* Expand builtin macros */
-	if (me && (me->flags & ME_BUILTIN)) {
-	    int havearg = (me->flags & ME_HAVEARG) ? 1 : 0;
-	    if (havearg != (g != NULL)) {
-		mbErr(mb, 1, "%%%s: %s\n", me->name, havearg ?
-			_("argument expected") : _("unexpected argument"));
-		continue;
-	    }
-	    if (me->flags & ME_PARSE) {
-		parseFunc parse = me->func;
-		s = parse(mb, me, se);
-	    } else {
-		macroFunc func = me->func;
-		func(mb, me, g, gn);
-		s = se;
-	    }
-	    continue;
-	}
-
 	/* XXX Special processing for flags and existance test */
 	if (*f == '-' || chkexist) {
 	    if ((me == NULL && !negate) ||	/* Without existance, skip %{?...} */
@@ -1548,6 +1529,25 @@ expandMacro(MacroBuf mb, const char *src, size_t slen)
 	    /* XXX hack to permit non-overloaded %foo to be passed */
 	    c = '%';	/* XXX only need to save % */
 	    mbAppend(mb, c);
+	    continue;
+	}
+
+	/* Expand builtin macros */
+	if (me->flags & ME_BUILTIN) {
+	    int havearg = (me->flags & ME_HAVEARG) ? 1 : 0;
+	    if (havearg != (g != NULL)) {
+		mbErr(mb, 1, "%%%s: %s\n", me->name, havearg ?
+			_("argument expected") : _("unexpected argument"));
+		continue;
+	    }
+	    if (me->flags & ME_PARSE) {
+		parseFunc parse = me->func;
+		s = parse(mb, me, se);
+	    } else {
+		macroFunc func = me->func;
+		func(mb, me, g, gn);
+		s = se;
+	    }
 	    continue;
 	}
 
