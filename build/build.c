rpm-software-management/rpm

commit c223d84fbf1ebb7a862f297e65878a2a78653e8e

Eliminate use of obsolete gethostbyname() function
We only actually used gethostbyname() for canonicalizing buildhost,
convert that to use getaddrinfo() instead, which actually has an
option for retrieving exactly what we want.

The other "use" was to initialize name services, but as we don't need
or use hostnames for any operation, we can just as well drop it. User
and group names are what we care about.

build/build.c
@@ -7,6 +7,8 @@
 
 #include <errno.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
 #include <time.h>
 
@@ -42,21 +44,26 @@ static rpm_time_t getBuildTime(void)
 static char * buildHost(void)
 {
     char* hostname;
-    struct hostent *hbn;
     char *bhMacro;
 
     bhMacro = rpmExpand("%{?_buildhost}", NULL);
     if (strcmp(bhMacro, "") != 0) {
         rasprintf(&hostname, "%s", bhMacro);
     } else {
-        hostname = rcalloc(1024, sizeof(*hostname));
-        (void) gethostname(hostname, 1024);
-        hbn = gethostbyname(hostname);
-        if (hbn)
-            strcpy(hostname, hbn->h_name);
-        else
-            rpmlog(RPMLOG_WARNING,
+	hostname = rcalloc(NI_MAXHOST + 1, sizeof(*hostname));
+	if (gethostname(hostname, NI_MAXHOST) == 0) {
+	    struct addrinfo *ai, hints;
+	    memset(&hints, 0, sizeof(hints));
+	    hints.ai_flags = AI_CANONNAME;
+
+	    if (getaddrinfo(hostname, NULL, &hints, &ai) == 0) {
+		strcpy(hostname, ai->ai_canonname);
+	    } else {
+		rpmlog(RPMLOG_WARNING,
                     _("Could not canonicalize hostname: %s\n"), hostname);
+	    }
+	    freeaddrinfo(ai);
+	}
     }
     free(bhMacro);
     return(hostname);

configure.ac
@@ -621,7 +621,7 @@ AC_CHECK_FUNCS([secure_getenv __secure_getenv])
 
 AC_CHECK_FUNCS(
    [mkstemp getcwd basename dirname realpath setenv unsetenv regcomp lchown \
-    utimes getline localtime_r statvfs ],
+    utimes getline localtime_r statvfs getaddrinfo ],
    [], [AC_MSG_ERROR([function required by rpm])])
 
 AC_LIBOBJ(fnmatch)

lib/rpmug.c
@@ -174,7 +174,6 @@ static void loadLibs(void)
     endpwent();
     (void) getgrnam(GID_0_GROUP);
     endgrent();
-    (void) gethostbyname("localhost");
 }
 
 int rpmugInit(void)

commit d911636a3f2112aaf0495e9b6f728ec0f760bcab

Fix brown paperbag regression in c223d84fbf1ebb7a862f297e65878a2a78653e8e
Only call free on the success path, getaddrinfo() is like most this
type functions and only allocates on success and so, in failure we'd
end up freeing an uninitialized pointer.
Reported by Scott Andrews on rpm-list.

build/build.c
@@ -58,11 +58,11 @@ static char * buildHost(void)
 
 	    if (getaddrinfo(hostname, NULL, &hints, &ai) == 0) {
 		strcpy(hostname, ai->ai_canonname);
+		freeaddrinfo(ai);
 	    } else {
 		rpmlog(RPMLOG_WARNING,
                     _("Could not canonicalize hostname: %s\n"), hostname);
 	    }
-	    freeaddrinfo(ai);
 	}
     }
     free(bhMacro);