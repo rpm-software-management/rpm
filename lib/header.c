22106f5d33628515d22c09c1c15dfd2217535116
Demi M. Obenour <demiobenour@gmail.com>
2020-12-29 22:58:51 -0500

    Check that len is in range before using it
    This prevents integer overflows.

--- lib/header.c
+++ lib/header.c
@@ -298,9 +298,9 @@ static rpmRC hdrblobVerifyInfo(hdrblob blob, char **emsg)
 	/* Verify the data actually fits */
 	len = dataLength(info.type, ds + info.offset,
 			 info.count, 1, ds + blob->dl);
-	end = info.offset + len;
-	if (hdrchkRange(blob->dl, end) || len <= 0)
+	if (hdrchkRange(blob->dl - info.offset, len))
 	    goto err;
+	end = info.offset + len;
 	if (blob->regionTag) {
 	    /*
 	     * Verify that the data does not overlap the region trailer.  The
