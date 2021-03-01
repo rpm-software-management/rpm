rpm-software-management/rpm

commit fb5888417727edbd8ce1b389b1b6ee9ac899d8e4

Fix a minor memory leak on failed sqlite database open (RhBug:1896301)
Curiously, sqlite allocates resources that need freeing even in case
of failure to open. Quoting from https://www.sqlite.org/c3ref/open.html:

> Whether or not an error occurs when it is opened, resources associated
> with the database connection handle should be released by passing it to
> sqlite3_close() when it is no longer required.

I disagree, but as it's documented behavior there's no point filing a
bug. So lets close the non-open connection and chug on.

lib/backend/sqlite.c
@@ -145,6 +145,8 @@ static int sqlite_init(rpmdb rdb, const char * dbhome)
 	    xx = sqlite3_open_v2(dbfile, &sdb, flags, NULL);
 	    /* Attempt to create if missing, discarding OPEN_READONLY (!) */
 	    if (xx == SQLITE_CANTOPEN && (flags & SQLITE_OPEN_READONLY)) {
+		/* Sqlite allocates resources even on failure to open (!) */
+		sqlite3_close(sdb);
 		flags &= ~SQLITE_OPEN_READONLY;
 		flags |= (SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
 		retry_open++;

commit 36aeb17beee459dea1b204aa28646e34013ea7f2

Fix regression causing access to already closed sqlite handle
Commit fb5888417727edbd8ce1b389b1b6ee9ac899d8e4 introduced a regression
where on database open failure we end up accessing the already freed
handle when trying to report the error.

Now that we're no longer implicitly creating non-existent databases,
just simplify the whole logic to get rid of it. 4.16.x branch will need
a different fix, maybe use sqlite3_errstr() instead of _errmsg().

Reported and initial patch by Demi M. Obenour.

lib/backend/sqlite.c
@@ -134,24 +134,17 @@ static int sqlite_init(rpmdb rdb, const char * dbhome)
     if (rdb->db_dbenv == NULL) {
 	dbfile = rpmGenPath(dbhome, rdb->db_ops->path, NULL);
 	sqlite3 *sdb = NULL;
-	int xx, flags = 0;
-	int retry_open = 1;
+	int flags = 0;
 	if ((rdb->db_mode & O_ACCMODE) == O_RDONLY)
 	    flags |= SQLITE_OPEN_READONLY;
 	else
 	    flags |= (SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
 
-	while (retry_open--) {
-	    xx = sqlite3_open_v2(dbfile, &sdb, flags, NULL);
-	    if (xx == SQLITE_CANTOPEN) {
-		/* Sqlite allocates resources even on failure to open (!) */
-		sqlite3_close(sdb);
-	    }
-	}
-
-	if (xx != SQLITE_OK) {
+	if (sqlite3_open_v2(dbfile, &sdb, flags, NULL) != SQLITE_OK ) {
 	    rpmlog(RPMLOG_ERR, _("Unable to open sqlite database %s: %s\n"),
 		    dbfile, sqlite3_errmsg(sdb));
+	    /* Sqlite allocates resources even on failure to open (!) */
+	    sqlite3_close(sdb);
 	    rc = 1;
 	    goto exit;
 	}