2882bd073e331ab99e968dd6a36c1cf3a28ac0c0
Jerry James <loganjerry@gmail.com>
2021-03-31 20:57:08 -0600

    Add type hints to the Python interface

--- python/rpm/__init__.py
+++ python/rpm/__init__.py
@@ -34,6 +34,9 @@ is a transaction set tied to an alternate database, which
 is very useful for resolving dependencies.
 """
 
+from __future__ import annotations
+from collections.abc import Iterable
+from typing import Any, AnyStr, Final, List, Optional, Tuple, Union
 import warnings
 from rpm._rpm import *
 from rpm.transaction import *
@@ -43,21 +46,21 @@ _RPMVSF_NOHEADER = _rpm._RPMVSF_NOHEADER
 _RPMVSF_NOPAYLOAD = _rpm._RPMVSF_NOPAYLOAD
 _RPMVSF_NOSIGNATURES = _rpm._RPMVSF_NOSIGNATURES
 
-__version__ = _rpm.__version__
-__version_info__ = tuple(__version__.split('.'))
+__version__: Final[str] = _rpm.__version__
+__version_info__: Final[Tuple[str, ...]] = tuple(__version__.split('.'))
 
 # backwards compatibility + give the same class both ways
 ts = TransactionSet
 
 
-def headerLoad(*args, **kwds):
+def headerLoad(*args: AnyFDOrHdr, **kwds: AnyFDOrHdr) -> hdr:
     """DEPRECATED! Use rpm.hdr() instead."""
     warnings.warn("Use rpm.hdr() instead.", DeprecationWarning, stacklevel=2)
     return hdr(*args, **kwds)
 
 
-def _doHeaderListFromFD(rpm_fd, retrofit):
-    hlist = []
+def _doHeaderListFromFD(rpm_fd: AnyFD, retrofit: bool) -> List[hdr]:
+    hlist: List[hdr] = []
     while 1:
         try:
             h = hdr(rpm_fd)
@@ -70,26 +73,26 @@ def _doHeaderListFromFD(rpm_fd, retrofit):
     return hlist
 
 
-def readHeaderListFromFD(file_desc, retrofit=True):
+def readHeaderListFromFD(file_desc: AnyFD, retrofit: bool=True) -> List[hdr]:
     if not isinstance(file_desc, fd):
         file_desc = fd(file_desc)
     return _doHeaderListFromFD(file_desc, retrofit)
 
 
-def readHeaderListFromFile(path, retrofit=True):
+def readHeaderListFromFile(path: AnyFD, retrofit: bool=True) -> List[hdr]:
     f = fd(path)
     hlist = _doHeaderListFromFD(f, retrofit)
     f.close()
     return hlist
 
 
-def readHeaderFromFD(file_desc):
+def readHeaderFromFD(file_desc: AnyFD) -> Tuple[Optional[hdr], Optional[int]]:
     """Return (header, pos_before_hdr)"""
     if not isinstance(file_desc, fd):
         file_desc = fd(file_desc)
     try:
-        offset = file_desc.tell()
-        h = hdr(file_desc)
+        offset: Optional[int] = file_desc.tell()
+        h: Optional[hdr] = hdr(file_desc)
     except (_rpm.error, IOError):
         offset = None
         h = None
@@ -97,9 +100,9 @@ def readHeaderFromFD(file_desc):
     return (h, offset)
 
 
-def signalsCaught(siglist):
+def signalsCaught(siglist: Iterable[int]) -> List[int]:
     """Returns list of signals that were caught."""
-    caught = []
+    caught: List[int] = []
     for sig in siglist:
         if signalCaught(sig):
             caught.append(sig)
@@ -107,7 +110,7 @@ def signalsCaught(siglist):
     return caught
 
 
-def dsSingle(TagN, N, EVR="", Flags=RPMSENSE_ANY):
+def dsSingle(TagN: AnyTag, N: str, EVR: str="", Flags: int=RPMSENSE_ANY) -> ds:
     """
     Creates a single entry dependency set (ds)
 
