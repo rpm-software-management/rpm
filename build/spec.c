efad86d00150c75f0e50d0a57cdc3f0655a4855f
Panu Matilainen <pmatilai@redhat.com>
2021-04-26 11:27:34 +0300

    Fix memory leak regression from Lua sources/patches
    A leftover ifdef from commit 7f3fdf6868d72fbdef3386345ff39b3744fa4b14
    prevents Lua sources/patches tables from getting properly freed.

--- build/spec.c
+++ build/spec.c
@@ -296,12 +296,10 @@ rpmSpec rpmSpecFree(rpmSpec spec)
     }
     spec->BANames = _free(spec->BANames);
 
-#ifdef WITH_LUA
     // only destroy lua tables if there are no BASpecs left
     if (spec->recursing || spec->BACount == 0) {
 	spec->lua = specLuaFini(spec);
     }
-#endif
 
     spec->sources = freeSources(spec->sources);
     spec->packages = freePackages(spec->packages);
