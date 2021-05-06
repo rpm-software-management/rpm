9383a647dfffb50035bf3f8c4beb56e0907c53cb
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-05-06 19:21:06 -0400

    pgpPrtPubkeyParams(): better checks for OID length
    The old code could perform out of bounds pointer arithmetic if the OID
    was bad.

--- rpmio/rpmpgp.c
+++ rpmio/rpmpgp.c
@@ -710,8 +710,8 @@ static int pgpPrtPubkeyParams(uint8_t pubkey_algo,
     int i, curve = 0;
     pgpDigAlg keyalg;
     if (pubkey_algo == PGPPUBKEYALGO_EDDSA) {
-	int len = p + 1 < pend ? p[0] : 0;
-	if (len == 0 || len == 0xff || p + 1 + len > pend)
+	const int len = 1 < pend - p ? p[0] : 0;
+	if (len == 0 || len == 0xff || len >= pend - p)
 	    goto exit;
 	curve = pgpCurveByOid(p + 1, len);
 	p += len + 1;
