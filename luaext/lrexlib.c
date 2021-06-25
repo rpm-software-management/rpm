8762bd35e03dc2cb33d2004aadc28464356b4b18
Michal Domonkos <mdomonko@redhat.com>
2021-06-25 15:15:08 +0200

    Fix memory leaks in Lua rex extension
    This covers the following usage:
    
    expr = rex.newPOSIX(<regex>)
    expr:match(<string>)           # A leak occurred here
    expr:gmatch(<string>, <func>)  # A leak occurred here
    
    Found by Coverity.
    
    (cherry picked from commit b7a1e996326ee29a163d67ceb1e6127fdc251c14)

--- luaext/lrexlib.c
+++ luaext/lrexlib.c
@@ -80,6 +80,7 @@ static void rex_push_matches(lua_State *L, const char *text, regmatch_t *match,
 
 static int rex_match(lua_State *L)
 {
+  int rc = 0;
   int res;
 #ifdef REG_BASIC
   size_t len;
@@ -109,9 +110,10 @@ static int rex_match(lua_State *L)
     lua_pushstring(L, "n");
     lua_pushnumber(L, ncapt);
     lua_rawset(L, -3);
-    return 3;
-  } else
-    return 0;
+    rc = 3;
+  }
+  free(match);
+  return rc;
 }
 
 static int rex_gmatch(lua_State *L)
@@ -158,6 +160,7 @@ static int rex_gmatch(lua_State *L)
       break;
   }
   lua_pushnumber(L, nmatch);
+  free(match);
   return 1;
 }
 
