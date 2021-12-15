30a5f0a7d06b4128b91bea649f6631be5885ac03
Panu Matilainen <pmatilai@redhat.com>
2021-12-15 12:34:52 +0200

    Further improve build warning/error summary
    Suppress summary output entirely on --quiet where the summary is wholly
    redundant, and non-sensical (as witnessed by the test-case outputs)
    at that because the summary title is omitted due to its NOTICE log level.
    
    A build may have warnings without having any errors, lift the warnings
    summary out of the rc-condition.
    
    Finally, condense the output a bit, two empty lines at front seems a bit
    excessive.
    
    (cherry picked from commit 3d46bc95c5ebd61330921f131af51fa4b88f2512)

--- build/build.c
+++ build/build.c
@@ -432,17 +432,21 @@ exit:
     free(cookie);
     spec->rootDir = NULL;
 
-    if (rc != RPMRC_OK && rc != RPMRC_MISSINGBUILDREQUIRES) {
+    /* Suppress summaries on --quiet */
+    if (rpmIsNormal()) {
 	unsigned maskWarn = RPMLOG_MASK(RPMLOG_WARNING);
 	unsigned maskErrs = RPMLOG_UPTO(RPMLOG_ERR);
 
 	if (rpmlogGetNrecsByMask(maskWarn)) {
-	    rpmlog(RPMLOG_NOTICE, _("\n\nRPM build warnings:\n"));
+	    rpmlog(RPMLOG_NOTICE, _("\nRPM build warnings:\n"));
 	    rpmlogPrintByMask(NULL, maskWarn);
 	}
-	if (rpmlogGetNrecsByMask(maskErrs)) {
-	    rpmlog(RPMLOG_NOTICE, _("\n\nRPM build errors:\n"));
-	    rpmlogPrintByMask(NULL, maskErrs);
+
+	if (rc != RPMRC_OK && rc != RPMRC_MISSINGBUILDREQUIRES) {
+	    if (rpmlogGetNrecsByMask(maskErrs)) {
+		rpmlog(RPMLOG_NOTICE, _("\nRPM build errors:\n"));
+		rpmlogPrintByMask(NULL, maskErrs);
+	    }
 	}
     }
 
