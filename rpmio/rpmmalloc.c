6aec7c673f25ff8339730cca45207aed0a6e7c42
Panu Matilainen <pmatilai@redhat.com>
2022-02-08 09:23:58 +0200

    Add rreallocn() to librpmio public API
    rreallocn() is like realloc() but with multiplication protection.
    Inspired by glibc's reallocarray() but I dislike that name.
    
    While at it, add xmallocn() as a shortcut to rreallocn(NULL, ...) case.
    
    Based on initial patch by Demi Marie Obenour.

--- rpmio/rpmmalloc.c
+++ rpmio/rpmmalloc.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include "debug.h"
 
@@ -53,6 +54,18 @@ void * rcalloc (size_t nmemb, size_t size)
     if (size == 0) size++;
     if (nmemb == 0) nmemb++;
     value = calloc (nmemb, size);
+    if (value == NULL)
+	value = vmefail(size);
+    return value;
+}
+
+void * rreallocn (void *ptr, size_t nmemb, size_t size)
+{
+    register void *value = NULL;
+    if (size == 0) size++;
+    if (nmemb == 0) nmemb++;
+    if (nmemb < SIZE_MAX / size)
+	value = realloc(ptr, nmemb * size);
     if (value == NULL)
 	value = vmefail(size);
     return value;
