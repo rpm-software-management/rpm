90b1dd6999bfa60dd58c89e0ebe298e14d4254c2
Panu Matilainen <pmatilai@redhat.com>
2022-03-03 11:41:44 +0200

    Drop rpmdb shutdown on exit()
    This is continuation to the previous commit, this was necessary for
    clean Berkeley DB shutdown but current backends do not need it. And
    as we no longer route signals through a polled machinery to get an
    exit() from eg ctrl-c, this is for all practical purposes dead code,
    callers are expected to free their iterators as needed anyway.
    
    As a bonus, eliminating these static structures improves thread-safety
    without complicated locking.

--- lib/misc.h
+++ lib/misc.h
@@ -62,8 +62,6 @@ RPM_GNUC_INTERNAL
 void rpmRelocationBuild(Header h, rpmRelocation *rawrelocs,
 		int *rnrelocs, rpmRelocation **rrelocs, uint8_t **rbadrelocs);
 
-RPM_GNUC_INTERNAL
-void rpmAtExit(void);
 #ifdef __cplusplus
 }
 #endif
