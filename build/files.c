ba689237f1bf9bbab51728aedf065d2f16ab21e7
Michal Domonkos <mdomonko@redhat.com>
2022-05-19 15:25:25 +0200

    Refactor processBinaryFile()
    Less nesting, easier on the eyes.
    
    No functional change.

--- build/files.c
+++ build/files.c
@@ -2164,6 +2164,9 @@ static rpmRC processBinaryFile(Package pkg, FileList fl, const char * fileName,
     rpmRC rc = RPMRC_OK;
     size_t fnlen = strlen(fileName);
     int trailing_slash = (fnlen > 0 && fileName[fnlen-1] == '/');
+    ARGV_t argv = NULL;
+    int argc = 0;
+    int i;
 
     /* XXX differentiate other directories from explicit %dir */
     if (trailing_slash && !fl->cur.isDir)
@@ -2190,26 +2193,23 @@ static rpmRC processBinaryFile(Package pkg, FileList fl, const char * fileName,
     if (fl->cur.isDir)
 	diskPath = rstrcat(&diskPath, "/");
 
-    if (doGlob) {
-	ARGV_t argv = NULL;
-	int argc = 0;
-	int i;
-
-	if (fl->cur.devtype) {
-	    rpmlog(RPMLOG_ERR, _("%%dev glob not permitted: %s\n"), diskPath);
-	    rc = RPMRC_FAIL;
-	    goto exit;
-	}
-
-	if (rpmGlob(diskPath, GLOB_NOMAGIC | GLOB_NOCHECK, &argc, &argv) == 0) {
-	    for (i = 0; i < argc; i++) {
-		rc = addFile(fl, argv[i], NULL);
-	    }
-	    argvFree(argv);
-	}
-    } else {
+    if (!doGlob) {
 	rc = addFile(fl, diskPath, NULL);
+	goto exit;
     }
+
+    if (fl->cur.devtype) {
+	rpmlog(RPMLOG_ERR, _("%%dev glob not permitted: %s\n"), diskPath);
+	rc = RPMRC_FAIL;
+	goto exit;
+    }
+
+    if (rpmGlob(diskPath, GLOB_NOMAGIC | GLOB_NOCHECK, &argc, &argv) != 0)
+	goto exit;
+
+    for (i = 0; i < argc; i++)
+	rc = addFile(fl, argv[i], NULL);
+    argvFree(argv);
 
 exit:
     free(diskPath);
