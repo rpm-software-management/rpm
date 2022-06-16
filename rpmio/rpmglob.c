ee613c27cc1ec3bebc738d908889e1d3ebb74905
Michal Domonkos <mdomonko@redhat.com>
2022-06-16 15:59:51 +0200

    Return arg count 0 from rpmGlob()
    Do set *argcPtr even if the actual count is 0, rather than leaving it
    unchanged and thus possibly undefined in the caller.  This is also
    consistent with how glob(3) works.

--- rpmio/rpmglob.c
+++ rpmio/rpmglob.c
@@ -150,11 +150,9 @@ int rpmGlob(const char * pattern, int * argcPtr, ARGV_t * argvPtr)
 
 exit:
     argc = argvCount(*argvPtr);
-    if (argc > 0) {
-	if (argcPtr)
-	    *argcPtr = argc;
-	rc = 0;
-    } else if (rc == 0)
+    if (argcPtr)
+	*argcPtr = argc;
+    if (argc == 0 && rc == 0)
 	rc = 1;
 
 
