659709239016571c8da1d96afed531391353132b
Florian Festi <ffesti@redhat.com>
2022-07-04 13:38:06 +0200

    rpm2cpio.sh: Deal with null byte in lzma magic
    As the shell can't deal with null bytes only read two bytes and check
    for proper match. This way we can match for the null byte even if it is
    not part of the string.
    
    This also silents the warning from the shell that there is a null byte
    being ignored in the magic string for lzma.

--- scripts/rpm2cpio.sh
+++ scripts/rpm2cpio.sh
@@ -54,11 +54,11 @@ sigsize=$rsize
 calcsize $(($offset + (8 - ($sigsize % 8)) % 8))
 hdrsize=$rsize
 
-case "$(_dd $offset bs=3 count=1 | tr -d '\0')" in
-	"$(printf '\102\132')"*) _dd $offset | bunzip2 ;; # '\x42\x5a'
-	"$(printf '\037\213')"*) _dd $offset | gunzip  ;; # '\x1f\x8b'
-	"$(printf '\375\067')"*) _dd $offset | xzcat   ;; # '\xfd\x37'
-	"$(printf '\135\000')"*) _dd $offset | unlzma  ;; # '\x5d\x00'
-	"$(printf '\050\265')"*) _dd $offset | unzstd  ;; # '\x28\xb5'
+case "$(_dd $offset bs=2 count=1 | tr -d '\0')" in
+	"$(printf '\102\132')") _dd $offset | bunzip2 ;; # '\x42\x5a'
+	"$(printf '\037\213')") _dd $offset | gunzip  ;; # '\x1f\x8b'
+	"$(printf '\375\067')") _dd $offset | xzcat   ;; # '\xfd\x37'
+	"$(printf '\135')") _dd $offset | unlzma      ;; # '\x5d\x00'
+	"$(printf '\050\265')") _dd $offset | unzstd  ;; # '\x28\xb5'
 	*) fatal "Unrecognized rpm file: $pkg" ;;
 esac
