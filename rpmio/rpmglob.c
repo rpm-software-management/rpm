9656065503b9181c8ad4fce47a8643acec7f4a07
Michal Domonkos <mdomonko@redhat.com>
2022-07-27 12:11:23 +0200

    Fix non-glob fallback regression in rpmGlob()
    Replacing our non-glob short-circuitry with the GLOB_NOMAGIC flag might
    have seemed like a straightforward change in commit
    c10e2310e4c41a626b524ae71b3c4f87a29134b2 but it turns out it's not a 1:1
    replacement due to the following differences from the original behavior:
    
      - Non-glob filenames still make it past glob(3) to the URL and
        lstat(2) code
    
      - Patterns that are not well-formed (e.g. no closing bracket or brace)
        are still treated as globs
    
      - Curly braces aren't treated as "magic" chars by GLOB_NOMAGIC
    
      - Trailing slash in a non-glob directory is stripped away by glob(3)
    
    Avoid any surprise in the existing callers by re-adding the glob check,
    only slightly simpler than the original rpmIsGlob() and
    __glob_pattern_p() combo.
    
    Treat the backslash as a "magic" char, though, as we want those to
    always be discarded by glob(3) from the resulting filename, in order to
    preserve the escape feature added in the above commit.

--- rpmio/rpmglob.c
+++ rpmio/rpmglob.c
@@ -51,6 +51,36 @@ static inline const char *next_brace_sub(const char *begin)
     return *cp != '\0' ? cp : NULL;
 }
 
+static int ismagic(const char *pattern)
+{
+    register const char *p, *q;
+    int openBrackets = 0;
+
+    for (p = pattern; *p != '\0'; p++)
+	switch (*p) {
+	case '?':
+	case '*':
+	case '\\':
+	case '~':
+	    return 1;
+	case '[':
+	    openBrackets = 1;
+	    break;
+	case ']':
+	    if (openBrackets)
+		return 1;
+	    break;
+	case '{':
+	    q = p;
+	    while (*q != '}')
+		if ((q = next_brace_sub(q + 1)) == NULL)
+		    return 0;
+	    return 1;
+	}
+
+    return 0;
+}
+
 /* librpmio exported interfaces */
 
 int rpmGlob(const char * pattern, int * argcPtr, ARGV_t * argvPtr)
@@ -66,7 +96,7 @@ int rpmGlob(const char * pattern, int * argcPtr, ARGV_t * argvPtr)
     size_t plen = strlen(path);
     int dir_only = (plen > 0 && path[plen-1] == '/');
     glob_t gl;
-    int flags = GLOB_NOMAGIC;
+    int flags = 0;
 #ifdef ENABLE_NLS
     char * old_collate = NULL;
     char * old_ctype = NULL;
@@ -92,7 +122,7 @@ int rpmGlob(const char * pattern, int * argcPtr, ARGV_t * argvPtr)
     (void) setlocale(LC_CTYPE, "C");
 #endif
 
-    if (!local) {
+    if (!local || !ismagic(pattern)) {
 	argvAdd(&argv, pattern);
 	goto exit;
     }
