42b48a81aa3a085fd7d508ae374eeec00b64d335
xujing <xujing125@huawei.com>
2022-11-25 17:18:33 +0800

    Fix Header leak when running rpm2cpio
    Header "h" is alloced in rpmReadPackageFile but not freed when
    running rpm2cpio. Fix it.

--- rpm2cpio.c
+++ rpm2cpio.c
@@ -96,6 +96,8 @@ int main(int argc, char *argv[])
 
     rc = (ufdCopy(gzdi, fdo) == payload_size) ? EXIT_SUCCESS : EXIT_FAILURE;
 
+    headerFree(h);
+
     Fclose(fdo);
 
     Fclose(gzdi);	/* XXX gzdi == fdi */
