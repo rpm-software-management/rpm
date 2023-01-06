f5516d581cc7313f361b01901445ffbf1b97b776
wangpeng <wangpengb@uniontech.com>
2023-01-06 09:49:50 +0800

    Quote source file names in %setup
    Resolves: #2335
    Signed-off-by: wangpeng <wangpengb@uniontech.com>
    Co-authored-by Florian Festi <ffesti@redhat.com>

--- build/parsePrep.c
+++ build/parsePrep.c
@@ -136,7 +136,7 @@ static char *doUntar(rpmSpec spec, uint32_t c, int quietly)
     }
 
     buf = rpmExpand("%{__rpmuncompress} -x ",
-		    quietly ? "" : "-v ", sp->path, NULL);
+		    quietly ? "" : "-v ","%{shescape:", sp->path, "}", NULL);
     rstrcat(&buf,
 	"\nSTATUS=$?\n"
 	"if [ $STATUS -ne 0 ]; then\n"
