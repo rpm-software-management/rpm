rpm-software-management/rpm

commit 3255273ae0fabd03c9738249a29c9c1e15f28f64

Change rpmLeadWrite() to take header as its arg instead of a lead
The only "user" of the lead data structure is the signing code
which just wants to copy it over to the signed package. We can
just as well regenerate it from the header. Even if it doesn't
end up identical (which it should), it doesn't make a damnest
difference because nothing that matters looks at the lead anyway.

sign/rpmgensig.c
@@ -682,7 +682,6 @@ static int rpmSign(const char *rpm, int deleting, int signfiles)
 {
     FD_t fd = NULL;
     FD_t ofd = NULL;
-    rpmlead lead = NULL;
     char *trpm = NULL;
     Header sigh = NULL;
     Header h = NULL;
@@ -702,7 +701,7 @@ static int rpmSign(const char *rpm, int deleting, int signfiles)
     if (manageFile(&fd, rpm, O_RDWR))
 	goto exit;
 
-    if ((rc = rpmLeadRead(fd, &lead, NULL, &msg)) != RPMRC_OK) {
+    if ((rc = rpmLeadRead(fd, NULL, NULL, &msg)) != RPMRC_OK) {
 	rpmlog(RPMLOG_ERR, "%s: %s\n", rpm, msg);
 	goto exit;
     }
@@ -813,7 +812,7 @@ static int rpmSign(const char *rpm, int deleting, int signfiles)
 	}
 
 	/* Write the lead/signature of the output rpm */
-	rc = rpmLeadWrite(ofd, lead);
+	rc = rpmLeadWrite(ofd, h);
 	if (rc != RPMRC_OK) {
 	    rpmlog(RPMLOG_ERR, _("%s: writeLead failed: %s\n"), trpm,
 		Fstrerror(ofd));
@@ -852,7 +851,6 @@ exit:
 
     headerFree(sigh);
     headerFree(h);
-    rpmLeadFree(lead);
     free(msg);
 
     /* Clean up intermediate target */

build/pack.c
@@ -440,16 +440,12 @@ static rpmRC writeRPM(Package pkg, unsigned char ** pkgidp,
     }
 
     /* Write the lead section into the package. */
-    {	
-	rpmlead lead = rpmLeadFromHeader(pkg->header);
-	rc = rpmLeadWrite(fd, lead);
-	rpmLeadFree(lead);
-	if (rc != RPMRC_OK) {
-	    rc = RPMRC_FAIL;
-	    rpmlog(RPMLOG_ERR, _("Unable to write package: %s\n"),
-		 Fstrerror(fd));
-	    goto exit;
-	}
+    rc = rpmLeadWrite(fd, pkg->header);
+    if (rc != RPMRC_OK) {
+	rc = RPMRC_FAIL;
+	rpmlog(RPMLOG_ERR, _("Unable to write package: %s\n"),
+	     Fstrerror(fd));
+	goto exit;
     }
 
     /* Estimate cpio archive size to decide if use 32 bits or 64 bit tags. */

lib/rpmlead.c
@@ -78,9 +78,11 @@ rpmlead rpmLeadFree(rpmlead lead)
 }
 
 /* The lead needs to be 8 byte aligned */
-rpmRC rpmLeadWrite(FD_t fd, rpmlead lead)
+rpmRC rpmLeadWrite(FD_t fd, Header h)
 {
     rpmRC rc = RPMRC_FAIL;
+
+    rpmlead lead = rpmLeadFromHeader(h);
 
     if (lead != NULL) {
 	struct rpmlead_s l;
@@ -93,7 +95,10 @@ rpmRC rpmLeadWrite(FD_t fd, rpmlead lead)
 	    
 	if (Fwrite(&l, 1, sizeof(l), fd) == sizeof(l))
 	    rc = RPMRC_OK;
+
+	rpmLeadFree(lead);
     }
+
     return rc;
 }
 

lib/rpmlead.h
@@ -38,10 +38,10 @@ rpmlead rpmLeadFree(rpmlead lead);
 /** \ingroup lead
  * Write lead to file handle.
  * @param fd		file handle
- * @param lead		package lead
+ * @param h		package header
  * @return		RPMRC_OK on success, RPMRC_FAIL on error
  */
-rpmRC rpmLeadWrite(FD_t fd, rpmlead lead);
+rpmRC rpmLeadWrite(FD_t fd, Header h);
 
 /** \ingroup lead
  * Read lead from file handle.

commit f27db9be270188582695d98361c107393dd715d0

Copy original lead on signing instead of recreating
Fixes a regression from commit 3255273ae0fabd03c9738249a29c9c1e15f28f64
where the arch and os fields of the lead may change depending on the
package arch and the host where --addsign/--delsign is executed.

This is hard to test for specifically as the test-suite can run on any
architecture, but the bit-per-bit comparison test added in the previous
commit will catch differences here too.

Fixes: #1326

sign/rpmgensig.c
@@ -661,14 +661,14 @@ static int rpmSign(const char *rpm, int deleting, int flags)
     if (sigh == NULL)	/* XXX can't happen */
 	goto exit;
 
+    if (Fseek(fd, insSig ? sigStart : 0, SEEK_SET) < 0) {
+	rpmlog(RPMLOG_ERR, _("Could not seek in file %s: %s\n"),
+		rpm, Fstrerror(fd));
+	goto exit;
+    }
+
     if (insSig) {
 	/* Insert new signature into original rpm */
-	if (Fseek(fd, sigStart, SEEK_SET) < 0) {
-	    rpmlog(RPMLOG_ERR, _("Could not seek in file %s: %s\n"),
-		    rpm, Fstrerror(fd));
-	    goto exit;
-	}
-
 	if (rpmWriteSignature(fd, sigh)) {
 	    rpmlog(RPMLOG_ERR, _("%s: rpmWriteSignature failed: %s\n"), rpm,
 		Fstrerror(fd));
@@ -676,6 +676,7 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 	}
 	res = 0;
     } else {
+	char lbuf[RPMLEAD_SIZE];
 	/* Replace orignal rpm with new rpm containing new signature */
 	rasprintf(&trpm, "%s.XXXXXX", rpm);
 	ofd = rpmMkTemp(trpm);
@@ -684,11 +685,12 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 	    goto exit;
 	}
 
-	/* Write the lead/signature of the output rpm */
-	rc = rpmLeadWrite(ofd, h);
-	if (rc != RPMRC_OK) {
-	    rpmlog(RPMLOG_ERR, _("%s: writeLead failed: %s\n"), trpm,
-		Fstrerror(ofd));
+	/* Copy lead from original */
+	if (!(Fread(lbuf, 1, RPMLEAD_SIZE, fd) == RPMLEAD_SIZE &&
+	      Fwrite(lbuf, 1, RPMLEAD_SIZE, ofd) == RPMLEAD_SIZE))
+	{
+	    rpmlog(RPMLOG_ERR, _("%s: failed to copy rpm lead: %s\n"),
+				trpm, Fstrerror(ofd));
 	    goto exit;
 	}
 