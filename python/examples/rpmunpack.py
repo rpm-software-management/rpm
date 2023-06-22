57c1ed778cdb3e6d94a26860a44816c25d4a7256
Panu Matilainen <pmatilai@redhat.com>
2023-06-22 09:20:54 +0300

    Add some Python binding examples
    It's not whether these are particularly important examples or not
    (they're not), but adding more will be so much easier once the place
    to add them has been established.
    
    Fixes: #2075
    (cherry picked from commit 9e3bb9fd43ccacefc33ecf895a8544fd7116ad62)

---
+++ python/examples/rpmunpack.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# A simple rpm unpacking demonstration.
+# Accepts package files as arguments and unpacks them relative to current
+# directory, similar to 'rpm2cpio <pkg> | cpio -idv'.
+
+import rpm
+import sys
+import stat
+import os
+
+def unpack(h, fd):
+    files = rpm.files(h)
+    payload = rpm.fd(fd, 'r', h['payloadcompressor'])
+    archive = files.archive(payload)
+    for f in archive:
+        os.makedirs('./%s' % f.dirname, exist_ok=True)
+        fn = './%s' % f.name
+        if stat.S_ISREG(f.mode):
+            if archive.hascontent():
+                wfd = rpm.fd(fn, 'w')
+                archive.readto(wfd)
+                wfd.close()
+                # handle hardlinks
+                if f.nlink > 1:
+                    for l in f.links:
+                        if l.inode != f.inode:
+                            os.link(fn, l.name)
+        elif stat.S_ISDIR(f.mode):
+            os.mkdir(fn)
+        elif stat.S_ISLNK(f.mode):
+            os.symlink(f.linkto, fn)
+
+if __name__ == '__main__':
+    ts = rpm.ts()
+    for arg in sys.argv[1:]:
+        fd = rpm.fd(arg, 'r')
+        h = ts.hdrFromFdno(fd)
+        unpack(h, fd)
