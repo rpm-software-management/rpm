3b1855894a1dab60bc285c32fc4b79a293f3803d
Panu Matilainen <pmatilai@redhat.com>
2023-09-13 12:29:28 +0300

    Move top-level headers to misc/ and include that everywhere
    Having stuff in the top-level directory is messy and prevents control
    over what you can include from where. No functional changes.
    
    (cherry picked from commit d4eb1f2fe6f8712f874becbd10f29a3a542bed46)

--- build/files.c
+++ build/files.c
@@ -28,7 +28,7 @@
 #include <rpm/rpmbase64.h>
 
 #include "rpmio/rpmio_internal.h"	/* XXX rpmioSlurp */
-#include "misc/rpmfts.h"
+#include "rpmfts.h"
 #include "lib/rpmfi_internal.h"	/* XXX fi->apath */
 #include "lib/rpmug.h"
 #include "build/rpmbuild_internal.h"
