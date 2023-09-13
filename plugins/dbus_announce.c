rpm-software-management/rpm

commit 071be753cf05b2753efb0d3c51d4fa59b9641d09

Suppress inhibition lock warning message when DBus service is not available
Same as commit 708e61307bc3fd027b016fdf5a1d1a5274c1843c, this message
will just annoy and confuse users in situations where DBus service is
not running at all, such as single-user mode and minimal container type
environments. Like our own test-suite. Rather than entirely suppress
the error, issue a debug log though.

plugins/dbus_announce.c
@@ -66,7 +66,9 @@ static rpmRC open_dbus(rpmPlugin plugin, rpmts ts)
     }
     return RPMRC_OK;
  err:
-    rpmlog(RPMLOG_WARNING,
+    int ignore = dbus_error_has_name(&err, DBUS_ERROR_NO_SERVER) ||
+		 dbus_error_has_name(&err, DBUS_ERROR_FILE_NOT_FOUND);
+    rpmlog(ignore ? RPMLOG_DEBUG : RPMLOG_WARNING,
 	   "dbus_announce plugin: Error connecting to dbus (%s)\n",
 	   err.message);
     dbus_error_free(&err);

commit 34d983fa2a9b9276fc540b6bd554605b2c72689a

Fix compile regression from 071be753cf05b2753efb0d3c51d4fa59b9641d09
A declaration immediately following a label is not valid C99, as crazy
as it is in a language that otherwise allows free placement of declarations.
It looks like something people forgot to update in the standard, first
in C99 and apparently C11 too. Recent gcc doesn't complain about it
but it doesn't make it right.

Suggested-by: Dmitry Mikushin <dmitry@kernelgen.org>

plugins/dbus_announce.c
@@ -33,6 +33,7 @@ static rpmRC open_dbus(rpmPlugin plugin, rpmts ts)
 {
     DBusError err;
     int rc = 0;
+    int ignore = 0;
     struct dbus_announce_data * state = rpmPluginGetData(plugin);
 
     /* Already open */
@@ -66,7 +67,7 @@ static rpmRC open_dbus(rpmPlugin plugin, rpmts ts)
     }
     return RPMRC_OK;
  err:
-    int ignore = dbus_error_has_name(&err, DBUS_ERROR_NO_SERVER) ||
+    ignore = dbus_error_has_name(&err, DBUS_ERROR_NO_SERVER) ||
 		 dbus_error_has_name(&err, DBUS_ERROR_FILE_NOT_FOUND);
     rpmlog(ignore ? RPMLOG_DEBUG : RPMLOG_WARNING,
 	   "dbus_announce plugin: Error connecting to dbus (%s)\n",