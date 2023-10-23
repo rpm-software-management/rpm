afd352481bacea521ce5ba01e989866478278532
Panu Matilainen <pmatilai@redhat.com>
2023-10-23 10:46:56 +0300

    Turn legacy %patchN syntax into a hard error
    This stupid syntax is the thing preventing %prep from becoming a normal
    script, and that in turn blocks so many things now that this has got to
    go.
    
    Considering how widely used this syntax is, this is a disruptive
    and abrupt change - we only started warning on %patchN in 4.18. So lets
    at least print out a legible error message with suggestions how to fix
    for now, even if it requires specifically checking for the obsolete
    syntax.

--- build/spec.c
+++ build/spec.c
@@ -231,7 +231,6 @@ rpmSpec newSpec(void)
     spec->numSources = 0;
     spec->autonum_patch = -1;
     spec->autonum_source = -1;
-    spec->numConverted = 0;
 
     spec->sourceRpmName = NULL;
     spec->sourcePkgId = NULL;
