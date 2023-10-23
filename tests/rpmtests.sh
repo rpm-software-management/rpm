0dc534891cea3905ba1c3a2100843e61df416bb1
Michal Domonkos <mdomonko@redhat.com>
2023-10-23 21:56:28 +0200

    Add --shell command to rpmtests wrapper
    This invokes an interactive shell in a dummy test where all the usual
    test commands work, and is meant as a replacement for what mktree.fedora
    currently does for its shell/atshell commands.  This feature basically
    emulates a single test so it really belongs to the rpmtests wrapper, not
    mktree.
    
    For consistency, also do the permission fixup for rpmtests.dir (just
    like in mktree.fedora), this will be needed once we drop mktree.fedora.

--- tests/rpmtests.sh
+++ tests/rpmtests.sh
@@ -5,11 +5,22 @@
 #       like original -C but loads atlocal from script's directory if missing
 #   -L, --log
 #       print the test log when done
+#   -S, --shell [CMD]
+#       create an empty test and run a shell (or CMD) in it
+#   -R, --reset
+#       delete the test created with --shell
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 SCRIPT_FILES="atlocal mktree.common"
-
+SHELL_DIR=$PWD/rpmtests.dir/shell
 PRINT_LOG=0
+RUN_SHELL=0
+RC=0
+
+fixperms()
+{
+    chmod -Rf u+rwX "$@"
+}
 
 while [ $# != 0 ]; do
     case $1 in
@@ -19,6 +30,13 @@ while [ $# != 0 ]; do
         ;;
         -L | --log )
             PRINT_LOG=1
+        ;;
+        -S | --shell )
+            RUN_SHELL=1
+        ;;
+        -R | --reset )
+            rm -rf "$SHELL_DIR"
+            exit
         ;;
         *)
             break
@@ -32,9 +50,26 @@ for file in $SCRIPT_FILES; do
     [ -f "$file" ] || ln -s $SCRIPT_DIR/$file .
 done
 
-# Run the test suite
-$SCRIPT_DIR/rpmtests "$@"; RC=$?
-[ $PRINT_LOG == 1 ] && cat rpmtests.log
+# Run the test suite (or a shell)
+if [ $RUN_SHELL == 0 ]; then
+    $SCRIPT_DIR/rpmtests "$@"; RC=$?
+    [ $PRINT_LOG == 1 ] && cat rpmtests.log
+    fixperms rpmtests.dir/*/{diff,work}
+else
+    # Emulate a single, writable test
+    set -a
+    source ./atlocal
+    trap : INT
+    RPMTEST=$SHELL_DIR/tree
+    snapshot mount $SHELL_DIR
+    if [ $# == 0 ]; then
+        $SHELL
+    else
+        "$@"
+    fi
+    snapshot umount
+    fixperms $SHELL_DIR
+fi
 
 # Clean up the symlinks
 for file in $SCRIPT_FILES; do
