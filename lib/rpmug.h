152057cb2b9a4a096e137bd2840512c518805984
Panu Matilainen <pmatilai@redhat.com>
2023-11-30 13:46:48 +0200

    Make rpmug*() properly internal to librpm now that we can

--- lib/rpmug.h
+++ lib/rpmug.h
@@ -1,16 +1,22 @@
 #ifndef _RPMUG_H
 #define _RPMUG_H
 
+#include <rpm/rpmutil.h>
 #include <sys/types.h>
 
+RPM_GNUC_INTERNAL
 int rpmugUid(const char * name, uid_t * uid);
 
+RPM_GNUC_INTERNAL
 int rpmugGid(const char * name, gid_t * gid);
 
+RPM_GNUC_INTERNAL
 const char * rpmugUname(uid_t uid);
 
+RPM_GNUC_INTERNAL
 const char * rpmugGname(gid_t gid);
 
+RPM_GNUC_INTERNAL
 void rpmugFree(void);
 
 #endif /* _RPMUG_H */
