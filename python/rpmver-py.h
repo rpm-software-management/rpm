55cd896a13b4636b4babc96c2e66cb95bc196b96
Beñat Gartzia <bgartzia@redhat.com>
2024-01-11 13:38:39 +0100

    Support rpmver-py comparison operator inheritance
    Although we were able to write python rpm.ver subclasses, we were not
    actually able to inherit the comparison operators of the base class:
    Trying to compare instances of the subclass would raise a
    NotImplementedError.
    
    The reason behind that was the verObject_Check macro checked that an
    instance's type was matching rpmver_Type, rather than running a wider
    check. Then, that check is applied to each of the operands of a
    comparison by the function implementing the tp_richcompare slot
    function.
    
    This patch turns the type check into a PyObject_TypeCheck call, which
    will also permit subclasses to go through. Then, it adds another test to
    cover such behavior.
    
    Signed-off-by: Beñat Gartzia <bgartzia@redhat.com>

--- python/rpmver-py.h
+++ python/rpmver-py.h
@@ -8,7 +8,7 @@ typedef struct rpmverObject_s rpmverObject;
 extern PyTypeObject* rpmver_Type;
 extern PyType_Spec rpmver_Type_Spec;
 
-#define verObject_Check(v)	(((PyObject*)v)->ob_type == rpmver_Type)
+#define verObject_Check(v)	PyObject_TypeCheck(v, rpmver_Type)
 
 int verFromPyObject(PyObject *item, rpmver *ver);
 PyObject * rpmver_Wrap(PyTypeObject *subtype, rpmver ver);
