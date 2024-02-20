5a685fb5eb085d5bc37723ec29ce72434db6bd4e
Panu Matilainen <pmatilai@redhat.com>
2024-02-20 14:32:12 +0200

    Don't populate os and arch in the lead structure
    The only thing that looks at these fields is "file", and even that would
    be better off not doing so because the canonical numbers are so
    meaningless: i386/x86_64 is the same thing, right?

--- lib/rpmlead.c
+++ lib/rpmlead.c
@@ -45,18 +45,11 @@ struct rpmlead_s {
 static int rpmLeadFromHeader(Header h, struct rpmlead_s *l)
 {
     if (h != NULL) {
-	int archnum, osnum;
 	char * nevr = headerGetAsString(h, RPMTAG_NEVR);
-
-	/* FIXME: should grab these from header instead (RhBug:717898) */
-	rpmGetArchInfo(NULL, &archnum);
-	rpmGetOsInfo(NULL, &osnum);
 
 	memset(l, 0, sizeof(*l));
 	l->major = 3;
 	l->minor = 0;
-	l->archnum = archnum;
-	l->osnum = osnum;
 	l->signature_type = RPMSIGTYPE_HEADERSIG;
 	l->type = (headerIsSource(h) ? 1 : 0);
 
