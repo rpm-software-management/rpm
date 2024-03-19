6ede5d3ed8081ddcece6a44176ed38863c848e3f
Panu Matilainen <pmatilai@redhat.com>
2024-03-19 11:08:08 +0200

    Allow building rpm without OpenPGP support
    For bootstrapping purposes, having rpm depend on Rust is painful, but
    directing people to unmaintained crypto code as an alternative is
    hair-raising. As a middle ground, let rpm be built without OpenPGP
    support at all, which at least gives you a functional rpm and rpm-build
    even if you can't sign or verify signatures.
    
    Achieving this is a moderately complex dance which can't meaningfully
    be split into multiple commits because everything is interconnected:
    
    Add a new WITH_SEQUOIA option to control use of Sequoia, on by default.
    When Sequoia is disabled, default to a newly added dummy PGP implementation
    instead which just returns error on everything. And finally, if the
    older WITH_INTERNAL_OPENPGP is enabled, use the old PGP implementation.
    
    As the intent is to cut out rpmpgp_legacy to a separate repository,
    sanity requires that we also split the openssl/libgcrypt code at the
    digest/signature fault line. It's not ideal, but the alternative of
    having unused crypto code on which an external component depends on
    is just not sustainable. This way, the signature side of things is
    quite neatly cut off with the PGP stuff. The diff looks big but there
    are no code/functional changes in the libgcrypt/openssl split.

--- rpmio/rpmpgp_legacy/digest_libgcrypt.c
+++ rpmio/rpmpgp_legacy/digest_libgcrypt.c
@@ -6,50 +6,6 @@
 #include "rpmpgp_internal.h"
 #include "debug.h"
 
-/**
- * MD5/SHA1 digest private data.
- */
-struct DIGEST_CTX_s {
-    rpmDigestFlags flags;	/*!< Bit(s) to control digest operation. */
-    int algo;			/*!< Used hash algorithm */
-    gcry_md_hd_t h;
-};
-
-
-/****************************  init   ************************************/
-
-int rpmInitCrypto(void) {
-    gcry_check_version (NULL);
-    gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
-    return 0;
-}
-
-int rpmFreeCrypto(void) {
-    return 0;
-}
-
-/****************************  digest ************************************/
-
-size_t rpmDigestLength(int hashalgo)
-{
-    switch (hashalgo) {
-    case RPM_HASH_MD5:
-	return 16;
-    case RPM_HASH_SHA1:
-	return 20;
-    case RPM_HASH_SHA224:
-	return 28;
-    case RPM_HASH_SHA256:
-	return 32;
-    case RPM_HASH_SHA384:
-	return 48;
-    case RPM_HASH_SHA512:
-	return 64;
-    default:
-	return 0;
-    }
-}
-
 static int hashalgo2gcryalgo(int hashalgo)
 {
     switch (hashalgo) {
@@ -69,71 +25,6 @@ static int hashalgo2gcryalgo(int hashalgo)
 	return 0;
     }
 }
-
-DIGEST_CTX rpmDigestInit(int hashalgo, rpmDigestFlags flags)
-{
-    gcry_md_hd_t h;
-    DIGEST_CTX ctx;
-    int gcryalgo = hashalgo2gcryalgo(hashalgo);
-
-    if (!gcryalgo || gcry_md_open(&h, gcryalgo, 0) != 0)
-	return NULL;
-
-    ctx = xcalloc(1, sizeof(*ctx));
-    ctx->flags = flags;
-    ctx->algo = hashalgo;
-    ctx->h = h;
-    return ctx;
-}
-
-int rpmDigestUpdate(DIGEST_CTX ctx, const void * data, size_t len)
-{
-    if (ctx == NULL)
-	return -1;
-    gcry_md_write(ctx->h, data, len);
-    return 0;
-}
-
-int rpmDigestFinal(DIGEST_CTX ctx, void ** datap, size_t *lenp, int asAscii)
-{
-    unsigned char *digest;
-    int digestlen;
-    if (ctx == NULL)
-	return -1;
-    digest = gcry_md_read(ctx->h, 0);
-    digestlen = rpmDigestLength(ctx->algo);
-    if (!asAscii) {
-	if (lenp)
-	    *lenp = digestlen;
-	if (datap) {
-	    *datap = xmalloc(digestlen);
-	    memcpy(*datap, digest, digestlen);
-	}
-    } else {
-	if (lenp)
-	    *lenp = 2 * digestlen + 1;
-	if (datap) {
-	    *datap = rpmhex((const uint8_t *)digest, digestlen);
-	}
-    }
-    gcry_md_close(ctx->h);
-    free(ctx);
-    return 0;
-}
-
-DIGEST_CTX rpmDigestDup(DIGEST_CTX octx)
-{
-    DIGEST_CTX nctx = NULL;
-    if (octx) {
-        gcry_md_hd_t h;
-	if (gcry_md_copy(&h, octx->h))
-	    return NULL;
-	nctx = memcpy(xcalloc(1, sizeof(*nctx)), octx, sizeof(*nctx));
-	nctx->h = h;
-    }
-    return nctx;
-}
-
 
 /****************************** RSA **************************************/
 
