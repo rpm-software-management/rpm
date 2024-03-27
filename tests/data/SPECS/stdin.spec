rpm-software-management/rpm

commit e5d7a823c99b84d65ecbf3810b68aab372ef5d14

Run build scriptlets with closed stdin to enforce unattended builds
Even max-rpm philosophy section points out that rpm builds are
unattended, but then we do nothing at all to prevent it? I first though
maybe this regressed when we switched the build scripts to use rpmfcExec()
a few years ago, but there wasn't anything before that either. Weird.

build/rpmfc.c
@@ -310,9 +310,14 @@ static int getOutputFrom(ARGV_t argv,
     if (child == 0) {
 	close(toProg[1]);
 	close(fromProg[0]);
-	
-	dup2(toProg[0], STDIN_FILENO);   /* Make stdin the in pipe */
-	close(toProg[0]);
+
+	if (writePtr) {
+	    /* Make stdin the in pipe */
+	    dup2(toProg[0], STDIN_FILENO);
+	    close(toProg[0]);
+	} else {
+	    close(STDIN_FILENO);
+	}
 
 	dup2(fromProg[1], STDOUT_FILENO); /* Make stdout the out pipe */
 	close(fromProg[1]);

tests/rpmbuild.at
@@ -2754,3 +2754,15 @@ rpmspec --define "nosuchmacro /bbb" --parse /data/SPECS/badftrigger.spec
 [ignore],
 [])
 RPMTEST_CLEANUP
+
+AT_SETUP([rpmbuild interactive])
+AT_KEYWORDS([build])
+RPMDB_INIT
+
+RPMTEST_CHECK([
+runroot rpmbuild --quiet -bb /data/SPECS/interact.spec
+],
+[1],
+[ignore],
+[ignore])
+RPMTEST_CLEANUP

tests/data/SPECS/interact.spec
@@ -1,0 +1,12 @@
+Name: interact
+Version: 1.0
+Release: 1
+Summary: test
+License: Public Domain
+
+# This must abort the build
+%prep
+read EHLO
+echo hello $EHLO
+
+%files

commit b6cacb7521066937d45700562705b11ddaa4c6a2

Use a less intrusive unattended enforcement in rpmbuild
Commit e5d7a823c99b84d65ecbf3810b68aab372ef5d14 broke builds relying
on libtool (and no doubt some others as well) because libtool redirects
stdin in some of its tests and this fails if stdin is closed.
Instead of closing, pass the write-only end of the pipe as the stdin
to build scriptlets. This way the descriptor is there and can be
further redirected, but any attempt to read from the (unredirected)
stdin will error out. Add a test-case too.

build/rpmfc.c
@@ -316,12 +316,16 @@ static int getOutputFrom(ARGV_t argv,
 	close(toProg[1]);
 	close(fromProg[0]);
 
+	/*
+	 * When expecting input, make stdin the in pipe as you'd normally do.
+	 * Otherwise pass stdout(!) as the in pipe to cause reads to error
+	 * out. Just closing the fd breaks some software (eg libtool).
+	 */
 	if (writePtr) {
-	    /* Make stdin the in pipe */
 	    dup2(toProg[0], STDIN_FILENO);
 	    close(toProg[0]);
 	} else {
-	    close(STDIN_FILENO);
+	    dup2(fromProg[1], STDIN_FILENO);
 	}
 
 	dup2(fromProg[1], STDOUT_FILENO); /* Make stdout the out pipe */

tests/data/SPECS/stdin.spec
@@ -1,0 +1,13 @@
+Name: stdin
+Version: 1.0
+Release: 1
+Summary: libtool weirdness test-case
+License: Public Domain
+
+%prep
+# func_lalib_unsafe_p in libtool does this
+echo Fo > bar
+exec 5<&0 < bar
+exec 0<&2 2<&-
+
+%files

tests/rpmbuild.at
@@ -2917,6 +2917,13 @@ runroot rpmbuild --quiet -bb /data/SPECS/interact.spec
 [1],
 [ignore],
 [ignore])
+
+RPMTEST_CHECK([
+runroot rpmbuild --quiet -bp /data/SPECS/stdin.spec
+],
+[0],
+[ignore],
+[ignore])
 RPMTEST_CLEANUP
 
 AT_SETUP([rpmbuild bpf file])