rpm-software-management/rpm

commit ce6c3812af4c2a9e9fc6c2dc77e00e1e1487bb83

Avoid c++ reserved keywords new, class and namespace

build/rpmfc.c
@@ -261,12 +261,12 @@ static int rpmfcExpandAppend(ARGV_t * argvp, ARGV_const_t av)
 }
 
 static rpmds rpmdsSingleNS(rpmstrPool pool,
-			rpmTagVal tagN, const char *namespace,
+			rpmTagVal tagN, const char *namespc,
 			const char * N, const char * EVR, rpmsenseFlags Flags)
 {
     rpmds ds = NULL;
-    if (namespace) {
-	char *NSN = rpmExpand(namespace, "(", N, ")", NULL);
+    if (namespc) {
+	char *NSN = rpmExpand(namespc, "(", N, ")", NULL);
 	ds = rpmdsSinglePool(pool, tagN, NSN, EVR, Flags);
 	free(NSN);
     } else {
@@ -594,7 +594,7 @@ static ARGV_t runCall(const char *name, const char *buildRoot, ARGV_t fns)
 
 struct addReqProvDataFc {
     rpmfc fc;
-    const char *namespace;
+    const char *namespc;
     regex_t *exclude;
 };
 
@@ -604,10 +604,10 @@ static rpmRC addReqProvFc(void *cbdata, rpmTagVal tagN,
 {
     struct addReqProvDataFc *data = cbdata;
     rpmfc fc = data->fc;
-    const char *namespace = data->namespace;
+    const char *namespc = data->namespc;
     regex_t *exclude = data->exclude;
 
-    rpmds ds = rpmdsSingleNS(fc->pool, tagN, namespace, N, EVR, Flags);
+    rpmds ds = rpmdsSingleNS(fc->pool, tagN, namespc, N, EVR, Flags);
     /* Add to package and file dependencies unless filtered */
     if (regMatch(exclude, rpmdsDNEVR(ds)+2) == 0)
 	rpmfcAddFileDep(&fc->fileDeps, ds, index);
@@ -683,7 +683,7 @@ static int genDeps(const char *mname, int multifile, rpmTagVal tagN,
 static int rpmfcHelper(rpmfc fc, int *ixs, int n, const char *proto,
 		       const struct exclreg_s *excl,
 		       rpmsenseFlags dsContext, rpmTagVal tagN,
-		       const char *namespace, const char *mname)
+		       const char *namespc, const char *mname)
 {
     int rc = 0;
     const char **paths = xcalloc(n + 1, sizeof(*paths));
@@ -708,7 +708,7 @@ static int rpmfcHelper(rpmfc fc, int *ixs, int n, const char *proto,
 
     struct addReqProvDataFc data;
     data.fc = fc;
-    data.namespace = namespace;
+    data.namespc = namespc;
     data.exclude = excl->exclude;
 
     if (proto && rstreq(proto, "multifile")) {
@@ -1116,7 +1116,7 @@ static int applyAttr(rpmfc fc, int aix,
 	char *mname = rstrscat(NULL, "__", aname, "_", dep->name, NULL);
 
 	if (rpmMacroIsDefined(NULL, mname)) {
-	    char *ns = rpmfcAttrMacro(aname, "namespace", NULL);
+	    char *ns = rpmfcAttrMacro(aname, "namespc", NULL);
 	    rc = rpmfcHelper(fc, ixs, n, attr->proto,
 			    excl, dep->type, dep->tag, ns, mname);
 	    free(ns);

lib/formats.c
@@ -32,13 +32,13 @@ typedef char * (*headerTagFormatFunction) (rpmtd td, char **emsg);
 struct headerFmt_s {
     rpmtdFormats fmt;	/*!< Value of extension */
     const char *name;	/*!< Name of extension. */
-    rpmTagClass class;	/*!< Class of source data (RPM_ANY_CLASS for any) */
+    rpmTagClass tclass;	/*!< Class of source data (RPM_ANY_CLASS for any) */
     headerTagFormatFunction func;	/*!< Pointer to formatter function. */	
 };
 
-static const char *classEr(rpmTagClass class)
+static const char *classEr(rpmTagClass tclass)
 {
-    switch (class) {
+    switch (tclass) {
     case RPM_BINARY_CLASS:	 return _("(not a blob)");
     case RPM_NUMERIC_CLASS:	 return _("(not a number)");
     case RPM_STRING_CLASS:	 return _("(not a string)");
@@ -652,8 +652,8 @@ char *rpmHeaderFormatCall(headerFmt fmt, rpmtd td)
     char *ret = NULL;
     char *err = NULL;
 
-    if (fmt->class != RPM_ANY_CLASS && rpmtdClass(td) != fmt->class)
-	err = xstrdup(classEr(fmt->class));
+    if (fmt->tclass != RPM_ANY_CLASS && rpmtdClass(td) != fmt->tclass)
+	err = xstrdup(classEr(fmt->tclass));
     else
 	ret = fmt->func(td, &err);
 

tools/rpmsort.c
@@ -144,17 +144,17 @@ static void add_input(const char *filename, char ***package_names,
     while (input_buffer && *input_buffer &&
 	   (position_of_newline = strchrnul(input_buffer, '\n'))) {
 	size_t sz = position_of_newline - input_buffer;
-	char *new;
+	char *newl;
 
 	if (sz == 0) {
 	    input_buffer = position_of_newline + 1;
 	    continue;
 	}
 
-	new = rstrndup(input_buffer, sz);
+	newl = rstrndup(input_buffer, sz);
 
 	names = xrealloc(names, sizeof(char *) * (n_names + 1));
-	names[n_names] = new;
+	names[n_names] = newl;
 	n_names++;
 
 	/* Move buffer ahead to next line. */

commit 6651f9cafd1fb9134d8b4d216c0520dd1f551ed5

Fix a regression on dependency generator namespace directive
Commit ce6c3812af4c2a9e9fc6c2dc77e00e1e1487bb83 accidentally changed
this namespace string literal too, oops. Possible because there was
no test on it, so add one.

build/rpmfc.c
@@ -1045,7 +1045,7 @@ static int applyAttr(rpmfc fc, int aix,
 	}
 
 	if (rpmMacroIsDefined(NULL, mname)) {
-	    char *ns = rpmfcAttrMacro(aname, "namespc", NULL);
+	    char *ns = rpmfcAttrMacro(aname, "namespace", NULL);
 	    rc = rpmfcHelper(fc, fnx.data(), fnx.size(), attr->proto,
 			    excl, dep->type, dep->tag, ns, mname);
 	    free(ns);

tests/rpmbuild.at
@@ -1120,11 +1120,12 @@ RPMDB_INIT
 runroot rpmbuild -bb --quiet \
 		--define '__script_mime text/plain' \
 		--define '__script_magic %{nil}' \
+		--define '__script_namespace mine' \
 		/data/SPECS/shebang.spec
 runroot rpm -qp --requires /build/RPMS/noarch/shebang-0.1-1.noarch.rpm|grep -v ^rpmlib
 ],
 [0],
-[/bin/blabla
+[mine(/bin/blabla)
 ],
 [])
 RPMTEST_CLEANUP