rpm-software-management/rpm

commit 13a02b15d6552bc8e440235b846ea5257514d397

Undo weird designated initializers use in rpmsignfiles.c
It's legit C99 but it's rare and weird.

sign/rpmsignfiles.c
@@ -20,16 +20,17 @@
 #define MAX_SIGNATURE_LENGTH 1024
 
 static const char *hash_algo_name[] = {
-    [RPM_HASH_MD5]          = "md5",
-    [RPM_HASH_SHA1]         = "sha1",
-    [RPM_HASH_RIPEMD160]    = "rmd160",
-    [RPM_HASH_MD2]          = "md2",
-    [RPM_HASH_TIGER192]     = "tgr192",
-    [RPM_HASH_HAVAL_5_160]  = "haval5160",
-    [RPM_HASH_SHA256]       = "sha256",
-    [RPM_HASH_SHA384]       = "sha384",
-    [RPM_HASH_SHA512]       = "sha512",
-    [RPM_HASH_SHA224]       = "sha224",
+    "none",	/* invalid */
+    "md5", 	/* RPM_HASH_MD5 */
+    "sha1", 	/* RPM_HASH_SHA1 */
+    "rmd160",	/* RPM_HASH_RIPEMD160 */
+    "md2",	/* RPM_HASH_MD2 */
+    "tgr192", 	/* RPM_HASH_TIGER192 */
+    "haval5160",/* RPM_HASH_HAVAL_5_160 */
+    "sha256",	/* RPM_HASH_SHA256 */
+    "sha384",	/* RPM_HASH_SHA384 */
+    "sha512",	/* RPM_HASH_SHA512 */
+    "sha224",	/* RPM_HASH_SHA224 */
 };
 
 #define ARRAY_SIZE(a)  (sizeof(a) / sizeof(a[0]))

commit 94e76034bca66aff71cd932e93d5eecfb2714369

Fix signfiles regression from 13a02b15d6552bc8e440235b846ea5257514d397
The sneaky construct hid more than just values starting from 1: there's
no value 4 in rpmHashAlgo enum, so skipping that causes all the later
hash names to be off by one.

Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2291183

sign/rpmsignfiles.c
@@ -23,6 +23,7 @@ static const char *hash_algo_name[] = {
     "md5", 	/* RPM_HASH_MD5 */
     "sha1", 	/* RPM_HASH_SHA1 */
     "rmd160",	/* RPM_HASH_RIPEMD160 */
+    "reserved1",/* reserved */
     "md2",	/* RPM_HASH_MD2 */
     "tgr192", 	/* RPM_HASH_TIGER192 */
     "haval5160",/* RPM_HASH_HAVAL_5_160 */