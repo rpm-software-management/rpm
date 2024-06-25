02a5d77029465564da33a753687a510a92906a1b
Panu Matilainen <pmatilai@redhat.com>
2024-06-25 13:25:20 +0300

    Move DBSWAP() to bdb_ro, nothing else needs this

--- lib/backend/dbi.h
+++ lib/backend/dbi.h
@@ -105,19 +105,6 @@ struct dbiIndex_s {
     void * dbi_db;		/*!< Backend private handle */
 };
 
-union _dbswap {
-    unsigned int ui;
-    unsigned char uc[4];
-};
-
-#define	_DBSWAP(_a) \
-\
-  { unsigned char _b, *_c = (_a).uc; \
-    _b = _c[3]; _c[3] = _c[0]; _c[0] = _b; \
-    _b = _c[2]; _c[2] = _c[1]; _c[1] = _b; \
-\
-  }
-
 typedef rpmRC (*idxfunc)(dbiIndex dbi, dbiCursor dbc,
 			const char *keyp, size_t keylen, dbiIndexItem rec);
 
