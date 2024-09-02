bf13d2db6bd2a8569de81a5ccaebf8362f61cf84
Panu Matilainen <pmatilai@redhat.com>
2024-09-02 16:21:22 +0300

    Filter Lua deprecation warnings based on the originating rpm version
    Issuing deprecation warnings on packages built long time ago is
    just antisocial behavior when the user is powerless to do anything
    about it.
    
    When running scripts, pass the builder rpm version to Lua through the
    registry, and filter out the warnings when the package was built with
    an rpm version where these functions were not yet deprecated.
    Usage through rpmlua and macros always gets warnings: those are things
    that can technically be fixed by the user.
    
    (backported from commit 532e6b90652154842fc765f87f60c97f20f7d656)

--- rpmio/lposix.h
+++ rpmio/lposix.h
@@ -4,7 +4,7 @@
 #include <rpm/rpmutil.h>
 
 RPM_GNUC_INTERNAL
-void check_deprecated(lua_State *L, const char *func);
+void check_deprecated(lua_State *L, const char *func, const char *deprecated_in);
 
 RPM_GNUC_INTERNAL
 int luaopen_posix (lua_State *L);
