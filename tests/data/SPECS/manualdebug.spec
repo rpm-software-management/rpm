rpm-software-management/rpm

commit 1a9803d0f8daf15bb706dc17783ab19589906487

Fix %__debug_package value mismatch on -bb --short-circuit builds
%__debug_package is used internally for determining whether debug
packages are being generated or not. This gets defined as a side-effect
of expanding %debug_package macro from the %install section template,
IFF %install is processed. If we skip straight to package generation
with -bb --short-circuit, that step is missed but rpm-debuginfo.specpart
is still being read, causing a mismatch between expectations. This
only works now because of bug #3057 and without this change, the
the tests on -bb --short-circuit would fail when that bug is fixed.

To fix the discrepancy, place a duplicate %__debug_package definition
into the rpm-debuginfo.specpart so it gets defined whenever that file
is read. Unfortunately the definition there is not enough for other
parts of the machinery, the %install template macros need
%__debug_package defined before we get to reading rpm-debuginfo.specpart
for the first time.

macros.in
@@ -194,9 +194,12 @@ package or when debugging this package.\
 %files debugsource -f debugsourcefiles.list\
 %{nil}
 
+# The duplicate __debug_package definition is needed to ensure matching
+# state when %install is skipped due to short-circuit.
 %debug_package \
 %ifnarch noarch\
 %global __debug_package 1\
+%%global __debug_package 1\
 %_debuginfo_template\
 %{?_debugsource_packages:%_debugsource_template}\
 %endif\

commit 1a8af2bad09a95122fce42cb1e87d93df141337f

Fix regression preventing manual %debug_package usage
Apparently rpmfluff creates specs that invoke the %debug_package macro
directly, and commit 1a9803d0f8daf15bb706dc17783ab19589906487 broke
that: when the literal unexpanded %global ends up inside a spec, it'll
trigger various errors depending on the exact context.

rpmfluff is using this manual method because there's no %setup in the
produced spec and so, no %buildsubdir is defined and so, no debuginfo
packages produced. Conditionalize writing the escaped %global on
%buildsubdir being defined, a condition where automatic debuginfo
packages normally would be generated anyhow. It may not be ideal but
I think it'll do for now.

Fixes: #3290

macros.in
@@ -195,11 +195,12 @@ package or when debugging this package.\
 %{nil}
 
 # The duplicate __debug_package definition is needed to ensure matching
-# state when %install is skipped due to short-circuit.
+# state when %install is skipped due to short-circuit, IFF buildsubdir
+# is defined (indicating use of automatic debuginfo generation)
 %debug_package \
 %ifnarch noarch\
 %global __debug_package 1\
-%%global __debug_package 1\
+%{?buildsubdir:%%global __debug_package 1}\
 %_debuginfo_template\
 %{?_debugsource_packages:%_debugsource_template}\
 %endif\

tests/data/SPECS/manualdebug.spec
@@ -1,0 +1,31 @@
+%global __os_install_post %{nil}
+Name: manualdebug
+Version: 0.1
+Release: 1
+License: GPL
+Summary: Testing manual debug_package use
+
+%description
+%{summary}
+
+%prep
+cat << EOF > main.c
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	printf("hello\n");
+	return 0;
+}
+EOF
+
+%build
+gcc -g main.c
+
+%install
+mkdir -p $RPM_BUILD_ROOT/usr/bin
+cp a.out $RPM_BUILD_ROOT/usr/bin/hello-world
+
+%files
+/usr/bin/hello-world
+
+%debug_package

tests/rpmbuild.at
@@ -2499,6 +2499,18 @@ runroot rpmbuild --quiet -bb \
 [ignore])
 RPMTEST_CLEANUP
 
+AT_SETUP([manual %debug_package use])
+AT_KEYWORDS([build debuginfo])
+RPMDB_INIT
+RPMTEST_CHECK([
+runroot rpmbuild -bb \
+		/data/SPECS/manualdebug.spec
+],
+[0],
+[ignore],
+[ignore])
+RPMTEST_CLEANUP
+
 AT_SETUP([explicit %_enable_debug_package on noarch])
 AT_KEYWORDS([build debuginfo])
 RPMTEST_CHECK([