1fbf8aeb4e78b8b4afeeaafcbc97b3cbf7cfeaba
Panu Matilainen <pmatilai@redhat.com>
2024-09-17 08:31:35 +0300

    Enforce the same sanity checks on db add and rebuild
    It doesn't make a whole lot of sense to allow inserting headers
    that will get removed as invalid on the next rebuild. Funny what
    oddities have survived all this time...
    
    Fixes: #3306

--- lib/rpmdb.c
+++ lib/rpmdb.c
@@ -2176,6 +2176,18 @@ exit:
     return (rc == 0) ? RPMRC_OK : RPMRC_FAIL;
 }
 
+static bool validHeader(Header h)
+{
+    if (!(headerIsEntry(h, RPMTAG_NAME) &&
+	headerIsEntry(h, RPMTAG_VERSION) &&
+	headerIsEntry(h, RPMTAG_RELEASE) &&
+	headerIsEntry(h, RPMTAG_HEADERIMMUTABLE)))
+    {
+	return false;
+    }
+    return true;
+}
+
 int rpmdbAdd(rpmdb db, Header h)
 {
     dbiIndex dbi = NULL;
@@ -2189,7 +2201,7 @@ int rpmdbAdd(rpmdb db, Header h)
 	return 0;
 
     hdrBlob = (uint8_t *)headerExport(h, &hdrLen);
-    if (hdrBlob == NULL || hdrLen == 0) {
+    if (!validHeader(h) || hdrBlob == NULL || hdrLen == 0) {
 	ret = -1;
 	goto exit;
     }
@@ -2424,11 +2436,7 @@ int rpmdbRebuild(const char * prefix, rpmts ts,
 	while ((h = rpmdbNextIterator(mi)) != NULL) {
 
 	    /* let's sanity check this record a bit, otherwise just skip it */
-	    if (!(headerIsEntry(h, RPMTAG_NAME) &&
-		headerIsEntry(h, RPMTAG_VERSION) &&
-		headerIsEntry(h, RPMTAG_RELEASE) &&
-		headerIsEntry(h, RPMTAG_HEADERIMMUTABLE)))
-	    {
+	    if (!validHeader(h)) {
 		rpmlog(RPMLOG_ERR,
 			_("header #%u in the database is bad -- skipping.\n"),
 			rpmdbGetIteratorOffset(mi));
