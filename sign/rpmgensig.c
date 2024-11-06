a7b64bcbaa521a078e0e9ad4973508a85d896721
Panu Matilainen <pmatilai@redhat.com>
2024-11-06 10:30:15 +0200

    Simplify the v3 signature logic a bit
    Pass the flags down to the function that needs to do stuff. No
    functional changes.

--- sign/rpmgensig.c
+++ sign/rpmgensig.c
@@ -407,7 +407,8 @@ static int putSignature(Header sigh, rpmtd sigtd)
     return (headerPut(sigh, sigtd, HEADERPUT_DEFAULT) == 0);
 }
 
-static int replaceSignature(Header sigh, sigTarget sigt_v3, sigTarget sigt_v4)
+static int replaceSignature(Header sigh, rpmSignFlags flags,
+			sigTarget sigt_v3, sigTarget sigt_v4)
 {
     int rc = -1;
     rpmtd sigtd = NULL;
@@ -427,7 +428,7 @@ static int replaceSignature(Header sigh, sigTarget sigt_v3, sigTarget sigt_v4)
     if (putSignature(sigh, sigtd))
 	goto exit;
 
-    if (sigt_v3) {
+    if (flags & RPMSIGN_FLAG_RPMV3) {
 	rpmtdFree(sigtd);
 
 	/* Assume the same signature test holds for v3 signature too */
@@ -653,7 +654,6 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 	deleteSigs(sigh);
     } else {
 	/* Signature target containing header + payload */
-	int v3 = (flags & RPMSIGN_FLAG_RPMV3);
 	sigt_v3.fd = fd;
 	sigt_v3.start = headerStart;
 	sigt_v3.fileName = rpm;
@@ -663,7 +663,7 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 	sigt_v4 = sigt_v3;
 	sigt_v4.size = headerSizeof(h, HEADER_MAGIC_YES);
 
-	res = replaceSignature(sigh, v3 ? &sigt_v3 : NULL, &sigt_v4);
+	res = replaceSignature(sigh, flags, &sigt_v3, &sigt_v4);
 	if (res != 0) {
 	    if (res == 1) {
 		rpmlog(RPMLOG_WARNING,
