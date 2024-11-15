aa5fb1e1fca588ed53fc16492c1e42855f905f1e
Panu Matilainen <pmatilai@redhat.com>
2024-11-15 11:05:51 +0200

    Drop leftover buildhost override from pinned tests
    The buildhost gets centrally set to "testhost" nowadays, there's no
    need to do anything in this test. And, turns out this hasn't actually
    been honored, the built packages get "testhost" from the upper level
    settings as it is. This confused us greatly.

--- tests/pinned/common/buildrepr.sh
+++ tests/pinned/common/buildrepr.sh
@@ -4,7 +4,6 @@ runroot rpmbuild -bb --quiet \
 	--define "optflags -O2 -g" \
 	--define "_target_platform noarch-linux" \
 	--define "_binary_payload w.ufdio" \
-	--define "_buildhost localhost" \
 	--define "use_source_date_epoch_as_buildtime 1" \
 	--define "source_date_epoch_from_changelog 1" \
 	--define "build_mtime_policy clamp_to_source_date_epoch" \
