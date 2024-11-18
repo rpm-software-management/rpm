b8bc68c2c36cf7e3086ef86756e7e1a0f647804b
Panu Matilainen <pmatilai@redhat.com>
2024-11-18 15:50:06 +0200

    Add support for multiple OpenPGP signatures per package, part 2/2
    Add a tag extension for RPMTAG_OPENPGP (on top of the concrete tag) to
    handle compatibility with v3/v4 signatures: the extension collects all
    legacy signatures under the same umbrella so users don't need to query
    multiple different tags, you just query for RPMTAG_OPENPGP to get all
    them at once. Extend :pgpsig tag format to handle the new string
    array/base64 variant.
    
    Update --info/-i query to use the extension and output all existing
    signatures, one per line. The no-signature case of "Signature  : (none)"
    is preserved as-is to help backwards compatibility with scripts parsing
    the output.
    
    Related: #3385

--- lib/tagexts.cc
+++ lib/tagexts.cc
@@ -1052,6 +1052,39 @@ static int sysusersTag(Header h, rpmtd td, headerGetFlags hgflags)
     return (td->count > 0);
 }
 
+static void trySigTag(Header h, rpmTagVal tag, ARGV_t *sigs)
+{
+    struct rpmtd_s td;
+    if (headerGet(h, tag, &td, HEADERGET_ALLOC)) {
+	char *b64 = rpmBase64Encode((uint8_t *)td.data, td.count, 0);
+	if (b64) {
+	    argvAdd(sigs, b64);
+	    free(b64);
+	}
+	rpmtdFreeData(&td);
+    }
+}
+
+static int openpgpTag(Header h, rpmtd td, headerGetFlags hgflags)
+{
+    if (headerGet(h, RPMTAG_OPENPGP, td, HEADERGET_ALLOC))
+	return 1;
+
+    ARGV_t sigs = NULL;
+    trySigTag(h, RPMTAG_RSAHEADER, &sigs);
+    trySigTag(h, RPMTAG_DSAHEADER, &sigs);
+    trySigTag(h, RPMTAG_SIGPGP, &sigs);
+    trySigTag(h, RPMTAG_SIGGPG, &sigs);
+
+    if (sigs) {
+	td->data = sigs;
+	td->count = argvCount(sigs);
+	td->type = RPM_STRING_ARRAY_TYPE;
+	td->flags = RPMTD_ALLOCED|RPMTD_PTR_ALLOCED;
+    }
+    return td->count != 0;
+}
+
 static const struct headerTagFunc_s rpmHeaderTagExtensions[] = {
     { RPMTAG_GROUP,		groupTag },
     { RPMTAG_DESCRIPTION,	descriptionTag },
@@ -1093,6 +1126,7 @@ static const struct headerTagFunc_s rpmHeaderTagExtensions[] = {
     { RPMTAG_FILENLINKS,	filenlinksTag },
     { RPMTAG_SYSUSERS,		sysusersTag },
     { RPMTAG_FILEMIMES,		filemimesTag },
+    { RPMTAG_OPENPGP,		openpgpTag },
     { 0, 			NULL }
 };
 
