56c37fc734237fb5f06dfb857ac2f6dc27d7e86f
Panu Matilainen <pmatilai@redhat.com>
2024-11-28 15:05:52 +0200

    Make macros::define() take the argument as a C++ string

--- rpmio/rpmmacro_internal.hh
+++ rpmio/rpmmacro_internal.hh
@@ -57,7 +57,7 @@ public:
     void clear();
     /* Copy all macros from this context to another one */
     void copy(rpm::macros & dest, int level);
-    int define(const char *macro, int level);
+    int define(const std::string & macro, int level);
     void dump(FILE *fp = stderr);
     /* Expand macros to a C++ string, with a return code (rc, string) */
     std::pair<int,std::string> expand(const std::string & src, int flags = 0);
