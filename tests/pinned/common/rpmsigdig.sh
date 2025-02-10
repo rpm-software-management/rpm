4422f527b8f00fca8ba8b6212722f91942e3e717
Panu Matilainen <pmatilai@redhat.com>
2025-02-10 14:19:48 +0200

    Enable enforcing signature checking by default
    This is all about tediously updating test results to match the new
    expectations. Some --nosignature disablers added on tests where
    we know it's unsigned and the extra output from signature checking
    is not relevant for the tests.
    
    Fixes: #1573

--- tests/pinned/common/rpmsigdig.sh
+++ tests/pinned/common/rpmsigdig.sh
@@ -2,4 +2,4 @@
 for v in SHA256HEADER SHA1HEADER SIGMD5 PAYLOADDIGEST PAYLOADDIGESTALT PAYLOADSIZE PAYLOADSIZEALT; do
     runroot rpm -q --qf "${v}: %{${v}}\n" ${pkg}
 done
-runroot rpmkeys -Kv ${pkg}
+runroot rpmkeys -Kv --nosignature ${pkg}
