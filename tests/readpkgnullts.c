rpm-software-management/rpm

commit 09510e413dbbdecfa6d65406a31517c79dd6c773

rpmKeyring: Support keys with the same key ID
Loop over the candidates during signature verification and use the one
verifying it - iff any. Otherwise print error messages from all
candidates.

Resolves: #3334

rpmio/rpmkeyring.cc
@@ -6,6 +6,7 @@
 #include <shared_mutex>
 #include <string>
 #include <map>
+#include <cassert>
 
 #include <rpm/rpmstring.h>
 #include <rpm/rpmpgp.h>
@@ -31,7 +32,7 @@ using wrlock = std::unique_lock<std::shared_mutex>;
 using rdlock = std::shared_lock<std::shared_mutex>;
 
 struct rpmKeyring_s {
-    std::map<std::string,rpmPubkey> keys; /* pointers to keys */
+    std::multimap<std::string,rpmPubkey> keys; /* pointers to keys */
     std::atomic_int nrefs;
     std::shared_mutex mutex;
 };
@@ -126,16 +127,21 @@ int rpmKeyringModify(rpmKeyring keyring, rpmPubkey key, rpmKeyringModifyMode mod
 
     /* check if we already have this key, but always wrlock for simplicity */
     wrlock lock(keyring->mutex);
-    auto item = keyring->keys.find(key->keyid);
-    if (item != keyring->keys.end() && mode == RPMKEYRING_DELETE) {
+    auto range = keyring->keys.equal_range(key->keyid);
+    auto item = range.first;
+    for (; item != range.second; ++item) {
+	if (item->second->fp == key->fp)
+	    break;
+    }
+    if (item != range.second && mode == RPMKEYRING_DELETE) {
 	rpmPubkeyFree(item->second);
 	keyring->keys.erase(item);
 	rc = 0;
-    } else if (item != keyring->keys.end() && mode == RPMKEYRING_REPLACE) {
+    } else if (item != range.second && mode == RPMKEYRING_REPLACE) {
 	rpmPubkeyFree(item->second);
 	item->second = rpmPubkeyLink(key);
 	rc = 0;
-    } else if (item == keyring->keys.end() && (mode == RPMKEYRING_ADD ||mode == RPMKEYRING_REPLACE) ) {
+    } else if (item == range.second && (mode == RPMKEYRING_ADD || mode == RPMKEYRING_REPLACE) ) {
 	keyring->keys.insert({key->keyid, rpmPubkeyLink(key)});
 	rc = 0;
     }
@@ -153,9 +159,13 @@ rpmPubkey rpmKeyringLookupKey(rpmKeyring keyring, rpmPubkey key)
     if (keyring == NULL || key == NULL)
 	return NULL;
     rdlock lock(keyring->mutex);
-    auto item = keyring->keys.find(key->keyid);
-    rpmPubkey rkey = item == keyring->keys.end() ? NULL : rpmPubkeyLink(item->second);
-    return rkey;
+
+    auto range = keyring->keys.equal_range(key->keyid);
+    for (auto item = range.first; item != range.second; ++item) {
+	if (item->second->fp == key->fp)
+	    return rpmPubkeyLink(item->second);
+    }
+    return NULL;
 }
 
 rpmKeyring rpmKeyringLink(rpmKeyring keyring)
@@ -363,50 +373,67 @@ pgpDigParams rpmPubkeyPgpDigParams(rpmPubkey key)
     return params;
 }
 
-static rpmPubkey findbySig(rpmKeyring keyring, pgpDigParams sig)
+rpmRC pubKeyVerify(rpmPubkey key, pgpDigParams sig, DIGEST_CTX ctx, std::vector<std::pair<int, std::string>> &results)
 {
-    rpmPubkey key = NULL;
+    rpmRC rc = RPMRC_FAIL;
+    char *lints = NULL;
+    rc = pgpVerifySignature2(key ? key->pgpkey : NULL, sig, ctx, &lints);
 
-    if (keyring && sig) {
-	rdlock lock(keyring->mutex);
-	auto keyid = key2str(pgpDigParamsSignID(sig));
-	auto item = keyring->keys.find(keyid);
-	if (item != keyring->keys.end()) {
-	    key = item->second;
-	    pgpDigParams pub = key->pgpkey;
-	    /* Do the parameters match the signature? */
-	    if ((pgpDigParamsAlgo(sig, PGPVAL_PUBKEYALGO)
-                 != pgpDigParamsAlgo(pub, PGPVAL_PUBKEYALGO)) ||
-                memcmp(pgpDigParamsSignID(sig), pgpDigParamsSignID(pub),
-                       PGP_KEYID_LEN)) {
-		key = NULL;
-	    }
-	}
-    }
-    return key;
+    /* found right key, discard error messages from wrong keys */
+    if (rc == RPMRC_OK)
+	results.clear();
+
+    results.push_back({rc, std::string(lints ? lints : "")});
+    free(lints);
+    return rc;
 }
 
 rpmRC rpmKeyringVerifySig2(rpmKeyring keyring, pgpDigParams sig, DIGEST_CTX ctx, rpmPubkey * keyptr)
 {
     rpmRC rc = RPMRC_FAIL;
+    rpmPubkey key = NULL;
 
     if (sig && ctx) {
-	pgpDigParams pgpkey = NULL;
-	rpmPubkey key = findbySig(keyring, sig);
+	rdlock lock(keyring->mutex);
+	auto keyid = key2str(pgpDigParamsSignID(sig));
+	std::vector<std::pair<int, std::string>> results = {};
 
-	if (key)
-	    pgpkey = key->pgpkey;
+	/* Look for verifying key */
+	auto range = keyring->keys.equal_range(keyid);
 
-	/* We call verify even if key not found for a signature sanity check */
-	char *lints = NULL;
-	rc = pgpVerifySignature2(pgpkey, sig, ctx, &lints);
-	if (lints) {
-	    rpmlog(rc ? RPMLOG_ERR : RPMLOG_WARNING, "%s\n", lints);
-	    free(lints);
+	for (auto it = range.first; it != range.second; ++it) {
+	    key = it->second;
+
+	    /* Do the parameters match the signature? */
+	    if (pgpDigParamsAlgo(sig, PGPVAL_PUBKEYALGO)
+		!= pgpDigParamsAlgo(key->pgpkey, PGPVAL_PUBKEYALGO))
+	    {
+		continue;
+	    }
+
+	    rc = pubKeyVerify(key, sig, ctx, results);
+
+	    if (rc == RPMRC_OK)
+		break;
 	}
-	if (keyptr) {
-	    *keyptr = pubkeyPrimarykey(key);
+
+	/* No key, sanity check signature*/
+	if (results.empty()) {
+	    key = NULL;
+	    rc = pubKeyVerify(NULL, sig, ctx, results);
+	    assert(rc != RPMRC_OK);
 	}
+
+	for (auto const & item : results) {
+	    if (!item.second.empty()) {
+		rpmlog(item.first ? RPMLOG_ERR : RPMLOG_WARNING, "%s\n",
+		       item.second.c_str());
+	    }
+	}
+    }
+
+    if (keyptr) {
+	*keyptr = pubkeyPrimarykey(key);
     }
 
     return rc;

tests/data/keys/keyidcollision1.asc
@@ -1,0 +1,8 @@
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+xVgEZxYoexYJKwYBBAHaRw8BAQdAw36CCvOf5G1jhEQe/j5CfVQi3DrYCdQriSwp
+G/C/aB8AAP9fqZy7LBMwXaJ4ozz4Edw+zRaWXRMW4etZUmZMyI+FcQ+7zQDCYQQT
+FggAEwUCZxYoewkQQmVadRVrPeACGwMAAEy6AQD6lIb8pOjIwIV+DrsKhuXLtCsX
+Q2gUwumc/6WLB8m1RAEA4VNRScVPrw69XloBSFY8Q1RVNfxWiYfpy/OrLD/QFAg=
+=KcD6
+-----END PGP PRIVATE KEY BLOCK-----

tests/data/keys/keyidcollision1.pub
@@ -1,0 +1,8 @@
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mDMEZxYoexYJKwYBBAHaRw8BAQdAw36CCvOf5G1jhEQe/j5CfVQi3DrYCdQriSwp
+G/C/aB+0AIhhBBMWCAATBQJnFih7CRBCZVp1FWs94AIbAwAATLoBAPqUhvyk6MjA
+hX4OuwqG5cu0KxdDaBTC6Zz/pYsHybVEAQDhU1FJxU+vDr1eWgFIVjxDVFU1/FaJ
+h+nL86ssP9AUCA==
+=fev2
+-----END PGP PUBLIC KEY BLOCK-----

tests/data/keys/keyidcollision2.asc
@@ -1,0 +1,8 @@
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+xVgEZxYoexYJKwYBBAHaRw8BAQdAGGMA/Vn1CsPFvLJ6jNawd7rWgHmO+ar3epiy
+D7wvXy4AAQD5L8KyW9QkHBYY6mxTeAaz9AZv22IstQZE84SAx2VB7g83zQDCYQQT
+FggAEwUCZxYoewkQQmVadRVrPeACGwMAALddAQDGGgUSvspZUepR5VF8JlWd2sz1
+Vhu5n9dphlNg/0Q7xgD/TrlZI+x+BMAQZ/62tHHX1d36JgTwX40D5PBD+rSjLg4=
+=PNIB
+-----END PGP PRIVATE KEY BLOCK-----

tests/data/keys/keyidcollision2.pub
@@ -1,0 +1,8 @@
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mDMEZxYoexYJKwYBBAHaRw8BAQdAGGMA/Vn1CsPFvLJ6jNawd7rWgHmO+ar3epiy
+D7wvXy60AIhhBBMWCAATBQJnFih7CRBCZVp1FWs94AIbAwAAt10BAMYaBRK+yllR
+6lHlUXwmVZ3azPVWG7mf12mGU2D/RDvGAP9OuVkj7H4EwBBn/ra0cdfV3fomBPBf
+jQPk8EP6tKMuDg==
+=mfU5
+-----END PGP PUBLIC KEY BLOCK-----

tests/rpmsigdig.at
@@ -1455,3 +1455,81 @@ POST-IMPORT
 
 gpgconf --kill gpg-agent
 RPMTEST_CLEANUP
+
+# ------------------------------
+# Test key id collision
+AT_SETUP([key id collision])
+AT_KEYWORDS([rpmsign signature])
+AT_SKIP_IF([test x$PGP = xdummy])
+RPMDB_INIT
+gpg2 --import ${RPMTEST}/data/keys/keyidcollision1.asc
+# Our keys have no passphrases to be asked, silence GPG_TTY warning
+export GPG_TTY=""
+RPMTEST_CHECK([
+RPMDB_INIT
+
+cp "${RPMTEST}"/data/RPMS/hello-2.0-1.x86_64.rpm "${RPMTEST}"/tmp/
+run rpmsign --key-id 79cc07f167fee8841829acaa42655a75156b3de0 --digest-algo sha256 --addsign "${RPMTEST}"/tmp/hello-2.0-1.x86_64.rpm > /dev/null
+echo PRE-IMPORT
+runroot rpmkeys -Kv /tmp/hello-2.0-1.x86_64.rpm|grep -v digest
+echo POST-IMPORT
+runroot rpmkeys --import /data/keys/keyidcollision2.pub
+runroot rpmkeys -Kv /tmp/hello-2.0-1.x86_64.rpm|grep -v digest
+runroot rpmkeys --import /data/keys/keyidcollision1.pub
+runroot rpmkeys -Kv /tmp/hello-2.0-1.x86_64.rpm|grep -v digest
+],
+[0],
+[PRE-IMPORT
+/tmp/hello-2.0-1.x86_64.rpm:
+    Header V4 EdDSA/SHA256 Signature, key ID 42655a75156b3de0: NOKEY
+POST-IMPORT
+/tmp/hello-2.0-1.x86_64.rpm:
+    Header V4 EdDSA/SHA256 Signature, Key Fingerprint: 94706f8da571389e8642bdfd42655a75156b3de0: BAD
+/tmp/hello-2.0-1.x86_64.rpm:
+    Header V4 EdDSA/SHA256 Signature, Key Fingerprint: 79cc07f167fee8841829acaa42655a75156b3de0: OK
+],
+[error: Verifying a signature using certificate 94706F8DA571389E8642BDFD42655A75156B3DE0 ():
+  Certificate 42655A75156B3DE0 does not contain key 79CC07F167FEE8841829ACAA42655A75156B3DE0 or it is not valid
+])
+
+gpgconf --kill gpg-agent
+RPMTEST_CLEANUP
+
+# ------------------------------
+# Test key id collision
+AT_SETUP([key id collision])
+AT_KEYWORDS([rpmsign signature])
+AT_SKIP_IF([test x$PGP = xdummy])
+RPMDB_INIT
+gpg2 --import ${RPMTEST}/data/keys/keyidcollision2.asc
+# Our keys have no passphrases to be asked, silence GPG_TTY warning
+export GPG_TTY=""
+RPMTEST_CHECK([
+RPMDB_INIT
+
+cp "${RPMTEST}"/data/RPMS/hello-2.0-1.x86_64.rpm "${RPMTEST}"/tmp/
+run rpmsign --key-id 94706f8da571389e8642bdfd42655a75156b3de0 --digest-algo sha256 --addsign "${RPMTEST}"/tmp/hello-2.0-1.x86_64.rpm > /dev/null
+echo PRE-IMPORT
+runroot rpmkeys -Kv /tmp/hello-2.0-1.x86_64.rpm|grep -v digest
+echo POST-IMPORT
+runroot rpmkeys --import /data/keys/keyidcollision1.pub
+runroot rpmkeys -Kv /tmp/hello-2.0-1.x86_64.rpm|grep -v digest
+runroot rpmkeys --import /data/keys/keyidcollision2.pub
+runroot rpmkeys -Kv /tmp/hello-2.0-1.x86_64.rpm|grep -v digest
+],
+[0],
+[PRE-IMPORT
+/tmp/hello-2.0-1.x86_64.rpm:
+    Header V4 EdDSA/SHA256 Signature, key ID 42655a75156b3de0: NOKEY
+POST-IMPORT
+/tmp/hello-2.0-1.x86_64.rpm:
+    Header V4 EdDSA/SHA256 Signature, Key Fingerprint: 79cc07f167fee8841829acaa42655a75156b3de0: BAD
+/tmp/hello-2.0-1.x86_64.rpm:
+    Header V4 EdDSA/SHA256 Signature, Key Fingerprint: 94706f8da571389e8642bdfd42655a75156b3de0: OK
+],
+[error: Verifying a signature using certificate 79CC07F167FEE8841829ACAA42655A75156B3DE0 ():
+  Certificate 42655A75156B3DE0 does not contain key 94706F8DA571389E8642BDFD42655A75156B3DE0 or it is not valid
+])
+
+gpgconf --kill gpg-agent
+RPMTEST_CLEANUP

commit b2c574b8757170bce793cd83c9b75cad847e36cd

Fix a regression when calling rpmReadPackageFile() with NULL ts
Commit 09510e413dbbdecfa6d65406a31517c79dd6c773 introduced unconditional
dereferencing of the keyring during verification, and so segfaults on
NULL keyring. That can't happen within rpm itself, so we need a new
test program to trigger it: call rpmReadPackageFile() with NULL ts.

Start a new test group of API tests, we'll need to do more of these.
I wasted an hour or two looking for a way to reproduce this by rpm or
the python bindings when writing this tiny test-program took all of
five minutes.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2362996

rpmio/rpmkeyring.cc
@@ -429,27 +429,29 @@ rpmRC rpmKeyringVerifySig2(rpmKeyring keyring, pgpDigParams sig, DIGEST_CTX ctx,
     rpmPubkey key = NULL;
 
     if (sig && ctx) {
-	rdlock lock(keyring->mutex);
-	auto keyid = key2str(pgpDigParamsSignID(sig));
 	std::vector<std::pair<int, std::string>> results = {};
+	if (keyring) {
+	    auto keyid = key2str(pgpDigParamsSignID(sig));
+	    rdlock lock(keyring->mutex);
 
-	/* Look for verifying key */
-	auto range = keyring->keys.equal_range(keyid);
+	    /* Look for verifying key */
+	    auto range = keyring->keys.equal_range(keyid);
 
-	for (auto it = range.first; it != range.second; ++it) {
-	    key = it->second;
+	    for (auto it = range.first; it != range.second; ++it) {
+		key = it->second;
 
-	    /* Do the parameters match the signature? */
-	    if (pgpDigParamsAlgo(sig, PGPVAL_PUBKEYALGO)
-		!= pgpDigParamsAlgo(key->pgpkey, PGPVAL_PUBKEYALGO))
-	    {
-		continue;
+		/* Do the parameters match the signature? */
+		if (pgpDigParamsAlgo(sig, PGPVAL_PUBKEYALGO)
+		    != pgpDigParamsAlgo(key->pgpkey, PGPVAL_PUBKEYALGO))
+		{
+		    continue;
+		}
+
+		rc = pubKeyVerify(key, sig, ctx, results);
+
+		if (rc == RPMRC_OK)
+		    break;
 	    }
-
-	    rc = pubKeyVerify(key, sig, ctx, results);
-
-	    if (rc == RPMRC_OK)
-		break;
 	}
 
 	/* No key, sanity check signature*/

tests/CMakeLists.txt
@@ -28,6 +28,7 @@ set(TESTSUITE_AT
 	rpmgeneral.at
 	rpmvercmp.at
 	rpmmacro.at
+	rpmapi.at
 	rpmdevel.at
 	rpmpython.at
 	rpmpython2.at
@@ -61,10 +62,10 @@ foreach(at ${TESTSUITE_AT})
 	FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/rpmtests.at "m4_include([${at}])\n")
 endforeach()
 
-set(TESTPROGS rpmpgpcheck rpmpgppubkeyfingerprint)
+set(TESTPROGS rpmpgpcheck rpmpgppubkeyfingerprint readpkgnullts)
 foreach(prg ${TESTPROGS})
 	add_executable(${prg} EXCLUDE_FROM_ALL ${prg}.c)
-	target_link_libraries(${prg} PRIVATE librpmio)
+	target_link_libraries(${prg} PRIVATE librpm)
 endforeach()
 string(REPLACE ";" " " TESTPROG_NAMES "${TESTPROGS}")
 

tests/readpkgnullts.c
@@ -1,0 +1,18 @@
+#include <stdio.h>
+#include <rpm/rpmlib.h>
+
+int main(int argc, char *argv[])
+{
+    Header h = NULL;
+    FD_t fd = Fopen(argv[1], "r");
+    int rc = rpmReadPackageFile(NULL, fd, argv[1], &h);
+    if (h != NULL) {
+	char *nevra = headerGetAsString(h, RPMTAG_NEVRA);
+	printf("%s\n", nevra);
+	free(nevra);
+    }
+
+    Fclose(fd);
+    headerFree(h);
+    return rc;
+}

tests/rpmapi.at
@@ -1,0 +1,13 @@
+AT_BANNER([RPM API tests])
+
+RPMTEST_SETUP([[rpmReadPackage() with NULL ts]])
+AT_KEYWORDS([[api signature]])
+RPMTEST_CHECK([[
+readpkgnullts /data/RPMS/hello-2.0-1.x86_64-signed.rpm
+]],
+[4],
+[hello-2.0-1.x86_64
+],
+[warning: /data/RPMS/hello-2.0-1.x86_64-signed.rpm: Header OpenPGP V4 RSA/SHA256 signature, key ID 4344591e1964c5fc: NOKEY
+])
+RPMTEST_CLEANUP