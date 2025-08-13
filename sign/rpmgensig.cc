rpm-software-management/rpm

commit 1847fd6bea41f96ca545e744ee9ecc2896f6378a

Fix possible package corruption on --delsign/resign/addsign
Make sure we don't overrun the original signature header when
adjusting reserved size. Fixes a brainfart introduced in commit
be950eabb84a88e5773e096435c37b92e3d47ebb: the count reservation
size is relative to the size of the new header, obviously.

Another crucial difference is that when considering whether we can
transplant the new signature header in the originals place we need
to consider the real on-disk signature, not the size of its
immutable region. The immutable region can be much much smaller than
the physical header if eg the IMA signatures are misplaced outside it,
making our calculations way off.

Fixes: #3469

sign/rpmgensig.cc
@@ -698,8 +698,8 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 	    flags &= ~(RPMSIGN_FLAG_RPMV4|RPMSIGN_FLAG_RPMV3);
     }
 
-    unloadImmutableRegion(&sigh, RPMTAG_HEADERSIGNATURES);
     origSigSize = headerSizeof(sigh, HEADER_MAGIC_YES);
+    unloadImmutableRegion(&sigh, RPMTAG_HEADERSIGNATURES);
 
     if (flags & RPMSIGN_FLAG_IMA) {
 	if (includeFileSignatures(&sigh, &h))
@@ -745,12 +745,13 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 
     /* Adjust reserved size for added/removed signatures */
     if (headerGet(sigh, reserveTag, &utd, HEADERGET_MINMEM)) {
-	int diff = headerSizeof(sigh, HEADER_MAGIC_YES) - origSigSize;
+	unsigned newSize = headerSizeof(sigh, HEADER_MAGIC_YES);
+	int diff = newSize - origSigSize;
 
 	/* diff can be zero if nothing was added or removed */
 	if (diff) {
 	    utd.count -= diff;
-	    if (utd.count > 0 && utd.count < origSigSize) {
+	    if (utd.count > 0 && newSize + utd.count <= origSigSize) {
 		uint8_t *zeros = (uint8_t *)xcalloc(utd.count, sizeof(*zeros));
 		utd.data = zeros;
 		headerMod(sigh, &utd);

tests/rpmsigdig.at
@@ -1847,7 +1847,6 @@ RPMTEST_CLEANUP
 
 AT_SETUP([--delsign with misplaced ima signature])
 AT_KEYWORDS([rpmsign file signature])
-AT_XFAIL_IF([test $RPM_XFAIL -ne 0])
 RPMTEST_CHECK([
 cp /data/RPMS/hello-2.0-1.x86_64-badima.rpm .
 rpmsign --delsign hello-2.0-1.x86_64-badima.rpm

commit 0b70071adaeb5931babf0b00069e0996f2a13412

Fix regression on signature size reservation not being used
The signature reservation adjustment is one of those whack-a-mole bugs
where fixes keep making new bugs appear: the signature reservation was
broken by 1847fd6bea41f96ca545e744ee9ecc2896f6378a which came from
be950eabb84a88e5773e096435c37b92e3d47ebb which came from
5c279fb149a44a1bc4d19e11c3c01942732b8486 and so on.

This seemingly simple thing is pretty tricksy, especially when needing
to deal with 3rd party signed packages that don't always follow our
expectations. What 1847fd6bea41f96ca545e744ee9ecc2896f6378a got wrong
is that the header size before unloadImmutableRegion() will include the the
region index and data entries, totalling 32 bytes, whereas the other
calculations are lacking it. And without adjusting for it, the
difference either causes the mechanism not to work or corrupted
packages. It also made a mess of the sanity check.

Always take the signature size before the grabbing the immutable region,
just adjust for the region entry size difference. This handles the
misplaced IMA signatures in #3469 and other similar quirks with minimum fuss.
Simplify the reservation adjustment: the diff is for the reservation
size. If it's negative, the reservation needs to shrink, if positive
then it needs to grow. This is easier to follow than substracting
negative numbers to grow it. And now we just need to check that the
shrink operation doesn't wrap around, otherwise the math takes care of
itself. Famous last words.

Adjust the post-addsign rpmdump values to match the expected
behavior rather than the broken one.

Fixes: #3768

sign/rpmgensig.cc
@@ -717,7 +717,8 @@ static int rpmSign(const char *rpm, int deleting, int flags)
 	flags &= ~(RPMSIGN_FLAG_IMA | RPMSIGN_FLAG_FSVERITY);
     }
 
-    origSigSize = headerSizeof(sigh, HEADER_MAGIC_YES);
+    /* Adjust for the region index entry + data getting stripped: 32 bytes */
+    origSigSize = headerSizeof(sigh, HEADER_MAGIC_YES) - 32;
     unloadImmutableRegion(&sigh, RPMTAG_HEADERSIGNATURES);
 
     if (flags & RPMSIGN_FLAG_IMA) {
@@ -770,18 +771,15 @@ static int rpmSign(const char *rpm, int deleting, int flags)
     /* Adjust reserved size for added/removed signatures */
     if (headerGet(sigh, reserveTag, &utd, HEADERGET_MINMEM)) {
 	unsigned newSize = headerSizeof(sigh, HEADER_MAGIC_YES);
-	int diff = newSize - origSigSize;
+	int diff = origSigSize - newSize;
 
-	/* diff can be zero if nothing was added or removed */
-	if (diff) {
-	    utd.count -= diff;
-	    if (utd.count > 0 && newSize + utd.count <= origSigSize) {
-		uint8_t *zeros = (uint8_t *)xcalloc(utd.count, sizeof(*zeros));
-		utd.data = zeros;
-		headerMod(sigh, &utd);
-		insSig = 1;
-		free(zeros);
-	    }
+	if ((diff < 0 && abs(diff) < utd.count) || diff > 0) {
+	    utd.count += diff;
+	    uint8_t *zeros = (uint8_t *)xcalloc(utd.count, sizeof(*zeros));
+	    utd.data = zeros;
+	    headerMod(sigh, &utd);
+	    free(zeros);
+	    insSig = 1;
 	}
     }
 

tests/rpmsigdig.at
@@ -1770,7 +1770,6 @@ runroot rpmbuild -bb --quiet \
 [],
 [])
 
-# XXX the reserve tag size is currently WRONG here due to #3768
 RPMTEST_CHECK([
 cp /data/misc/rpmdump6-addsign-v6.txt expout
 runroot ${RPM_CONFIGDIR_PATH}/rpmdump /build/RPMS/noarch/attrtest-1.0-1.noarch.rpm| sed -n '/^Signature:$/,/^Header:$/p'