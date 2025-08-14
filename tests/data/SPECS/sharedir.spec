rpm-software-management/rpm

commit 08a6a5e848f007c2e8d83fc1df8712d1a43229f4

Replace by-fingerprint rpmFpHash with STL unordered_map
This is a bit trickier than the average hash because it uses pointers
to structs as keys, but we need to hash and compare the actual structs
and not the pointers.

This is was also the last user of rpmhash in the codebase.

lib/fprint.c
@@ -19,15 +19,6 @@
 #include "debug.h"
 #include <libgen.h>
 
-/* Create by-fingerprint hash table */
-#define HASHTYPE rpmFpHash
-#define HTKEYTYPE const fingerPrint *
-#define HTDATATYPE struct rpmffi_s
-#include "rpmhash.H"
-#include "rpmhash.C"
-#undef HASHTYPE
-#undef HTKEYTYPE
-#undef HTDATATYPE
 
 static unsigned int sidHash(rpmsid sid)
 {
@@ -67,7 +58,41 @@ struct fingerPrint {
 	    ((a).subDirId == (b).subDirId) \
     )
 
-using rpmFpEntryHash = std::unordered_multimap<rpmsid,fprintCacheEntry_s>
+/* Hash value for a finger print pointer, based on dev and inode only! */
+struct fpHash
+{
+    size_t operator() (const fingerPrint *fp) const
+    {
+	unsigned int hash = 0;
+	int j;
+
+	hash = sidHash(fp->baseNameId);
+	if (fp->subDirId) hash ^= sidHash(fp->subDirId);
+
+	hash ^= ((unsigned)fp->entry->dev);
+	for (j=0; j<4; j++) hash ^= ((fp->entry->ino >> (8*j)) & 0xFF) << ((3-j)*8);
+
+	return hash;
+    }
+};
+
+struct fpEqual
+{
+    bool operator()(const fingerPrint * k1, const fingerPrint * k2) const
+    {
+	/* If the addresses are the same, so are the values. */
+	if (k1 == k2)
+	    return true;
+
+	/* Otherwise, compare fingerprints by value. */
+	if (FP_EQUAL(*k1, *k2))
+	    return true;
+	return false;
+    }
+};
+
+using rpmFpEntryHash = std::unordered_multimap<rpmsid,fprintCacheEntry_s>;
+using rpmFpHash = std::unordered_map<fingerPrint *,rpmffi_s,fpHash,fpEqual>;
 
 /**
  * Finger print cache.
@@ -88,7 +113,6 @@ fingerPrintCache fpCacheCreate(int sizeHint, rpmstrPool pool)
 fingerPrintCache fpCacheFree(fingerPrintCache cache)
 {
     if (cache) {
-	cache->fp = rpmFpHashFree(cache->fp);
 	cache->pool = rpmstrPoolFree(cache->pool);
 	delete cache;
     }
@@ -227,39 +251,6 @@ int fpLookupId(fingerPrintCache cache,
     return doLookupId(cache, dirNameId, baseNameId, *fp);
 }
 
-/**
- * Return hash value for a finger print.
- * Hash based on dev and inode only!
- * @param fp		pointer to finger print entry
- * @return hash value
- */
-static unsigned int fpHashFunction(const fingerPrint * fp)
-{
-    unsigned int hash = 0;
-    int j;
-
-    hash = sidHash(fp->baseNameId);
-    if (fp->subDirId) hash ^= sidHash(fp->subDirId);
-
-    hash ^= ((unsigned)fp->entry->dev);
-    for (j=0; j<4; j++) hash ^= ((fp->entry->ino >> (8*j)) & 0xFF) << ((3-j)*8);
-    
-    return hash;
-}
-
-int fpEqual(const fingerPrint * k1, const fingerPrint * k2)
-{
-    /* If the addresses are the same, so are the values. */
-    if (k1 == k2)
-	return 0;
-
-    /* Otherwise, compare fingerprints by value. */
-    if (FP_EQUAL(*k1, *k2))
-	return 0;
-    return 1;
-
-}
-
 const char * fpEntryDir(fingerPrintCache cache, fingerPrint *fp)
 {
     const char * dn = NULL;
@@ -325,10 +316,6 @@ static void fpLookupSubdir(rpmFpHash symlinks, fingerPrintCache fpc, fingerPrint
     struct fingerPrint current_fp;
     const char *currentsubdir;
     size_t lensubDir, bnStart, bnEnd;
-
-    struct rpmffi_s * recs;
-    int numRecs;
-    int i;
     int symlinkcount = 0;
 
     for (;;) {
@@ -353,11 +340,11 @@ static void fpLookupSubdir(rpmFpHash symlinks, fingerPrintCache fpc, fingerPrint
 						    currentsubdir + bnStart,
 						    bnEnd - bnStart, 1);
 
-	    rpmFpHashGetEntry(symlinks, &current_fp, &recs, &numRecs, NULL);
-
-	    for (i = 0; i < numRecs; i++) {
-		rpmfiles foundfi = rpmteFiles(recs[i].p);
-		char const *linktarget = rpmfilesFLink(foundfi, recs[i].fileno);
+	    auto range = symlinks.equal_range(&current_fp);
+	    for (auto it = range.first; it != range.second; ++it) {
+		const rpmffi_s & rec = it->second;
+		rpmfiles foundfi = rpmteFiles(rec.p);
+		char const *linktarget = rpmfilesFLink(foundfi, rec.fileno);
 		char *link;
 		rpmsid linkId;
 
@@ -416,11 +403,10 @@ fingerPrint * fpCacheGetByFp(fingerPrintCache cache,
 			     struct fingerPrint * fp, int ix,
 			     std::vector<struct rpmffi_s> & recs)
 {
-    struct rpmffi_s * rrecs = NULL;
-    int numRecs = 0;
-    if (rpmFpHashGetEntry(cache->fp, fp + ix, &rrecs, &numRecs, NULL)) {
-	for (int i = 0; i < numRecs; ++i)
-	    recs.push_back(rrecs[i]);
+    auto range = cache->fp.equal_range(fp + ix);
+    if (range.first != range.second) {
+	for (auto it = range.first; it != range.second; ++it)
+	    recs.push_back(it->second);
 	return fp + ix;
     } else
 	return NULL;
@@ -435,11 +421,7 @@ void fpCachePopulate(fingerPrintCache fpc, rpmts ts, int fileCount)
     int i, fc;
     int havesymlinks = 0;
 
-    if (fpc->fp == NULL)
-	fpc->fp = rpmFpHashCreate(fileCount/2 + 10001, fpHashFunction, fpEqual,
-				  NULL, NULL);
-
-    rpmFpHash symlinks = rpmFpHashCreate(fileCount/16+16, fpHashFunction, fpEqual, NULL, NULL);
+    rpmFpHash symlinks;
 
     /* populate the fingerprints of all packages in the transaction */
     /* also create a hash of all symlinks in the new packages */
@@ -466,7 +448,7 @@ void fpCachePopulate(fingerPrintCache fpc, rpmts ts, int fileCount)
 		    continue;
 		ffi.p = p;
 		ffi.fileno = i;
-		rpmFpHashAddEntry(symlinks, fpList + i, ffi);
+		symlinks.insert({fpList + i, ffi});
 		havesymlinks = 1;
 	    }
 	}
@@ -504,14 +486,12 @@ void fpCachePopulate(fingerPrintCache fpc, rpmts ts, int fileCount)
 	    }
 	    ffi.p = p;
 	    ffi.fileno = i;
-	    rpmFpHashAddEntry(fpc->fp, fpList + i, ffi);
+	    fpc->fp.insert({fpList + i, ffi});
 	    lastfp = fpList + i;
 	}
 	(void) rpmswExit(rpmtsOp(ts, RPMTS_OP_FINGERPRINT), 0);
 	rpmfilesFree(fi);
     }
     rpmtsiFree(pi);
-
-    rpmFpHashFree(symlinks);
 }
 

lib/fprint.h
@@ -87,16 +87,6 @@ int fpLookupId(fingerPrintCache cache,
 	       fingerPrint **fp);
 
 /**
- * Compare two finger print entries.
- * This routine is exactly equivalent to the FP_EQUAL macro.
- * @param key1		finger print 1
- * @param key2		finger print 2
- * @return result of comparing key1 and key2
- */
-RPM_GNUC_INTERNAL
-int fpEqual(const fingerPrint * key1, const fingerPrint * key2);
-
-/**
  * Return finger prints of an array of file paths.
  * @param cache		pointer to fingerprint cache
  * @param pool		pointer to file name pool

commit e079ea313f356ef6bfc40865b3a241760a0ef02d

Add a minimal testcase for shared directory erasure from #3762
Commit 08a6a5e848f007c2e8d83fc1df8712d1a43229f4 broke this, expect
failure failure now.

tests/data/SPECS/sharedir.spec
@@ -1,0 +1,51 @@
+Name: sharedir
+Version: 1.0
+Release: 1
+License: Public domain
+BuildArch: noarch
+Summary: Testing shared directory behavior
+
+%description
+%{summary}
+
+%package a
+Summary: %{summary}
+Requires: %{name}
+
+%description a
+%{summary}
+
+%package b
+Summary: %{summary}
+Requires: %{name}
+
+%description b
+%{summary}
+
+%package c
+Summary: %{summary}
+Requires: %{name}
+
+%description c
+%{summary}
+
+%install
+mkdir -p ${RPM_BUILD_ROOT}/opt/sharedir/sub
+for x in a b c; do
+    echo ${x} > ${RPM_BUILD_ROOT}/opt/sharedir/sub/${x}
+done
+
+%files
+/opt/sharedir/
+
+%files a
+%dir /opt/sharedir/sub/
+/opt/sharedir/sub/a
+
+%files b
+%dir /opt/sharedir/sub/
+/opt/sharedir/sub/b
+
+%files c
+%dir /opt/sharedir/sub/
+/opt/sharedir/sub/c

tests/rpme.at
@@ -24,6 +24,37 @@ missing   d /usr/share/doc/hello-2.0/README
 [])
 RPMTEST_CLEANUP
 
+RPMTEST_SETUP_RW([rpm -e shared directories])
+AT_KEYWORDS([install erase rpmdb])
+AT_XFAIL_IF([test $RPM_XFAIL -ne 0])
+
+rpmbuild -bb --quiet /data/SPECS/sharedir.spec
+
+RPMTEST_CHECK([
+runroot rpm -U /build/RPMS/noarch/sharedir*.rpm
+runroot find /opt | sort
+],
+[0],
+[/opt
+/opt/sharedir
+/opt/sharedir/sub
+/opt/sharedir/sub/a
+/opt/sharedir/sub/b
+/opt/sharedir/sub/c
+],
+[])
+
+RPMTEST_CHECK([
+runroot rpm -e sharedir-b sharedir sharedir-c sharedir-a
+runroot find /opt | sort
+],
+[0],
+[/opt
+],
+[])
+RPMTEST_CLEANUP
+
+
 RPMTEST_SETUP_RW([rpm -e suid hardlink])
 AT_KEYWORDS([install erase])
 