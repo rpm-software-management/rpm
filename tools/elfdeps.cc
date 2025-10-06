f674e996ffd2e06aeff844eed9bb26845b730b67
Panu Matilainen <pmatilai@redhat.com>
2025-10-06 16:10:15 +0300

    Drop -Wno-sign-compare from tools/ as well
    These are just a couple of trivial local variable type changes.

--- tools/elfdeps.cc
+++ tools/elfdeps.cc
@@ -193,7 +193,7 @@ static void processDynamic(Elf_Scn *scn, GElf_Shdr *shdr, elfInfo *ei)
     if (shdr->sh_entsize == 0)
 	return;
     while ((data = elf_getdata(scn, data)) != NULL) {
-	for (int i = 0; i < (shdr->sh_size / shdr->sh_entsize); i++) {
+	for (unsigned i = 0; i < (shdr->sh_size / shdr->sh_entsize); i++) {
 	    const char *s = NULL;
 	    GElf_Dyn dyn_mem, *dyn;
 
