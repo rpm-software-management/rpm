d31467ae9cb678d3d39136065ce5e1708e27dd66
Michal Domonkos <mdomonko@redhat.com>
2025-12-01 11:53:42 +0100

    Add generic program name handlers
    This is useful for platforms that don't have a native way to get/set the
    program name, such as AIX.
    
    In a way, this reverts commit fa06b68ed7640c0e42a8a63dd9cb0bf8d4c73525
    but makes use of the newly added wrapper functions.
    
    Related: #4053

--- rpmio/progname.cc
+++ rpmio/progname.cc
@@ -6,8 +6,9 @@
 #include <stdlib.h>
 #elif defined(HAVE___PROGNAME) /* glibc and others */
 extern const char *__progname;
-#else
-# error "Did not find any sutable implementation of getprogname/setprogname"
+#else /* generic implementation */
+#include <string.h> /* strrchr */
+static const char *rprogname = NULL;
 #endif
 
 const char *rgetprogname(void)
@@ -16,8 +17,8 @@ const char *rgetprogname(void)
     return getprogname();
 #elif defined(HAVE___PROGNAME) /* glibc and others */
     return __progname;
-#else
-# error "Did not find any sutable implementation of getprogname"
+#else /* generic implementation */
+    return (rprogname != NULL) ? rprogname : "";
 #endif
 }
 
@@ -26,7 +27,13 @@ void rsetprogname(const char *pn)
 #if defined(HAVE_SETPROGNAME) /* BSD'ish systems */
     setprogname(pn);
 #elif defined(HAVE___PROGNAME) /* glibc and others */
-#else
-# error "Did not find any sutable implementation of setprogname"
+#else /* generic implementation */
+    if (pn != NULL && rprogname == NULL /* set the value only once */) {
+	const char *p = strrchr(pn, '/'); /* locate the last occurrence of '/' */
+	if (p != NULL)
+	    rprogname = p + 1 /* strip beginning '/' */;
+	else
+	    rprogname = pn;
+    }
 #endif
 }
