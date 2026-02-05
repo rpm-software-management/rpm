rpm-software-management/rpm

commit e360ca338e5215a2015fef6ed843cfefd8463f2e

Use a friend function in delete_key()
Both the fs and rpmdb keystores share the same deletion logic now so
refactor it into a separate function. Make it a friend of both classes
so that it can access their private delete_key() methods.

Note that this short keyid fallback logic is only relevant to those two
keystore implementations, not in general, so we don't want to put this
into the public delete_key() in the base class.

No functional change.

lib/keystore.cc
@@ -113,7 +113,6 @@ exit:
     return rc;
 }
 
-
 static rpmRC delete_file_store(std::string path)
 {
     try {
@@ -124,6 +123,19 @@ static rpmRC delete_file_store(std::string path)
     return RPMRC_OK;
 }
 
+/* Wrapper for private delete_key() methods that falls back to short keyid */
+static rpmRC rpm::delete_key_compat(auto keystore, rpmtxn txn, rpmPubkey key, auto skip)
+{
+    rpmRC rc = RPMRC_NOTFOUND;
+    string fp = rpmPubkeyFingerprintAsHex(key);
+
+    if (keystore->delete_key(txn, fp, skip) == RPMRC_NOTFOUND) {
+	/* make sure an old, short keyid version gets removed */
+	keystore->delete_key(txn, fp.substr(32), skip);
+    }
+
+    return rc;
+}
 
 /*****************************************************************************/
 
@@ -151,22 +163,9 @@ rpmRC keystore_fs::delete_key(rpmtxn txn, const string & keyid, const string & n
     return rc;
 }
 
-rpmRC keystore_fs::delete_key(rpmtxn txn, rpmPubkey key, const string & newname)
-{
-    rpmRC rc = RPMRC_NOTFOUND;
-    string fp = rpmPubkeyFingerprintAsHex(key);
-
-    if (delete_key(txn, fp, newname) == RPMRC_NOTFOUND) {
-	/* make sure an old, short keyid version gets removed */
-	delete_key(txn, fp.substr(32), newname);
-    }
-
-    return rc;
-}
-
 rpmRC keystore_fs::delete_key(rpmtxn txn, rpmPubkey key)
 {
-    return delete_key(txn, key, "");
+    return delete_key_compat(this, txn, key, "");
 }
 
 rpmRC keystore_fs::import_key(rpmtxn txn, rpmPubkey key, int replace, rpmFlags flags)
@@ -181,7 +180,7 @@ rpmRC keystore_fs::import_key(rpmtxn txn, rpmPubkey key, int replace, rpmFlags f
 
     if (!rc && replace) {
 	/* find and delete the old pubkey entry */
-	delete_key(txn, key, keyfmt);
+	delete_key_compat(this, txn, key, keyfmt);
     }
 
     free(dir);
@@ -358,22 +357,9 @@ rpmRC keystore_rpmdb::delete_key(rpmtxn txn, const string & keyid, unsigned int 
     return rc;
 }
 
-rpmRC keystore_rpmdb::delete_key(rpmtxn txn, rpmPubkey key, unsigned int newinstance)
-{
-    rpmRC rc = RPMRC_NOTFOUND;
-    string fp = rpmPubkeyFingerprintAsHex(key);
-
-    if (delete_key(txn, fp, newinstance) == RPMRC_NOTFOUND) {
-	/* make sure an old, short keyid version gets removed */
-	delete_key(txn, fp.substr(32), newinstance);
-    }
-
-    return rc;
-}
-
 rpmRC keystore_rpmdb::delete_key(rpmtxn txn, rpmPubkey key)
 {
-    return delete_key(txn, key, 0);
+    return delete_key_compat(this, txn, key, 0);
 }
 
 rpmRC keystore_rpmdb::delete_store(rpmtxn txn)
@@ -415,7 +401,7 @@ rpmRC keystore_rpmdb::import_key(rpmtxn txn, rpmPubkey key, int replace, rpmFlag
     if (!rc && replace) {
 	/* find and delete the old pubkey entry */
 	unsigned int newinstance = headerGetInstance(h);
-	delete_key(txn, key, newinstance);
+	delete_key_compat(this, txn, key, newinstance);
     }
     headerFree(h);
 

lib/keystore.hh
@@ -25,8 +25,9 @@ public:
     rpmRC delete_key(rpmtxn txn, rpmPubkey key) override;
     rpmRC delete_store(rpmtxn txn) override;
 private:
-    rpmRC delete_key(rpmtxn txn, rpmPubkey key, const std::string & newname);
     rpmRC delete_key(rpmtxn txn, const std::string & keyid, const std::string & newname);
+
+    friend rpmRC delete_key_compat(auto keystore, rpmtxn txn, rpmPubkey key, auto skip);
 };
 
 class keystore_rpmdb : public keystore {
@@ -36,8 +37,9 @@ public:
     rpmRC delete_key(rpmtxn txn, rpmPubkey key) override;
     rpmRC delete_store(rpmtxn txn) override;
 private:
-    rpmRC delete_key(rpmtxn txn, rpmPubkey key, unsigned int newinstance);
     rpmRC delete_key(rpmtxn txn, const std::string & keyid, unsigned int newinstance);
+
+    friend rpmRC delete_key_compat(auto keystore, rpmtxn txn, rpmPubkey key, auto skip);
 };
 
 class keystore_openpgp_cert_d : public keystore {

commit 8b2e0a0cd7546eb14d021b01b923945cfd5b3374

Fix delete_key_compat() friend definition
Commit e360ca338e5215a2015fef6ed843cfefd8463f2e mistakenly defined the
function as static while also declaring it as a friend function in the
keystore header file. While GCC didn't seem to mind, Clang failed with:

    error: static declaration of 'delete_key_compat' follows non-static
    declaration

And rightfully so. We just didn't notice since most of us build with GCC
(including the CI), oops.

Fix by dropping the static keyword. This is fine because access control
and visibility is handled by C++ here (since it's a friend function, not
a standalone one), and the keystore API isn't public anyway.

Fixes: #4070

lib/keystore.cc
@@ -126,7 +126,7 @@ static rpmRC delete_file_store(std::string path)
 }
 
 /* Wrapper for private delete_key() methods that falls back to short keyid */
-static rpmRC rpm::delete_key_compat(auto keystore, rpmtxn txn, rpmPubkey key, auto skip)
+rpmRC rpm::delete_key_compat(auto keystore, rpmtxn txn, rpmPubkey key, auto skip)
 {
     rpmRC rc = RPMRC_NOTFOUND;
     string fp = rpmPubkeyFingerprintAsHex(key);