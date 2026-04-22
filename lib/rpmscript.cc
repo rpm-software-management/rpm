e66ba865ea83897ef34abbe1d93d78a0d599b3bd
Panu Matilainen <pmatilai@redhat.com>
2026-04-22 10:19:49 +0300

    Fix regression running Lua scripts from non-rpm generated packages
    Commit 532e6b90652154842fc765f87f60c97f20f7d656 introduced a deprecation
    warning based on the optional RPMVERSION tag. The Lua-side of the patch
    does handle that value missing, but the librpm side does not, oops.
    
    Report and reproducer from Daniel Möser.
    
    Fixes: #4189

--- lib/rpmscript.cc
+++ lib/rpmscript.cc
@@ -181,9 +181,12 @@ static rpmRC runLuaScript(rpmPlugins plugins, ARGV_const_t prefixes,
 	mode_t oldmask = umask(0);
 	umask(oldmask);
 
-	lua_pushstring(L, "RPM_PACKAGE_RPMVERSION");
-	lua_pushstring(L, script->rpmver);
-	lua_settable(L, LUA_REGISTRYINDEX);
+	/* Not all packages out there are built by RPM */
+	if (script->rpmver) {
+	    lua_pushstring(L, "RPM_PACKAGE_RPMVERSION");
+	    lua_pushstring(L, script->rpmver);
+	    lua_settable(L, LUA_REGISTRYINDEX);
+	}
 
 	if (scriptFd) {
 	    sfd = fdopen(dup(Fileno(scriptFd)), "a");
@@ -201,9 +204,11 @@ static rpmRC runLuaScript(rpmPlugins plugins, ARGV_const_t prefixes,
 	    fclose(sfd);
 	}
 
-	lua_getfield(L, LUA_REGISTRYINDEX, "RPM_PACKAGE_RPMVERSION");
-	lua_pushnil(L);
-	lua_settable(L, LUA_REGISTRYINDEX);
+	if (script->rpmver) {
+	    lua_getfield(L, LUA_REGISTRYINDEX, "RPM_PACKAGE_RPMVERSION");
+	    lua_pushnil(L);
+	    lua_settable(L, LUA_REGISTRYINDEX);
+	}
 
 	/* This failing would be fatal, return something different for it... */
 	if (fchdir(cwd)) {
