rpm-software-management/rpm

commit ab1991084fc72d4036a8818e35c8966bcf7b7c86

Refactor macro definition parsing to utilize C++ strings a bit
Copy the name, opts and body into separate C++ strings as we parse them
instead of relying on (poorly) hand-counted buffer sizes and numerous
different pointers.

Most actual parsing is raw C pointers still, changing that would require
vast changes throughout the macro engine due to various internal API
dependencies.

Non-NULL opts signifies parametric macros, and various assumptions
around this go quite deep in both the API and the implementing code.
Changing that is a crusade for another day.

rpmio/macro.cc
@@ -361,15 +361,13 @@ printExpansion(rpmMacroBuf mb, rpmMacroEntry me, const char * t, const char * te
 #define	COPYNAME(_ne, _s, _c)	\
     {	SKIPBLANK(_s,_c);	\
 	while (((_c) = *(_s)) && (risalnum(_c) || (_c) == '_')) \
-		*(_ne)++ = *(_s)++; \
-	*(_ne) = '\0';		\
+		(_ne) += *(_s)++; \
     }
 
 #define	COPYOPTS(_oe, _s, _c)	\
     { \
 	while (((_c) = *(_s)) && (_c) != ')') \
-		*(_oe)++ = *(_s)++; \
-	*(_oe) = '\0';		\
+		(_oe) += *(_s)++; \
     }
 
 /**
@@ -601,29 +599,26 @@ static void
 doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, size_t *parsed)
 {
     const char *se = str.c_str();
-    const char *start = se;
     const char *s = se;
-    char *buf = (char *)xmalloc(strlen(s) + 3); /* Some leeway for termination issues... */
-    char *n = buf, *ne = n;
-    char *o = NULL, *oe;
-    char *b, *be, *ebody = NULL;
+    const char *o = NULL;
     int c;
-    int oc = ')';
     const char *sbody; /* as-is body start */
     int rc = 1; /* assume failure */
     int flags = ME_NONE;
+    std::string name, opts, body;
 
     /* Copy name */
-    COPYNAME(ne, s, c);
+    COPYNAME(name, s, c);
+    const char *n = name.c_str();
 
     /* Copy opts (if present) */
-    oe = ne + 1;
     if (*s == '(') {
 	s++;	/* skip ( */
 	/* Options must be terminated with ')' */
 	if (strchr(s, ')')) {
-	    o = oe;
-	    COPYOPTS(oe, s, oc);
+	    int oc = ')';
+	    COPYOPTS(opts, s, oc);
+	    o = opts.c_str();
 	    s++;	/* skip ) */
 	} else {
 	    rpmMacroBufErr(mb, 1, _("Macro %%%s has unterminated opts\n"), n);
@@ -632,13 +627,11 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
     }
 
     /* Copy body, skipping over escaped newlines */
-    b = be = oe + 1;
     sbody = s;
     SKIPBLANK(s, c);
     if (!parsed) {
-	strcpy(b, s);
-	be = b + strlen(b);
-	s += strlen(s);
+	body = s;
+	s += body.size();
     } else if (c == '{') {	/* XXX permit silent {...} grouping */
 	if ((se = matchchar(s, c, '}')) == NULL) {
 	    rpmMacroBufErr(mb, 1, _("Macro %%%s has unterminated body\n"), n);
@@ -646,9 +639,7 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 	    goto exit;
 	}
 	s++;	/* XXX skip { */
-	strncpy(b, s, (se - 1 - s));
-	b[se - 1 - s] = '\0';
-	be += strlen(b);
+	body = str.substr(s - str.c_str(), (se - 1 - s));
 	s = se;	/* move scan forward */
     } else {	/* otherwise free-field */
 	int bc = 0, pc = 0, xc = 0;
@@ -662,10 +653,10 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 		    break;
 		case '%':
 		    switch (*(s+1)) {
-			case '{': *be++ = *s++; bc++; break;
-			case '(': *be++ = *s++; pc++; break;
-			case '[': *be++ = *s++; xc++; break;
-			case '%': *be++ = *s++; break;
+			case '{': body += *s++; bc++; break;
+			case '(': body += *s++; pc++; break;
+			case '[': body += *s++; xc++; break;
+			case '%': body += *s++; break;
 		    }
 		    break;
 		case '{': if (bc > 0) bc++; break;
@@ -675,9 +666,8 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 		case '[': if (xc > 0) xc++; break;
 		case ']': if (xc > 0) xc--; break;
 	    }
-	    *be++ = *s++;
+	    body += *s++;
 	}
-	*be = '\0';
 
 	if (bc || pc || xc) {
 	    rpmMacroBufErr(mb, 1, _("Macro %%%s has unterminated body\n"), n);
@@ -686,9 +676,8 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 	}
 
 	/* Trim trailing blanks/newlines */
-	while (--be >= b && (c = *be) && (risblank(c) || iseol(c)))
-	    {};
-	*(++be) = '\0';	/* one too far */
+	while (risblank(body.back()) || iseol(body.back()))
+	    body.pop_back();
     }
 
     /* Move scan over body */
@@ -696,10 +685,10 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 	s++;
     se = s;
 
-    if (!validName(mb, n, ne - n, expandbody ? "%global": "%define"))
+    if (!validName(mb, n, name.size(), expandbody ? "%global": "%define"))
 	goto exit;
 
-    if ((be - b) < 1) {
+    if (body.empty()) {
 	rpmMacroBufErr(mb, 1, _("Macro %%%s has empty body\n"), n);
 	goto exit;
     }
@@ -708,26 +697,26 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 	rpmMacroBufErr(mb, 0, _("Macro %%%s needs whitespace before body\n"), n);
 
     if (expandbody) {
+	char *ebody = NULL;
 	int eflags = RPMEXPAND_KEEP_QUOTED;
-	if (expandThis(mb, b, 0, &ebody, &eflags)) {
+	if (expandThis(mb, body.c_str(), 0, &ebody, &eflags)) {
 	    rpmMacroBufErr(mb, 1, _("Macro %%%s failed to expand\n"), n);
 	    goto exit;
 	}
-	b = ebody;
+	body = ebody;
 	if (eflags & RPMEXPAND_HAVE_QUOTED)
 	    flags |= ME_QUOTED;
+	_free(ebody);
     }
 
-    pushMacro(mb->mc, n, o, b, level, flags);
+    pushMacro(mb->mc, name, o, body, level, flags);
     rc = 0;
 
 exit:
     if (rc)
 	mb->error = 1;
-    _free(buf);
-    _free(ebody);
     if (parsed)
-	*parsed += se - start;
+	*parsed += se - str.c_str();
 }
 
 static int undefineMacro(rpmMacroBuf mb, const std::string & n)

commit e355cc8ba33b0c6af8d5a52384a4808149839bcc

Fix regression when parsing macro define with an empty body
Commit ab1991084fc72d4036a8818e35c8966bcf7b7c86 introduced a regression
where we can attempt to access an empty string. Add tests to cover
the case.

Fixes: #4195

rpmio/macro.cc
@@ -695,7 +695,7 @@ doDefine(rpmMacroBuf mb, const std::string & str, int level, int expandbody, siz
 	}
 
 	/* Trim trailing blanks/newlines */
-	while (risblank(body.back()) || iseol(body.back()))
+	while (!body.empty() && (risblank(body.back()) || iseol(body.back())))
 	    body.pop_back();
     }
 

tests/rpmmacro.at
@@ -108,6 +108,22 @@ rpm --eval '%define _ that'
 [],
 [error: Macro %_ has illegal name (%define)
 ])
+
+RPMTEST_CHECK([
+rpm --eval '%define this'
+],
+[1],
+[],
+[error: Macro %this has empty body
+])
+
+RPMTEST_CHECK([
+rpm --eval '%define this {}'
+],
+[1],
+[],
+[error: Macro %this has empty body
+])
 RPMTEST_CLEANUP
 
 # ------------------------------