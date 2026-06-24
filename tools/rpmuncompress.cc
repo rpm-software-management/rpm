rpm-software-management/rpm

commit b8b54f92c65bca7bfdd80c3ecf717d5c48b02e91

Eliminate variable length array (VLA) use in rpmuncompress gem handling
See previous commits for general rationale.

Just use a C++ string instead. This whole piece could use some additional
C++ love, but we're only here for the VLA's. Besides, we don't have tests
for gems so better keep it on the safe side.

Related: #4105

tools/rpmuncompress.cc
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <string>
+
 #include <archive.h>
 #include <archive_entry.h>
 
@@ -214,10 +216,9 @@ static char *doUntar(const char *fn)
 	    size_t nvlen = strlen(bn) - 3;
 	    char *gem = rpmGetPath("%{__gem}", NULL);
 	    char *gemspec = NULL;
-	    char gemnameversion[nvlen];
+	    std::string gemnameversion(bn, nvlen);
 
-	    rstrlcpy(gemnameversion, bn, nvlen);
-	    gemspec = rpmGetPath("", gemnameversion, ".gemspec", NULL);
+	    gemspec = rpmGetPath("", gemnameversion.c_str(), ".gemspec", NULL);
 
 	    rasprintf(&buf, "%s '%s' && %s spec '%s' --ruby > '%s'",
 			zipper, fn, gem, fn, gemspec);

commit f7d1f552f6a148d90dbdd646aecf8224501f4523

Fix rubygem extract regression from b8b54f92c65bca7bfdd80c3ecf717d5c48b02e91
As b8b54f92c65bca7bfdd80c3ecf717d5c48b02e91 notes, this could've used
some C++ love and maybe avoided the regression too by being waaaayy simpler.
Adjust test expectation to pass now.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2492128

tools/rpmuncompress.cc
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <filesystem>
 #include <string>
 
 #include <archive.h>
@@ -16,6 +17,8 @@
 #include <rpm/rpmstring.h>
 
 #include "debug.h"
+
+namespace fs = std::filesystem;
 
 static int extract = 0;
 static int dryrun = 0;
@@ -217,21 +220,13 @@ static char *doUntar(const char *fn)
 	if (needtar) {
 	    rasprintf(&buf, "%s %s '%s' | %s %s - %s", mkdir, zipper, fn, tar, taropts, stripcd);
 	} else if (at->compressed == COMPRESSED_GEM) {
-	    char *tmp = xstrdup(fn);
-	    const char *bn = basename(tmp);
-	    size_t nvlen = strlen(bn) - 3;
+	    auto bn = fs::path(fn).stem();
 	    char *gem = rpmGetPath("%{__gem}", NULL);
-	    char *gemspec = NULL;
-	    std::string gemnameversion(bn, nvlen);
 
-	    gemspec = rpmGetPath("", gemnameversion.c_str(), ".gemspec", NULL);
+	    rasprintf(&buf, "%s '%s' && %s spec '%s' --ruby > '%s.gemspec'",
+			zipper, fn, gem, fn, bn.c_str());
 
-	    rasprintf(&buf, "%s '%s' && %s spec '%s' --ruby > '%s'",
-			zipper, fn, gem, fn, gemspec);
-
-	    free(gemspec);
 	    free(gem);
-	    free(tmp);
 	} else {
 	    rasprintf(&buf, "%s%s '%s' %s", mkdir, zipper, fn, stripcd);
 	}

tests/rpmmacro.at
@@ -519,7 +519,6 @@ RPMTEST_CLEANUP
 
 RPMTEST_SETUP([uncompress 5])
 AT_KEYWORDS([macros rpmuncompress])
-AT_XFAIL_IF([test $RPM_XFAIL -ne 0])
 RPMTEST_CHECK([
 runroot rpmuncompress -n /data/SOURCES/hola-0.1.3.gem
 ],